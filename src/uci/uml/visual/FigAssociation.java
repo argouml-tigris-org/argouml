7242448edb068533d3eb86dc2dc933cc5e65c945
abonner <abonner@tigris.org>
1998-04-13 18:42:34 +0000

    Added so the resizing for FigClasses works, and FigAssoc has some text.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@52 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/uci/uml/visual/FigAssociation.java
+++ src/uci/uml/visual/FigAssociation.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 import uci.gef.*;
 import uci.uml.Foundation.Core.*;
+import uci.uml.generate.*;
 
 public class FigAssociation extends FigEdgeLine {
 
@@ -13,6 +14,11 @@ public class FigAssociation extends FigEdgeLine {
 
     ArrowHeadDiamond endArrow = new ArrowHeadDiamond();
     endArrow.setFillColor(Color.white);
+
+    FigText _dummy_text = new FigText(10,30,90,20, Color.blue, "Times", 10);
+    _dummy_text.setText("Association Label");
+
+    addPathItem(_dummy_text, new PathConvPercent(this, (float).50, 0));
 
     setDestArrowHead(endArrow);
     setBetweenNearestPoints(true);
