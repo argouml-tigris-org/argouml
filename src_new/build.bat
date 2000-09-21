71159af3119bad3d4398b33c6a3fa068f02631a7
Curt Arnold <carnold@tigris.org>
2000-09-21 06:33:44 +0000

    Changed build.xml so that "run" will work, slight tweak on build.bat, Removed another extraneous PropPanel (Realization)
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@476 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/build.bat
+++ src_new/build.bat
@@ -7,10 +7,10 @@ echo
 SET JAVA_HOME=c:\jdk1.2.2
 SET PATH=%JAVA_HOME%/bin
 SET ANT_HOME=c:\ant1.1\jakarta-ant\lib
-SET NSUML_HOME=c:\argo08
+SET NSUML_HOME=..\lib
 SET XML_HOME=%NSUML_HOME%
 SET OCL_HOME=%NSUML_HOME%
-SET GEF_HOME=c:\gef\gef\src
+SET GEF_HOME=%NSUML_HOME%
 SET LPATH=%ANT_HOME%\ant.jar;%ANT_HOME%\jaxp.jar;%ANT_HOME%\parser.jar;%JAVA_HOME%\lib\tools.jar;%GEF_HOME%\gef.jar;%NSUML_HOME%\nsuml.jar;%NSUML_HOME%\xml4j.jar;%OCL_HOME%\ocl-argo.jar
 echo Building with classpath $CLASSPATH:$LOCALCLASSPATH
 echo
