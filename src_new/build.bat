af7abc2029c18d098e83ad99967911a7d6ad4337
Toby Baier <toby@tigris.org>
2000-09-29 14:15:30 +0000

    Andrews tweaks. PR: Obtained from:Andrew Smith Submitted by:Toby Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@494 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/build.bat
+++ src_new/build.bat
@@ -1,21 +1,19 @@
-rem @echo off
-if not "%ANT_HOME%"=="" goto checkJava
+@echo off
 
+if not "%ANT_HOME%"=="" goto CHECKJAVA
 rem missing variable: ANT_HOME
-echo.
-echo ANT_HOME environment variable is not set.
-echo   Please set ANT_HOME to the installation directory of Ant 
-echo   (this should be the one containing \bin and \lib directory).
-echo   Latest version can be found on the Apache Jakarta Project site:
-echo   http://jakarta.apache.org/
-echo.
+echo ******************************************************************
+echo      Warning: ANT_HOME environment variable is not set.
+echo   Please set ANT_HOME to the installation directory of the build
+echo   utility Ant (this should be the one containing \bin and \lib
+echo   directories). The latest version of ANT can be found on the
+echo   Apache Jakarta Project site: http://jakarta.apache.org/
+echo ******************************************************************
 goto END
 
-
 :CHECKJAVA
-if "%JAVACMD%" == "" set JAVACMD=java
+if "%JAVACMD%" == "" set JAVACMD=JAVA
 set LOCALCP=%CLASSPATH%;%ANT_HOME%\lib\ant.jar;%ANT_HOME%\lib\jaxp.jar;%ANT_HOME%\lib\parser.jar
-
 
 rem add tools.jar
 if "%JAVA_HOME%" == "" goto NOJAVAHOME
@@ -23,19 +21,16 @@ if exist %JAVA_HOME%\lib\tools.jar SET LOCALCP=%LOCALCP%;%JAVA_HOME%\lib\tools.j
 if exist %JAVA_HOME%\jre\lib\classes.zip SET LOCALCP=%LOCALCP%;%JAVA_HOME%\jre\lib\classes.zip
 goto RUNANT
 
-
 :NOJAVAHOME
-echo.
-echo Warning: JAVA_HOME environment variable is not set.
-echo   If build fails because sun.* classes could not be found
-echo   you will need to set the JAVA_HOME environment variable
-echo   to the directory where the JDK is installed.
-echo.
-
+echo ***********************************************************
+echo    Warning: JAVA_HOME environment variable is not set.
+echo   Please set the JAVA_HOME environment variable to the 
+echo   directory where the JDK is installed.
+echo ***********************************************************
+goto END
 
 :RUNANT
-%JAVACMD% -classpath "%LOCALCP%" -Dant.home="%ANT_HOME%" %ANT_OPTS% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
-
+%JAVA_HOME%\BIN\%JAVACMD% -classpath %LOCALCP% -Dant.home=%ANT_HOME% -Dant.opts=%ANT_OPTS% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :END
-set LOCALCLASSPATH=
+set LOCALCP=
