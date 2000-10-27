86dc73ae1d52201bce681fddf8ecf54150cd7356
Toby Baier <toby@tigris.org>
2000-10-27 13:50:49 +0000

    version 0.8.1a including the ClassGenerationDialog fix PR: Obtained from:Toby Submitted by:Toby Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/argo-nsuml@533 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/uci/uml/ui/ClassGenerationDialog.java
+++ src/uci/uml/ui/ClassGenerationDialog.java
@@ -67,10 +67,10 @@ public class ClassGenerationDialog extends JFrame implements ActionListener {
     super("Generate Classes");
 
     Vector dirs = getClasspathEntries();
-    if (dirs.size() == 0) { 
-	dispose();
-	return;
-    }
+//     if (dirs.size() == 0) { 
+// 	dispose();
+// 	return;
+//     }
     _dir = new JComboBox(Converter.convert(getClasspathEntries()));
     _dir.setEditable(true);
 
