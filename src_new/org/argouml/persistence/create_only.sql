1dfecc0573f6be1cc884f6e6b272c97f0001c501
Toby Baier <toby@tigris.org>
2001-03-01 12:26:31 +0000

    added database documentation and helper files for database administration. also, the schema was deadly outdated... PR: Obtained from:Toby Submitted by:Toby Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@625 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/persistence/create_only.sql
+++ src_new/org/argouml/persistence/create_only.sql
@@ -2,64 +2,52 @@
 #----------------
 # data types
 #----------------
-DROP TABLE tAggregationKind;
 CREATE TABLE tAggregationKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tBoolean;
 CREATE TABLE tBoolean(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tChangeableKind;
 CREATE TABLE tChangeableKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tExpression;
 CREATE TABLE tExpression(
     language VARCHAR(20),
     body VARCHAR(100),
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
 
-DROP TABLE tBooleanExpression;
 CREATE TABLE tBooleanExpression(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
 
-DROP TABLE tProcedureExpression;
 CREATE TABLE tProcedureExpression(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
 
-DROP TABLE tParameterDirectionKind;
 CREATE TABLE tParameterDirectionKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tMessageDirectionKind;
 CREATE TABLE tMessageDirectionKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
  
-DROP TABLE tScopeKind;
 CREATE TABLE tScopeKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tVisibilityKind;
 CREATE TABLE tVisibilityKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tCallConcurrencyKind;
 CREATE TABLE tCallConcurrencyKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tMultiplicityRange;
 CREATE TABLE tMultiplicityRange(
     lower INTEGER,
     upper INTEGER,
@@ -67,19 +55,16 @@ CREATE TABLE tMultiplicityRange(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tMultiplicity;
 CREATE TABLE tMultiplicity(
     range INTEGER(5) NOT NULL, # REFERENCES tMultiplicityRange(MultiplicityRangeId),
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tMapping;
 CREATE TABLE tMapping(
     body VARCHAR(100),
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
  
-DROP TABLE tOrderingKind;
 CREATE TABLE tOrderingKind(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid)); 
@@ -87,7 +72,6 @@ CREATE TABLE tOrderingKind(
 #---------------------
 # extension mechanisms
 #---------------------
-DROP TABLE tStereotype;
 CREATE TABLE tStereotype(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -97,7 +81,6 @@ CREATE TABLE tStereotype(
     extendedElement INTEGER(5) , # REFERENCES tModelElement(ModelElementId),
     ConstraintId INTEGER(5) ); # REFERENCES tConstraint(ConstraintId));
  
-DROP TABLE tTaggedValue;
 CREATE TABLE tTaggedValue(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -110,13 +93,11 @@ CREATE TABLE tTaggedValue(
 # core
 #-----------------
 
-DROP TABLE tAttribute;
 CREATE TABLE tAttribute(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     initialValue VARCHAR(100));
  
-DROP TABLE tOperation;
 CREATE TABLE tOperation(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -127,14 +108,12 @@ CREATE TABLE tOperation(
     specification VARCHAR(100),
     MethodId INTEGER(5) ); # REFERENCES tMethod(MethodId)); # NOT USED!!!
  
-DROP TABLE tMethod;
 CREATE TABLE tMethod(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     body VARCHAR(100),
     specification INTEGER(5) ); # REFERENCES tOperation(OperationId)); 
  
-DROP TABLE tStructuralFeature;
 CREATE TABLE tStructuralFeature(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -143,20 +122,17 @@ CREATE TABLE tStructuralFeature(
     targetScope INTEGER(5), # REFERENCES tScopeKind(ScopeKindId),
     type INTEGER(5) ); # REFERENCES tClassifier(ClassifierId));
 
-DROP TABLE tBehavioralFeature;
 CREATE TABLE tBehavioralFeature(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     isQuery BIT,
     parameter INTEGER(5) ); # REFERENCES tParameter(ParameterId));
  
-DROP TABLE tClassifier;
 CREATE TABLE tClassifier(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     feature INTEGER(5) ); # REFERENCES tFeature(FeatureId)); # NOT USED!!!
  
-DROP TABLE tFeature;
 CREATE TABLE tFeature(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -164,13 +140,11 @@ CREATE TABLE tFeature(
     visibility INTEGER(5), # REFERENCES tVisibilityKind(VisibilityKindId),
     owner INTEGER(5) ); # REFERENCES tClassifier(ClassifierId));
 
-DROP TABLE tNamespace;
 CREATE TABLE tNamespace(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     ownedElement INTEGER(5) ); # REFERENCES tModelElement(ModelElementId)); # NOT USED!!!
 
-DROP TABLE tGeneralizableElement;
 CREATE TABLE tGeneralizableElement(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -178,7 +152,6 @@ CREATE TABLE tGeneralizableElement(
     isLeaf BIT,
     isAbstract BIT);
 
-DROP TABLE tParameter;
 CREATE TABLE tParameter(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -187,14 +160,12 @@ CREATE TABLE tParameter(
     BehavioralFeatureId INTEGER(5), # REFERENCES tBehavioralFeature(BehavioralFeatureId),
     type INTEGER(5) ); # REFERENCES tClassifier(ClassifierId));
 
-DROP TABLE tConstraint;
 CREATE TABLE tConstraint(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     body VARCHAR(100),
     constrainedElement INTEGER(5) ); # REFERENCES tModelElement(ModelElementId));
 
-DROP TABLE tModelElement;
 CREATE TABLE tModelElement(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -203,7 +174,6 @@ CREATE TABLE tModelElement(
     mconstraint INTEGER(5), # REFERENCES tConstraint(ConstraintId),
     PackageId INTEGER(5)); # REFERENCES tPackage(PackageId));
 
-DROP TABLE tAssociation;
 CREATE TABLE tAssociation(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -212,7 +182,6 @@ CREATE TABLE tAssociation(
     associationEnd1 INTEGER(5), # REFERENCES tAssociationEnd(AssociationEndId));
     associationEnd2 INTEGER(5)); # REFERENCES tAssociationEnd(AssociationEndId));
 
-DROP TABLE tAssociationEnd;
 CREATE TABLE tAssociationEnd(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
@@ -226,42 +195,35 @@ CREATE TABLE tAssociationEnd(
     type INTEGER(5), # REFERENCES tClassifier(ClassifierId),
     AssociationId INTEGER(5) ); # REFERENCES tAssociation(AssociationId));
 
-DROP TABLE tClass;
 CREATE TABLE tClass(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     isActive BIT);
 
-DROP TABLE tDependency;
 CREATE TABLE tDependency(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     supplier INTEGER(5), # REFERENCES tModelElement(ModelElementId),
     client INTEGER(5)); # REFERENCES tModelElement(ModelElementId));
 
-DROP TABLE tBinding;
 CREATE TABLE tBinding(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     argument INTEGER(5)); # REFERENCES tModelElement(ModelElementId));
 
-DROP TABLE tUsage;
 CREATE TABLE tUsage(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
 
-DROP TABLE tPermission;
 CREATE TABLE tPermission(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
 
-DROP TABLE tAbstraction;
 CREATE TABLE tAbstraction(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid),
     mapping VARCHAR(100));
 
-DROP TABLE tInterface;
 CREATE TABLE tInterface(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
@@ -270,12 +232,10 @@ CREATE TABLE tInterface(
 # model management
 #------------------------
 
-DROP TABLE tModel;
 CREATE TABLE tModel(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
 
-DROP TABLE tPackage;
 CREATE TABLE tPackage(
     uuid VARCHAR(50) NOT NULL,
     PRIMARY KEY(uuid));
