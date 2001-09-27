14860ab61f3ab0f49fd1cb3fbf662de395baf009
Thierry Lach <thierrylach@tigris.org>
2001-09-27 16:20:21 +0000

    Correcting incorrectly spelled class name in javadocs.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@842 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/cognitive/checklist/ChInterface.java
+++ org/argouml/uml/cognitive/checklist/ChInterface.java
@@ -36,7 +36,7 @@ import ru.novosoft.uml.model_management.*;
  *  @deprecated Now created by loading strings from 
  *     UMLCognitiveResourceBundle into a generic UMLChecklist instance
  *           
- *  @see UMLCognitiveResouceBundle
+ *  @see UMLCognitiveResourceBundle
  *  @see UMLChecklist
  * 
  */
