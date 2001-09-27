f7518f8d503cbb06e915e8aa280f231c8685dffb
Thierry Lach <thierrylach@tigris.org>
2001-09-27 02:10:08 +0000

    Have Argo SecurityManager throw an ArgoSecurityException so that it can be identified and trapped elsewhere.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@826 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/application/security/ArgoSecurityException.java
+++ org/argouml/application/security/ArgoSecurityException.java
@@ -23,58 +23,29 @@
 
 package org.argouml.application.security;
 
-// Import the following classes fully qualified to ensure that
-// no one can short-circuit our intended inheritance.
-import java.lang.ClassLoader;
-import java.lang.SecurityManager;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-import java.util.jar.Attributes;
-import java.security.Permission;
-import java.lang.Thread;
-import java.lang.Class;
-import java.lang.ThreadGroup;
-import java.lang.Object;
-import java.lang.String;
-import java.io.FileDescriptor;
-import java.net.InetAddress;
-// import java.security.GuardedObject;
-// import java.security.Guard;
+import java.lang.SecurityException;
 
-/** The Argo custom security manager.
+/** An Argo security exception.
  *
  *  @author Thierry Lach
  *  @since 0.9.4
  */
-public final class ArgoSecurityManager extends SecurityManager
+public class ArgoSecurityException extends SecurityException
 {
+    private boolean _hidden = false;
 
-    private boolean _allowExit = false;
+    public ArgoSecurityException() {
+        this(false);
+    } 
 
-    private static ArgoSecurityManager SINGLETON = new ArgoSecurityManager();
-
-    public final static ArgoSecurityManager getInstance() {
-        return SINGLETON;
+    public ArgoSecurityException(boolean hidden) {
+        super();
+	_hidden = hidden;
+	System.out.println ("ArgoSecurityException(" + hidden + ")");
     }
 
-    private ArgoSecurityManager() {
+    public boolean isHidden() {
+        return _hidden;
     }
 
-    public void checkPermission(Permission perm) {
-	if (perm instanceof java.lang.RuntimePermission) {
-	    if ("exitVM".equals(perm.getName())) {
-		if (! _allowExit) {
-		    throw new ArgoSecurityException(true);
-		}
-	    }
-	}
-    }
-
-    public boolean getAllowExit() {
-        return _allowExit;
-    }
-
-    public void setAllowExit(boolean allowExit) {
-        _allowExit = allowExit;
-    }
 }
