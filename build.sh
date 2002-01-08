27ac52179ea906c31c685d947d97823ec657de36
Thierry Lach <thierrylach@tigris.org>
2002-01-08 22:22:25 +0000

    Issue number:  451 Obtained from: Jeremy Bennet Submitted by:  Thierry Lach Reviewed by:   Luc Maisonobe
    Problems with build.sh
    
    CVS: ----------------------------------------------------------------------
    CVS: Issue number:
    CVS:   If this change addresses one or more issues,
    CVS:   then enter the issue number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to the project by someone else; i.e.,
    CVS:   they sent us a patch or a set of diffs, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@1277 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- build.sh
+++ build.sh
@@ -1,24 +1,60 @@
 #!/bin/sh
 
+# Modifications:
+# ==============
+
+# 10 Dec 2001:  Jeremy Bennett. LOCALCLASSPATH modified to point to lib sudir
+#               of $ANT_HOME for ant.jar. Checks added for $JAVA_HOME and
+#               $ARGO_HOME - these should be set in the environment.
+
 echo
 echo "Argo Build System (borrowed from FOP)"
 echo "-------------------------------------"
 echo
+
+# Check JAVA_HOME and ARGO_HOME are set
+
+if [ "$JAVA_HOME" = "" ]
+then
+  echo "ERROR: JAVA_HOME not found in your environment."
+  echo
+  echo "Please, set the JAVA_HOME variable in your environment to match the"
+  echo "location of the Java Virtual Machine you want to use."
+  exit 1
+fi
+
+if [ "$ARGO_HOME" = "" ]
+then
+    #  If it isn't set, check to see if we are in the src_new
+    #  directory and try it.
+    if [ -f ../src_new/org/argouml.mf ]
+    then
+        ARGO_HOME=..
+    fi
+fi
+
+if [ "$ARGO_HOME" = "" ]
+then
+  echo "ERROR: ARGO_HOME not found in your environment."
+  echo
+  echo "Please, set the ARGO_HOME variable in your environment to match the"
+  echo "location of your copy of the  ArgoUML CVS source tree."
+  exit 1
+fi
+
+# If $ANT_HOME is not set, use ARGO's copy
+
+if [ "$ANT_HOME" = "" ]
+then
+  ANT_HOME=$ARGO_HOME
+fi
+
+
 PATH=$PATH:$JAVA_HOME/bin
-ARGO_HOME=$HOME/argo09/argouml
-ANT_HOME=/usr/share/java
 NSUML_HOME=$ARGO_HOME/lib
 XML_HOME=$NSUML_HOME
 OCL_HOME=$NSUML_HOME
-LOCALCLASSPATH=$NSUML_HOME/nsuml.jar:$ANT_HOME/ant.jar:$NSUML_HOME/xerces.jar:$OCL_HOME/ocl-argo.jar:$JAVA_HOME/lib/tools.jar
-
-#if [ "$JAVA_HOME" = "" ] ; then
-#  echo "ERROR: JAVA_HOME not found in your environment."
-#  echo
-#  echo "Please, set the JAVA_HOME variable in your environment to match the"
-#  echo "location of the Java Virtual Machine you want to use."
-#  exit 1
-#fi
+LOCALCLASSPATH=$ANT_HOME/lib/ant.jar:$JAVA_HOME/lib/tools.jar
 
 echo Building with classpath $CLASSPATH:$LOCALCLASSPATH
 echo
@@ -28,4 +64,3 @@ echo
 
 java -Dant.home=$ANT_HOME -classpath $CLASSPATH:$LOCALCLASSPATH org.apache.tools.ant.Main $*
 
-#$JAVA_HOME/bin/java -Dant.home=$ANT_HOME -classpath "$LOCALCLASSPATH:$CLASSPATH" org.apache.tools.ant.Main $*
