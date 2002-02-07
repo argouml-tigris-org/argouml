64b9bea284c50a34a231ecd07ab6053e6261bdba
Jeremy Bennett <jeremybennett@tigris.org>
2002-02-07 14:40:06 +0000

    Updates to the Singleton Violated critic, to correctly recognise violation of the Singleton specification. Also requires updates to the cognitive resource bundle, which corrects the diagnosis.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@1347 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/pattern/cognitive/critics/CrSingletonViolated.java
+++ src_new/org/argouml/pattern/cognitive/critics/CrSingletonViolated.java
@@ -1,3 +1,4 @@
+
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -28,53 +29,154 @@
 // Original Author: jrobbins@ics.uci.edu
 // $Id$
 
+// 28 Jan 2002: Bug in triggering fixed by Jeremy Bennett
+// (mail@jeremybennett.com)
+
+// 28 Jan 2002: Jeremy Bennett (mail@jeremybennett.com). Bug in triggering
+// fixed.
+
+// 31 Jan 2002: Jeremy Bennett (mail@jeremybennett.com). Extended to recognise
+// any as a constructor operation with stereotype <<create>> as constructor.
+
+// 4 Feb 2002: Jeremy Bennett (mail@jeremybennett.com). Code factored by use of
+// static methods in central org.argouml.cognitive.critics.CriticUtils utility
+// class.
+
+
 package org.argouml.pattern.cognitive.critics;
 
 import java.util.*;
 
 import ru.novosoft.uml.foundation.core.*;
+import ru.novosoft.uml.foundation.data_types.*;
 import ru.novosoft.uml.foundation.extension_mechanisms.*;
 
 import org.argouml.cognitive.*;
+import org.argouml.cognitive.critics.*;
 import org.argouml.uml.*;
 import org.argouml.uml.cognitive.critics.*;
 
-/** A critic to detect whether a class violates the conditions required for
-    using a Singleton Stereotype. */
+/**
+ * <p>A critic to detect whether a class violates the conditions required for
+ * using a Singleton Stereotype.</p>
+ *
+ * <p>This stereotype is used to indicate a class which only ever has a single
+ * instance. The critic will trigger whenever a class has stereotype
+ * &laquo;Singleton&raquo; (or &laquo;singleton&raquo;), but does not meet the
+ * requirements of a Singleton class. These are:</p>
+ *
+ * <ol>
+ *   <li>An static variable to hold the sole instance of the class;</li>
+ *
+ *   <li>only private constructors to create the sole instance; and</li>
+ *
+ *   <li>At least one constructor to override the default constructor.</li>
+ * </ol>
+ *
+ * <p>The original version would always trigger for any class with stereotype
+ * &laquo;Singleton&raquo;. This version includes an implementation for the
+ * three tests above!</p>
+ *
+ * <p>Internally we use some of the static utility methods of the {@link
+ * org.argouml.cognitive.critics.CriticUtils CriticUtils} class.</p>
+ *
+ * @see <a href="http://argouml.tigris.org/documentation/snapshots/manual/argouml.html/#s2.ref.critics_singleton_violated">ArgoUML User Manual: Singleton Violated</a>
+ */
+
 public class CrSingletonViolated extends CrUML {
 
-  public CrSingletonViolated() {
-    setResource("CrSingletonViolated");
-    addSupportedDecision(CrUML.decPATTERNS);
-    setPriority(ToDoItem.LOW_PRIORITY);
-    addTrigger("stereotype");
-    addTrigger("structuralFeature");
-    addTrigger("associationEnd");
-  }
+    /**
+     * <p>Constructor for the critic.</p>
+     *
+     * <p>Sets up the resource name, which will allow headline and description
+     * to be found for the current locale. Provides a design issue category
+     * (PATTERNS), sets a priority for any to-do items (LOW) and adds triggers
+     * for metaclasses "stereotype", "structuralFeature" and
+     * "associationEnd".</p>
+     *
+     * @return  nothing returned since this is a constructor
+     */
 
-  protected void sd(String s) { setDescription(s); }
+    public CrSingletonViolated() {
 
-  public boolean predicate2(Object dm, Designer dsgr) {
-    if (!(dm instanceof MClass)) return NO_PROBLEM;
-    MClass cls = (MClass) dm;
-    Vector str = new Vector(MMUtil.SINGLETON.getAttributes(cls));
-    Vector ends = new Vector(cls.getAssociationEnds());
+        setResource("CrSingletonViolated");
 
-    boolean markedSingleton = false;
-    //if it is not marked Singleton, nevermind
-    MStereotype st = cls.getStereotype();
-    if (st != null && st.getName().equals("Singleton")) {
-		return PROBLEM_FOUND;
-		//markedSingleton = true;
-      }
-	else return NO_PROBLEM;
+        addSupportedDecision(CrUML.decPATTERNS);
+        setPriority(ToDoItem.LOW_PRIORITY);
 
-    //needs-more-work
-    // (1) The class must have at least one static attr with of the same type
-    // (2) All constructors should be private
-    // (3) There must be at least on constructor to override default constructor
+        // These may not actually make any difference at present (the code
+        // behind addTrigger needs more work).
 
-  }
+        addTrigger("stereotype");
+        addTrigger("structuralFeature");
+        addTrigger("associationEnd");
+    }
+
+
+    /**
+     * <p>The trigger for the critic.</p>
+     *
+     * <p>First check we are actually stereotyped "Singleton" (or we will
+     * accept "singleton").</p>
+     *
+     * <p>Next check for a static attribute with the same type as the Singleton
+     * class that will hold the instance of the Singleton class when its
+     * created.<p>
+     *
+     * <p>Finally check that there is at least one constructor to override the
+     * default constructor, and that all constructors are private.</p>
+     *
+     * <p>The original version of this critic was only partially implemented
+     * and triggered for any class with stereotype "Singleton".</p>
+     *
+     * @param  dm    the {@link java.lang.Object Object} to be checked against
+     *               the critic.
+     *
+     * @param  dsgr  the {@link org.argouml.cognitive.Designer Designer}
+     *               creating the model. Not used, this is for future
+     *               development of ArgoUML.
+     *
+     * @return       {@link #PROBLEM_FOUND PROBLEM_FOUND} if the critic is
+     *               triggered, otherwise {@link #NO_PROBLEM NO_PROBLEM}.  
+     */
+
+    public boolean predicate2(Object dm, Designer dsgr) {
+
+        // Only look at classes
+
+        if (!(dm instanceof MClass)) {
+            return NO_PROBLEM;
+        }
+
+        // Now we know it is a class, handle the object as a class
+
+        MClass cls = (MClass) dm;
+
+        // Check it actually is stereotyped as Singleton. Although "Singleton"
+        // is the usual naming, we'll accept "singleton"
+
+        if( !(CriticUtils.hasStereotype(cls,"Singleton") ||
+              CriticUtils.hasStereotype(cls,"singleton"))) {
+            return NO_PROBLEM;
+        }
+
+        // Now check if it has a static Attribute with the same type as the
+        // class. If not then we have a problem
+
+        if (!(CriticUtils.hasStaticAttrForClass(cls))) {
+            return PROBLEM_FOUND;
+        }
+
+        // Look at all the constructors and check i) there is at least one and
+        // ii) they are all private.
+
+        if (CriticUtils.hasOnlyPrivateConstructors(cls)) {
+            return NO_PROBLEM;
+        }
+        else {
+            return PROBLEM_FOUND;
+        }
+    }
 
 } /* end class CrSingletonViolated */
 
