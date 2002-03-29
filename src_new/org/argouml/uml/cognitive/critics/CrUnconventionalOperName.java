d298c189b9bafd11793f11670c149fb7852f99fe
Linus Tolke <linus@tigris.org>
2002-03-29 09:04:37 +0000

    Added a new Wizard for operations names. This helps in making constructors into constructors.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@1526 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/cognitive/critics/CrUnconventionalOperName.java
+++ src_new/org/argouml/uml/cognitive/critics/CrUnconventionalOperName.java
@@ -68,7 +68,7 @@ public class CrUnconventionalOperName extends CrUML {
     return NO_PROBLEM;
   }
 
-    public ToDoItem toDoItem(Object dm, Designer dsgr) {
+  public ToDoItem toDoItem(Object dm, Designer dsgr) {
     MFeature f = (MFeature) dm;
     VectorSet offs = computeOffenders(f);
     return new ToDoItem(this, offs, dsgr);
@@ -93,19 +93,42 @@ public class CrUnconventionalOperName extends CrUML {
     return res;
   }
 
+
+    /** candidateForConstructor tests if the operation name is the same
+     * as the class name. If so, an alternative path in the wizard is 
+     * possible where we are suggested to make the operation a constructor.
+     */
+    protected boolean candidateForConstructor(MModelElement me) {
+	if (!(me instanceof MOperation)) return false;
+	MOperation oper = (MOperation) me;
+	String myName = oper.getName();
+	if (myName == null || myName.equals("")) return false;
+	MModelElement cl = oper.getOwner();
+	String nameCl = cl.getName();
+	if (nameCl == null || nameCl.equals("")) return false;
+	if (myName.equals(nameCl)) return true;
+	return false;
+    }
+
+
   public void initWizard(Wizard w) {
-    if (w instanceof WizMEName) {
+    if (w instanceof WizOperName) {
       ToDoItem item = w.getToDoItem();
       MModelElement me = (MModelElement) item.getOffenders().elementAt(0);
       String sug = me.getName();
       sug = sug.substring(0,1).toLowerCase() + sug.substring(1);
+      boolean cand = candidateForConstructor(me);
       String ins = "Change the operation name to start with a "+
-	"lowercase letter.";
-      ((WizMEName)w).setInstructions(ins);
-      ((WizMEName)w).setSuggestion(sug);
+	"lowercase letter";
+      if (cand)
+	  ins = ins + " or make it a constructor";
+      ins = ins + ".";
+      ((WizOperName)w).setInstructions(ins);
+      ((WizOperName)w).setSuggestion(sug);
+      ((WizOperName)w).setPossibleConstructor(cand);
     }
   }
-  public Class getWizardClass(ToDoItem item) { return WizMEName.class; }
+  public Class getWizardClass(ToDoItem item) { return WizOperName.class; }
 
 } /* end class CrUnconventionalOperName */
 
