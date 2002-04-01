9774d8ab9a26e25f428da0886d9e06138889207f
Markus Klink <mkl@tigris.org>
2002-04-01 04:24:12 +0000

    adds display of stereotypes to Package adds context menu to toggle display Issue number: Obtained from: Submitted by:  Anders W Tell Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@1530 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/static_structure/ui/FigPackage.java
+++ org/argouml/uml/diagram/static_structure/ui/FigPackage.java
@@ -36,13 +36,17 @@ import javax.swing.*;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 
 import ru.novosoft.uml.foundation.core.*;
+import ru.novosoft.uml.foundation.extension_mechanisms.*;
+import ru.novosoft.uml.foundation.data_types.*;
 import ru.novosoft.uml.model_management.*;
+import ru.novosoft.uml.*;
 
 import org.tigris.gef.base.*;
 import org.tigris.gef.presentation.*;
 import org.tigris.gef.graph.*;
 
 import org.argouml.application.api.*;
+
 import org.argouml.uml.*;
 import org.argouml.uml.ui.*;
 import org.argouml.uml.generator.*;
@@ -71,32 +75,110 @@ public class FigPackage extends FigNodeModelElement {
 
   FigText _body;
 
+
+	/**
+	 * Flags that indicates if the stereotype should be shown even if it is specified or not.
+	 */
+	protected	boolean _showStereotype	= true;
+
+    /**
+     * <p>A rectangle to blank out the line that would otherwise appear at the
+     *   bottom of the stereotype text box.</p>
+     */
+	
+    protected FigRect _stereoLineBlinder;
+
+
+    /**
+     * <p>Flag to indicate that we have just been created. This is to fix the
+     *   problem with loading classes that have stereotypes already
+     *   defined.</p>
+     */
+
+    private boolean _newlyCreated = false;
+
   ////////////////////////////////////////////////////////////////
   // constructors
 
   public FigPackage() {
     Color handleColor = Globals.getPrefs().getHandleColor();
     _bigPort = new FigRect(x, y, width, height, null, null) {
-      public void paint(Graphics g) {
-        super.paint(g);
-        g.setColor(Color.black);
-        g.fillRect(_body.getX()+_shadowSize,_body.getY()+_body.getHeight(),_body.getWidth()-_shadowSize,_shadowSize);
-        g.fillRect(_body.getX()+_body.getWidth(),_body.getY()+_shadowSize,_shadowSize,_body.getHeight());
-        if (_name.getHeight() > _shadowSize)
-          g.fillRect(_name.getX()+_name.getWidth(),_name.getY()+_shadowSize,_shadowSize,_name.getHeight()-_shadowSize);
-      }
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.setColor(Color.black);
+			g.fillRect(_body.getX()+_shadowSize,_body.getY()+_body.getHeight(),_body.getWidth()-_shadowSize,_shadowSize);
+			g.fillRect(_body.getX()+_body.getWidth(),_body.getY()+_shadowSize,_shadowSize,_body.getHeight());
+			if (_stereo.getHeight() > _shadowSize)
+				{
+				g.fillRect(_stereo.getX()+_stereo.getWidth(),
+						   _stereo.getY()+_shadowSize,
+						   _shadowSize,
+						   _stereo.getHeight());
+				int blinderheight = (_name.getY()-1 - (_stereo.getY()+_stereo.getHeight()+1));
+				if (blinderheight> 0)
+					{
+//					g.fillRect(_stereo.getX()+_stereo.getWidth(),
+//							   _stereo.getY()+_stereo.getHeight(),
+//							   _shadowSize,
+//							   blinderheight);
+					}
+				}
+			if (_name.getHeight() > _shadowSize)
+				{
+				g.fillRect(_name.getX()+_name.getWidth(),
+						   _name.getY()+_shadowSize,
+						   _shadowSize,
+						   _name.getHeight()-_shadowSize);
+				}
+		}
     };
     _body = new FigText(x, y + textH, width, height - textH);
     _body.setEditable(false);
+
     _name.setBounds(x, y, width - indentX, textH + 2);
     _name.setJustification(FigText.JUSTIFY_LEFT);
 
     _bigPort.setFilled(false);
     _bigPort.setLineWidth(0);
 
+
+	// Set properties of the stereotype box. Make it 1 pixel higher than
+	// before, so it overlaps the name box, and the blanking takes out both
+	// lines. Initially not set to be displayed, but this will be changed
+	// when we try to render it, if we find we have a stereotype.
+	
+	_stereo.setExpandOnly(true);
+	_stereo.setFilled(true);
+	_stereo.setLineWidth(1);
+	_stereo.setEditable(false);
+	_stereo.setHeight(STEREOHEIGHT+1);
+	_stereo.setDisplayed(false);
+	
+	// A thin rectangle to overlap the boundary line between stereotype
+	// and name. This is just 2 pixels high, and we rely on the line
+	// thickness, so the rectangle does not need to be filled. Whether to
+	// display is linked to whether to display the stereotype.
+	
+	_stereoLineBlinder = new FigRect(11, 10+STEREOHEIGHT, 58, 2,
+									 Color.white, Color.white);
+	_stereoLineBlinder.setLineWidth(1);
+	_stereoLineBlinder.setDisplayed(false);
+	
+	// Mark this as newly created. This is to get round the problem with
+	// creating figs for loaded classes that had stereotypes. They are
+	// saved with their dimensions INCLUDING the stereotype, but since we
+	// pretend the stereotype is not visible, we add height the first time
+	// we render such a class. This is a complete fudge, and really we
+	// ought to address how class objects with stereotypes are saved. But
+	// that will be hard work.
+	
+	_newlyCreated = true;
+
     // add Figs to the FigNode in back-to-front order
     addFig(_bigPort);
+	addFig(_stereo);
     addFig(_name);
+	addFig(_stereoLineBlinder);
     addFig(_body);
 
     setBlinkPorts(false); //make port invisble unless mouse enters
@@ -125,8 +207,10 @@ public class FigPackage extends FigNodeModelElement {
     FigPackage figClone = (FigPackage) super.clone();
     Vector v = figClone.getFigs();
     figClone._bigPort = (FigRect) v.elementAt(0);
-    figClone._name = (FigText) v.elementAt(1);
-    figClone._body = (FigText) v.elementAt(2);
+	figClone._stereo = (FigText) v.elementAt(1);
+    figClone._name = (FigText) v.elementAt(2);
+	figClone._stereoLineBlinder = (FigRect) v.elementAt(3);
+    figClone._body = (FigText) v.elementAt(4);
     return figClone;
   }
 
@@ -134,28 +218,107 @@ public class FigPackage extends FigNodeModelElement {
   // Fig accessors
 
   public void setLineColor(Color col) {
+    super.setLineColor(col);
+    _stereo.setLineColor(col);
     _name.setLineColor(col);
     _body.setLineColor(col);
+	_stereoLineBlinder.setLineColor(_stereoLineBlinder.getFillColor());
   }
  public Color getLineColor() { return _body.getLineColor(); }
 
   public void setFillColor(Color col) {
+    super.setFillColor(col);
+    _stereo.setFillColor(col);
     _name.setFillColor(col);
     _body.setFillColor(col);
+	_stereoLineBlinder.setLineColor(col);
   }
   public Color getFillColor() { return _body.getFillColor(); }
 
   public void setFilled(boolean f) {
+    _stereo.setFilled(f);
     _name.setFilled(f);
     _body.setFilled(f);
   }
   public boolean getFilled() { return _body.getFilled(); }
 
   public void setLineWidth(int w) {
+    _stereo.setLineWidth(w);
     _name.setLineWidth(w);
     _body.setLineWidth(w);
   }
   public int getLineWidth() { return _body.getLineWidth(); }
+
+
+  ////////////////////////////////////////////////////////////////
+
+  public void renderingChanged() {
+    super.renderingChanged();
+//REMOVED called in modelChanged    updateStereotypeText();
+    modelChanged();
+  }
+    protected void updateStereotypeText() {
+        MModelElement me = (MModelElement) getOwner();
+		
+        if (me == null) 
+			{
+			return;
+			}
+		
+		Rectangle   rect   = getBounds();
+        MStereotype stereo = me.getStereotype();
+		
+		/* check if stereotype is defined */
+        if ((stereo == null) ||
+            (stereo.getName() == null) ||
+            (stereo.getName().length() == 0)) 
+			{
+            if (_stereo.isDisplayed()) 
+				{
+                _stereoLineBlinder.setDisplayed(false);
+                _stereo.setDisplayed(false);
+//                rect.y      += STEREOHEIGHT;
+//                rect.height -= STEREOHEIGHT;
+				}
+			}
+        else 
+			{
+			/* we got stereotype */
+            _stereo.setText(Notation.generateStereotype(this,stereo));
+			
+			if( !_showStereotype )
+				{
+				_stereoLineBlinder.setDisplayed(false);
+				_stereo.setDisplayed(false);
+				}
+
+            else if ( !_stereo.isDisplayed() ) 
+				{
+				if( _showStereotype )
+					{
+					_stereoLineBlinder.setDisplayed(true);
+					_stereo.setDisplayed(true);
+					
+					// Only adjust the stereotype height if we are not newly
+					// created. This gets round the problem of loading classes with
+					// stereotypes defined, which have the height already including
+					// the stereotype.
+					
+					if( !_newlyCreated ) 
+						{
+//						rect.y      -= STEREOHEIGHT;
+//						rect.height += STEREOHEIGHT;
+						}
+					}
+				}
+			}
+		
+        // Whatever happened we are no longer newly created, so clear the
+        // flag. Then set the bounds for the rectangle we have defined.
+		
+        _newlyCreated = false;
+		setBounds(rect.x, rect.y, rect.width, rect.height);
+    }
 
   ////////////////////////////////////////////////////////////////
   // user interaction methods
@@ -196,26 +359,133 @@ public class FigPackage extends FigNodeModelElement {
   public boolean getUseTrapRect() { return true; }
 
   public Dimension getMinimumSize() {
-    Dimension nameMinimum = _name.getMinimumSize();
+	  // Use "aSize" to build up the minimum size. Start with the size of the
+	  // name compartment and build up.
+	  
+	  Dimension aSize = _name.getMinimumSize();
+	  
+	  int w = aSize.width + indentX;
+	  int h = aSize.height+ height- textH;
 
-    int widthP = nameMinimum.width + indentX;
-    int heightP = nameMinimum.height + height - textH;
-    return new Dimension(widthP, heightP);
+	  // Ensure that the minimum height of the name compartment is at least
+	  // 21 pixels (hardcoded).
+
+	  if (aSize.height < 21) 
+		  {
+		  aSize.height = 21;
+		  }
+	  
+	  int minWidth = Math.max(0, indentX +1 + _shadowSize);
+	  if (aSize.width < minWidth) 
+		  {
+		  aSize.width = minWidth;
+		  }
+
+	  // If we have a stereotype displayed, then allow some space for that
+	  // (width and height)
+	  
+	  if (_stereo.isDisplayed()) 
+		  {
+		  aSize.width = Math.max(aSize.width,
+								 _stereo.getMinimumSize().width);
+		  aSize.height += STEREOHEIGHT;
+		  }
+	  // And now aSize has the answer
+
+        return aSize;
   }
 
+
+    /** 
+     * <p>Sets the bounds, but the size will be at least the one returned by
+     *   {@link #getMinimunSize()}.</p> 
+     *
+     * <p>If the required height is bigger, then the additional height is
+     *   equally distributed among all figs (i.e. compartments), such that the
+     *   cumulated height of all visible figs equals the demanded height<p>.
+     *
+     * <p>Some of this has "magic numbers" hardcoded in. In particular there is
+     *   a knowledge that the minimum height of a name compartment is 21
+     *   pixels.</p> 
+     *
+     * @param x  Desired X coordinate of upper left corner
+     *
+     * @param y  Desired Y coordinate of upper left corner
+     *
+     * @param w  Desired width of the FigClass
+     *
+     * @param h  Desired height of the FigClass
+     */
   public void setBounds(int x, int y, int w, int h) {
-    if (_name == null) return;
-    Rectangle oldBounds = getBounds();
-    Dimension nameMinimum = _name.getMinimumSize();
+	  // Save our old boundaries (needed later), and get minimum size
+	  // info. "aSize will be used to maintain a running calculation of our
+	  // size at various points.
+	  	  
+	  Rectangle oldBounds = getBounds();
+	  Dimension aSize = getMinimumSize();
+	  
+	  int newW = Math.max(w,aSize.width);
+	  int newH = h;
+	  
+	  int extra_each = 0;
+	  int height_correction = 0;
+	  
+	  // First compute all nessessary height data. Easy if we want less than
+	  // the minimum
+	  
+	  if (newH <= aSize.height)
+		  {	  
+	      // Just use the mimimum
+	      newH = aSize.height;		  
+		  } 
 
-    _name.setBounds(x, y, w - indentX, nameMinimum.height + 2);
-    _body.setBounds(x, y+1 + nameMinimum.height,
-		    w, h - 1 - nameMinimum.height);
-    _bigPort.setBounds(x+1, y+1,
-		       w-2, h - 2);
+	  // Now resize all sub-figs, including not displayed figs. Start by the
+	  // name. We override the getMinimumSize if it is less than our view (21
+	  // pixels hardcoded!). Add in the shared extra, plus in this case the
+	  // correction.
+	  
+	  int height = _name.getMinimumSize().height;
+	  
+	  if (height < 21) 
+		  {
+		  height = 21;
+		  }
+	  	  
+	  // Now sort out the stereotype display. If the stereotype is displayed,
+	  // move the upper boundary of the name compartment up and set new
+	  // bounds for the name and the stereotype compatments and the
+	  // stereoLineBlinder that blanks out the line between the two
+	  
+	  int currentY = y;
+	  
+	  if (_stereo.isDisplayed()) 
+		  {
+		  currentY += STEREOHEIGHT;
+		  }
+	  
+	  _name.setBounds(  x, currentY, newW-indentX, height);
+	  _stereo.setBounds(x, y,        newW-indentX, STEREOHEIGHT + 1);
+	  _stereoLineBlinder.setBounds(x + 1,y + STEREOHEIGHT,newW - 2-indentX,2);
+	  	  
+	  // Advance currentY to where the start of the body box is,
+	  // remembering that it overlaps the next box by 1 pixel. Calculate the
+	  // size of the attribute box, and update the Y pointer past it if it is
+	  // displayed.
+	  
+	  currentY += height - 1;  // -1 for 1 pixel overlap
+	  _body.setBounds(x, currentY, newW, newH + y - currentY);
 
-    calcBounds(); //_x = x; _y = y; _w = w; _h = h;
-    firePropChange("bounds", oldBounds, getBounds());
+	  // set bounds of big box
+	  
+	  _bigPort.setBounds(x,y,newW,newH);
+	  
+	  // Now force calculation of the bounds of the figure, update the edges
+	  // and trigger anyone who's listening to see if the "bounds" property
+	  // has changed.
+	  
+	  calcBounds();
+	  updateEdges();
+	  firePropChange("bounds", oldBounds, getBounds());
   }
 
   /**
@@ -227,6 +497,7 @@ public class FigPackage extends FigNodeModelElement {
   public Vector getPopUpActions(MouseEvent me) {
     Vector popUpActions = super.getPopUpActions(me);
     MPackage mclass = (MPackage) getOwner();
+
     ArgoJMenu modifierMenu = new ArgoJMenu("Modifiers");
 
     modifierMenu.addCheckItem(new ActionModifier("Abstract", "isAbstract", "isAbstract", "setAbstract", mclass));
@@ -235,9 +506,38 @@ public class FigPackage extends FigNodeModelElement {
 
     popUpActions.insertElementAt(modifierMenu, popUpActions.size() - 1);
 
+
+    ArgoJMenu showMenu = new ArgoJMenu("Show");
+
+	if ( !_showStereotype  )
+		  {
+		  showMenu.add(new UMLAction ("Show Stereotype", UMLAction.NO_ICON) {
+			  public void actionPerformed(ActionEvent ae) {
+				  _showStereotype = true;
+				  renderingChanged();
+				  damage();
+			  }
+		  });
+		  }
+	else 
+		{
+		  showMenu.add(new UMLAction ("Hide Stereotype", UMLAction.NO_ICON) {
+			  public void actionPerformed(ActionEvent ae) {
+				  _showStereotype = false;
+				  renderingChanged();
+				  damage();
+			  }
+		  });
+		  }
+
+    popUpActions.insertElementAt(showMenu, popUpActions.size() - 1);
+
+
+
     return popUpActions;
   }
 
 
 } /* end class FigPackage */
+
 
