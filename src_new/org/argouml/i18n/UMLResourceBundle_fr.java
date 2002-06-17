e0d87f65664947cf7bdeb32a42e8be78b92205f8
Jean-Hugues de Raigniac <jhraigniac@tigris.org>
2002-06-17 15:29:05 +0000

    Key resorting and completion
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@1740 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/i18n/UMLResourceBundle_fr.java
+++ src_new/org/argouml/i18n/UMLResourceBundle_fr.java
@@ -43,6 +43,19 @@ import org.argouml.util.*;
 public class UMLResourceBundle_fr extends ListResourceBundle {
 
     private static final Object[][] _contents = {
+        { "abstract", "abstrait" },
+        { "Abstract", "Abstrait" },  
+        { "active", "actif" },
+        { "Add actor", "Ajouter un acteur" },  
+        { "Add association" , "Ajouter une association" },
+        { "Add association end", "Ajouter une extr\u00e9mit\u00e9 d'association" },  
+        { "Add attribute" , "Ajouter un attribut" },
+        { "Add class", "Ajouter une classe" },  
+        { "Add datatype" , "Ajouter un type de donn\u00e9e" },
+        { "Add enumeration literal", "Ajouter un litt\u00e9ral d'enum\u00e9ration" },  
+        { "Add generalization", "Ajouter g\u00e9n\u00e9ralisation" },  
+        { "Add inner class" , "Ajouter une classe interne" },
+        { "Add interface", "Ajouter une interface" },  
         { "Add_Menu_Actor" , "Acteur..." } ,
         { "Add_Menu_Class" , "Classe..." } ,
         { "Add_Menu_Datatype" , "Type de Donn\u00e9e..." } ,
@@ -50,21 +63,26 @@ public class UMLResourceBundle_fr extends ListResourceBundle {
         { "Add_Menu_Interface" , "Interface..." } ,
         { "Add_Menu_Signal" , "Signal..." } ,
         { "Add_Menu_UseCase" , "Cas d'Utilisation..." },
-        { "Add association" , "Ajouter une association" },
-        { "Add attribute" , "Ajouter un attribut" },
-        { "Add datatype" , "Ajouter un type de donn\u00e9e" },
-        { "Add inner class" , "Ajouter une classe interne" },
         { "Add operation" , "Ajouter une operation" },
         { "Add parameter" , "Ajouter un param\u00e8tre" },
+        { "Add realization", "Ajouter r\u00e9alisation" },  
+        { "Add stereotype", "Ajouter un st\u00e9r\u00e9otype" },  
+        { "Add subpackage", "Ajouter un sous-paquetage" },  
         { "Add use case" , "Ajouter un cas d'utilisation" },
         { "Association:" , "Association:" },
+        { "Association Ends:", "Extr\u00e9mit\u00e9s d'Association:" },  
         { "Associations:" , "Associations:" },
         { "Attributes:" , "Attributs:" },
+
         { "Base Class:" , "Super-classe:" },
-        { "Class" , "Classe" },
+
         { "changeable" , "modifiable" },
+        { "Checklist", "Liste de contr\u00f4le" },         
+        { "Class" , "Classe" },
         { "Components:" , "Composants:" },
         { "Connections:" , "Liens:" },
+        { "Constraints", "Contraintes" },                  
+
         { "Delete actor" , "Supprimer l'acteur" },
         { "Delete association" , "Supprimer l'association" },
         { "Delete association end" , "Supprimer l'extr\u00e9mit\u00e9 de l'association" },
@@ -75,17 +93,32 @@ public class UMLResourceBundle_fr extends ListResourceBundle {
         { "Delete package" , "Supprimer le paquetage" },
         { "Delete parameter" , "Supprimer le param\u00e8tre" },
         { "Derived:" , "D\u00e9rivation:" },  // TBC, probably completely wrong
+        { "Documentation", "Documentation" },              
+
         { "Expression:" , "Expression:" },
         { "Extends:" , "Enrichit:" },
+
+        { "final", "final" },
+        { "Final", "Final" },  
+
         { "Go back" , "Revenir en arri\u00e8re" },
         { "Go forward" , "Aller en avant" },
+        { "Go up", "Remonter" },
+
+        { "Implementations:", "Impl\u00e9mentations:" },  
         { "Implements:" , "Impl\u00e9mente:" },
         { "Incoming:" , "Entr\u00e9es:" },  // TBC, probably completely wrong
+
         { "Language:" , "Langage:" },
         { "Literals:" , "Lit\u00e9raux:" },
+
+        { "Modifiers:", "Modificateurs:" },
         { "Multiplicity:" , "Multiplicit\u00e9:" },
+
         { "Name:" , "Nom:" },
         { "Namespace:" , "Espace de nommage:" },
+        { "NavClass", "Aller \u00e0 la classe s\u00e9lectionn\u00e9e" },
+        { "NavStereo", "Aller au st\u00e9r\u00e9otype s\u00e9lectionn\u00e9" },
         { "New actor" , "Nouvel acteur" },
         { "New association" , "Nouvelle association" },
         { "New attribute" , "Nouvel attribut" },
@@ -94,57 +127,38 @@ public class UMLResourceBundle_fr extends ListResourceBundle {
         { "New interface" , "Nouvelle interface" },
         { "New operation" , "Nouvelle op\u00e9ration" },
         { "New signal" , "Nouveau signal" },
+        { "New stereotype", "Nouveau st\u00e9r\u00e9otype" },  
         { "none" , "aucun" },
+
         { "Operations:" , "Op\u00e9rations:" },
         { "Ordering:" , "Ordre:" },
         { "Outgoing:" , "Sorties:" }, // TBC, probably completely wrong
+        { "Owned Elements", "\u00c9l\u00e9ments poss\u00e9d\u00e9s" },  
+        { "Owned Elements:", "\u00c9l\u00e9ments poss\u00e9d\u00e9s:" },
         { "Owner:" , "Propri\u00e9taire:" },
+
+        { "Properties", "Propri\u00e9t\u00e9s" },          
+        { "public", "public" },
+        { "Public", "Public" },  
+
         { "Receiver:" , "R\u00e9cepteur:" },
         { "Receives:" , "Re\u00e7oit:" },
+        { "root", "racine" },
+        { "Root", "Racine" },  
+
         { "Sends:" , "Envoie:" },
         { "sorted" , "tri\u00e9s" },
+        { "Source", "Source" },                            
+        { "Stereotype:", "St\u00e9r\u00e9otype:" },
+        { "Style", "Style" },                              
+
+        { "TaggedValues", "\u00c9tiquettes" },            
+        { "ToDoItem", "\u00c9l\u00e9ment \u00e0 corriger" },
         { "Transition" , "Transition" },
         { "Trigger:" , "Trigger:" },
         { "Type:" , "Type:" },
+
         { "Visibility:" , "Visibilit\u00e9:" },
-        {"public", "public" },
-        {"abstract", "abstrait" },
-        {"final", "final" },
-        {"root", "racine" },
-        {"Stereotype:", "St\u00e9r\u00e9otype:" },
-        {"Modifiers:", "Modificateurs:" },
-        {"active", "actif" },
-        {"Owned Elements:", "\u00c9l\u00e9ments poss\u00e9d\u00e9s:" },
-        {"Go up", "Remonter" },
-        {"Add generalization", "Ajouter g\u00e9n\u00e9ralisation" },  
-        {"Add realization", "Ajouter r\u00e9alisation" },  
-        {"Owned Elements", "\u00c9l\u00e9ments poss\u00e9d\u00e9s" },  
-        {"Add class", "Ajouter une classe" },  
-        {"Add interface", "Ajouter une interface" },  
-        {"Add stereotype", "Ajouter un st\u00e9r\u00e9otype" },  
-        {"Add actor", "Ajouter un acteur" },  
-        {"Add subpackage", "Ajouter un sous-paquetage" },  
-        {"Abstract", "Abstrait" },  
-        {"Final", "Final" },  
-        {"Root", "Racine" },  
-        {"Association Ends:", "Extr\u00e9mit\u00e9s d'Association:" },  
-        {"Add association end", "Ajouter une extr\u00e9mit\u00e9 d'association" },  
-        {"Public", "Public" },  
-        {"Implementations:", "Impl\u00e9mentations:" },  
-        {"New stereotype", "Nouveau st\u00e9r\u00e9otype" },  
-        {"Add enumeration literal", "Ajouter un litt\u00e9ral d'enum\u00e9ration" },  
-        {"NavStereo", "Aller au st\u00e9r\u00e9otype s\u00e9lectionn\u00e9" },
-        {"NavClass", "Aller \u00e0 la classe s\u00e9lectionn\u00e9e" },
-
-
-        { "Checklist", "Liste de contr\u00f4le" },         
-        { "Constraints", "Contraintes" },                  
-        { "Documentation", "Documentation" },              
-        { "Properties", "Propri\u00e9t\u00e9s" },          
-        { "Source", "Source" },                            
-        { "Style", "Style" },                              
-        { "TaggedValues", "\u00c9tiquettes" },            
-        { "ToDoItem", "\u00c9l\u00e9ment \u00e0 corriger" }
     };
 
     public Object[][] getContents() {
