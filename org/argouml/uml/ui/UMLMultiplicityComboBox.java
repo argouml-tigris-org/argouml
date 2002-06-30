056e8fc6c1dc2a38c032d00a4e4352b20857885c
Thomas Neustupny <thn@tigris.org>
2002-06-30 19:15:49 +0000

    obtained from: Lutz Bichler UMLMultiplicityComboBox enhancement, which allows to freely edit association end multiplicities.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@1819 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/UMLMultiplicityComboBox.java
+++ org/argouml/uml/ui/UMLMultiplicityComboBox.java
@@ -34,30 +34,61 @@ public class UMLMultiplicityComboBox extends JComboBox implements ItemListener, 
     private UMLUserInterfaceContainer _container;
     private Method _getMethod;
     private Method _setMethod;
-    private static String[] _mults = { "","0..1","0..2","0..3","0..*",
-                                "1..1","1..2","1..3","1..*",
-                                       "2..2","2..3","2..*",
-                                              "3..3","3..*" };
-    private static int[] _lower = { -1,0,0,0,0,1,1,1,1,2,2,2,3,3 };
-    private static int[] _upper = { -1,1,2,3,-1,1,2,3,-1,2,3,-1,3,-1 };
-    
+
+
+    private static String[] _sMultiplicities = {
+	"*",
+	"0..1", "0..2", "0..3", "0..*",
+	"1..1", "1..2", "1..3", "1..*",
+	"2..2", "2..3", "2..*",
+	"3..3", "3..*" 
+    };
+
+    private static int[] _iLower = {
+	-1,
+	0, 0, 0, 0,
+	1, 1, 1, 1,
+	2, 2, 2,
+	3, 3 
+    };
+
+    private static int[] _iUpper = {
+	-1,
+	1, 2, 3, -1,
+	1, 2, 3, -1,
+	2, 3, -1,
+	3, -1 
+    };
+
+   
     private static final Object[] _noArg = {};
     
     public UMLMultiplicityComboBox(UMLUserInterfaceContainer container,Class elementClass) {
+
         super();
-        setModel(new DefaultComboBoxModel(_mults));
+
+        setModel(new DefaultComboBoxModel(_sMultiplicities));
         _container = container;
+
+	setEditable(true);
+
         addItemListener(this);
+
         Class[] getArgs = {};
         Class[] setArgs = { MMultiplicity.class };
+
         try {
+
             _getMethod = elementClass.getMethod("getMultiplicity",getArgs);
-            _setMethod = elementClass.getMethod("setMultiplicity",setArgs);
-        }
-        catch(Exception e) {
+	    _setMethod = elementClass.getMethod("setMultiplicity",setArgs);
+
+        } catch(Exception e) {
+
             setEnabled(false);
             System.out.println(e.toString() + " in UMLMultiplicityComboBox()");
+
         }
+
     }
 
     public void targetChanged() {
@@ -69,14 +100,19 @@ public class UMLMultiplicityComboBox extends JComboBox implements ItemListener, 
     
     public void roleAdded(final MElementEvent p1) {
     }
+
     public void recovered(final MElementEvent p1) {
     }
+
     public void roleRemoved(final MElementEvent p1) {
     }
+
     public void listRoleItemSet(final MElementEvent p1) {
     }
+
     public void removed(final MElementEvent p1) {
     }
+
     public void propertySet(final MElementEvent event) {
         String eventProp = event.getName();
         if(eventProp == null || eventProp.equals("multiplicity")) {
@@ -85,63 +121,172 @@ public class UMLMultiplicityComboBox extends JComboBox implements ItemListener, 
     }
     
     private void update() {
+
         int lower = -1;
         int upper = -1;
         
         try {
-            Object obj = _getMethod.invoke(_container.getTarget(),_noArg);
+         
+	    Object obj = _getMethod.invoke(_container.getTarget(),_noArg);
             if(obj instanceof MMultiplicity) {
                 MMultiplicity mult = (MMultiplicity) obj;
                 lower = mult.getLower();
                 upper = mult.getUpper();
             }
-        }
-        catch(Exception e) {
+
+        } catch(Exception e) {
+
             System.out.println(e.toString() + " in UMLMultiplicityComboBox.update()");
+
         }
+
         boolean match = false;
-        for(int i = 0; i < _mults.length; i++) {
-            if(lower == _lower[i] && upper == _upper[i]) {
+	int i = 0;
+        while (i < _sMultiplicities.length && match==false) {
+
+            if (lower == _iLower[i] && upper == _iUpper[i]) {
+
                 setSelectedIndex(i);
                 match = true;
-                break;
+
             }
+
+	    i++;
+
         }
-        if(!match) {
+
+        if (match==false) {
+
             StringBuffer buf = new StringBuffer();
-            if(lower <= 0) {
+
+            if (lower <= 0) {
+
                 buf.append("0..");
-            }
-            else {
+
+            } else {
+
                 buf.append(Integer.toString(lower));
                 buf.append("..");
             }
-            if(upper < 0) {
+
+            if (upper < 0) {
+
                 buf.append("*");
-            }
-            else {
+
+            } else {
+
                 buf.append(Integer.toString(upper));
+
             }
+
             setSelectedItem(buf.toString());
+
         }
+
     }
     
     public void itemStateChanged(ItemEvent event) {
+
         if(event.getStateChange() == ItemEvent.SELECTED) {
+
             int index = getSelectedIndex();
-            int lower = _lower[index];
-            int upper = _upper[index];
-            MMultiplicity mult = null;
-            if(lower >= 0) {
-                mult = new MMultiplicity(lower,upper);
+
+	    int lower=0, upper=0;
+
+	    if (index >= 0 && index < _iLower.length) {
+
+		lower = _iLower[index];
+		upper = _iUpper[index];
+
+	    } else {
+
+		boolean ok = true;
+
+		String s = (String)getSelectedItem();
+		s = s.trim();
+		int length = s.length();
+
+		if (length>0) {
+
+		    String sLower = new String(s);
+		    String sUpper = new String(s);
+
+		    int i = 0;
+		    while (i<length&&Character.isDigit(s.charAt(i))) i++;
+
+		    if (i==0) {
+
+			ok = false;
+		
+		    }
+
+		    sLower = s.substring(0, i);
+		    sUpper = s.substring(i).trim();
+		    
+		    if (sUpper!=null && sUpper.length()>0) {
+
+			if (!sUpper.startsWith("..")) {
+
+			    ok = false;
+		
+			} else {
+
+			    sUpper = sUpper.substring(2);
+
+			    length = sUpper.length();
+			    i = 0;
+			    while (i<length&&Character.isDigit(s.charAt(i))) i++;
+
+			    if (i!=sUpper.length()) {
+
+				ok = false;
+				
+			    } 
+
+			}
+
+		    } else {
+
+			sUpper = new String(sLower);
+
+		    }
+
+
+		    if (ok==true) {
+
+			Integer intLower = new Integer(sLower);
+			Integer intUpper = new Integer(sUpper);
+
+			lower = intLower.intValue();
+			upper = intUpper.intValue();
+
+		    }
+
+		}		
+
+	    }
+		
+	    
+	    MMultiplicity mult = null;
+	    if(lower >= -1) {
+
+		mult = new MMultiplicity(lower,upper);
+
             }
+
             try {
+
                 _setMethod.invoke(_container.getTarget(),new Object[] { mult });
-            }
-            catch(Exception e) {
+
+            } catch(Exception e) {
+
                 System.out.println(e.toString() + " in UMLMultiplicityComboBox.itemStateChanged()");
+
             }
-            
+
         }
+
     }
+
 }
+
