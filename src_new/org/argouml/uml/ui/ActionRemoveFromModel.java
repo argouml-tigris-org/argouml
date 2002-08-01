425326584bd25840e954ce90a8adcb9d6672ce8d
Jaap Branderhorst <kataka@tigris.org>
2002-08-01 11:44:18 +0000

    Added comment. Improved deleting diagrams (it's possible now :))
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@2148 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/ActionRemoveFromModel.java
+++ src_new/org/argouml/uml/ui/ActionRemoveFromModel.java
@@ -25,6 +25,7 @@ package org.argouml.uml.ui;
 
 import org.argouml.kernel.*;
 import org.argouml.ui.*;
+import org.argouml.uml.diagram.ui.UMLDiagram;
 import org.argouml.application.api.*;
 
 import org.tigris.gef.base.*;
@@ -32,15 +33,30 @@ import org.tigris.gef.presentation.*;
 import org.tigris.gef.util.*;
 
 import ru.novosoft.uml.foundation.core.*;
+import ru.novosoft.uml.model_management.MModel;
+import ru.novosoft.uml.model_management.MModelImpl;
 
 import java.util.*;
 import java.awt.event.*;
 import java.text.MessageFormat;
 
 import javax.swing.*;
+import javax.swing.tree.TreePath;
+
+/**
+ * Action for removing (moving to trash) objects from the model. Objects can be:
+ * - Modelelements (NSUML)
+ * - Diagrams (argodiagram and it's children)
+ * The root model and the last diagram in the project can not be removed. The 
+ * reason for this is to prevent problems updating the detailspane and the 
+ * navpane. Besides that, it is not possible to make a new root model.
+ * 
+ * @author original author not known.
+ * @author jaap.branderhorst@xs4all.nl extensions
+ */
 
 public class ActionRemoveFromModel extends UMLChangeAction {
-    
+ 
     ////////////////////////////////////////////////////////////////
     // static variables
     
@@ -56,84 +72,117 @@ public class ActionRemoveFromModel extends UMLChangeAction {
         );
     }
     
-    ////////////////////////////////////////////////////////////////
-    // main methods
     
-    public boolean shouldBeEnabled() {
-        ProjectBrowser pb = ProjectBrowser.TheInstance;
-        Object target = pb.getDetailsTarget();
-        
-        if (target instanceof MModelElement) {
-            return true;
-        }
-        int size = 0;
-        try {
-            // needs-more-work: trashing diagrams
-            Editor ce = Globals.curEditor();
-            Vector figs = ce.getSelectionManager().getFigs();
-            size = figs.size();
-        }
-        catch(Exception e) {}
-        
-        if (size > 0) {
-            return true;
-        }
-        //     for (int i = 0; i < size; i++) {
-        //       Fig f = (Fig) figs.elementAt(i);
-        //       Object owner = f.getOwner();
-        //       if (owner instanceof MModelElement) return true;
-        //     }
-        
-        return false;
+    /**
+     * Returns the selected object. It is possible that the selected target on
+     * the detailspane is not selected on the navpane. This is a defect. 
+     * Therefore we give priority to the selection at the detailspane.
+     * @return Object
+     */
+    protected Object getTarget() {
+    	ProjectBrowser pb = ProjectBrowser.TheInstance;
+    	Object target = pb.getDetailsTarget();
+    	if (target == null) { // nothing selected on detailspane
+    		target = pb.getNavPane().getSelectedObject();
+    	}
+    	return target;
     }
     
+    /**
+     * Only disabled when nothing is selected. Necessary to use since this 
+     * option works via the menu too. A user cannot delete the last diagram. 
+     * A user cannot delete the root model.
+     * @see org.argouml.uml.ui.UMLAction#shouldifBeEnabled()
+     */	
+    public boolean shouldBeEnabled() {
+        if (getTarget() instanceof Diagram) { // we cannot delete the last diagram
+            return ProjectBrowser.TheInstance.getProject().getDiagrams().size() > 1;
+        }
+        if (getTarget() instanceof MModel &&  // we cannot delete the model itself
+            getTarget().equals(ProjectBrowser.TheInstance.getProject().getModel())) {
+                return false;
+        }
+    	return getTarget() != null;
+    	
+    }
+    
+    /**
+     * Moves the selected target to the trash bin. Moves the selected target 
+     * after the remove to the parent of the selected target (that is: the next
+     * level up in the navpane). In case of a diagram the selected target will 
+     * be the next diagram in the list with diagrams.
+     * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+     */
     public void actionPerformed(ActionEvent ae) {
-        ProjectBrowser pb = ProjectBrowser.TheInstance;
-        Object target = pb.getDetailsTarget();
-        Project p = pb.getProject();
-        
-        if (target instanceof MModelElement) {
-            if (sureRemove((MModelElement) target)) {
+        Object target = getTarget();
+        if (target != null) {
+            if (sureRemove(target)) {
                 // Argo.log.info("deleting "+target+"+ "+(((MModelElement)target).getMElementListeners()).size());
-                p.moveToTrash(target);
-            }
-        }
-        // needs-more-work: trashing diagrams
-        else {
-            int size = 0;
-            try {
-                Editor ce = Globals.curEditor();
-                Vector figs = ce.getSelectionManager().getFigs();
-                size = figs.size();
-                for (int i = 0; i < size; i++) {
-                    Fig f = (Fig) figs.elementAt(i);
-                    Object owner = f.getOwner();
-                    if (owner instanceof MModelElement) {
-                        if (! sureRemove((MModelElement) owner)) {
-                            return;
-                        }
+                // move the pointer to the target in the NavPane to some other target (up)
+                ProjectBrowser pb = ProjectBrowser.TheInstance;
+                TreePath path = pb.getNavPane().getParentPath();
+                if (path != null) {
+                    Object o = path.getLastPathComponent();
+                    Project pr = pb.getProject();
+                    if (o instanceof MModel && // root model
+                        o.equals(pr.getModel()) &&
+                        target instanceof Diagram) {
+                        int targetIndex = 
+                            pr.getDiagrams().indexOf(target);
+                        o = targetIndex == 0? pr.getDiagrams().get(1): 
+                            pr.getDiagrams().get(targetIndex-1);
+                    }
+                    if (o instanceof Diagram) {
+                        ProjectBrowser.TheInstance.setTarget(o);
+                    } else {
+                        ProjectBrowser.TheInstance.select(o);
                     }
                 }
-                
-                for (int i = 0; i < size; i++) {
-                    Fig f = (Fig) figs.elementAt(i);
-                    Object owner = f.getOwner();
-                    if (owner == null) {
-                        f.delete();
-                    }
-                    else if (owner instanceof MModelElement) {
-                        p.moveToTrash(owner);
-                    }
-                }
-            }
-            catch(Exception ex) {
-                Argo.log.error("ActionRemoveFromModel.actionPerformed()Exception = ");
+                ProjectBrowser.TheInstance.getProject().moveToTrash(target); 
             }
         }
-        
         super.actionPerformed(ae);
     }
     
+    /**
+     * A utility method that asks the user if he is sure to remove the selected
+     * target. 
+     * @param target
+     * @return boolean
+     */
+    public boolean sureRemove(Object target) {
+    	// usage of other sureRemove method is legacy. They should be integrated.
+    	boolean sure = false;
+    	if (target instanceof MModelElement) sure = sureRemove((MModelElement)target); 
+    	if (target instanceof UMLDiagram) {
+    		// lets see if this diagram has some figs on it
+    		UMLDiagram diagram = (UMLDiagram)target;
+    		Vector nodes = diagram.getNodes();
+    		Vector edges = diagram.getNodes();
+    		if ((nodes.size() + edges.size()) > 0) {
+    			 // the diagram contains figs so lets ask the user if he/she is sure
+    			 String confirmStr = MessageFormat.format(Localizer.localize("Actions",
+                                         "template.remove_from_model.confirm_delete"),
+                                          new Object[] {diagram.getName(), ""});
+                 int response = JOptionPane.showConfirmDialog(ProjectBrowser.TheInstance, confirmStr,
+                                            Localizer.localize("Actions", 
+                                            "text.remove_from_model.confirm_delete_title"),
+                                            JOptionPane.YES_NO_OPTION);
+        		sure = (response == JOptionPane.YES_OPTION);
+    		} else { // no content of diagram
+    			sure = true;
+    		}
+    	}
+    	return sure;
+    }
+    
+    /**
+     * An utility method that asks the user if he is sure to remove a selected 
+     * modelement.
+     * @see ActionRemoveFromModel#sureRemove(Object)
+     * @param me
+     * @return boolean
+     */
     public boolean sureRemove(MModelElement me) {
         ProjectBrowser pb = ProjectBrowser.TheInstance;
         Project p = pb.getProject();
