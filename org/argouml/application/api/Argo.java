c4d2b606a6d51684bad6b4ecd64f25c7ada6d702
Linus Tolke <linus@tigris.org>
2002-08-11 20:03:09 +0000

    Fixed unreadable file format.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@2206 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/application/api/Argo.java
+++ org/argouml/application/api/Argo.java
@@ -1,69 +1,37 @@
 // Copyright (c) 1996-2001 The Regents of the University of California. All
-
 // Rights Reserved. Permission to use, copy, modify, and distribute this
-
 // software and its documentation without fee, and without a written
-
 // agreement is hereby granted, provided that the above copyright notice
-
 // and this paragraph appear in all copies.  This software program and
-
 // documentation are copyrighted by The Regents of the University of
-
 // California. The software program and documentation are supplied "AS
-
 // IS", without any accompanying services from The Regents. The Regents
-
 // does not warrant that the operation of the program will be
-
 // uninterrupted or error-free. The end-user understands that the program
-
 // was developed for research purposes and is advised not to rely
-
 // exclusively on the program for any reason.  IN NO EVENT SHALL THE
-
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
-
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-
 // THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-
 // SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
-
 // WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-
 // PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
-
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
 package org.argouml.application.api;
 
 import org.argouml.application.configuration.*;
-
 import org.argouml.application.modules.*;
-
 import org.argouml.application.events.*;
-
 import org.argouml.util.logging.*;
-
 import org.apache.log4j.*;
-
 import org.apache.log4j.helpers.*;
-
 import org.apache.log4j.or.*;
-
 import org.apache.log4j.spi.*;
-
 import java.util.*;
-
 import java.io.*;
-
 import javax.swing.*;
 
 /**
@@ -88,302 +56,230 @@ import javax.swing.*;
  * </ul>
  *
  */
-
 public class Argo
 {
 
-	/** Key for argo resource directory.
-	 */
+    /** Key for argo resource directory.
+     */
+    public static final String RESOURCEDIR = "/org/argouml/resource/";
 
-	public static final String RESOURCEDIR = "/org/argouml/resource/";
+    /** argo.ini path
+     */
+    public static final String ARGOINI = "/org/argouml/argo.ini";
 
-	/** argo.ini path
-	 */
+    /** Key for menu resource bundle.
+     */
+    public static final String MENU_BUNDLE = "CoreMenu";
 
-	public static final String ARGOINI = "/org/argouml/argo.ini";
+    /** Key for default startup directory.
+     */
+    public static final ConfigurationKey KEY_STARTUP_DIR =
+	Configuration.makeKey("default", "user", "dir");
 
-	/** Key for menu resource bundle.
-	 */
+    /** Key to show splash screen.
+     */
+    public static final ConfigurationKey KEY_SPLASH =
+	Configuration.makeKey("init", "splash");
 
-	public static final String MENU_BUNDLE = "CoreMenu";
+    /** Key to preload classes.
+     */
+    public static final ConfigurationKey KEY_PRELOAD =
+	Configuration.makeKey("init", "preload");
 
-	/** Key for default startup directory.
-	 */
+    /** Key to report usage statistics.
+     */
+    public static final ConfigurationKey KEY_EDEM = Configuration.makeKey("init", "edem");
 
-	public static final ConfigurationKey KEY_STARTUP_DIR =
-		Configuration.makeKey("default", "user", "dir");
+    /** Key to profile initialization.
+     */
+    public static final ConfigurationKey KEY_PROFILE =
+	Configuration.makeKey("init", "profile");
 
-	/** Key to show splash screen.
-	 */
+    /** Key for last saved project URL.
+     */
+    public static final ConfigurationKey KEY_MOST_RECENT_PROJECT_FILE =
+	Configuration.makeKey("project", "mostrecent", "file");
 
-	public static final ConfigurationKey KEY_SPLASH =
-		Configuration.makeKey("init", "splash");
+    /** Key to reload last saved project on startup.
+     */
+    public static final ConfigurationKey KEY_RELOAD_RECENT_PROJECT =
+	Configuration.makeKey("init", "project", "loadmostrecent");
 
-	/** Key to preload classes.
-	 */
+    /** Key for screen top
+     */
+    public static final ConfigurationKey KEY_SCREEN_TOP_Y =
+	Configuration.makeKey("screen", "top");
 
-	public static final ConfigurationKey KEY_PRELOAD =
-		Configuration.makeKey("init", "preload");
+    /** Key for screen left
+     */
+    public static final ConfigurationKey KEY_SCREEN_LEFT_X =
+	Configuration.makeKey("screen", "left");
 
-	/** Key to report usage statistics.
-	 */
+    /** Key for screen width
+     */
+    public static final ConfigurationKey KEY_SCREEN_WIDTH =
+	Configuration.makeKey("screen", "width");
 
-	public static final ConfigurationKey KEY_EDEM = Configuration.makeKey("init", "edem");
+    /** Key for screen height
+     */
+    public static final ConfigurationKey KEY_SCREEN_HEIGHT =
+	Configuration.makeKey("screen", "height");
 
-	/** Key to profile initialization.
-	 */
+    /** Key for southwest pane width
+     */
+    public static final ConfigurationKey KEY_SCREEN_SOUTHWEST_WIDTH =
+	Configuration.makeKey("screen", "southwest", "width");
 
-	public static final ConfigurationKey KEY_PROFILE =
-		Configuration.makeKey("init", "profile");
+    /** Key for west pane width
+     */
+    public static final ConfigurationKey KEY_SCREEN_WEST_WIDTH =
+	Configuration.makeKey("screen", "west", "width");
 
-	/** Key for last saved project URL.
-	 */
+    /** Key for south pane height
+     */
+    public static final ConfigurationKey KEY_SCREEN_SOUTH_HEIGHT =
+	Configuration.makeKey("screen", "south", "height");
 
-	public static final ConfigurationKey KEY_MOST_RECENT_PROJECT_FILE =
-		Configuration.makeKey("project", "mostrecent", "file");
+    /** Key for theme
+     */
+    public static final ConfigurationKey KEY_SCREEN_THEME =
+	Configuration.makeKey("screen", "theme");
 
-	/** Key to reload last saved project on startup.
-	 */
+    /** Key for user email address
+     */
+    public static final ConfigurationKey KEY_USER_EMAIL =
+	Configuration.makeKey("user", "email");
 
-	public static final ConfigurationKey KEY_RELOAD_RECENT_PROJECT =
-		Configuration.makeKey("init", "project", "loadmostrecent");
+    /** Key for user full name
+     */
+    public static final ConfigurationKey KEY_USER_FULLNAME =
+	Configuration.makeKey("user", "fullname");
 
-	/** Key for screen top
-	 */
+    /** Standard definition of the logging category for the console.
+     */
+    public static final String CONSOLE_LOG = "argo.console.log";
 
-	public static final ConfigurationKey KEY_SCREEN_TOP_Y =
-		Configuration.makeKey("screen", "top");
+    /** Standard definition of the logging category for the console.
+     */
+    public static final String ARGO_CONSOLE_SUPPRESS = "argo.console.suppress";
 
-	/** Key for screen left
-	 */
+    /** Standard definition of system variable to add text prefix to console log.
+     */
+    public static final String ARGO_CONSOLE_PREFIX = "argo.console.prefix";
 
-	public static final ConfigurationKey KEY_SCREEN_LEFT_X =
-		Configuration.makeKey("screen", "left");
+    /** Define a static log4j category variable for ArgoUML to log to
+     *  the console.  This would be used in preference to System.out.println
+     *  for console output because this information can be intercepted
+     *  by <code>log4j</code> and included in other logs.  This also allows
+     *  custom <code>log4j</code> formatters
+     *  to be used on objects displayed on the console log.
+     */
+    public final static Category log;
 
-	/** Key for screen width
-	 */
+    /** Don't let this class be instantiated. */
+    private Argo()
+    {
 
-	public static final ConfigurationKey KEY_SCREEN_WIDTH =
-		Configuration.makeKey("screen", "width");
+    }
 
-	/** Key for screen height
-	 */
+    /** Change the default startup directory.
+     */
+    public static void setDirectory(String dir)
+    {
+	// Store in the user configuration, and
+	// let gef know also.
+	org.tigris.gef.base.Globals.setLastDirectory(dir);
 
-	public static final ConfigurationKey KEY_SCREEN_HEIGHT =
-		Configuration.makeKey("screen", "height");
+	// Configuration.setString(KEY_STARTUP_DIR, dir);
+    }
 
-	/** Key for southwest pane width
-	 */
+    /** Get the default startup directory.
+     */
+    public static String getDirectory()
+    {
+	// Use the configuration if it exists, otherwise
+	// use what gef thinks.
+	return Configuration.getString(
+				       KEY_STARTUP_DIR,
+				       org.tigris.gef.base.Globals.getLastDirectory());
+    }
 
-	public static final ConfigurationKey KEY_SCREEN_SOUTHWEST_WIDTH =
-		Configuration.makeKey("screen", "southwest", "width");
+    /** Helper for localization to eliminate the need to import
+     *  the gef util library.
+     */
+    public static String localize(String bundle, String key)
+    {
+	return org.tigris.gef.util.Localizer.localize(bundle, key);
+    }
 
-	/** Key for west pane width
-	 */
+    /** Returns a vector of plugins of the class type passed
+     *  which satisfy both of the contexts required.
+     *
+     *  If no plugins are available, returns null.
+     */
+    public static final ArrayList getPlugins(Class pluginType, Object[] context)
+    {
+	return ModuleLoader.getInstance().getPlugins(pluginType, context);
+    }
 
-	public static final ConfigurationKey KEY_SCREEN_WEST_WIDTH =
-		Configuration.makeKey("screen", "west", "width");
+    /** Returns a vector of all plugins of the class type passed.
+     *
+     *  If no plugins are available, returns null.
+     */
+    public static final ArrayList getPlugins(Class pluginType)
+    {
+	return ModuleLoader.getInstance().getPlugins(pluginType, null);
+    }
 
-	/** Key for south pane height
-	 */
+    /** Initializes the module loader.  Multiple calls are ignored.
+     */
+    public static final void initializeModules()
+    {
+	ModuleLoader.getInstance().initialize();
+    }
 
-	public static final ConfigurationKey KEY_SCREEN_SOUTH_HEIGHT =
-		Configuration.makeKey("screen", "south", "height");
+    public static String getArgoHome()
+    {
+	return ModuleLoader.getInstance().getArgoHome();
+    }
 
-	/** Key for theme
-	 */
+    public static String getArgoRoot()
+    {
+	return ModuleLoader.getInstance().getArgoRoot();
+    }
 
-	public static final ConfigurationKey KEY_SCREEN_THEME =
-		Configuration.makeKey("screen", "theme");
+    public static Icon lookupIconResource(String arg1)
+    {
+	return org.tigris.gef.util.ResourceLoader.lookupIconResource(arg1);
+    }
 
-	/** Key for user email address
-	 */
+    public static Icon lookupIconResource(String arg1, String arg2)
+    {
+	return org.tigris.gef.util.ResourceLoader.lookupIconResource(arg1, arg2);
+    }
 
-	public static final ConfigurationKey KEY_USER_EMAIL =
-		Configuration.makeKey("user", "email");
+    static {
+	// Create a throwable renderer
+	ThrowableRenderer tr = new ThrowableRenderer();
+	// Create a separate hierarchy for the argo logger
+	Hierarchy hier = new Hierarchy(new RootCategory(Priority.INFO));
+	// Add the ThrowableRenderer
+	hier.getRendererMap().put(Throwable.class, tr);
+	// Set up the argo console logger in its own hierarchy
+	Category cat = hier.getInstance(CONSOLE_LOG);
+	cat.addAppender(new ConsoleAppender(
+	    new PatternLayout(System.getProperty(ARGO_CONSOLE_PREFIX, "")
+			      + "%m%n"),
+	    ConsoleAppender.SYSTEM_OUT));
 
-	/** Key for user full name
-	 */
+	// Add the throwable renderer
+	cat.getRoot().getHierarchy().getRendererMap().put(Throwable.class, tr);
 
-	public static final ConfigurationKey KEY_USER_FULLNAME =
-		Configuration.makeKey("user", "fullname");
-
-	/** Standard definition of the logging category for the console.
-	 */
-
-	public static final String CONSOLE_LOG = "argo.console.log";
-
-	/** Standard definition of the logging category for the console.
-	 */
-
-	public static final String ARGO_CONSOLE_SUPPRESS = "argo.console.suppress";
-
-	/** Standard definition of system variable to add text prefix to console log.
-	 */
-
-	public static final String ARGO_CONSOLE_PREFIX = "argo.console.prefix";
-
-	/** Define a static log4j category variable for ArgoUML to log to
-	 *  the console.  This would be used in preference to System.out.println
-	 *  for console output because this information can be intercepted
-	 *  by <code>log4j</code> and included in other logs.  This also allows
-	 *  custom <code>log4j</code> formatters
-	 *  to be used on objects displayed on the console log.
-	 */
-
-	public final static Category log;
-
-	/** Don't let this class be instantiated. */
-
-	private Argo()
-	{
-
+	if (System.getProperty(ARGO_CONSOLE_SUPPRESS) != null) {
+	    cat.getRoot().getHierarchy().disableAll();
 	}
 
-	/** Change the default startup directory.
-	 */
-
-	public static void setDirectory(String dir)
-	{
-
-		// Store in the user configuration, and
-
-		// let gef know also.
-
-		org.tigris.gef.base.Globals.setLastDirectory(dir);
-
-		// Configuration.setString(KEY_STARTUP_DIR, dir);
-
-	}
-
-	/** Get the default startup directory.
-	 */
-
-	public static String getDirectory()
-	{
-
-		// Use the configuration if it exists, otherwise
-
-		// use what gef thinks.
-
-		return Configuration.getString(
-			KEY_STARTUP_DIR,
-			org.tigris.gef.base.Globals.getLastDirectory());
-
-	}
-
-	/** Helper for localization to eliminate the need to import
-	 *  the gef util library.
-	 */
-
-	public static String localize(String bundle, String key)
-	{
-
-		return org.tigris.gef.util.Localizer.localize(bundle, key);
-
-	}
-
-	/** Returns a vector of plugins of the class type passed
-	 *  which satisfy both of the contexts required.
-	 *
-	 *  If no plugins are available, returns null.
-	 */
-
-	public static final ArrayList getPlugins(Class pluginType, Object[] context)
-	{
-
-		return ModuleLoader.getInstance().getPlugins(pluginType, context);
-
-	}
-
-	/** Returns a vector of all plugins of the class type passed.
-	 *
-	 *  If no plugins are available, returns null.
-	 */
-
-	public static final ArrayList getPlugins(Class pluginType)
-	{
-
-		return ModuleLoader.getInstance().getPlugins(pluginType, null);
-
-	}
-
-	/** Initializes the module loader.  Multiple calls are ignored.
-	 */
-
-	public static final void initializeModules()
-	{
-
-		ModuleLoader.getInstance().initialize();
-
-	}
-
-	public static String getArgoHome()
-	{
-
-		return ModuleLoader.getInstance().getArgoHome();
-
-	}
-
-	public static String getArgoRoot()
-	{
-
-		return ModuleLoader.getInstance().getArgoRoot();
-
-	}
-
-	public static Icon lookupIconResource(String arg1)
-	{
-
-		return org.tigris.gef.util.ResourceLoader.lookupIconResource(arg1);
-
-	}
-
-	public static Icon lookupIconResource(String arg1, String arg2)
-	{
-
-		return org.tigris.gef.util.ResourceLoader.lookupIconResource(arg1, arg2);
-
-	}
-
-	static {
-
-		// Create a throwable renderer
-
-		ThrowableRenderer tr = new ThrowableRenderer();
-
-		// Create a separate hierarchy for the argo logger
-
-		Hierarchy hier = new Hierarchy(new RootCategory(Priority.INFO));
-
-		// Add the ThrowableRenderer
-
-		hier.getRendererMap().put(Throwable.class, tr);
-
-		// Set up the argo console logger in its own hierarchy
-
-		Category cat = hier.getInstance(CONSOLE_LOG);
-
-		cat.addAppender(
-			new ConsoleAppender(
-				new PatternLayout(System.getProperty(ARGO_CONSOLE_PREFIX, "") + "%m%n"),
-				ConsoleAppender.SYSTEM_OUT));
-
-		// Add the throwable renderer
-
-		cat.getRoot().getHierarchy().getRendererMap().put(Throwable.class, tr);
-
-		if (System.getProperty(ARGO_CONSOLE_SUPPRESS) != null)
-		{
-
-			cat.getRoot().getHierarchy().disableAll();
-
-		}
-
-		// Set log here.  No going back.
-
-		log = cat;
-
-	}
-
+	// Set log here.  No going back.
+	log = cat;
+    }
 }
