8fc0f17ff5e2e40d668c1f818ed4cfe91ab65de0
Commits manufactured by cvs2svn <no-author@argouml.tigris.org>
2002-09-12 19:26:23 +0000

    This commit was manufactured by cvs2svn to create branch 'avendor'.
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/avendor@2500 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ modules/java_rte/src/org/argouml/language/java/ui/ActionSynchronize.java
@@ -0,0 +1,105 @@
+// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.language.java.ui;
+
+//import org.argouml.language.java.rte.*;
+import org.argouml.application.api.*;
+import org.argouml.uml.ui.*;
+import org.argouml.ui.ProjectBrowser;
+
+import java.awt.event.*;
+import java.lang.reflect.*;
+import java.util.*;
+import javax.swing.*;
+
+/** Plugin for synchronizing Java sources (RTE).
+ *
+ *  @author Thomas Neustupny
+ *  @since  0.11.3
+ */
+public class ActionSynchronize extends UMLAction implements PluggableMenu
+{
+  /**
+   * Create a new ActionSynchronize (this is not public)
+   */
+  protected ActionSynchronize() {
+    super("Synchronize model/code...", false);
+  }
+
+  /**
+   * Opens the synchronization dialog and fills it with 'differences'
+   */
+  public void actionPerformed(ActionEvent event) {
+    //Argo.log.info("User clicked on '" + event.getActionCommand() + "'");
+    SynchronizeDialog syndia = new SynchronizeDialog(ProjectBrowser.TheInstance,true);
+    syndia.show();
+  }
+
+  public void setModuleEnabled(boolean v) {}
+
+  /**
+   * Initialize module
+   */
+  public boolean initializeModule() {
+    //Argo.log.info ("+--------------------------+");
+    //Argo.log.info ("| Plugin Java RTE enabled! |");
+    //Argo.log.info ("+--------------------------+");
+    return true;
+  }
+
+  public Object[] buildContext(JMenuItem a, String b) {
+    return new Object[] { a, b };
+  }
+
+  public boolean inContext(Object[] o) {
+    if (o.length < 2) return false;
+    // We are in context for any JMenuItem.
+    if (o[0] instanceof JMenuItem && o[1].equals("Tools")) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isModuleEnabled() { return true; }
+  public Vector getModulePopUpActions(Vector v, Object o) { return null; }
+  public boolean shutdownModule() { return true; }
+  public String getModuleName() { return "ActionSynchronize"; }
+  public String getModuleDescription() { return "Menu Item for Synchronizing Model and Java Code"; }
+  public String getModuleAuthor() { return "Thomas Neustupny"; }
+  public String getModuleVersion() { return "0.11.3"; }
+  public String getModuleKey() { return "module.language.java.rte"; }
+
+  public JMenuItem getMenuItem(JMenuItem mi, String s) {
+    return getMenuItem(buildContext(mi, s));
+  }
+
+  public JMenuItem getMenuItem(Object [] context) {
+    if (!inContext(context)) {
+      return null;
+	}
+    JMenuItem _menuItem = new JMenuItem("Synchronize model/code...");
+	_menuItem.addActionListener(this);
+    return _menuItem;
+  }
+}
