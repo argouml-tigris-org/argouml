2687f77b5dd81d4372ecdf837cdb100bde7f0332
Michael Stockman <d00mst@tigris.org>
2002-09-26 23:06:39 +0000

    Issue 1223.
    NPE in setOwner fixed. Also removes the current figure from the previous
    owner, which seems like a sensible thing to do.
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@2558 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/static_structure/ui/FigClass.java
+++ org/argouml/uml/diagram/static_structure/ui/FigClass.java
@@ -1006,13 +1006,24 @@ public class FigClass extends FigNodeModelElement {
 	 * @see org.tigris.gef.presentation.Fig#setOwner(Object)
 	 */
 	public void setOwner(Object own) {
+		MClass cl;
+		cl = (MClass)getOwner();
+		if (cl != null) {
+			Iterator it = cl.getFeatures().iterator();
+			while (it.hasNext()) {
+				MFeature feat = (MFeature)it.next();
+				feat.removeMElementListener(this);
+			}
+		}
 		super.setOwner(own);
-		MClass cl = (MClass)own;
-		Iterator it = cl.getFeatures().iterator();
-		while (it.hasNext()) {
-			MFeature feat = (MFeature)it.next();
-			feat.removeMElementListener(this);
-			feat.addMElementListener(this);
+		cl = (MClass)own;
+		if (cl != null) {
+			Iterator it = cl.getFeatures().iterator();
+			while (it.hasNext()) {
+				MFeature feat = (MFeature)it.next();
+				feat.removeMElementListener(this);
+				feat.addMElementListener(this);
+			}
 		}
 	}
 
