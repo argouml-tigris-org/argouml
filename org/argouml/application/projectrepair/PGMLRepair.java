68b46b8adbe202de19c37c6ba43b99153c78f426
Jaap Branderhorst <kataka@tigris.org>
2002-10-02 10:14:02 +0000

    Replaced projectrepair to HEAD branch
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@2590 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/application/projectrepair/PGMLRepair.java
@@ -0,0 +1,26 @@
+package org.argouml.application.projectrepair;
+
+/**
+ * @author Administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class PGMLRepair extends AbstractRepairImpl {
+
+    /**
+     * Constructor for PGMLRepair.
+     */
+    public PGMLRepair() {
+        super();
+    }
+
+    /**
+     * @see org.argouml.filerepair.AbstractRepairImpl#internalRepairDocument()
+     */
+    protected void internalRepairDocument() {
+    }
+
+}
