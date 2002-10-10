d598f588b4ddd600d22cf4032f2e214eae50357f
Bob Tarling <bobtarling@tigris.org>
2002-10-10 21:00:12 +0000

    New toolbar container
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/work_refactor_mklink@2667 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src_new/org/argouml/ui/MultiToolbar.java
@@ -0,0 +1,18 @@
+/*
+ * MultiToolbar.java
+ *
+ * Created on 22 September 2002, 22:44
+ */
+
+package org.argouml.ui;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MultiToolbar extends JComponent {	
+    
+    public MultiToolbar() {
+        setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+        this.setBackground(Color.blue);
+    }
+} 
