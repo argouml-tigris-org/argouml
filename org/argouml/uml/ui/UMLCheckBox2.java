0be3160efce79369cff568500057e73e93304216
Jaap Branderhorst <kataka@tigris.org>
2002-10-12 16:47:41 +0000

    Added UMLCheckBox2. A simple and reliable checkbox to depict boolean attributes from modelelements
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@2703 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/ui/UMLCheckBox2.java
@@ -0,0 +1,159 @@
+// Copyright (c) 1996-99 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+// $header$
+package org.argouml.uml.ui;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JCheckBox;
+
+import ru.novosoft.uml.MElementEvent;
+
+/**
+ * The checkbox to be used to show boolean attributes in the GUI's. Mostly used
+ * on proppanels. The other new GUI elements (like UMLLinkedList) divide the 
+ * responsibility of showing an attribute and maintaining the state of the 
+ * attribute between a GUI element and a model. This is not the case for the 
+ * UMLCheckBox2. Reason for this is that the model is just to simple to allow
+ * extra classes for the model. 
+ * 
+ * @since Oct 12, 2002
+ * @author jaap.branderhorst@xs4all.nl
+ */
+public abstract class UMLCheckBox2
+    extends JCheckBox
+    implements UMLUserInterfaceComponent {
+
+    private UMLUserInterfaceContainer _container;
+    private Object _target;
+    
+    /**
+     * Constructor for UMLCheckBox2.
+     * @param text
+     * @param selected
+     */
+    public UMLCheckBox2(UMLUserInterfaceContainer container, String text, Action a) {
+        super(text);
+        setContainer(container);
+        addActionListener(a);
+        setActionCommand((String)a.getValue(a.ACTION_COMMAND_KEY));
+    }
+
+    /**
+     * @see org.argouml.uml.ui.UMLUserInterfaceComponent#targetChanged()
+     */
+    public void targetChanged() {
+        buildModel();
+    }
+
+    /**
+     * @see org.argouml.uml.ui.UMLUserInterfaceComponent#targetReasserted()
+     */
+    public void targetReasserted() {
+        buildModel();
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#propertySet(ru.novosoft.uml.MElementEvent)
+     */
+    public void propertySet(MElementEvent e) {
+        buildModel();
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#roleAdded(ru.novosoft.uml.MElementEvent)
+     */
+    public void roleAdded(MElementEvent e) {
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#roleRemoved(ru.novosoft.uml.MElementEvent)
+     */
+    public void roleRemoved(MElementEvent e) {
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#listRoleItemSet(ru.novosoft.uml.MElementEvent)
+     */
+    public void listRoleItemSet(MElementEvent e) {
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#removed(ru.novosoft.uml.MElementEvent)
+     */
+    public void removed(MElementEvent e) {
+    }
+
+    /**
+     * @see ru.novosoft.uml.MElementListener#recovered(ru.novosoft.uml.MElementEvent)
+     */
+    public void recovered(MElementEvent e) {
+    }
+    
+    /**
+     * Returns the container.
+     * @return UMLUserInterfaceContainer
+     */
+    public UMLUserInterfaceContainer getContainer() {
+        return _container;
+    }
+
+    /**
+     * Returns the target. The target is directly asked from the _target attribute
+     * and not from the _container attribute to make the move to a new targeting
+     * system as easy as possible.
+     * @return Object
+     */
+    public Object getTarget() {
+        return _target;
+    }
+
+    /**
+     * Sets the container.
+     * @param container The container to set
+     */
+    public void setContainer(UMLUserInterfaceContainer container) {
+        _container = container;
+        if (container != null) {
+            setTarget(container.getTarget());
+        }
+    }
+
+    /**
+     * Sets the target. This method will not be used untill the target does
+     * not come via the container.
+     * @param target The target to set
+     */
+    public void setTarget(Object target) {
+        _target = target;
+    }
+    
+    /**
+     * Builds the model. That is: it sets the checkbox to true or false. The name of this
+     * method is choosen to be compliant with for example UMLModelElementListModel2
+     */
+    abstract public void buildModel();
+       
+
+}
