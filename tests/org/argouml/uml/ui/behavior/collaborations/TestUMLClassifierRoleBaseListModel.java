f8941267ccd3b6c376ed3fd4bba4bed004d9a019
Jaap Branderhorst <kataka@tigris.org>
2002-10-27 21:30:02 +0000

    More tests
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/work_modeleventpump_jaap@2851 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ tests/org/argouml/uml/ui/behavior/collaborations/TestUMLClassifierRoleBaseListModel.java
@@ -0,0 +1,85 @@
+// Copyright (c) 1996-99 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+// $header$
+package org.argouml.uml.ui.behavior.collaborations;
+
+import org.argouml.model.uml.behavioralelements.collaborations.CollaborationsFactory;
+import org.argouml.model.uml.foundation.core.CoreFactory;
+import org.argouml.uml.ui.AbstractUMLModelElementListModel2Test;
+import org.argouml.uml.ui.UMLUserInterfaceContainer;
+import ru.novosoft.uml.MBase;
+import ru.novosoft.uml.behavior.collaborations.MClassifierRole;
+import ru.novosoft.uml.foundation.core.MClassifier;
+
+/**
+ * @since Oct 27, 2002
+ * @author jaap.branderhorst@xs4all.nl
+ */
+public class TestUMLClassifierRoleBaseListModel
+    extends AbstractUMLModelElementListModel2Test {
+
+    /**
+     * Constructor for TestUMLClassifierRoleBaseListModel.
+     * @param arg0
+     */
+    public TestUMLClassifierRoleBaseListModel(String arg0) {
+        super(arg0);
+    }
+
+    /**
+     * @see org.argouml.uml.ui.AbstractUMLModelElementListModel2Test#buildElement()
+     */
+    protected void buildElement() {
+        elem = CollaborationsFactory.getFactory().createClassifierRole();
+    }
+
+    /**
+     * @see org.argouml.uml.ui.AbstractUMLModelElementListModel2Test#buildModel(org.argouml.uml.ui.UMLUserInterfaceContainer)
+     */
+    protected void buildModel(UMLUserInterfaceContainer cont) {
+        model = new UMLClassifierRoleBaseListModel(cont);
+    }
+
+    /**
+     * @see org.argouml.uml.ui.AbstractUMLModelElementListModel2Test#fillModel()
+     */
+    protected MBase[] fillModel() {
+        MClassifier[] bases = new MClassifier[10];
+        for(int i = 0 ; i < 10 ; i++) {
+            bases[i] = CoreFactory.getFactory().createClass();
+            ((MClassifierRole)elem).addBase(bases[i]);
+        }
+        return bases;
+    }
+
+    /**
+     * @see org.argouml.uml.ui.AbstractUMLModelElementListModel2Test#removeHalfModel(ru.novosoft.uml.MBase)
+     */
+    protected void removeHalfModel(MBase[] elements) {
+        for (int i = 0 ; i < 5 ; i++) {
+            ((MClassifierRole)elem).removeBase((MClassifier)elements[i]);
+        }
+    }
+
+}
