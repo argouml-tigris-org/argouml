fc9b51c425ef6793262d79cfb6ea56df43620b47
Markus Klink <mkl@tigris.org>
2002-11-02 10:06:53 +0000

    rearranged the components of the dialog a little bit. Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@2893 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/cognitive/ui/DesignIssuesDialog.java
+++ org/argouml/cognitive/ui/DesignIssuesDialog.java
@@ -1,4 +1,4 @@
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// Copyright (c) 1996-2002 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -35,179 +35,185 @@ import ru.novosoft.uml.model_management.*;
 
 import org.argouml.cognitive.*;
 
+/** A dialog to set the priorities for decisions. These will be evulated
+ *  against the critics, so that the user will only see todo items which match
+ *  the priorties for a certain decision.
+ */
 public class DesignIssuesDialog extends JDialog
-implements ActionListener, ChangeListener {
-  public static int _numDecisionModel = 0;
+    implements ActionListener, ChangeListener {
+    public static int _numDecisionModel = 0;
 
-  ////////////////////////////////////////////////////////////////
-  // constants
-  public final int WIDTH = 300;
-  public final int HEIGHT = 450;
+    ////////////////////////////////////////////////////////////////
+    // constants
+    public final int WIDTH = 300;
+    public final int HEIGHT = 450;
 
-  ////////////////////////////////////////////////////////////////
-  // instance variables
-  protected JPanel  _mainPanel = new JPanel();
-  protected JButton _okButton = new JButton("OK");
-  protected Hashtable _slidersToDecisions = new Hashtable();
-  protected Hashtable _slidersToDigits = new Hashtable();
+    ////////////////////////////////////////////////////////////////
+    // instance variables
+    protected JPanel  _mainPanel = new JPanel();
+    protected JButton _okButton = new JButton("OK");
+    protected Hashtable _slidersToDecisions = new Hashtable();
+    protected Hashtable _slidersToDigits = new Hashtable();
 
-  ////////////////////////////////////////////////////////////////
-  // constructors
+    ////////////////////////////////////////////////////////////////
+    // constructors
 
-  public DesignIssuesDialog(Frame parent) {
-    super(parent, "Design Issues", false);
+    public DesignIssuesDialog(Frame parent) {
+        super(parent, "Design Issues", false);
 
-    int x = parent.getLocation().x + (parent.getSize().width - WIDTH) / 2;
-    int y = parent.getLocation().y + (parent.getSize().height - HEIGHT) / 2;
-    setLocation(x, y);
-    setSize(WIDTH, HEIGHT);
-    Container content = getContentPane();
-    content.setLayout(new BorderLayout());
-    initMainPanel();
-    JScrollPane scroll =
-      new JScrollPane(_mainPanel,
-		      ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-		      ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-    content.add(scroll, BorderLayout.CENTER);
+        int x = parent.getLocation().x + (parent.getSize().width - WIDTH) / 2;
+        int y = parent.getLocation().y + (parent.getSize().height - HEIGHT) / 2;
+        setLocation(x, y);
+        setSize(WIDTH, HEIGHT);
+        Container content = getContentPane();
+        content.setLayout(new BorderLayout());
+        initMainPanel();
+        JScrollPane scroll =
+            new JScrollPane(_mainPanel,
+                            ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        content.add(scroll, BorderLayout.CENTER);
 
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-    JPanel buttonInner = new JPanel(new GridLayout(1, 1));
-    buttonInner.add(_okButton);
-    buttonPanel.add(buttonInner);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        JPanel buttonInner = new JPanel(new GridLayout(1, 1));
+        buttonInner.add(_okButton);
+        buttonPanel.add(buttonInner);
 
-    content.add(buttonPanel, BorderLayout.SOUTH);
-    _okButton.addActionListener(this);
+        content.add(buttonPanel, BorderLayout.SOUTH);
+        _okButton.addActionListener(this);
 
-    getRootPane().setDefaultButton(_okButton);
+        getRootPane().setDefaultButton(_okButton);
 
-    _numDecisionModel++;
-  }
-
-
-  public void initMainPanel() {
-    DecisionModel dm = Designer.TheDesigner.getDecisionModel();
-    Vector decs = dm.getDecisions();
-
-    GridBagLayout gb = new GridBagLayout();
-    _mainPanel.setLayout(gb);
-
-    GridBagConstraints c = new GridBagConstraints();
-    c.fill = GridBagConstraints.BOTH;
-    c.weightx = 1.0;
-    c.weighty = 0.0;
-    c.ipadx = 3; c.ipady = 3;
-
-
-    
-//     c.gridy = 0;
-//     c.gridx = 0;
-//     JLabel priLabel = new JLabel("Priority:");
-//     gb.setConstraints(priLabel, c);
-//     _mainPanel.add(priLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 1;
-//     JLabel offLabel = new JLabel("Off");
-//     gb.setConstraints(offLabel, c);
-//     _mainPanel.add(offLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 2;
-//     JLabel lowLabel = new JLabel("Low");
-//     gb.setConstraints(lowLabel, c);
-//     _mainPanel.add(lowLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 3;
-//     JLabel twoLabel = new JLabel("ad");
-//     gb.setConstraints(twoLabel, c);
-//     _mainPanel.add(twoLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 4;
-//     JLabel threeLabel = new JLabel("asd");
-//     gb.setConstraints(threeLabel, c);
-//     _mainPanel.add(threeLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 5;
-//     JLabel fourLabel = new JLabel("asd");
-//     gb.setConstraints(fourLabel, c);
-//     _mainPanel.add(fourLabel);
-
-//     c.gridy = 0;
-//     c.gridx = 6;
-//     JLabel highLabel = new JLabel("High");
-//     gb.setConstraints(highLabel, c);
-//     _mainPanel.add(highLabel);
-
-
-    c.gridy = 1;
-    java.util.Enumeration enum = decs.elements();
-    while (enum.hasMoreElements()) {
-      Decision d = (Decision) enum.nextElement();
-      JLabel decLabel = new JLabel(d.getName());
-      JLabel valueLabel = new JLabel(getValueText(d.getPriority()));
-      JSlider decSlide = new JSlider(SwingConstants.HORIZONTAL,
-				     0, 3, d.getPriority());
-      decSlide.setInverted(true);
-      decSlide.setMajorTickSpacing(1);
-      decSlide.setPaintTicks(true);
-      decSlide.setSnapToTicks(true);
-      // decSlide.setPaintLabels(true);
-      decSlide.addChangeListener(this);
-      Dimension origSize = decSlide.getPreferredSize();
-      Dimension smallSize = new Dimension(origSize.width/2, origSize.height);
-      decSlide.setSize(smallSize);
-      decSlide.setPreferredSize(smallSize);
-
-      _slidersToDecisions.put(decSlide, d);
-      _slidersToDigits.put(decSlide, valueLabel);
-
-      c.gridx = 0;
-      c.gridwidth = 1;
-      c.weightx = 0.0;
-      c.ipadx = 3;
-      gb.setConstraints(decLabel, c);
-      _mainPanel.add(decLabel);
-
-      c.gridx = 1;
-      c.gridwidth = 1;
-      c.weightx = 0.0;
-      c.ipadx = 0;
-      gb.setConstraints(valueLabel, c);
-      _mainPanel.add(valueLabel);
-
-      c.gridx = 2;
-      c.gridwidth = 6;
-      c.weightx = 1.0;
-      gb.setConstraints(decSlide, c);
-      _mainPanel.add(decSlide);
-
-      c.gridy++;
+        _numDecisionModel++;
     }
-  }
-  
-  ////////////////////////////////////////////////////////////////
-  // event handlers
-  
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == _okButton) {
-      setVisible(false);
-      dispose();
+
+
+    public void initMainPanel() {
+        DecisionModel dm = Designer.TheDesigner.getDecisionModel();
+        Vector decs = dm.getDecisions();
+
+        GridBagLayout gb = new GridBagLayout();
+        _mainPanel.setLayout(gb);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 0.0;
+        c.ipadx = 3; c.ipady = 3;
+
+
+        c.gridy = 0;
+        c.gridx = 0;
+        c.gridwidth = 1;
+        JLabel decTitleLabel = new JLabel("Decision");
+        gb.setConstraints(decTitleLabel, c);
+        _mainPanel.add(decTitleLabel);
+
+        c.gridy = 0;
+        c.gridx = 2;
+        c.gridwidth = 8;
+        JLabel priLabel = new JLabel("Priority");
+        gb.setConstraints(priLabel, c);
+        _mainPanel.add(priLabel);
+
+        c.gridy = 1;
+        c.gridx = 2;
+        c.gridwidth = 2;
+        JLabel offLabel = new JLabel("Off");
+        gb.setConstraints(offLabel, c);
+        _mainPanel.add(offLabel);
+
+        c.gridy = 1;
+        c.gridx = 4;
+        c.gridwidth = 2;
+        JLabel lowLabel = new JLabel("Low");
+        gb.setConstraints(lowLabel, c);
+        _mainPanel.add(lowLabel);
+
+        c.gridy = 1;
+        c.gridx = 6;
+        c.gridwidth = 2;
+        JLabel mediumLabel = new JLabel("Medium");
+        gb.setConstraints(mediumLabel, c);
+        _mainPanel.add(mediumLabel);
+
+        c.gridy = 1;
+        c.gridx = 8;
+        c.gridwidth = 2;
+        JLabel highLabel = new JLabel("High");
+        gb.setConstraints(highLabel, c);
+        _mainPanel.add(highLabel);
+
+
+        c.gridy = 2;
+        java.util.Enumeration enum = decs.elements();
+        while (enum.hasMoreElements()) {
+            Decision d = (Decision) enum.nextElement();
+            JLabel decLabel = new JLabel(d.getName());
+            JLabel valueLabel = new JLabel(getValueText(d.getPriority()));
+            JSlider decSlide = 
+                new JSlider(SwingConstants.HORIZONTAL,
+                            1, 4, (d.getPriority() == 0 ? 4 : d.getPriority()));
+
+            decSlide.setInverted(true);
+            decSlide.setMajorTickSpacing(1);
+            decSlide.setPaintTicks(true);
+            decSlide.setSnapToTicks(true);
+            // decSlide.setPaintLabels(true);
+            decSlide.addChangeListener(this);
+            Dimension origSize = decSlide.getPreferredSize();
+            Dimension smallSize = 
+                new Dimension(origSize.width/2, origSize.height);
+            decSlide.setSize(smallSize);
+            decSlide.setPreferredSize(smallSize);
+
+            _slidersToDecisions.put(decSlide, d);
+            _slidersToDigits.put(decSlide, valueLabel);
+
+            c.gridx = 0;
+            c.gridwidth = 1;
+            c.weightx = 0.0;
+            c.ipadx = 3;
+            gb.setConstraints(decLabel, c);
+            _mainPanel.add(decLabel);
+
+            c.gridx = 1;
+            c.gridwidth = 1;
+            c.weightx = 0.0;
+            c.ipadx = 0;
+            gb.setConstraints(valueLabel, c);
+            _mainPanel.add(valueLabel);
+
+            c.gridx = 2;
+            c.gridwidth = 8;
+            c.weightx = 1.0;
+            gb.setConstraints(decSlide, c);
+            _mainPanel.add(decSlide);
+
+            c.gridy++;
+        }
     }
-  }
+  
+    ////////////////////////////////////////////////////////////////
+    // event handlers
+  
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == _okButton) {
+            setVisible(false);
+            dispose();
+        }
+    }
 
 
-  public void stateChanged(ChangeEvent ce) {
-    JSlider srcSlider = (JSlider) ce.getSource();
-    Decision d = (Decision) _slidersToDecisions.get(srcSlider);
-    JLabel valLab = (JLabel) _slidersToDigits.get(srcSlider);
-    int pri = srcSlider.getValue();
-    d.setPriority(pri);
-    valLab.setText(getValueText(pri));
-  }
+    public void stateChanged(ChangeEvent ce) {
+        JSlider srcSlider = (JSlider) ce.getSource();
+        Decision d = (Decision) _slidersToDecisions.get(srcSlider);
+        JLabel valLab = (JLabel) _slidersToDigits.get(srcSlider);
+        int pri = srcSlider.getValue();
+        d.setPriority((pri == 4) ? 0 : pri);
+        valLab.setText(getValueText(pri));
+    }
 
     protected String getValueText(int priority) {
         String label = "";
@@ -215,7 +221,8 @@ implements ActionListener, ChangeListener {
         case 0: label = " off"; break;
         case 1: label = "   1"; break;
         case 2: label = "   2"; break;
-        case 3: label = "   3";
+        case 3: label = "   3"; break;
+        case 4: label = " off"; break;
         }
         return label;
     }
