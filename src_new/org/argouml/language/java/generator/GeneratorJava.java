5a1a631a85863070c6796ae548f8db2866b1f05b
Jaap Branderhorst <kataka@tigris.org>
2002-11-06 12:37:32 +0000

    fix for having NP's when generating a message/action
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@2920 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/language/java/generator/GeneratorJava.java
+++ src_new/org/argouml/language/java/generator/GeneratorJava.java
@@ -1492,14 +1492,14 @@ implements PluggableNotation, FileGenerator {
 
   public String generateAction(MAction m) {
       // return m.getName();
-      if ((m.getScript() != null) && (m.getScript().getBody() != null))
+      if (m != null && (m.getScript() != null) && (m.getScript().getBody() != null))
 	  return m.getScript().getBody();
       return "";
   }
 
   public String generateGuard(MGuard m) {
       //return generateExpression(m.getExpression());
-      if (m.getExpression() != null)
+      if (m != null && m.getExpression() != null)
 	  return generateExpression(m.getExpression());
       return "";
   }
