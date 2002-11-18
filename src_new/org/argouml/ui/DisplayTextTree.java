d3c04f6c165589561c9c24d225fcf9a62f9cdd84
Linus Tolke <linus@tigris.org>
2002-11-18 18:45:39 +0000

    Removed UpdateTreeHack (it is replaced with DisplayTextTreeRun.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@2996 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ui/DisplayTextTree.java
+++ src_new/org/argouml/ui/DisplayTextTree.java
@@ -60,7 +60,6 @@ implements MElementListener, VetoableChangeListener {
 
   Hashtable _expandedPathsInModel = new Hashtable();
   boolean _reexpanding = false;
-  UpdateTreeHack _myUpdateTreeHack = new UpdateTreeHack(this);
 
   public DisplayTextTree() {
     setCellRenderer(new UMLTreeCellRenderer());
@@ -211,11 +210,7 @@ implements MElementListener, VetoableChangeListener {
 
 	public void vetoableChange(PropertyChangeEvent e) {
 		cat.debug("DisplayTextTree vetoableChange: " + e.getPropertyName());
-		if (!_myUpdateTreeHack.pending) {
-			SwingUtilities.invokeLater(_myUpdateTreeHack);
-			_myUpdateTreeHack.pending = true;
-		}
-		else cat.debug("update already pending");
+		_doit.onceMore();
 	}
 
 
