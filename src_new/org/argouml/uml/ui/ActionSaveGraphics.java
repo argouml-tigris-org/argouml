eef227caf000b83cde0a111fdab285fb10346d30
Linus Tolke <linus@tigris.org>
2003-01-07 22:36:44 +0000

    Updated to compile with jdk1.2.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@3225 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/ActionSaveGraphics.java
+++ src_new/org/argouml/uml/ui/ActionSaveGraphics.java
@@ -94,13 +94,9 @@ public class ActionSaveGraphics extends UMLAction {
 		if( chooser == null ) chooser = OsUtil.getFileChooser();
 
 		chooser.setDialogTitle( "Save Diagram as Graphics: " + defaultName );
-		// 2002-07-17
-		// Jaap Branderhorst
-		// Patch for issue 590
-		// removed support for all files. People can only select some format and save accordingly to that format.
-		// new code:
-		chooser.setAcceptAllFileFilterUsed(false);
-		// end new code patch 590
+		// Only specified format are allowed.
+		chooser.removeChoosableFileFilter(chooser.
+						  getAcceptAllFileFilter());
 		chooser.addChoosableFileFilter( FileFilters.GIFFilter );
 		chooser.addChoosableFileFilter( FileFilters.PSFilter );
 		chooser.addChoosableFileFilter( FileFilters.EPSFilter );
