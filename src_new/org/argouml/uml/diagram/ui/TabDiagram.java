d45e008003322d6c446b3f28ded1105b564e7bbe
Jaap Branderhorst <kataka@tigris.org>
2003-01-31 20:32:25 +0000

    Fix for issue 896
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@3383 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/ui/TabDiagram.java
+++ src_new/org/argouml/uml/diagram/ui/TabDiagram.java
@@ -1,4 +1,4 @@
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// Copyright (c) 1996-2003 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -21,25 +21,37 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
+// $Id$
+
 package org.argouml.uml.diagram.ui;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import java.awt.BorderLayout;
+import java.util.Arrays;
+import java.util.Vector;
 
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.event.*;
-
-import org.tigris.gef.base.*;
-import org.tigris.gef.graph.presentation.*;
-import org.tigris.gef.event.*;
-import org.tigris.gef.ui.*;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
 
 import org.apache.log4j.Category;
-import org.argouml.ui.*;
-import org.argouml.uml.ui.*;
+import org.argouml.ui.ProjectBrowser;
+import org.argouml.ui.TabSpawnable;
+import org.argouml.uml.ui.TabModelTarget;
+import org.tigris.gef.base.Globals;
+import org.tigris.gef.base.ModeSelect;
+import org.tigris.gef.event.GraphSelectionEvent;
+import org.tigris.gef.event.GraphSelectionListener;
+import org.tigris.gef.event.ModeChangeEvent;
+import org.tigris.gef.event.ModeChangeListener;
+import org.tigris.gef.graph.presentation.JGraph;
+import org.tigris.gef.ui.ToolBar;
 
+
+/**
+ * The TabDiagram is the tab in the multieditorpane that holds a diagram. The 
+ * TabDiagram consists of a JGraph (with the figs) and of a toolbar. It is
+ * possible to spawn objects of this class into a dialog via the spawn method of
+ * its parent.
+ */
 public class TabDiagram extends TabSpawnable
 implements TabModelTarget, GraphSelectionListener, ModeChangeListener {
     
@@ -55,15 +67,9 @@ implements TabModelTarget, GraphSelectionListener, ModeChangeListener {
   protected UMLDiagram _target;
   
   /**
-   * The GEF JPanel that draws the target
+   * The GEF JGraph in where the figs are painted
    */
   protected JGraph _jgraph;
-  
-  /**
-   * not used
-   * TODO - delete
-   */
-  protected ButtonGroup _lineModeBG;
   
   /**
    * used but there doesn't appear to be a purpose.
@@ -75,11 +81,6 @@ implements TabModelTarget, GraphSelectionListener, ModeChangeListener {
    * the diagram. is added to diagramPanel
    */
   protected ToolBar _toolBar;
-  
-  /**
-   * the panel that holds the graph and the toolbar
-   */
-  //JPanel _diagramPanel;
 
   ////////////////////////////////////////////////////////////////
   // constructors
@@ -89,129 +90,143 @@ implements TabModelTarget, GraphSelectionListener, ModeChangeListener {
    */
   public TabDiagram() {
         this("Diagram");
-  }
+    }
 
-  /**
-   * 
-   */
-  public TabDiagram(String tag) {
-    super(tag);
-    setLayout(new BorderLayout());
-    _jgraph = new JGraph();
-    _jgraph.setDrawingSize((612-30) * 2, (792-55-20)*2);
-    // TODO: should update to size of diagram contents
+    public TabDiagram(String tag) {
+        super(tag);
+        setLayout(new BorderLayout());
+        _jgraph = new JGraph();
+        _jgraph.setDrawingSize((612 - 30) * 2, (792 - 55 - 20) * 2);
+        // TODO: should update to size of diagram contents
 
-    Globals.setStatusBar(ProjectBrowser.TheInstance);
-    //_toolBar = d.getToolBar();
-    //_jgraph.setToolBar(_toolBar); //I wish this had worked...
-    //add(_toolBar, BorderLayout.NORTH);
-    JPanel _diagramPanel = new JPanel(); 
-    _diagramPanel.setLayout(new BorderLayout());
-    _diagramPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
-    _diagramPanel.add(_jgraph, BorderLayout.CENTER);
-    add(_diagramPanel, BorderLayout.CENTER);
-    _jgraph.addGraphSelectionListener(this);
-    _jgraph.addModeChangeListener(this);
-  }
+        Globals.setStatusBar(ProjectBrowser.TheInstance);
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
+        p.add(_jgraph, BorderLayout.CENTER);
+        add(p, BorderLayout.CENTER);
+        _jgraph.addGraphSelectionListener(this);
+        _jgraph.addModeChangeListener(this);
+    }
 
+    /**
+     * The clone method that should clone the JGraph with it's contents and
+     * the toolbar with it's contents. Since both JGraph as the toolbar are
+     * coming from the GEF framework, cloning them will be hard work and should
+     * actually not be placed here but in a clone method of the JGraph and the
+     * Toolbar.
+     * @see java.lang.Object#clone()
+     */
     public Object clone() {
-	TabDiagram newPanel = new TabDiagram();
-	return newPanel;
+        // next statement gives us a clone JGraph but not a cloned Toolbar
+        TabDiagram newPanel = new TabDiagram();
+        // next statement moves the toolbar to the newPanel
+        if (_target != null) {
+            newPanel.setTarget(_target);
+        }
+        newPanel.setToolBar(_target.getToolBar());           
+        return newPanel;
+        
+    }
+
+    /**
+     * Sets the target of the tab. The target should allways be an instance of
+     * UMLDiagram
+     * @param t
+     */
+    public void setTarget(Object t) {
+        if (!(t instanceof UMLDiagram)) {
+            _shouldBeEnabled = false;
+            cat.warn("target is null in set target or not an instance of UMLDiagram");
+            return;
+        }
+        UMLDiagram target = (UMLDiagram)t;
+        _shouldBeEnabled = true;
+        setToolBar(target.getToolBar());
+        _jgraph.setDiagram(target);
+        _target = target;                
     }
     
+    public Object getTarget() {
+        return _target;
+    }
+    
+    public ToolBar getToolBar() {
+        return _toolBar;
+    }
+
+    public void refresh() {
+        setTarget(_target);
+    }
+
+    public boolean shouldBeEnabled() {
+        return _shouldBeEnabled;
+    }
 
   ////////////////////////////////////////////////////////////////
   // accessors
     
-  /**
-   * just calls setTarget(t, true);
-   */
-  public void setTarget(Object t) {
-  	setTarget(t, true);
-  }
+
   
-  /**
-   * Sets the target of the JGraph to be t (a UMLDiagram).
-   * <p>this method can also be called when adding a class to a 
-   * class diagram for example; therfore fail silently if t is not
-   * a UMLDiagram.
-   */
-  public void setTarget(Object t, boolean visible) {
-      
-    if (t == null) _shouldBeEnabled = false;
-    if (t instanceof UMLDiagram) {
-      _target = (UMLDiagram) t;
-      _shouldBeEnabled = true;
-    }
-    else {
-      _shouldBeEnabled = false;
-      return;
-    }
-    // TODO
-    // 2002-07-26
-    // Jaap Branderhorst
-    // Cool now we assume that's an UMLDiagram
-    UMLDiagram d = (UMLDiagram) _target;
-    _jgraph.setDiagram(d);
-    
-    // set the toolbar
-    if (_toolBar != null) {
-      setVisible(false);
-      remove(_toolBar);
-    }
-    _toolBar = d.getToolBar();
-    add(_toolBar, BorderLayout.NORTH);
-    
-    setTitle(d.getName());
-    
-    setVisible(visible);
-    invalidate();
-    validate();
-  }
+
   
-  public Object getTarget() { return _target; }
+ 
 
-  public void refresh() { setTarget(_target); }
 
-  public boolean shouldBeEnabled() { return _shouldBeEnabled; }
+    public JGraph getJGraph() {
+        return _jgraph;
+    }
 
-  public JGraph getJGraph() { return _jgraph; }
+    public void setVisible(boolean b) {
+        super.setVisible(b);
+        getJGraph().setVisible(b);
+    }
 
-  public void setVisible(boolean b) {
-    super.setVisible(b);
-    getJGraph().setVisible(b);
-  }
-  
-  ////////////////////////////////////////////////////////////////
-  // events
+    ////////////////////////////////////////////////////////////////
+    // events
 
-  public void selectionChanged(GraphSelectionEvent gse) {
-    Vector sels = gse.getSelections();
-    ProjectBrowser pb = ProjectBrowser.TheInstance;
+    public void selectionChanged(GraphSelectionEvent gse) {
+        Vector sels = gse.getSelections();
+        ProjectBrowser pb = ProjectBrowser.TheInstance;
 
-    if (sels.size() == 1) pb.setTarget(sels.elementAt(0));
-    else pb.setTarget(null);
-  }
+        if (sels.size() == 1)
+            pb.setTarget(sels.elementAt(0));
+        else
+            pb.setTarget(null);
+    }
 
-  public void removeGraphSelectionListener(GraphSelectionListener listener) {
-    _jgraph.removeGraphSelectionListener(listener);
-  }
+    public void removeGraphSelectionListener(GraphSelectionListener listener) {
+        _jgraph.removeGraphSelectionListener(listener);
+    }
 
-  public void modeChange(ModeChangeEvent mce) {
-    cat.debug("TabDiagram got mode change event");
-    if (!Globals.getSticky() && Globals.mode() instanceof ModeSelect)
-      _toolBar.unpressAllButtons();
-  }
+    public void modeChange(ModeChangeEvent mce) {
+        cat.debug("TabDiagram got mode change event");
+        if (!Globals.getSticky() && Globals.mode() instanceof ModeSelect) {
+            if (_target instanceof UMLDiagram)
+                _target.getToolBar().unpressAllButtons();
+        }
+    }
 
-  public void removeModeChangeListener(ModeChangeListener listener) {
-    _jgraph.removeModeChangeListener(listener);
-  }
+    public void removeModeChangeListener(ModeChangeListener listener) {
+        _jgraph.removeModeChangeListener(listener);
+    }
+    
+    /**
+     * Sets the toolbar. Adds the toolbar to the north borderlayout position of
+     * the diagram.
+     * @param toolbar
+     */
+    public void setToolBar(ToolBar toolbar) {
+        if (!Arrays.asList(getComponents()).contains(toolbar) ) {
+            /*
+            if (_target != null) {
+                remove(((TabDiagram)getTarget())._target.getToolBar());
+            }   
+            */       
+            add(toolbar, BorderLayout.NORTH);
+            invalidate();
+            validate();
+        }
+    }
 
 }
-
-
-
-
-
-
-
