f9b5f63160693f1f204e9d74813c14f7dcc44c04
Michael Stockman <d00mst@tigris.org>
2003-02-02 00:59:49 +0000

    Begun implementing an import function to facilitate importing elements from eg profile models.
    Used it in ParserDisplay, and fixed tests that were lax about namespaces.
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@3409 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/model/uml/modelmanagement/ModelManagementHelper.java
+++ org/argouml/model/uml/modelmanagement/ModelManagementHelper.java
@@ -30,13 +30,18 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.log4j.Category;
+
 import org.argouml.kernel.Project;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.ModelFacade;
+import org.argouml.model.uml.foundation.core.CoreHelper;
+import org.argouml.model.uml.foundation.core.CoreFactory;
 
 import ru.novosoft.uml.foundation.core.MModelElement;
 import ru.novosoft.uml.foundation.core.MNamespace;
 import ru.novosoft.uml.model_management.MModel;
+import ru.novosoft.uml.model_management.MPackage;
 import ru.novosoft.uml.model_management.MSubsystem;
 
 /**
@@ -49,6 +54,7 @@ import ru.novosoft.uml.model_management.MSubsystem;
  * @stereotype singleton
  */
 public class ModelManagementHelper {
+    protected static Category cat = Category.getInstance(ModelManagementHelper.class);
 
     /** Don't allow instantiation.
      */
@@ -238,6 +244,84 @@ public class ModelManagementHelper {
                 element.setNamespace(to);
             }
         }
+    }
+
+    private MNamespace findNamespace(MNamespace src, MModel targ) {
+	if (src instanceof MModel)
+	    return (MModel) targ;
+
+	if (src == null || !(src instanceof MNamespace)) {
+	    cat.warn("ModelManagementHelper::findPackage called with " +
+		     (src == null ? "(null)" : src.getClass().toString()));
+	    return null;
+	}
+
+	if (src.getModel() == targ)
+	    return src;
+
+	MNamespace ns = findNamespace(src.getNamespace(), targ);
+	if (ns == null)
+	    return null;
+
+	Iterator it = ns.getOwnedElements().iterator();
+	while (it.hasNext()) {
+	    MModelElement e = (MModelElement) it.next();
+	    if (e instanceof MNamespace && e.getClass() == src.getClass() &&
+		((src.getName() == null && e.getName() == null) ||
+		 (src.getName() != null && src.getName().equals(e.getName())))) {
+		return (MNamespace) e;
+	    }
+	}
+
+	if (!(src instanceof MPackage) ||
+	    src instanceof MModel ||
+	    src instanceof MSubsystem) {
+	    cat.warn("ModelManagementHelper::findPackage to copy " +
+		     src.getClass().toString());
+	    return null;
+	}
+
+	MPackage p = ModelManagementFactory.getFactory().createPackage();
+	p.setName(src.getName());
+	ns.addOwnedElement(p);
+	// TODO: copy constraints, imports, stereotype, tagged values from src
+	// Beware that it must be done after the addelement, so that elements
+	// from namespaces below src will be able to be created below p,
+	// otherwise there will be nasty stack overflows looming in the
+	// background.
+	return p;
+    }
+
+    /**
+     * Utility function for importing elements. The idea is that you make a
+     * copy of source, ie element. Then you call importElement to put your
+     * copy at the corresponding place in your model. If there is no
+     * corresponding place yet, it will be created.
+     *
+     * To simply transfer an object to a model it works perfectly fine to call
+     * importElement(element, element, model);
+     *
+     * Caution: Not all possible ownership paths can currently be copied, but
+     * it is guarenteed that element will always be owned by some namespace
+     * in 'to' if the function returns (this is not guarenteed if it throws).
+     *
+     * @param source is the source element.
+     * @param element is your new element.
+     * @param to is your model.
+     */
+    public void importElement(MModelElement source, MModelElement element, MModel to) {
+	if (source == null || element == null || to == null)
+	    throw new NullPointerException();
+
+	MNamespace ns = findNamespace(source.getNamespace(), to);
+	if (ns == null) {
+	    // This could be really really bad.
+	    cat.warn("ModelManagementHelper::findPackage failed. Parent: " +
+		source.getNamespace());
+	    to.addOwnedElement(element);
+	    return;
+	}
+	ns.addOwnedElement(element);
     }
 
     /**
