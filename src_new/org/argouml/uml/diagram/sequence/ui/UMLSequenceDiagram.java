008969e6ef456b5a7933fcf8d55cc2279d70a40e
Peter Nabbefeld <epdv@tigris.org>
2003-02-14 11:59:51 +0000

    Initial changes to migrate to specification level diagrams
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/work_sdnested_epdv@3509 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/sequence/ui/UMLSequenceDiagram.java
+++ src_new/org/argouml/uml/diagram/sequence/ui/UMLSequenceDiagram.java
@@ -38,13 +38,26 @@ import javax.swing.Action;
 import org.apache.log4j.Category;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.ui.CmdCreateNode;
-import org.argouml.uml.diagram.sequence.SequenceDiagramGraphModel;
+import org.argouml.uml.diagram.sequence.specification_level.ui.ActionAddMessage;
+import org.argouml.uml.diagram.sequence.specification_level.ui.ActionNestedCall;
+import org.argouml.uml.diagram.sequence.specification_level.ui.FigSeqMessage;
+import org.argouml.uml.diagram.sequence.specification_level.ui.SequenceDiagramLayout;
+import org.argouml.uml.diagram.sequence.specification_level.ui.SequenceDiagramRenderer;
+import org.argouml.uml.diagram.sequence.specification_level.MessageSupervisor;
+import org.argouml.uml.diagram.sequence.specification_level.OldLayoutConverter;
+import org.argouml.uml.diagram.sequence.specification_level.OldToSpecConverter;
+import org.argouml.uml.diagram.sequence.specification_level.SequenceDiagramGraphModel;
 import org.argouml.uml.diagram.ui.UMLDiagram;
 import org.argouml.uml.ui.ActionAddNote;
+
 import org.tigris.gef.base.Layer;
 import org.tigris.gef.base.LayerPerspective;
 import org.tigris.gef.presentation.Fig;
+import org.tigris.gef.presentation.FigPoly;
 import org.tigris.gef.ui.ToolBar;
+
+import ru.novosoft.uml.behavior.collaborations.MMessage;
+import ru.novosoft.uml.behavior.collaborations.MClassifierRole;
 import ru.novosoft.uml.behavior.common_behavior.MCallAction;
 import ru.novosoft.uml.behavior.common_behavior.MCreateAction;
 import ru.novosoft.uml.behavior.common_behavior.MDestroyAction;
@@ -57,22 +70,19 @@ import ru.novosoft.uml.foundation.core.MModelElement;
 import ru.novosoft.uml.foundation.core.MNamespace;
 
 public class UMLSequenceDiagram extends UMLDiagram {
+
     protected static Category cat = Category.getInstance(UMLSequenceDiagram.class);
 
     ////////////////
     // actions for toolbar
 
-    protected static Action _actionObject = new CmdCreateNode(MObject.class, "Object");
-
-    protected static Action _actionLinkWithStimulusCall = new ActionAddLink(MCallAction.class, "StimulusCall");
-
-    protected static Action _actionLinkWithStimulusCreate = new ActionAddLink(MCreateAction.class, "StimulusCreate");
-
-    protected static Action _actionLinkWithStimulusDestroy = new ActionAddLink(MDestroyAction.class, "StimulusDestroy");
-
-    protected static Action _actionLinkWithStimulusSend = new ActionAddLink(MSendAction.class, "StimulusSend");
-
-    protected static Action _actionLinkWithStimulusReturn = new ActionAddLink(MReturnAction.class, "StimulusReturn");
+    protected static Action _actionObject         = new CmdCreateNode   (MClassifierRole.class, "Object");
+    protected Action _actionMessageCall           = new ActionAddMessage(this, MCallAction.class,    "Call");
+    protected Action _actionMessageCreate         = new ActionAddMessage(this, MCreateAction.class,  "Create");
+    protected Action _actionMessageDestroy        = new ActionAddMessage(this, MDestroyAction.class, "Destroy");
+    protected Action _actionMessageSend           = new ActionAddMessage(this, MSendAction.class,    "Send");
+    protected Action _actionMessageReturn         = new ActionAddMessage(this, MReturnAction.class,  "Return");
+    protected static Action _actionNestedCall     = new ActionNestedCall(MCallAction.class);
 
     ////////////////////////////////////////////////////////////////
     // contructors
@@ -135,11 +145,12 @@ public class UMLSequenceDiagram extends UMLDiagram {
         _toolBar.add(_actionObject);
         _toolBar.addSeparator();
 
-        _toolBar.add(_actionLinkWithStimulusCall);
-        _toolBar.add(_actionLinkWithStimulusCreate);
-        _toolBar.add(_actionLinkWithStimulusDestroy);
-        _toolBar.add(_actionLinkWithStimulusSend);
-        _toolBar.add(_actionLinkWithStimulusReturn);
+        _toolBar.add(_actionMessageCall);
+        _toolBar.add(_actionMessageCreate);
+        _toolBar.add(_actionMessageDestroy);
+        _toolBar.add(_actionMessageSend);
+        _toolBar.add(_actionMessageReturn);
+        _toolBar.add(_actionNestedCall);
 
         // other actions
         _toolBar.addSeparator();
@@ -159,55 +170,42 @@ public class UMLSequenceDiagram extends UMLDiagram {
         _toolBar.add(_diagramName.getJComponent());
     }
 
-    /** every stimulus has to become a path item of its link
-      * to have a graphical connections between stimulus and link */
     public void postLoad() {
-
         super.postLoad();
+        if(completeOldStimulusData()) {
+            computeOldLayoutData();
+            MessageSupervisor.initialize((SequenceDiagramLayout)getLayer());
+            OldToSpecConverter.convertDiagramContents(this);
+        } else {
+            MessageSupervisor.initialize((SequenceDiagramLayout)getLayer());
+            MessageSupervisor.createContainerFromModel((SequenceDiagramLayout)getLayer());
+        }
+        ((SequenceDiagramLayout)getLayer()).placeAllFigures();
+        Collection c = ((SequenceDiagramLayout)getLayer()).getContents();
+        Iterator i = c.iterator();
+        Fig f = null;
+        for(; i.hasNext(); f.damage()) {
+            f = (Fig)i.next();
+        }
+    }
 
-        Collection stimuli;
-        Iterator stimuliIterator;
-        Iterator oeIterator = null;
-        Collection ownedElements = null;
-        if (getNamespace() != null)
-            ownedElements = getNamespace().getOwnedElements();
-        if (ownedElements != null)
-            oeIterator = ownedElements.iterator();
-        Layer lay = getLayer();
-        if (oeIterator != null && lay != null) {
-            Vector contents = new Vector();
-            boolean objFound = false;
-            FigSeqObject figSeqObj = null;
-            FigSeqLink figLink = null;
-            FigSeqStimulus figStim = null;
-
-            Vector createdObjs = new Vector();
-            Vector createLinks = new Vector();
-            FigSeqObject dest = null;
-
-            while (oeIterator.hasNext()) {
-                MModelElement me = (MModelElement) oeIterator.next();
-
-                if (me instanceof MLink) {
-                    stimuli = ((MLink) me).getStimuli();
-                    stimuliIterator = stimuli.iterator();
-                    while (stimuliIterator.hasNext()) {
-                        MStimulus stimulus = (MStimulus) stimuliIterator.next();
-                        FigSeqStimulus figStimulus = (FigSeqStimulus) lay.presentationFor(stimulus);
-                        if (figStimulus != null) {
-                            figStimulus.addPathItemToLink(lay);
-                        }
-                    }
+    public void preSave() {
+        for(Iterator itr = ((SequenceDiagramLayout)getLayer()).getContents().iterator(); itr.hasNext();) {
+            Object obj = itr.next();
+            if(obj instanceof FigSeqMessage) {
+                Iterator i = ((FigSeqMessage)obj).getPredecessors().iterator();
+                Vector vmm = new Vector(2, 2);
+                for(; i.hasNext(); vmm.add((MMessage)((FigSeqMessage)i.next()).getOwner())) {
                 }
+                ((MMessage)((FigSeqMessage)obj).getOwner()).setPredecessors(vmm);
             }
         }
-
     }
 
     /**
-         * Creates a new diagramname.
-          * @return String
-          */
+     * Creates a new diagramname.
+     * @return String
+     */
     protected static String getNewDiagramName() {
         String name = null;
         name = "Sequence Diagram " + _SequenceDiagramSerial;
@@ -218,4 +216,69 @@ public class UMLSequenceDiagram extends UMLDiagram {
         return name;
     }
 
+    /** every stimulus has to become a path item of its link
+      * to have a graphical connections between stimulus and link */
+    private boolean completeOldStimulusData() {
+        boolean r = false;
+        Iterator oeIterator = null;
+        Collection ownedElements = null;
+        if(getNamespace() != null) {
+            ownedElements = getNamespace().getOwnedElements();
+        }
+        if(ownedElements != null) {
+            oeIterator = ownedElements.iterator();
+        }
+        Layer lay = getLayer();
+        if(oeIterator != null && lay != null) {
+            while(oeIterator.hasNext()) {
+                MModelElement me = (MModelElement)oeIterator.next();
+                if(!r && ((me instanceof MLink) || (me instanceof MObject) || (me instanceof MStimulus))) {
+                    r = true;
+                }
+                if(me instanceof MLink) {
+                    Collection stimuli = ((MLink)me).getStimuli();
+                    for(Iterator stimuliIterator = stimuli.iterator(); stimuliIterator.hasNext();) {
+                        MStimulus stimulus = (MStimulus)stimuliIterator.next();
+                        FigSeqStimulus figStimulus = (FigSeqStimulus)lay.presentationFor(stimulus);
+                        if(figStimulus != null) {
+                            figStimulus.addPathItemToLink(lay);
+                        }
+                    }
+
+                }
+            }
+        }
+        return r;
+    }
+
+    private void computeOldLayoutData() {
+        OldLayoutConverter.computeOldLayoutData(getLayer());
+    }
+
+    /**
+     * This method is for debugging only; should be removed in later version
+     */
+    private void debugLinkData() {
+        Iterator i = getLayer().getContents().iterator();
+        Object o = null;
+        while(i.hasNext()) {
+            o = i.next();
+            if(o instanceof FigSeqLink) {
+                FigSeqLink lnk = (FigSeqLink)o;
+                int y = lnk.getFirstPoint().y;
+                System.out.println("y = " + y);
+                System.out.println("FirstPoint = " + lnk.getFirstPoint());
+                System.out.println("LastPoint  = " + lnk.getLastPoint());
+                System.out.println("Points     = " + lnk.getPoints());
+                System.out.println("Type       = " + lnk.getClass().getName());
+                System.out.println("PathItemFigs.size = " + lnk.getPathItemFigs().size());
+                for(int pi = 0; pi < lnk.getPathItemFigs().size(); pi++) {
+                    System.out.println("PathItemFigs(" + pi + ")   = " + lnk.getPathItemFigs().get(pi));
+                }
+                System.out.println("FigType    = " + lnk.getFig().getClass().getName());
+                System.out.println("Polygon    = " + ((FigPoly)lnk.getFig()).getPolygon().npoints);
+                System.out.println("Points[0]  = " + ((FigPoly)lnk.getFig()).getPolygon().ypoints[0]);
+            }
+        }
+    }
 } /* end class UMLSequenceDiagram */
