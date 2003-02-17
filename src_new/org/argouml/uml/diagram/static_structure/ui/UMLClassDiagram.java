37ec39de113249a1e6cc87bb0c65c7756bd3d819
Bob Tarling <bobtarling@tigris.org>
2003-02-17 00:27:22 +0000

    Comment out code for drop down toolbox. Hopefully this will resolve the issue of argo crashing on exit.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@3538 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/static_structure/ui/UMLClassDiagram.java
+++ src_new/org/argouml/uml/diagram/static_structure/ui/UMLClassDiagram.java
@@ -155,16 +155,17 @@ public class UMLClassDiagram extends UMLDiagram {
 
         _toolBar.add(_actionPackage);
         _toolBar.add(_actionClass);
-
+        
+        //_toolBar.add(_actionAssociation);
         // TODO
         // Bobs nasty hack. The PopupToolBoxButton class can only be trusted for
-        // now on metal. Use old style popup for oth plafs.
-        if (LookAndFeelMgr.SINGLETON.determineLookAndFeel().equals(LookAndFeelMgr.METAL_PLAF)) {
+        // now on metal. Use old style popup for other plafs.
+        //if (LookAndFeelMgr.SINGLETON.determineLookAndFeel().equals(LookAndFeelMgr.METAL_PLAF)) {
             _toolBar.add(buildAssociationPopup());
-        } else {
-            JButton associationButton = _toolBar.add(_actionAssociation);
-            _toolBar.add(buildUglyAssociationPopup(associationButton));
-        }
+        //} else {
+        //    JButton associationButton = _toolBar.add(_actionAssociation);
+        //    _toolBar.add(buildUglyAssociationPopup(associationButton));
+        //}
         
         _toolBar.add(_actionDepend);
         _toolBar.add(_actionPermission);
