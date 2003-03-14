37539f72ba2c2f9b43a4044dc09a9bb9e93e01ac
Jean-Hugues de Raigniac <jhraigniac@tigris.org>
2003-03-14 15:33:39 +0000

    Native russian character codes converted to unicode by Alexey Aphanasyev.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@3649 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/i18n/MenuResourceBundle_ru.java
+++ org/argouml/i18n/MenuResourceBundle_ru.java
@@ -1,4 +1,4 @@
-// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Copyright (c) 1996-2003 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -55,7 +55,7 @@ public class MenuResourceBundle_ru extends ListResourceBundle {
         {"Copy", "\u041a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c" },
         {"Paste", "\u0412\u0441\u0442\u0430\u0432\u0438\u0442\u044c" },
 	{"Settings...", "\u0423\u0441\u0442\u0430\u043d\u043e\u0432\u043a\u0438..."},
-        {"Set Source Path...", "Set Source Path..." },
+        {"Set Source Path...", "\u0423\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c \u043f\u0443\u0442\u044c \u043a \u0438\u0441\u0445\u043e\u0434\u043d\u0438\u043a\u0430\u043c..." },
         {"Delete From Diagram", "\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0438\u0437 \u0434\u0438\u0430\u0433\u0440\u0430\u043c\u043c\u044b" },
         {"Erase From Model", "\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0438\u0437 \u043c\u043e\u0434\u0435\u043b\u0438" },
         {"Empty Trash", "\u0423\u043d\u0438\u0447\u0442\u043e\u0436\u0438\u0442\u044c \u043c\u0443\u0441\u043e\u0440" },
@@ -83,7 +83,7 @@ public class MenuResourceBundle_ru extends ListResourceBundle {
         {"Generate Selected Classes", "\u0421\u0433\u0435\u043d\u0435\u0440\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u044b\u0431\u0440\u0430\u043d\u043d\u044b\u0435 \u043a\u043b\u0430\u0441\u0441\u044b..." },
         {"Generate All Classes", "\u0421\u0433\u0435\u043d\u0435\u0440\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u0441\u0435 \u043a\u043b\u0430\u0441\u0441\u044b..." },
         {"Generate Code for Project", "\u0421\u0433\u0435\u043d\u0435\u0440\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u043a\u043e\u0434 \u0434\u043b\u044f \u043f\u0440\u043e\u0435\u043a\u0442\u0430..." },
-        {"Settings for Generate for Project", "Settings for Generate for Project..." },
+        {"Settings for Generate for Project", "\u0423\u0441\u0442\u0430\u043d\u043e\u0432\u043a\u0438 \u0434\u043b\u044f \u0433\u0435\u043d\u0435\u0440\u0430\u0446\u0438\u0438 \u043f\u0440\u043e\u0435\u043a\u0442\u0430..." },
         {"Toggle Auto-Critique", "\u0412\u043a\u043b\u044e\u0447\u0438\u0442\u044c \u0430\u0432\u0442\u043e\u043a\u0440\u0438\u0442\u0438\u043a\u0443" },
         {"Design Issues...", "\u0421\u043f\u043e\u0440\u043d\u044b\u0435 \u043c\u043e\u043c\u0435\u043d\u0442\u044b \u043f\u0440\u043e\u0435\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u044f..." },
         {"Design Goals...", "\u0426\u0435\u043b\u0438 \u043f\u0440\u043e\u0435\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u044f..." },
@@ -127,7 +127,6 @@ public class MenuResourceBundle_ru extends ListResourceBundle {
         {"Mnemonic_RemoveFromDiagram", "R" },
         {"Mnemonic_DeleteFromModel", "D" },
         {"View", "\u041f\u0440\u043e\u0441\u043c\u043e\u0442\u0440" },
-        {"Zoom", "\u041c\u0430\u0441\u0448\u0442\u0430\u0431" },
         {"Mnemonic_View", "V" },
         {"Editor Tabs", "\u0417\u0430\u043a\u043b\u0430\u0434\u043a\u0438 \u043f\u0430\u043d\u0435\u043b\u0438 \u0440\u0435\u0434\u0430\u043a\u0442\u043e\u0440\u0430" },
         {"Details Tabs", "\u0417\u0430\u043a\u043b\u0430\u0434\u043a\u0438 \u043f\u0430\u043d\u0435\u043b\u0438 \u0434\u0435\u0442\u0430\u043b\u0435\u0439" },
@@ -141,14 +140,12 @@ public class MenuResourceBundle_ru extends ListResourceBundle {
         {"Distribute", "\u0420\u0430\u0441\u043f\u0440\u0435\u0434\u0435\u043b\u0438\u0442\u044c" },
         {"Reorder", "\u0420\u0435\u043e\u0440\u0433\u0430\u043d\u0438\u0437\u043e\u0432\u0430\u0442\u044c" },
         {"Nudge", "\u041f\u043e\u0434\u0442\u043e\u043b\u043a\u043d\u0443\u0442\u044c" },
-        {"Layout", "\u041a\u043e\u043c\u043f\u043e\u043d\u043e\u0432\u043a\u0430" },
         {"Generation", "\u0413\u0435\u043d\u0435\u0440\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u0435" },
         {"Mnemonic_Generate", "G" },
         {"Critique", "\u041a\u0440\u0438\u0442\u0438\u043a\u0430" },
         {"Mnemonic_Critique", "R" },
         {"Help", "\u041f\u043e\u043c\u043e\u0449\u044c" },
         {"Mnemonic_Help", "H" },
-        {"Tools", "\u0418\u043d\u0441\u0442\u0440\u0443\u043c\u0435\u043d\u0442\u044b" },
         {"Automatic", "\u0410\u0432\u0442\u043e\u043c\u0430\u0442\u0438\u0447\u0435\u0441\u043a\u0438" },
         {"Incremental", "\u0418\u043d\u043a\u0440\u0435\u043c\u0435\u043d\u0442\u043d\u043e" },
         {"As Diagram", "\u041a\u0430\u043a \u0434\u0438\u0430\u0433\u0440\u0430\u043c\u043c\u0430" },
@@ -161,7 +158,13 @@ public class MenuResourceBundle_ru extends ListResourceBundle {
         {"TaggedValues", "\u0418\u043c\u0435\u043d\u043e\u0432\u0430\u043d\u043d\u044b\u0435 \u0437\u043d\u0430\u0447\u0435\u043d\u0438\u044f" },
         {"Checklist", "\u041a\u043e\u043d\u0442\u0440\u043e\u043b\u044c\u043d\u044b\u0439 \u0441\u043f\u0438\u0441\u043e\u043a" },
         {"History", "\u0418\u0441\u0442\u043e\u0440\u0438\u044f" },
-
+        {"Layout", "\u041a\u043e\u043c\u043f\u043e\u043d\u043e\u0432\u043a\u0430" },
+        {"Zoom", "\u041c\u0430\u0441\u0448\u0442\u0430\u0431"},
+        {"Tools", "\u0418\u043d\u0441\u0442\u0440\u0443\u043c\u0435\u043d\u0442\u044b" },
+    // arrange menu
+        {"Set minimum size", "\u041f\u043e\u0434\u043e\u0433\u043d\u0430\u0442\u044c \u0440\u0430\u0437\u043c\u0435\u0440 \u043f\u043e \u0441\u043e\u0434\u0435\u0440\u0436\u0438\u043c\u043e\u043c\u0443"},
+        {"Toggle Auto Resizing", "\u0410\u0432\u0442\u043e\u043c\u0430\u0442\u0438\u0447\u0435\u0441\u043a\u0438 \u043f\u043e\u0434\u043e\u0433\u043d\u0430\u0442\u044c \u0440\u0430\u0437\u043c\u0435\u0440 \u043f\u043e \u0441\u043e\u0434\u0435\u0440\u0436\u0438\u043c\u043e\u043c\u0443"},
+    // shortcut keys
         { "Shortcut_New", KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_MASK) },
         { "Shortcut_Open", KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_MASK) },
         { "Shortcut_Save", KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_MASK) },
