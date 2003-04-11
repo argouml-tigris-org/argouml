acf3e0899394b1597e0b3593a8b4450bbc217b03
Alex Bagehot <alexb@tigris.org>
2003-04-11 14:23:06 +0000

    improved documentation & layout of these tree view classes. the functionality was not changed.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@3773 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/DisplayTextTree.java
+++ org/argouml/ui/DisplayTextTree.java
@@ -22,12 +22,8 @@
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 // File: DisplayTextTree.java
-// Classes: DisplayTextTree
-// Original Author:
-// $Id$
-
-// 26 Apr 2002: Jeremy Bennett (mail@jeremybennett.com). Patch to give a better
-// naming for extension points in convertValueToText.
+// Classes: DisplayTextTree, DipslayTextTreeRun
+// Original Author: ?
 
 package org.argouml.ui;
 
@@ -41,6 +37,7 @@ import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
 import org.apache.log4j.Category;
+
 import org.argouml.application.api.Configuration;
 import org.argouml.application.api.Notation;
 import org.argouml.cognitive.ToDoItem;
@@ -48,6 +45,7 @@ import org.argouml.cognitive.ToDoList;
 import org.argouml.model.uml.UmlModelEventPump;
 import org.argouml.uml.generator.GeneratorDisplay;
 import org.argouml.uml.ui.UMLTreeCellRenderer;
+
 import org.tigris.gef.base.Diagram;
 import org.tigris.gef.presentation.Fig;
 
@@ -61,37 +59,78 @@ import ru.novosoft.uml.foundation.core.MModelElement;
 import ru.novosoft.uml.foundation.extension_mechanisms.MStereotype;
 import ru.novosoft.uml.foundation.extension_mechanisms.MTaggedValue;
 
-public class DisplayTextTree extends JTree implements MElementListener {
-
-    protected static Category cat = Category.getInstance(DisplayTextTree.class);
-
-    Hashtable _expandedPathsInModel = new Hashtable();
-    boolean _reexpanding = false;
+/**
+ * This is the JTree that is the gui component view of the model navigation and
+ * todo list.
+ *
+ * <pre>
+ * // 26 Apr 2002: Jeremy Bennett (mail@jeremybennett.com). Patch to give a better
+ * // naming for extension points in convertValueToText.
+ * </pre>
+ *
+ * $Id$
+ */
+public class DisplayTextTree 
+    extends JTree
+    implements MElementListener{
     
+    protected static Category cat = Category.getInstance(DisplayTextTree.class);
+    
+    /** needs documenting */
+    public static final int DEPTH_LIMIT = 10;
+    /** needs documenting */
+    public static final int CHANGE = 1;
+    /** needs documenting */
+    public static final int ADD = 2;
+    /** needs documenting */
+    public static final int REMOVE = 3;
+    
+    /** needs documenting */
+    Hashtable _expandedPathsInModel;
+    
+    /** needs documenting */
+    boolean _reexpanding;
+    
+    /** needs documenting */
     private boolean showStereotype;
-
+    
+    /** needs documenting */
+    private DisplayTextTreeRun _doit;
+    
+    /** needs documenting */
     public DisplayTextTree() {
+        
         setCellRenderer(new UMLTreeCellRenderer());
         putClientProperty("JTree.lineStyle", "Angled");
-        //setEditable(true);
         showStereotype = Configuration
-                .getBoolean(Notation.KEY_SHOW_STEREOTYPES, false);
+                            .getBoolean(Notation.KEY_SHOW_STEREOTYPES, false);
+        _expandedPathsInModel = new Hashtable();
+        _reexpanding = false;
+        _doit = new DisplayTextTreeRun(cat, this);
     }
-
+    
+    // ------------ methods that override JTree methods ---------
+    
+    /**
+     * override default JTree implementation to display the
+     * appropriate text for any object that will be displayed in
+     * the Nav pane or todo list.
+     */
     public String convertValueToText(
-        Object value,
-        boolean selected,
-        boolean expanded,
-        boolean leaf,
-        int row,
-        boolean hasFocus) {
+                    Object value,
+                    boolean selected,
+                    boolean expanded,
+                    boolean leaf,
+                    int row,
+                    boolean hasFocus) {
         
         // do model elements first
         if (value instanceof MModelElement){
             
             String name=null;
             
-            if (value instanceof MTransition || value instanceof MExtensionPoint) { // Jeremy Bennett patch
+             // Jeremy Bennett patch
+            if(value instanceof MTransition || value instanceof MExtensionPoint){
                 name = GeneratorDisplay.Generate(value);
             }
             else{
@@ -100,14 +139,14 @@ public class DisplayTextTree extends JTree implements MElementListener {
             
             if (name == null || name.equals("")){
                 
-                name = "(anon " + ((MModelElement)value).getUMLClassName() + ")";
+                name = "(anon " +((MModelElement)value).getUMLClassName() + ")";
             }
             
             // Look for stereotype
             if (showStereotype) {
                 MStereotype st = ((MModelElement) value).getStereotype();
-                    if (st != null) {
-                        name += " " + GeneratorDisplay.Generate(st);
+                if (st != null) {
+                    name += " " + GeneratorDisplay.Generate(st);
                 }
             }
             
@@ -126,28 +165,19 @@ public class DisplayTextTree extends JTree implements MElementListener {
                 tagName = "(anon)";
             return ("1-" + tagName);
         }
-
+        
         if (value instanceof Diagram) {
             return ((Diagram) value).getName();
         }
         
         return value.toString();
     }
-
-    protected Vector getExpandedPaths() {
-        TreeModel tm = getModel();
-        Vector res = (Vector) _expandedPathsInModel.get(tm);
-        if (res == null) {
-            res = new Vector();
-            _expandedPathsInModel.put(tm, res);
-        }
-        return res;
-    }
-
+    
+    /** needs documenting */
     public void fireTreeWillExpand(TreePath path) {
         
         showStereotype = Configuration
-                .getBoolean(Notation.KEY_SHOW_STEREOTYPES, false);
+        .getBoolean(Notation.KEY_SHOW_STEREOTYPES, false);
     }
     
     /**
@@ -156,6 +186,7 @@ public class DisplayTextTree extends JTree implements MElementListener {
      * @param e  a Tree node insertion event
      */
     public void fireTreeExpanded(TreePath path) {
+        
         super.fireTreeExpanded(path);
         if (_reexpanding)
             return;
@@ -166,18 +197,70 @@ public class DisplayTextTree extends JTree implements MElementListener {
         expanded.addElement(path);
         addListenerToPath(path);
     }
-
+    
+    /** needs documenting */
+    public void fireTreeCollapsed(TreePath path) {
+        
+        super.fireTreeCollapsed(path);
+        if (path == null || _expandedPathsInModel == null)
+            return;
+        Vector expanded = getExpandedPaths();
+        expanded.removeElement(path);
+    }
+    
+    /** needs documenting */
+    public void setModel(TreeModel newModel) {
+        
+        Object r = newModel.getRoot();
+        if (r != null)
+            super.setModel(newModel);
+        
+        if (r instanceof MBase) {
+            // UmlModelEventPump.getPump().removeModelEventListener(this, (MBase)r);
+            UmlModelEventPump.getPump().addModelEventListener(this, (MBase) r);
+        }
+        
+        int childCount = newModel.getChildCount(r);
+        for (int i = 0; i < childCount; i++) {
+            Object child = newModel.getChild(r, i);
+            if (child instanceof MBase) {
+                // UmlModelEventPump.getPump().removeModelEventListener(this, (MBase)child);
+                UmlModelEventPump.getPump().addModelEventListener(
+                this,
+                (MBase) child);
+            }
+        }
+        reexpand();
+    }
+    
+    // ------------- other methods ------------------
+    
+    /** needs documenting */
+    protected Vector getExpandedPaths() {
+        TreeModel tm = getModel();
+        Vector res = (Vector) _expandedPathsInModel.get(tm);
+        if (res == null) {
+            res = new Vector();
+            _expandedPathsInModel.put(tm, res);
+        }
+        return res;
+    }
+    
+    /** needs documenting */
     protected void addListenerToPath(TreePath path) {
+        
         Object node = path.getLastPathComponent();
         addListenerToNode(node);
     }
-
+    
+    /** needs documenting */
     protected void addListenerToNode(Object node) {
+        
         if (node instanceof MBase) {
             // UmlModelEventPump.getPump().removeModelEventListener(this, ((MBase)node));
             UmlModelEventPump.getPump().addModelEventListener(
-                this,
-                ((MBase) node));
+                                            this,
+                                            ((MBase) node));
         }
         TreeModel tm = getModel();
         int childCount = tm.getChildCount(node);
@@ -185,59 +268,19 @@ public class DisplayTextTree extends JTree implements MElementListener {
             Object child = tm.getChild(node, i);
             if (child instanceof MBase) {
                 UmlModelEventPump.getPump().removeModelEventListener(
-                    this,
-                    ((MBase) child));
+                                                this,
+                                                ((MBase) child));
                 UmlModelEventPump.getPump().addModelEventListener(
-                    this,
-                    ((MBase) child));
-
+                                                this,
+                                                ((MBase) child));
             }
         }
     }
-
-    public void fireTreeCollapsed(TreePath path) {
-        super.fireTreeCollapsed(path);
-        if (path == null || _expandedPathsInModel == null)
-            return;
-        Vector expanded = getExpandedPaths();
-        expanded.removeElement(path);
-    }
-
-    public void setModel(TreeModel newModel) {
-        Object r = newModel.getRoot();
-        if (r != null)
-            super.setModel(newModel);
-
-        if (r instanceof MBase) {
-            // UmlModelEventPump.getPump().removeModelEventListener(this, (MBase)r);
-            UmlModelEventPump.getPump().addModelEventListener(this, (MBase) r);
-        }
-
-        int childCount = newModel.getChildCount(r);
-        for (int i = 0; i < childCount; i++) {
-            Object child = newModel.getChild(r, i);
-            if (child instanceof MBase) {
-                // UmlModelEventPump.getPump().removeModelEventListener(this, (MBase)child);
-                UmlModelEventPump.getPump().addModelEventListener(
-                    this,
-                    (MBase) child);
-            }
-        }
-        reexpand();
-    }
-
-    public static final int DEPTH_LIMIT = 10;
-    public static final int CHANGE = 1;
-    public static final int ADD = 2;
-    public static final int REMOVE = 3;
-    //public static Object path[] = new Object[DEPTH_LIMIT];
-
-    private DisplayTextTreeRun _doit = new DisplayTextTreeRun(cat, this);
-
+    
     /** Signals to the tree that something has changed and it is best
      * to update the tree.
      * <P>
-     * For complex operations such as import(?) and add attribute(8), that 
+     * For complex operations such as import(?) and add attribute(8), that
      * does several calls to this it is better if we defer the actual update
      * until later and if it is not performed don't do an extra.
      * Since import is done from invokeLater() we try to move this down
@@ -248,7 +291,7 @@ public class DisplayTextTree extends JTree implements MElementListener {
     public void forceUpdate() {
         _doit.onceMore();
     }
-
+    
     /**
      * This is the real update function. It won't return until the tree
      * really is updated.
@@ -258,6 +301,7 @@ public class DisplayTextTree extends JTree implements MElementListener {
      * @since 0.13.1
      */
     public void doForceUpdate() {
+        
         Object rootArray[] = new Object[1];
         rootArray[0] = getModel().getRoot();
         Object noChildren[] = null;
@@ -271,20 +315,10 @@ public class DisplayTextTree extends JTree implements MElementListener {
         }
         reexpand();
     }
-
-    //   public void forceUpdate_old() {
-    //     int n = 0;
-    //     ProjectBrowser pb = ProjectBrowser.TheInstance;
-    //     Vector pers = pb.getNavPane().getPerspectives();
-    //     NavPerspective curPerspective = pb.getNavPane().getCurPerspective();
-    //     if (curPerspective == null) return;
-    //     n = (pers.indexOf(curPerspective) + 1) % pers.size();
-    //     NavPerspective otherPerspective = (NavPerspective) pers.elementAt(n);
-    //     pb.getNavPane().setCurPerspective(otherPerspective);
-    //     pb.getNavPane().setCurPerspective(curPerspective);
-    //   }
-
+    
+    /** needs documenting */
     public void reexpand() {
+        
         if (_expandedPathsInModel == null)
             return;
         _reexpanding = true;
@@ -293,7 +327,7 @@ public class DisplayTextTree extends JTree implements MElementListener {
         TreeModelEvent tme = new TreeModelEvent(this, path2, null, null);
         treeModelListener.treeStructureChanged(tme);
         treeDidChange();
-
+        
         java.util.Enumeration enum = getExpandedPaths().elements();
         while (enum.hasMoreElements()) {
             TreePath path = (TreePath) enum.nextElement();
@@ -303,60 +337,115 @@ public class DisplayTextTree extends JTree implements MElementListener {
             // addListenerToPath(path);
         }
         _reexpanding = false;
-
     }
-
-    public void propertySet(MElementEvent mee) {
-    }
-    public void listRoleItemSet(MElementEvent mee) {
-    }
-    public void recovered(MElementEvent mee) {
-    }
-    public void removed(MElementEvent mee) {
-    }
-    public void roleAdded(MElementEvent mee) {
-    }
-    public void roleRemoved(MElementEvent mee) {
-    }
-
-    /** 
+    
+    /**
      * This methods sets the target of the treemodel to the given object. It's
      * a means to set the target programmatically from within the setTarget
      * method in the ProjectBrowser.
      * @param target
      */
     public void setTarget(Object target) {
+        
         if (getModel() instanceof NavPerspective) {
             if (target instanceof Fig) {
                 target = ((Fig)target).getOwner();
             }
             int index =
-                getModel().getIndexOfChild(getModel().getRoot(), target);
+            getModel().getIndexOfChild(getModel().getRoot(), target);
             if (index > -1)
                 setSelectionRow(index);
         }
     }
-
+    
+    // ------------ MElementListener impl. --------------
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void listRoleItemSet(MElementEvent e) {
+    }
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void propertySet(MElementEvent e) {
+    }
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void recovered(MElementEvent e) {
+    }
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void removed(MElementEvent e) {
+    }
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void roleAdded(MElementEvent e) {
+    }
+    
+    /** this is only needed so that this class can be added
+     * as a listener to the model event pump.
+     *
+     * empty implementation.
+     */
+    public void roleRemoved(MElementEvent e) {
+    }
+    
 } /* end class DisplayTextTree */
 
+
 /**
- * This class is used to defer the actual update until "late"
+ * Because there <strong>may</strong> be many calls from Argo to update the tree view in
+ * a very short space of time (eg. during some automatic processing)
+ * , this class will discard all update calls
+ * except one in order to improve the <strong>performance</strong> of Argo.
+ *
+ * <p>This class is used to defer the actual update until "late"
  * in the invokeLater()-call chain.
- * <P>
- * The real update will hopefully take place at the end of whatever long
+ *
+ * <P>The real update will hopefully take place at the end of whatever long
  * chain of forceUpdate:s that will be performed.
  */
 class DisplayTextTreeRun implements Runnable {
-    protected Category cat;
-    private DisplayTextTree _tree;
-    int _timesToRun = 0;
-    boolean _queued = false;
 
+    /** needs documenting */
+    protected Category cat;
+    
+    /** needs documenting */
+    private DisplayTextTree _tree;
+    
+    /** needs documenting */
+    int _timesToRun;
+    
+    /** needs documenting */
+    boolean _queued;
+    
+    /** needs documenting */
     public DisplayTextTreeRun(Category c, DisplayTextTree t) {
         cat = c;
         _tree = t;
+        _timesToRun = 0;
+        _queued = false;
     }
-
+    
+    /** needs documenting */
     public synchronized void onceMore() {
         if (!_queued) {
             _queued = true;
@@ -364,11 +453,12 @@ class DisplayTextTreeRun implements Runnable {
         }
         _timesToRun++;
     }
-
+    
+    /** needs documenting */
     public synchronized void run() {
         if (_timesToRun > 100)
             cat.debug("" + _timesToRun + " forceUpdates encountered.");
-
+        
         if (_timesToRun > 0) {
             // another forceUpdate was seen, wait again
             _queued = true;
