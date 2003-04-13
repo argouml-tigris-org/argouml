43b575eafe6f0653b9ffe7b676eee8c7efe28d4e
Alex Bagehot <alexb@tigris.org>
2003-04-13 10:06:06 +0000

    commenting
    Issue number: 1739
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@3793 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/AbstractGoRule.java
+++ org/argouml/ui/AbstractGoRule.java
@@ -12,113 +12,118 @@ import org.apache.log4j.Category;
 /**
  * Abstract class to be used as a convenience class for implementing 'go rules'.
  * Go rules are rules to which the navperspectives comply if they are registred
- * with the navperspectives. This usually happens in the static block of 
- * NavPerspective. 
- * If some modelelement is not showing up in a navperspective, in most cases this 
+ * with the navperspectives. This usually happens in the static block of
+ * NavPerspective.
+ * If some modelelement is not showing up in a navperspective, in most cases this
  * means that there is no go rule defined or an incorrect one. Via the abstract method
  * getChildren the children of some tree element in a perspective are returned.
  * Only the returned children are shown for some tree element.
- * 
- * 
+ *
+ *
  * @author jaap.branderhorst@xs4all.nl
  */
 public abstract class AbstractGoRule implements TreeModel {
-	
-	private static Category cat = Category.getInstance(org.argouml.ui.AbstractGoRule.class);
-
-	/**
-	 * @see javax.swing.tree.TreeModel#getRoot()
-	 */
-	public Object getRoot() {
-		return null;
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#getChild(Object, int)
-	 */
-	public Object getChild(Object parent, int index) {
-		Vector children = toVector(getChildren(parent));
-    	if (children != null) {
-    		return children.elementAt(index); 
-    	} else {
-    		cat.fatal("getChild should never get here");
-    		return null;
-    	}
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#getChildCount(Object)
-	 */
-	public int getChildCount(Object parent) {
-	    Collection c = getChildren(parent);
-
-	    if (c == null)
-		return 0;
-
-	    return c.size();
-	}
-	
-	private Vector toVector(Collection col) {
-		Vector ret = new Vector();
-		if (col != null && !col.isEmpty()) {
-			ret.addAll(col);
-		}
-		return ret;
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#isLeaf(Object)
-	 */
-	public abstract boolean isLeaf(Object node); 
-
-	/**
-	 * @see javax.swing.tree.TreeModel#valueForPathChanged(TreePath, Object)
-	 */
-	public void valueForPathChanged(TreePath path, Object newValue) {
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#getIndexOfChild(Object, Object)
-	 */
-	public int getIndexOfChild(Object parent, Object child) {
+    
+    private static Category cat = Category.getInstance(org.argouml.ui.AbstractGoRule.class);
+    
+    // ----------- TreeModel implementation -----------
+    
+    /**
+     * returns null.
+     *
+     * @see javax.swing.tree.TreeModel#getRoot()
+     */
+    public Object getRoot() {
+        return null;
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#getChild(Object, int)
+     */
+    public Object getChild(Object parent, int index) {
+        Vector children = toVector(getChildren(parent));
+        if (children != null) {
+            return children.elementAt(index);
+        } else {
+            cat.fatal("getChild should never get here");
+            return null;
+        }
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#getChildCount(Object)
+     */
+    public int getChildCount(Object parent) {
+        Collection c = getChildren(parent);
+        
+        if (c == null)
+            return 0;
+        
+        return c.size();
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#isLeaf(Object)
+     */
+    public abstract boolean isLeaf(Object node);
+    
+    /**
+     * @see javax.swing.tree.TreeModel#valueForPathChanged(TreePath, Object)
+     */
+    public void valueForPathChanged(TreePath path, Object newValue) {
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#getIndexOfChild(Object, Object)
+     */
+    public int getIndexOfChild(Object parent, Object child) {
         // the next line does not take into account multiple levels of the Tree
         // to change that we must change ALL implementations of getChildren(Object)
         // to return ALL elements of the tree instead of only the first level.
-        // since this is going to be quite complicated and a violation of the 
+        // since this is going to be quite complicated and a violation of the
         // design with GoRules (GoRules do only take into account one node) we have
         // to invent a way around it.
-		Vector children = toVector(getChildren(parent));
-    	if (children != null && children.contains(child))
-      		return children.indexOf(child);
-    	return -1;
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#addTreeModelListener(TreeModelListener)
-	 */
-	public void addTreeModelListener(TreeModelListener l) {
-	}
-
-	/**
-	 * @see javax.swing.tree.TreeModel#removeTreeModelListener(TreeModelListener)
-	 */
-	public void removeTreeModelListener(TreeModelListener l) {
-	}
-	
-	public abstract Collection getChildren(Object parent);
-
-    /** return the name of the rule as it is displayed in 
-     *  the nav perspective edit pane. Returns for example 
+        Vector children = toVector(getChildren(parent));
+        if (children != null && children.contains(child))
+            return children.indexOf(child);
+        return -1;
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#addTreeModelListener(TreeModelListener)
+     */
+    public void addTreeModelListener(TreeModelListener l) {
+    }
+    
+    /**
+     * @see javax.swing.tree.TreeModel#removeTreeModelListener(TreeModelListener)
+     */
+    public void removeTreeModelListener(TreeModelListener l) {
+    }
+    
+    // -------------- other helper methods --------------------
+    
+    public abstract Collection getChildren(Object parent);
+    
+    /** return the name of the rule as it is displayed in
+     *  the nav perspective edit pane. Returns for example
      *  "State->Substates".
      *  @see #toString()
-     */   
+     */
     public abstract String getRuleName();
     
     /** wrapper around getRuleName()
      */
     public String toString() { return getRuleName(); }
     
-
+    
+    private Vector toVector(Collection col) {
+        Vector ret = new Vector();
+        if (col != null && !col.isEmpty()) {
+            ret.addAll(col);
+        }
+        return ret;
+    }
 }
 
 
