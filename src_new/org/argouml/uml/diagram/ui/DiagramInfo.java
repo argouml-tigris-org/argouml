78b0d5db3a23264dc20f2c56ec14b2e3cfabca78
Linus Tolke <linus@tigris.org>
2003-06-21 08:27:21 +0000

    Buggfix issue 1993.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@4247 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/ui/DiagramInfo.java
+++ src_new/org/argouml/uml/diagram/ui/DiagramInfo.java
@@ -1,4 +1,5 @@
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// $Id$
+// Copyright (c) 1996-2003 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -24,7 +25,6 @@
 // File: DiagramInfo.java
 // Classes: DiagramInfo
 // Original Author: jrobbins@ics.uci.edy
-// $Id$
 
 package org.argouml.uml.diagram.ui;
 
@@ -70,6 +70,7 @@ public class DiagramInfo extends JComponent {
 	setLayout(new BorderLayout());
 	_name.setFont(MetalLookAndFeel.getSubTextFont());
 	add(_name, BorderLayout.CENTER);
+	updateName();
 	return this;
     }
 
