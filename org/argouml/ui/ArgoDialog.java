22a9529b4bde4b52b2757b353e44a4b3504b9287
Jeremy Jones <jjones@tigris.org>
2003-06-23 21:27:22 +0000

    Changed to use new Dialog constructors. Also added support for mnemonics.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@4282 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/ArgoDialog.java
+++ org/argouml/ui/ArgoDialog.java
@@ -7,30 +7,51 @@
 package org.argouml.ui;
 
 import java.awt.Frame;
+
+import javax.swing.JButton;
+
 import org.argouml.application.api.Argo;
 
 /**
- * A dialog with localized buttons
+ * A dialog with localized buttons.
  *
  * @author Bob Tarling
  */
 public class ArgoDialog extends org.argouml.swingext.Dialog {
     
     protected static final String BUNDLE = "Cognitive";
+    protected static final String MNEMONIC_KEY_SUFFIX = ".mnemonic";
     
     /**
-     * Create a new ArgoDialog
+     * Creates a new ArgoDialog with the default optionType.
      */
     public ArgoDialog(Frame owner, String title, boolean modal) {
         super(owner, title, modal);
     }
+
+    /**
+     * Creates a new ArgoDialog with the specified optionType.
+     */
+    public ArgoDialog(Frame owner, String title, int optionType, boolean modal) {
+        super(owner, title, optionType, modal);
+    }
+        
+    protected void nameButtons() {  
+        nameButton(getOkButton(), "button.ok");
+        nameButton(getCancelButton(), "button.cancel");
+        nameButton(getCloseButton(), "button.close");
+        nameButton(getYesButton(), "button.yes");
+        nameButton(getNoButton(), "button.no");
+        nameButton(getHelpButton(), "button.help");
+    }
     
-    protected void nameButtons() {
-        getOkButton().setText(Argo.localize(BUNDLE, "button.ok"));
-        getCancelButton().setText(Argo.localize(BUNDLE, "button.cancel"));
-        getCloseButton().setText(Argo.localize(BUNDLE, "button.close"));
-        getYesButton().setText(Argo.localize(BUNDLE, "button.yes"));
-        getNoButton().setText(Argo.localize(BUNDLE, "button.no"));
-        getHelpButton().setText(Argo.localize(BUNDLE, "button.help"));
+    protected void nameButton(JButton button, String key) {
+        if (button != null) {
+            button.setText(Argo.localize(BUNDLE, key));
+            String mnemonic = Argo.localize(BUNDLE, key + MNEMONIC_KEY_SUFFIX);
+            if (mnemonic != null && mnemonic.length() > 0) {
+                button.setMnemonic(mnemonic.charAt(0));
+            }
+        }
     }
 }
