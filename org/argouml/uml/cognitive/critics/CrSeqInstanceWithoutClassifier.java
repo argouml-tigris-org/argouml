29de3cc592657953d1b46d6cdde2a135b8c206f8
Linus Tolke <linus@tigris.org>
2003-06-30 15:20:14 +0000

    Coding of line endings.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@4383 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/cognitive/critics/CrSeqInstanceWithoutClassifier.java
+++ org/argouml/uml/cognitive/critics/CrSeqInstanceWithoutClassifier.java
@@ -37,8 +37,6 @@ import ru.novosoft.uml.foundation.core.*;
 import ru.novosoft.uml.behavior.common_behavior.*;
 
 import org.argouml.cognitive.*;
-import org.argouml.uml.diagram.sequence.ui.FigSeqLink;
-import org.argouml.uml.diagram.sequence.ui.FigSeqObject;
 import org.argouml.uml.diagram.sequence.ui.UMLSequenceDiagram;
 import org.argouml.uml.diagram.ui.FigNodeModelElement;
 
