5b11f9fa57091a2b1bb6cead26081dba2c0ad5b8
Jaap Branderhorst <kataka@tigris.org>
2003-08-07 20:26:29 +0000

    Removed the namespace thread
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/work_issue2128_2_kataka@4549 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
+++ src_new/org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
@@ -24,30 +24,16 @@
 
 package org.argouml.uml.ui.foundation.core;
 
+import org.argouml.model.ModelFacade;
 import org.argouml.model.uml.UmlModelEventPump;
 import org.argouml.model.uml.foundation.core.CoreHelper;
 import org.argouml.uml.ui.UMLComboBoxModel2;
 
 import ru.novosoft.uml.foundation.core.MModelElement;
 import ru.novosoft.uml.foundation.core.MNamespace;
-import ru.novosoft.uml.MBase;
-
-import java.util.Collection;
-import java.util.ArrayList;
-import javax.swing.SwingUtilities;
 
 /**
- * A model for a namespace combo box, a singleton.
- *
- * <p>the namespace combobox needs to be a singleton because it has a worker
- * thread that needs to be
- * updated by any propanel when a target is changed.
- * the namespace combobox has this optimisation because it is a performance
- * bottleneck with models of size > 100 classifiers.
- *
- * <p>Maybe in the future the bottleneck will disappear, then this class
- * can be simplefied back to its original implementation.
- *
+ * A model for a namespace combo box,
  * $Id$
  *
  * @since Oct 10, 2002
@@ -57,54 +43,28 @@ public class UMLModelElementNamespaceComboBoxModel extends UMLComboBoxModel2 {
     
     private static UMLModelElementNamespaceComboBoxModel theInstance;
     
-    private NamespaceListBuilderThread theBuilderThread;
     
-    /**
-     * singleton getter method
-     */
-    public static UMLModelElementNamespaceComboBoxModel getInstance() {
-        
-        if (theInstance == null) {
-            
-            theInstance = new UMLModelElementNamespaceComboBoxModel();
-            return theInstance;
-        }
-        else
-            return theInstance;
-    }
     
     /**
      * Constructor for UMLModelElementNamespaceComboBoxModel.
      */
-    private UMLModelElementNamespaceComboBoxModel() {
+    public UMLModelElementNamespaceComboBoxModel() {
         super("namespace", false);
-        UmlModelEventPump.getPump().addClassModelEventListener(this, MNamespace.class, "ownedElement");
-        
-        theBuilderThread =
-	    new NamespaceListBuilderThread("namespace combobox worker");
-        theBuilderThread.start();
+        UmlModelEventPump.getPump().addClassModelEventListener(this, MNamespace.class, "ownedElement");               
     }
     
     /**
      * @see org.argouml.uml.ui.UMLComboBoxModel2#isValidElement(ru.novosoft.uml.MBase)
      */
     protected boolean isValidElement(Object o) {
-        return o instanceof MNamespace && CoreHelper.getHelper().isValidNamespace((MModelElement) getTarget(), (MNamespace) o);
+        return ModelFacade.isANamespace(o) && CoreHelper.getHelper().isValidNamespace(getTarget(), o);
     }
     
-    /**
-     * this doesn't actually build the list, it only display "please wait..."
-     * in the dropdown list to tell the user the list is being built.
-     *
-     * <p> the list is now built in the worker thread.
-     *
+    /**  
      * @see org.argouml.uml.ui.UMLComboBoxModel2#buildModelList()
      */
     protected void buildModelList() {
-        
-        Collection building = new ArrayList();
-        building.add(new String("please wait ..."));
-        setElements(building);
+        setElements(CoreHelper.getHelper().getAllPossibleNamespaces((MModelElement)getTarget()));
     }
     
     /**
@@ -116,112 +76,5 @@ public class UMLModelElementNamespaceComboBoxModel extends UMLComboBoxModel2 {
         }
         return null;
     }
-    
-    /**
-     * overridden setTarget() to maintain consistent model state whilst using
-     * the worker thread.
-     *
-     * if the target has been changed we don't want to continue calculating the
-     * namespaces for the old target!
-     *
-     */
-    protected void setTarget(Object target) {
-        
-        if (theBuilderThread != null && theBuilderThread.isAlive()) {
-            try {
-                theBuilderThread.interrupt();
-            } catch (SecurityException seIgnore) { }
-        }
-        
-        if (_target instanceof MBase) {
-            UmlModelEventPump.getPump().removeModelEventListener(this,
-								 (MBase) _target,
-								 _propertySetName);
-        }
-        
-        if (target instanceof MBase) {
-            _target = target;
-            UmlModelEventPump.getPump().addModelEventListener(this,
-							      (MBase) _target,
-							      _propertySetName);
-        } else {
-            target = null;
-        }
-        _fireListEvents = false;
-        removeAllElements();
-        _fireListEvents = true;
-        if (target != null) {
-            buildModelList();
-            // calculate the new namespaces.
-            theBuilderThread.build();
-        }
-        
        
-    }
-    
-    /**
-     * worker thread to free the swing gui whilst namespaces are calculated.
-     * we only ever want one of this thread
-     */
-    class NamespaceListBuilderThread extends Thread {
-        
-        public NamespaceListBuilderThread(String name) {
-            super(name);
-            // let argo exit even if this is still running.
-            setDaemon(true);
-        }
-        
-        public void run() {
-            
-            synchronized (this) {
-                while (true) {
-                    
-                    try {
-                        // wait for the next change of target
-                        wait();
-                        // build the namespace list.
-                        buildList();
-                    }
-                    catch (InterruptedException ie) { /** no action needed */ }
-                }
-            }
-        }
-        
-        /**
-         * notifies this thread that it can build a new list.
-         */
-        public synchronized void build() {
-            
-            notify();
-        }
-        
-        /**
-         * does the time consuming work on this thread,
-         * then updates the gui on the event dispatching thread.
-         *
-         * <p> does not update the gui if the target was changed again..
-         */
-        private void buildList() {
-            
-            Object originalTarget = getTarget();
-            
-            final Collection namespaces =
-		CoreHelper.getHelper().
-		    getAllPossibleNamespaces((MModelElement) originalTarget);
-            
-            // don't update the gui if the target has changed!
-            if (originalTarget != getTarget()) {
-                return;
-            }
-            
-            SwingUtilities.invokeLater(new Runnable()
-				       {
-					   public void run() {
-                    
-					       setElements(namespaces);
-					       setSelectedItem(getSelectedModelElement());
-					   }
-				       } );
-        }
-    }
 }
