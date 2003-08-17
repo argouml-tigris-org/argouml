3439d5931d30dd9c2fbae7575fb23fb37d45b1bb
Bob Tarling <bobtarling@tigris.org>
2003-08-17 22:09:14 +0000

    Prepare for GEF 0.10 Don't directly access any GEF instance variables
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@4600 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/sequence/ui/SequenceDiagramLayout.java
+++ org/argouml/uml/diagram/sequence/ui/SequenceDiagramLayout.java
@@ -103,8 +103,10 @@ public class SequenceDiagramLayout extends LayerPerspective {
 
     public Vector getFigSeqObjects() {
 	Vector figSeqObjects = new Vector();
-	for (int i = 0; i < _contents.size(); i++) {
-	    if (_contents.elementAt(i) instanceof FigSeqObject) figSeqObjects.add( _contents.elementAt(i) );
+        Iterator it = getContents().iterator();
+	while (it.hasNext()) {
+            Object o = it.next();
+	    if (o instanceof FigSeqObject) figSeqObjects.add(o);
 	}
 	return figSeqObjects;
     }
@@ -113,9 +115,12 @@ public class SequenceDiagramLayout extends LayerPerspective {
     /** return all instances of FigSeqLink in the active layer */
     public Vector getFigSeqLinks() {
 	Vector figSeqLinks = new Vector();
-	for (int i = 0; i < _contents.size(); i++) {
-	    if (_contents.elementAt(i) instanceof FigSeqLink) 
-		figSeqLinks.add( _contents.elementAt(i) );
+        Iterator it = getContents().iterator();
+	while (it.hasNext()) {
+            Object o = it.next();
+	    if (o instanceof FigSeqLink) { 
+		figSeqLinks.add(o);
+            }
 	}
 	return figSeqLinks;
     }
@@ -123,8 +128,10 @@ public class SequenceDiagramLayout extends LayerPerspective {
     public Vector getFigSeqStimuli() {
 
 	Vector figSeqStimuli = new Vector();
-	for (int i = 0; i < _contents.size(); i++) {
-	    if (_contents.elementAt(i) instanceof FigSeqStimulus) figSeqStimuli.add( _contents.elementAt(i) );
+        Iterator it = getContents().iterator();
+	while (it.hasNext()) {
+            Object o = it.next();
+	    if (o instanceof FigSeqStimulus) figSeqStimuli.add(o);
 	}
 	return figSeqStimuli;
 
@@ -215,7 +222,7 @@ public class SequenceDiagramLayout extends LayerPerspective {
 
 	for (i = 0; i < linkSize; i++) {
 	    FigSeqLink link = (FigSeqLink) figSeqLinks.elementAt(i);
-	    int portNumber = link.getPortNumber(_contents);
+	    int portNumber = link.getPortNumber(getContents());
 	    FigSeqObject sourcePort = (FigSeqObject) link.getSourceFigNode();
 	    FigSeqObject destPort = (FigSeqObject) link.getDestFigNode();
 
