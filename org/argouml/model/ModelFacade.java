daa8e26cc1b1e7789a495268a8441a90b4f39294
Bob Tarling <bobtarling@tigris.org>
2003-09-04 00:03:29 +0000

    Get all NSUML references out of ClassDiagramGraphModel
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@4747 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/model/ModelFacade.java
+++ org/argouml/model/ModelFacade.java
@@ -1911,7 +1911,22 @@ public class ModelFacade {
     }
 
     /**
-     * Returns the incoming transitions for some statevertex
+     * Returns the instance of an AttributeLink or LinkEnd
+     * @param handle
+     * @return initial value
+     */
+    public static Object getInstance(Object handle) {
+        if (handle instanceof MAttributeLink) {
+            return ((MAttributeLink)handle).getInstance();
+        }
+        if (handle instanceof MLinkEnd) {
+            return ((MLinkEnd)handle).getInstance();
+        }
+        throw new IllegalArgumentException("Unrecognized object " + handle);
+    }
+
+    /**
+     * Returns the Instances for some Clasifier
      * @param handle
      * @return Collection
      */
@@ -1919,8 +1934,7 @@ public class ModelFacade {
         if (handle instanceof MClassifier) {
             return ((MClassifier) handle).getInstances();
         }
-        throw new IllegalArgumentException(
-					   "Unrecognized object " + handle);
+        throw new IllegalArgumentException("Unrecognized object " + handle);
     }
 
     /**
