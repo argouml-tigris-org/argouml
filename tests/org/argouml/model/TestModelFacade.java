07b8a8ca83b1318631781ebebfa36745d718bd9f
Thierry Lach <thierrylach@tigris.org>
2003-09-08 14:09:16 +0000

    Test for model facade. Currently only partially tests identifiers.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@4803 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ tests/org/argouml/model/TestModelFacade.java
@@ -0,0 +1,69 @@
+// Copyright (c) 2003 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.model;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.argouml.model.uml.Uml;
+
+/**
+ * @author Thierry Lach
+ */
+public class TestModelFacade extends GenericFacadeTestFixture
+{
+
+	/**
+	 * Constructor for TestModelFacade.
+	 * @param arg0
+	 */
+	public TestModelFacade(String arg0)
+	{
+		super(arg0);
+	}
+
+	/*
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	/*
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testAll() {
+		Map elements = Uml.getUmlClassList();
+		Iterator i = elements.keySet().iterator();
+		while (i.hasNext()) {
+			runGenericTests((String) i.next());
+		}
+	}
+
+}
