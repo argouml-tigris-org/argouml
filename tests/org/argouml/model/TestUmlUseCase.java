82b37f0a7abf8e38b5beff18e5f3755f285e92e0
Thierry Lach <thierrylach@tigris.org>
2003-09-08 14:10:13 +0000

    Basic test cases for a few example model elements.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@4805 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ tests/org/argouml/model/TestUmlUseCase.java
@@ -0,0 +1,60 @@
+// Copyright (c) 2003 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.model;
+
+import org.argouml.model.uml.Uml;
+
+/**
+ * @author Thierry Lach
+ */
+public class TestUmlUseCase extends GenericObjectTestFixture
+{
+	/**
+	 * Constructor for FakeUmlModelFacadeTest.
+	 * @param arg0
+	 */
+	public TestUmlUseCase(String arg0)
+	{
+		super(arg0);
+		validateTestClassIsGeneric(this);
+	}
+
+    public void testNamespace() {
+    	Object o = getFactory().create(Uml.USE_CASE);
+    	assertNotNull("Didn't create object", o);
+		assertTrue("Should be a base", ModelFacade.isABase(o));
+		assertTrue("Should be a use case", ModelFacade.isAUseCase(o));
+		runTruthTests(o);
+    }
+
+    protected void initializeTruth() {
+		setTruth(Uml.ELEMENT, true);
+		setTruth(Uml.MODEL_ELEMENT, true);
+		setTruth(Uml.GENERALIZABLE_ELEMENT, true);
+		setTruth(Uml.NAMESPACE, true);
+		setTruth(Uml.CLASSIFIER, true);
+		setTruth(Uml.USE_CASE, true);
+    }
+
+}
