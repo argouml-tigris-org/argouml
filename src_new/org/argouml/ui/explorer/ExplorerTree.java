fb3f879f434fafaab767be2a9517bd9c115a3e75
Alex Bagehot <alexb@tigris.org>
2003-11-09 12:25:15 +0000

    issue 2350
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/work_issue2350_alexb@5223 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ui/explorer/ExplorerTree.java
+++ src_new/org/argouml/ui/explorer/ExplorerTree.java
@@ -319,7 +319,6 @@ extends DisplayTextTree
                     break;
                 }
             }
-            
         }
         if (rowIndexCounter < targets.length) {
             int[] rowIndexestmp = rowIndexes;
@@ -350,12 +349,28 @@ extends DisplayTextTree
                 List targets = new ArrayList();
                 if (paths != null) {
                     for (int i = 0; i < paths.length; i++) {
-                        if (e.isAddedPath(paths[i]))
-                            targets.add(
-                                ((DefaultMutableTreeNode)paths[i]
-                                    .getLastPathComponent())
-                                        .getUserObject()
-                                );
+                        if (e.isAddedPath(paths[i])){
+                            
+                            Object element = ((DefaultMutableTreeNode)paths[i]
+                                                .getLastPathComponent())
+                                                    .getUserObject();
+                            
+                            // add a new target for notifying the other views
+                            targets.add(element);
+                            
+                            // scan the visible rows for duplicates of this elem
+                            int rows = getRowCount();
+                            for (int row = 0; row < rows; row++) {
+                                Object rowItem =
+                                ((DefaultMutableTreeNode)getPathForRow(row)
+                                .getLastPathComponent())
+                                .getUserObject();
+                                if (rowItem == element && 
+                                    !(isRowSelected(row)) ) {
+                                    addSelectionRow(row);
+                                }
+                            }
+                        }
                     }
                 }
                 TargetManager.getInstance().setTargets(targets);
