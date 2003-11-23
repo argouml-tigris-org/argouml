c511b1849009ac10964a4ed0dc75be33a66c73e6
Linus Tolke <linus@tigris.org>
2003-11-23 18:35:20 +0000

    Updated style. Changed some of the Failures into Errors by removing a catch.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5298 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/model/TestModelFacade3.java
+++ tests/org/argouml/model/TestModelFacade3.java
@@ -22,7 +22,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-
 package org.argouml.model; 
 
 import junit.framework.*;
@@ -32,82 +31,87 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.InvocationTargetException;
 
 
-/** This TestCase dynamically generates a suite of tests to throw arguments at
- * each method in the ModelFacade to check whether they throw an IllegalArgumentException.
+/**
+ * This TestCase dynamically generates a suite of tests to throw
+ * arguments at each method in the ModelFacade to check whether they
+ * throw an IllegalArgumentException.
  */
 public class TestModelFacade3 extends TestCase {
     
-        private Method methodToTest = null;
-        private ModelFacade _facade = null;
-        
-        public TestModelFacade3(Method method) {
-                super(method.toString());
-                methodToTest = method;
-        }
+    private Method _methodToTest = null;
+    private ModelFacade _facade = null;
+
+    /**
+     * Creates one of the test cases.
+     *
+     * @param method to be tested.
+     */
+    public TestModelFacade3(Method method) {
+	super(method.toString());
+	_methodToTest = method;
+    }
  
-        public static Test suite() {
+    public static Test suite() {
 	TestSuite suite = 
 	    new TestSuite("Tests for " 
 			  + TestModelFacade.class.getPackage().getName());
 
         Method methods[] = ModelFacade.class.getDeclaredMethods();
-	for (int i = 0; i<methods.length; i++) {
+	for (int i = 0; i < methods.length; i++) {
             suite.addTest(new TestModelFacade3(methods[i]));
 	}
 
 	return suite;
     }
 
-        /** This method checks whether a test should be executed for a given
-         * method.
-         * @return true if method is not eligible for testing (e.g. helper methods)
-         */
-        protected boolean methodForbidden() {
-            return methodToTest.getName().equals("getClassNull") ||
-                   methodToTest.getName().startsWith("isA");
-        }
+    /** This method checks whether a test should be executed for a given
+     * method.
+     *
+     * @return true if method is not eligible for testing (e.g. helper methods)
+     */
+    protected boolean methodForbidden() {
+	return _methodToTest.getName().equals("getClassNull") ||
+	    _methodToTest.getName().startsWith("isA");
+    }
         
-        protected void runTest() throws Throwable {
-            if (!methodForbidden()) {
-                if (methodToTest.getParameterTypes()!=null &&
-                    methodToTest.getParameterTypes().length>=1 &&
-                    Modifier.isPublic(methodToTest.getModifiers()) ) {
-                        testOneOrMoreParameters();
-                }
-            }
-        }
+    protected void runTest() throws Throwable {
+	if (!methodForbidden()) {
+	    if (_methodToTest.getParameterTypes() != null &&
+		_methodToTest.getParameterTypes().length >= 1 &&
+		Modifier.isPublic(_methodToTest.getModifiers()) ) {
+		testOneOrMoreParameters();
+	    }
+	}
+    }
         
-        /** testOneOrMoreParameters checks whether a public method in the
-         * ModelFacade throws an IllegalArgumentException when invoked with
-         * stupid arguments. Stupid arguments are arguments which are not
-         * from the UML domain, such as a plain Object.
-         */
-        public void testOneOrMoreParameters() {
-            Object[] foo = new Object[methodToTest.getParameterTypes().length];
+    /** 
+     * testOneOrMoreParameters checks whether a public method in the
+     * ModelFacade throws an IllegalArgumentException when invoked
+     * with stupid arguments. Stupid arguments are arguments which are
+     * not from the UML domain, such as a plain Object.
+     *
+     * @throws IllegalAccessException if there is an error in the test setup
+     */
+    public void testOneOrMoreParameters() throws IllegalAccessException {
+	Object[] foo = new Object[_methodToTest.getParameterTypes().length];
            
-            try {
-                methodToTest.invoke(_facade,  foo);
-                fail(methodToTest.getName() + " does not deliver an IllegalArgumentException");
-            }
+	try {
+	    _methodToTest.invoke(_facade, foo);
+	    fail(_methodToTest.getName()
+		 + " does not deliver an IllegalArgumentException");
+	}
+	catch (InvocationTargetException e) {
+	    if (e.getTargetException() instanceof IllegalArgumentException) {
+		return;
+	    }
+	    fail("Test failed for " + _methodToTest.getName()
+		 + " because of: " + e.getTargetException());
+	}
+    }
         
-            catch(InvocationTargetException e) {
-                if (e.getTargetException() instanceof IllegalArgumentException) {
-                    assertTrue(true);
-                    return;
-                }
-                fail("Test failed for " + methodToTest.getName() + " because of: " + 
-                    e.getTargetException());
-            }
-            catch(Exception e) {
-                fail("Test failed for " + methodToTest.getName() + " because of: " + 
-                    e.toString());                
-            }
-                
-        }
-        
-        protected void setUp() throws Exception {
-            super.setUp();
-            _facade = new ModelFacade();
-            assertNotNull("Cound not get ModelFacade", _facade);
-        }
+    protected void setUp() throws Exception {
+	super.setUp();
+	_facade = new ModelFacade();
+	assertNotNull("Cound not get ModelFacade", _facade);
+    }
 }
