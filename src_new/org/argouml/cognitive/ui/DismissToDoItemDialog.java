6eb579bc51a66153e16ccdc641bbfbbe88e3196f
Jeremy Jones <jjones@tigris.org>
2003-11-28 15:04:16 +0000

    Rewritten to extend ArgoDialog and improve layout. Also reformatted the whole file since it was in bad shape.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5335 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/cognitive/ui/DismissToDoItemDialog.java
+++ src_new/org/argouml/cognitive/ui/DismissToDoItemDialog.java
@@ -1,41 +1,39 @@
 // $Id$
-// Copyright (c) 1996-2003 The Regents of the University of California. All
-// Rights Reserved. Permission to use, copy, modify, and distribute this
-// software and its documentation without fee, and without a written
-// agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
-// documentation are copyrighted by The Regents of the University of
-// California. The software program and documentation are supplied "AS
-// IS", without any accompanying services from The Regents. The Regents
-// does not warrant that the operation of the program will be
-// uninterrupted or error-free. The end-user understands that the program
-// was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
-// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
-// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
-// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+//Copyright (c) 1996-99 The Regents of the University of California. All
+//Rights Reserved. Permission to use, copy, modify, and distribute this
+//software and its documentation without fee, and without a written
+//agreement is hereby granted, provided that the above copyright notice
+//and this paragraph appear in all copies.  This software program and
+//documentation are copyrighted by The Regents of the University of
+//California. The software program and documentation are supplied "AS
+//IS", without any accompanying services from The Regents. The Regents
+//does not warrant that the operation of the program will be
+//uninterrupted or error-free. The end-user understands that the program
+//was developed for research purposes and is advised not to rely
+//exclusively on the program for any reason.  IN NO EVENT SHALL THE
+//UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+//SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+//ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+//THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+//WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+//PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+//CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+//UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 package org.argouml.cognitive.ui;
 
-import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -47,179 +45,199 @@ import org.argouml.cognitive.ToDoList;
 import org.argouml.cognitive.UnresolvableException;
 import org.argouml.i18n.Translator;
 import org.argouml.kernel.ProjectManager;
+import org.argouml.ui.ArgoDialog;
 import org.argouml.ui.ProjectBrowser;
 
-public class DismissToDoItemDialog extends JDialog {
-    protected static Logger cat =
-	Logger.getLogger(DismissToDoItemDialog.class);
+public class DismissToDoItemDialog extends ArgoDialog {
+
+    private static final Logger cat =
+        Logger.getLogger(DismissToDoItemDialog.class);
 
     ////////////////////////////////////////////////////////////////
     // constants
-    
+
     // Resource-Bundle's
     private static final String DLGBUNDLE = "dialog";
     private static final String OPTBUNDLE = "optionpane";
-    
-    // TODO: This relies on the fact that the l10n code is initiated
-    // correctly when this class is loaded.
-    private static final String BAD_GOAL_LABEL =
-    Translator.localize("button.not-relevant-to-my-goals");
-    private static final String BAD_DECISION_LABEL = 
-    Translator.localize("button.not-of-concern-at-moment");
-    private static final String EXPLAIN_LABEL = 
-    Translator.localize("button.reason-given-below");
 
     ////////////////////////////////////////////////////////////////
     // instance variables
-    protected JRadioButton _badGoalButton = new JRadioButton(BAD_GOAL_LABEL);
-    protected JRadioButton _badDecButton = new JRadioButton(BAD_DECISION_LABEL);
-    protected JRadioButton _explainButton = new JRadioButton(EXPLAIN_LABEL);
-    protected ButtonGroup _actionGroup = new ButtonGroup();
-    protected JButton _okButton = new JButton(Translator.localize("button.ok"));
-    protected JButton _cancelButton = new JButton(Translator.localize("button.cancel"));
-    protected JTextArea _explaination = new JTextArea();
-    protected ToDoItem _target;
+
+    private JRadioButton    _badGoalButton;
+    private JRadioButton    _badDecButton;
+    private JRadioButton    _explainButton;
+    private ButtonGroup     _actionGroup;
+    private JTextArea       _explanation;
+    private ToDoItem        _target;
 
     ////////////////////////////////////////////////////////////////
     // constructors
-  
+
     public DismissToDoItemDialog() {
-	super(ProjectBrowser.getInstance(), Translator.localize(DLGBUNDLE, 
-                                            "dialog.title.dismiss-todo-item"));
-	JLabel instrLabel = new JLabel(Translator.localize("label.remove-item"));
+        super(
+            ProjectBrowser.getInstance(),
+            Translator.localize(DLGBUNDLE, "dialog.title.dismiss-todo-item"),
+            ArgoDialog.OK_CANCEL_OPTION,
+            true);
 
-        _okButton.setMnemonic(
-                Translator.localize("button.ok.mnemonic").charAt(0));
-        _cancelButton.setMnemonic(
-                Translator.localize("button.cancel.mnemonic").charAt(0));
+        JLabel instrLabel =
+            new JLabel(Translator.localize("label.remove-item"));
+            
+        _badGoalButton = new JRadioButton(Translator.localize(
+            "button.not-relevant-to-my-goals"));            
+        _badDecButton = new JRadioButton(Translator.localize(
+            "button.not-of-concern-at-moment"));
+        _explainButton = new JRadioButton(Translator.localize(
+            "button.reason-given-below"));
+        
         _badGoalButton.setMnemonic(
-                Translator.localize("button.not-relevant-to-my-goals.mnemonic")
-                    .charAt(0));
+            Translator.localize(
+                "button.not-relevant-to-my-goals.mnemonic").charAt(
+                0));
         _badDecButton.setMnemonic(
-                Translator.localize("button.not-of-concern-at-moment.mnemonic")
-                    .charAt(0));
+            Translator.localize(
+                "button.not-of-concern-at-moment.mnemonic").charAt(
+                0));
         _explainButton.setMnemonic(
-                Translator.localize("button.reason-given-below.mnemonic")
-                    .charAt(0));
-	setLocation(300, 200);
-	setSize(new Dimension(300, 250));
-	Container content = getContentPane();
-	GridBagLayout gb = new GridBagLayout();
-	GridBagConstraints c = new GridBagConstraints();
-	c.fill = GridBagConstraints.BOTH;
-	c.weightx = 1.0;
-	c.ipadx = 3; c.ipady = 3;
-	c.gridwidth = 2;
+            Translator.localize("button.reason-given-below.mnemonic").charAt(
+                0));
+                
+        JPanel content = new JPanel();
 
-	content.setLayout(gb);
+        GridBagLayout gb = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
 
-	JScrollPane explain = new JScrollPane(_explaination);
-	//set size?
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.gridwidth = 2;
 
-	c.gridx = 0;
-	c.gridy = 0;
-	gb.setConstraints(instrLabel, c);
-	content.add(instrLabel);
-	c.gridy = 1;
-	gb.setConstraints(_badGoalButton, c);
-	content.add(_badGoalButton);
-	c.gridy = 2;
-	gb.setConstraints(_badDecButton, c);
-	content.add(_badDecButton);
-	c.gridy = 3;
-	gb.setConstraints(_explainButton, c);
-	content.add(_explainButton);
-	c.gridy = 4;
-	c.weighty = 1.0;
-	gb.setConstraints(explain, c);
-	content.add(explain);
-	//c.fill = GridBagConstraints.NONE;
-	c.gridwidth = 1;
-	c.gridy = 5;
-	c.gridx = 0;
-	c.weighty = 0.;
-	gb.setConstraints(_okButton, c);
-	content.add(_okButton);
-	c.gridx = 1;
-	c.gridwidth = 1;
-	gb.setConstraints(_cancelButton, c);
-	content.add(_cancelButton);
+        content.setLayout(gb);
 
-	_okButton.addActionListener(new ActionListener() 
-	    {
-		public void actionPerformed(ActionEvent e) {
-		    if (_badGoalButton.getModel().isSelected()) {
-			badGoal(e);
-		    } else if (_badDecButton.getModel().isSelected()) {
-			badDec(e);
-		    } else if (_explainButton.getModel().isSelected()) {
-			explain(e);
-		    } else {
-			cat.warn("DissmissToDoItemDialog: Unknown action: "
-				 + e);
-		    }
-		    setVisible(false);
-		    dispose();
-		}
-	    });
-	_cancelButton.addActionListener(new ActionListener() 
-	    {
-		public void actionPerformed(ActionEvent e) {
-		    setVisible(false);
-		    dispose();
-		}
-	    });
+        _explanation = new JTextArea(6, 40);
+        _explanation.setLineWrap(true);
+        _explanation.setWrapStyleWord(true);
+        JScrollPane explain = new JScrollPane(_explanation);
 
-	_actionGroup.add(_badGoalButton);
-	_actionGroup.add(_badDecButton);
-	_actionGroup.add(_explainButton);
-	_actionGroup.setSelected(_explainButton.getModel(), true);
+        c.gridx = 0;
+        c.gridy = 0;
 
-	_explaination.setText(Translator.localize("label.enter-rationale-here"));
+        gb.setConstraints(instrLabel, c);
+        content.add(instrLabel);
+
+        c.gridy = 1;
+        c.insets = new Insets(5, 0, 0, 0);        
+
+        gb.setConstraints(_badGoalButton, c);
+        content.add(_badGoalButton);
+
+        c.gridy = 2;
+
+        gb.setConstraints(_badDecButton, c);
+        content.add(_badDecButton);
+
+        c.gridy = 3;
+        
+        gb.setConstraints(_explainButton, c);
+        content.add(_explainButton);
+
+        c.gridy = 4;
+        c.weighty = 1.0;
+                
+        gb.setConstraints(explain, c);
+        content.add(explain);
+        
+        setContent(content);
+
+        getOkButton().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (_badGoalButton.getModel().isSelected()) {
+                    badGoal(e);
+                }
+                else if (_badDecButton.getModel().isSelected()) {
+                    badDec(e);
+                }
+                else if (_explainButton.getModel().isSelected()) {
+                    explain(e);
+                }
+                else {
+                    cat.warn("DissmissToDoItemDialog: Unknown action: " + e);
+                }
+            }
+        });
+
+        _actionGroup = new ButtonGroup();
+        _actionGroup.add(_badGoalButton);
+        _actionGroup.add(_badDecButton);
+        _actionGroup.add(_explainButton);
+        _actionGroup.setSelected(_explainButton.getModel(), true);
+
+        _explanation.setText(
+            Translator.localize("label.enter-rationale-here"));
+            
+        _badGoalButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                _explanation.setEnabled(false);
+            }
+        });                
+        _badDecButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                _explanation.setEnabled(false);
+            }
+        });                
+        _explainButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                _explanation.setEnabled(true);
+                _explanation.requestFocus();
+                _explanation.selectAll();
+            }
+        });                
     }
 
-    public void setTarget(Object t) { _target = (ToDoItem) t; }
+    public void setTarget(Object t) {
+        _target = (ToDoItem) t;
+    }
 
     /** Prepare for typing in rationale field when window is opened. */
     public void setVisible(boolean b) {
-	super.setVisible(b);
-	if (b) {
-	    _explaination.requestFocus();
-	    _explaination.selectAll();
-	}
+        super.setVisible(b);
+        if (b) {
+            _explanation.requestFocus();
+            _explanation.selectAll();
+        }
     }
 
     ////////////////////////////////////////////////////////////////
     // event handlers
 
     private void badGoal(ActionEvent e) {
-	//cat.debug("bad goal");
-	GoalsDialog d = new GoalsDialog(ProjectBrowser.getInstance());
-	d.setVisible(true);
+        //cat.debug("bad goal");
+        GoalsDialog d = new GoalsDialog(ProjectBrowser.getInstance());
+        d.setVisible(true);
     }
 
     private void badDec(ActionEvent e) {
-	//cat.debug("bad decision");
-	DesignIssuesDialog d =
-	    new DesignIssuesDialog(ProjectBrowser.getInstance());
-	d.setVisible(true);
+        //cat.debug("bad decision");
+        DesignIssuesDialog d =
+            new DesignIssuesDialog(ProjectBrowser.getInstance());
+        d.setVisible(true);
     }
 
     private void explain(ActionEvent e) {
-	//cat.debug("I can explain!");
-	//TODO: make a new history item
-	ToDoList list = Designer.TheDesigner.getToDoList();
-	try {
-	    list.explicitlyResolve(_target, _explaination.getText());
-        ProjectManager.getManager().getCurrentProject().setNeedsSave(true);
-	} catch (UnresolvableException ure) {
-	    cat.error("Resolve failed (ure): " + ure);
-	    // TODO: Should be internationalized
-	    JOptionPane.showMessageDialog(this, ure.getMessage(),
-					  Translator.localize(OPTBUNDLE, 
-                                            "optionpane.dismiss-failed"),
-					  JOptionPane.ERROR_MESSAGE);
-	}
+        //cat.debug("I can explain!");
+        //TODO: make a new history item
+        ToDoList list = Designer.TheDesigner.getToDoList();
+        try {
+            list.explicitlyResolve(_target, _explanation.getText());
+            ProjectManager.getManager().getCurrentProject().setNeedsSave(true);
+        }
+        catch (UnresolvableException ure) {
+            cat.error("Resolve failed (ure): " + ure);
+            // TODO: Should be internationalized
+            JOptionPane.showMessageDialog(
+                this,
+                ure.getMessage(),
+                Translator.localize(OPTBUNDLE, "optionpane.dismiss-failed"),
+                JOptionPane.ERROR_MESSAGE);
+        }
     }
 } /* end class DismissToDoItemDialog */
-
