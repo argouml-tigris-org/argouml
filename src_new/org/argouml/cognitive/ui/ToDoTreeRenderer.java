4a5a42f16b7076f95c0e9acfa91177ca6e53532e
Markus Klink <mkl@tigris.org>
2003-12-09 14:48:06 +0000

    ToDoItems created by the designer are now displayed by their own icon in the ToDoList
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5422 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/cognitive/ui/ToDoTreeRenderer.java
+++ src_new/org/argouml/cognitive/ui/ToDoTreeRenderer.java
@@ -34,9 +34,11 @@ import javax.swing.plaf.metal.MetalIconFactory;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 import org.tigris.gef.base.Globals;
+import org.tigris.gef.presentation.Fig;
 
 import org.argouml.application.helpers.ResourceLoaderWrapper;
 import org.argouml.cognitive.Decision;
+import org.argouml.cognitive.Designer;
 import org.argouml.cognitive.Goal;
 import org.argouml.cognitive.Poster;
 import org.argouml.cognitive.ToDoItem;
@@ -46,86 +48,122 @@ import org.argouml.uml.ui.UMLTreeCellRenderer;
 public class ToDoTreeRenderer extends DefaultTreeCellRenderer {
     ////////////////////////////////////////////////////////////////
     // class variables
-    public ImageIcon _PostIt0     = lookupIconResource("PostIt0");
-    public ImageIcon _PostIt25    = lookupIconResource("PostIt25");
-    public ImageIcon _PostIt50    = lookupIconResource("PostIt50");
-    public ImageIcon _PostIt75    = lookupIconResource("PostIt75");
-    public ImageIcon _PostIt99    = lookupIconResource("PostIt99");
-    public ImageIcon _PostIt100   = lookupIconResource("PostIt100");
-    //public ImageIcon _MultiPostIt = lookupIconResource("MultiPostIt");
-
+    
+    // general icons for poster
+    public final ImageIcon _PostIt0     = lookupIconResource("PostIt0");
+    public final ImageIcon _PostIt25    = lookupIconResource("PostIt25");
+    public final ImageIcon _PostIt50    = lookupIconResource("PostIt50");
+    public final ImageIcon _PostIt75    = lookupIconResource("PostIt75");
+    public final ImageIcon _PostIt99    = lookupIconResource("PostIt99");
+    public final ImageIcon _PostIt100   = lookupIconResource("PostIt100");
+    
+    // specialised icons for designer
+    public final ImageIcon _PostItD0    = lookupIconResource("PostItD0");
+    public final ImageIcon _PostItD25   = lookupIconResource("PostItD25");
+    public final ImageIcon _PostItD50   = lookupIconResource("PostItD50");
+    public final ImageIcon _PostItD75   = lookupIconResource("PostItD75");
+    public final ImageIcon _PostItD99   = lookupIconResource("PostItD99");
+    public final ImageIcon _PostItD100  = lookupIconResource("PostItD100");
+    
     protected UMLTreeCellRenderer _navRenderer = new UMLTreeCellRenderer();
-
+    
     private static ImageIcon lookupIconResource(String name) {
-	ResourceLoaderWrapper w =
-	    ResourceLoaderWrapper.getResourceLoaderWrapper();
-	return w.lookupIconResource(name);
+        ResourceLoaderWrapper w =
+        ResourceLoaderWrapper.getResourceLoaderWrapper();
+        return w.lookupIconResource(name);
     }
-
+    
     ////////////////////////////////////////////////////////////////
     // TreeCellRenderer implementation
-
+    
     public Component getTreeCellRendererComponent(JTree tree, Object value,
-						  boolean sel,
-						  boolean expanded,
-						  boolean leaf, int row,
-						  boolean hasFocus) {
-
-	Component r = super.getTreeCellRendererComponent(tree, value, sel,
-							 expanded, leaf,
-							 row, hasFocus);
-
-	if (r instanceof JLabel) {
-	    JLabel lab = (JLabel) r;
-	    if (value instanceof ToDoItem) {
-		ToDoItem item = (ToDoItem) value;
-		if (item.getProgress() == 0) lab.setIcon(_PostIt0);
-		else if (item.getProgress() <= 25) lab.setIcon(_PostIt25);
-		else if (item.getProgress() <= 50) lab.setIcon(_PostIt50);
-		else if (item.getProgress() <= 75) lab.setIcon(_PostIt75);
-		else if (item.getProgress() <= 100) lab.setIcon(_PostIt99);
-		else lab.setIcon(_PostIt100);
-	    }
-	    else if (value instanceof Decision) {
-		lab.setIcon(MetalIconFactory.getTreeFolderIcon());
-	    }
-	    else if (value instanceof Goal) {
-		lab.setIcon(MetalIconFactory.getTreeFolderIcon());
-	    }
-	    else if (value instanceof Poster) {
-		lab.setIcon(MetalIconFactory.getTreeFolderIcon());
-	    }
-	    else if (value instanceof PriorityNode) {
-		lab.setIcon(MetalIconFactory.getTreeFolderIcon());
-	    }
-	    else if (value instanceof KnowledgeTypeNode) {
-		lab.setIcon(MetalIconFactory.getTreeFolderIcon());
-	    }
-	    else if (org.argouml.model.ModelFacade.isAModelElement(value)) {
-		return _navRenderer.getTreeCellRendererComponent(tree,
-								 value,
-								 sel,
-								 expanded,
-								 leaf,
-								 row,
-								 hasFocus);
-	    }
-
-	    String tip = lab.getText() + " ";
-	    lab.setToolTipText(tip);
-	    tree.setToolTipText(tip);
-
-	    if (!sel)
-		lab.setBackground(getBackgroundNonSelectionColor());
-	    else {
-		Color high = Globals.getPrefs().getHighlightColor();
-		high = high.brighter().brighter();
-		lab.setBackground(high);
-	    }
-	    lab.setOpaque(sel);
-	}
-	return r;
+    boolean sel,
+    boolean expanded,
+    boolean leaf, int row,
+    boolean hasFocus) {
+        
+        Component r = super.getTreeCellRendererComponent(tree, value, sel,
+        expanded, leaf,
+        row, hasFocus);
+        
+        if (r instanceof JLabel) {
+            JLabel lab = (JLabel) r;
+            if (value instanceof ToDoItem) {
+                ToDoItem item = (ToDoItem) value;
+                Poster post = item.getPoster();
+                if ( post instanceof Designer) {
+                    if (item.getProgress() == 0) lab.setIcon(_PostItD0);
+                    else if (item.getProgress() <= 25) lab.setIcon(_PostItD25);
+                    else if (item.getProgress() <= 50) lab.setIcon(_PostItD50);
+                    else if (item.getProgress() <= 75) lab.setIcon(_PostItD75);
+                    else if (item.getProgress() <= 100) lab.setIcon(_PostItD99);
+                    else lab.setIcon(_PostIt100);
+                } else {
+                    if (item.getProgress() == 0) lab.setIcon(_PostIt0);
+                    else if (item.getProgress() <= 25) lab.setIcon(_PostIt25);
+                    else if (item.getProgress() <= 50) lab.setIcon(_PostIt50);
+                    else if (item.getProgress() <= 75) lab.setIcon(_PostIt75);
+                    else if (item.getProgress() <= 100) lab.setIcon(_PostIt99);
+                    else lab.setIcon(_PostIt100);
+                }
+                
+            }
+            else if (value instanceof Decision) {
+                lab.setIcon(MetalIconFactory.getTreeFolderIcon());
+            }
+            else if (value instanceof Goal) {
+                lab.setIcon(MetalIconFactory.getTreeFolderIcon());
+            }
+            else if (value instanceof Poster) {
+                lab.setIcon(MetalIconFactory.getTreeFolderIcon());
+            }
+            else if (value instanceof PriorityNode) {
+                lab.setIcon(MetalIconFactory.getTreeFolderIcon());
+            }
+            else if (value instanceof KnowledgeTypeNode) {
+                lab.setIcon(MetalIconFactory.getTreeFolderIcon());
+            }
+            else if (org.argouml.model.ModelFacade.isADiagram(value)) {
+                return _navRenderer.getTreeCellRendererComponent(tree,
+                value,
+                sel,
+                expanded,
+                leaf,
+                row,
+                hasFocus);
+            }
+            else {
+                Object newValue = value;
+                if (newValue instanceof Fig)
+                    newValue = ((Fig) value).getOwner();
+                if (org.argouml.model.ModelFacade.isAModelElement(newValue)) {
+                    return _navRenderer.getTreeCellRendererComponent(tree,
+                    newValue,
+                    sel,
+                    expanded,
+                    leaf,
+                    row,
+                    hasFocus);
+                }
+            }
+            
+            
+            
+            String tip = lab.getText() + " ";
+            lab.setToolTipText(tip);
+            tree.setToolTipText(tip);
+            
+            if (!sel)
+                lab.setBackground(getBackgroundNonSelectionColor());
+            else {
+                Color high = Globals.getPrefs().getHighlightColor();
+                high = high.brighter().brighter();
+                lab.setBackground(high);
+            }
+            lab.setOpaque(sel);
+        }
+        return r;
     }
-
-
+    
+    
 } /* end class ToDoTreeRenderer */
