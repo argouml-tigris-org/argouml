9aae9d09e5fdc4ff340430575a4e08adc4a7209d
Jeremy Jones <jjones@tigris.org>
2004-01-08 20:31:20 +0000

    Changed "NavConfig" to "Configure Perspectives". Mods to PerspectiveConfigurator dialog: changed to use ArgoDialog, i18n, and improved spacing between components.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5583 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ui/explorer/PerspectiveConfigurator.java
+++ src_new/org/argouml/ui/explorer/PerspectiveConfigurator.java
@@ -24,8 +24,6 @@
 
 package org.argouml.ui.explorer;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
@@ -34,24 +32,26 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Vector;
-import java.util.Arrays;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.tree.TreeModel;
-
-import org.argouml.ui.SpacerPanel;
-import org.argouml.ui.explorer.rules.PerspectiveRule;
 
 import org.apache.log4j.Logger;
-import org.tigris.gef.util.Converter;
+import org.argouml.i18n.Translator;
+import org.argouml.ui.ArgoDialog;
+import org.argouml.ui.SpacerPanel;
+import org.argouml.ui.explorer.rules.PerspectiveRule;
 
 /**
  *
@@ -63,7 +63,7 @@ import org.tigris.gef.util.Converter;
  * Created on 21 December 2003, 21:47
  * @author  alexb
  */
-public class PerspectiveConfigurator extends JDialog{
+public class PerspectiveConfigurator extends ArgoDialog {
     
     private static Logger cat =
     Logger.getLogger(PerspectiveConfigurator.class);
@@ -71,17 +71,7 @@ public class PerspectiveConfigurator extends JDialog{
     public static int _numNavConfig = 0;
     
     ////////////////////////////////////////////////////////////////
-    // constants
-    public final int WIDTH = 400;
-    public final int HEIGHT = 600;
-    
-    ////////////////////////////////////////////////////////////////
     // instance variables
-    
-    private JPanel  _mainButtons;
-    
-    private JButton _okButton;
-    private JButton _cancelButton;
     
     private JPanel  _configPanel;
     private JList   _perspectiveList;
@@ -100,28 +90,16 @@ public class PerspectiveConfigurator extends JDialog{
     /** Creates a new instance of PerspectiveDesignerDialog */
     public PerspectiveConfigurator(Frame parent) {
         
-        super(parent, "Configure Perspectives", false);
-        
-        int x = parent.getLocation().x + (parent.getSize().width - WIDTH) / 2;
-        int y = parent.getLocation().y + (parent.getSize().height - HEIGHT) / 2;
-        setLocation(x, y);
-        setSize(WIDTH, HEIGHT);
-        
-        getContentPane().setLayout(new BorderLayout());
+        super(parent, Translator.localize("dialog.title.configure-perspectives"),
+            ArgoDialog.OK_CANCEL_OPTION, true);
         
         initPersPanel();
         loadData();
         
-        JPanel persTab = new JPanel(new BorderLayout(3, 3));
-        persTab.add(_configPanel, BorderLayout.CENTER);
+        setContent(_configPanel);
         
-        _mainButtons.add(_cancelButton);
-        _mainButtons.add(_okButton);
-        
-        getContentPane().add(persTab, BorderLayout.CENTER);
-        getContentPane().add(_mainButtons, BorderLayout.SOUTH);
-        
-        getRootPane().setDefaultButton(_okButton);
+        getOkButton().addActionListener(new OkListener());
+
         _numNavConfig++;
     }
     
@@ -164,19 +142,21 @@ public class PerspectiveConfigurator extends JDialog{
      */
     public void initPersPanel() {
         
-        _mainButtons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        _okButton = new JButton("OK");
-        _cancelButton = new JButton("Cancel");
         _configPanel = new JPanel();
         
         _perspectiveList = new JList(_perspectiveListModel = new DefaultListModel());
         _perspectiveRulesList = new JList(_perspectiveRulesListModel = new DefaultListModel());
         _ruleLibraryList = new JList(_ruleLibraryListModel = new DefaultListModel());
-        _newPerspectiveButton = new JButton("New");
-        _removePerspectiveButton = new JButton("Remove");
-        _dupPersButton = new JButton("Duplicate");
+        _newPerspectiveButton = new JButton();
+        nameButton(_newPerspectiveButton, "button.new");
+        _removePerspectiveButton = new JButton();
+        nameButton(_removePerspectiveButton, "button.remove");
+        _dupPersButton = new JButton();
+        nameButton(_dupPersButton, "button.duplicate");
         _addRuleButton = new JButton(">>");
+        _addRuleButton.setToolTipText(Translator.localize("button.add-rule"));
         _removeRuleButton = new JButton("<<");
+        _removeRuleButton.setToolTipText(Translator.localize("button.remove-rule"));
         
         _perspectiveList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         _perspectiveRulesList
@@ -188,7 +168,8 @@ public class PerspectiveConfigurator extends JDialog{
         GridBagConstraints c = new GridBagConstraints();
         c.ipadx = 3;      c.ipady = 3;
         
-        JLabel persLabel = new JLabel("Perspectives");
+        JLabel persLabel = new JLabel(
+            Translator.localize("label.perspectives"));
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;      c.gridy = 0;
         c.gridwidth = 3;
@@ -206,31 +187,33 @@ public class PerspectiveConfigurator extends JDialog{
         gb.setConstraints(persScroll, c);
         _configPanel.add(persScroll);
         
-        _newPerspectiveButton.setMargin(new Insets(0, 0, 0, 0));
-        _removePerspectiveButton.setMargin(new Insets(0, 0, 0, 0));
-        _dupPersButton.setMargin(new Insets(0, 0, 0, 0));
-        JPanel persButtons = new JPanel(new GridLayout(3, 1));
+        JPanel persButtons = new JPanel(new GridLayout(3, 1, 0, 5));
         persButtons.add(_newPerspectiveButton);
         persButtons.add(_removePerspectiveButton);
         persButtons.add(_dupPersButton);
-        JPanel persButtonWrapper = new JPanel();
+        JPanel persButtonWrapper = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
         persButtonWrapper.add(persButtons);
         
         c.gridx = 4;      c.gridy = 1;
         c.gridwidth = 1;
         c.weightx = 0.0;  c.weighty = 0.0;
+        c.ipadx = 0;      c.ipady = 0;
+        c.insets = new Insets(0, 5, 0, 0);
         gb.setConstraints(persButtonWrapper, c);
         _configPanel.add(persButtonWrapper);
         
-        JLabel ruleLibLabel = new JLabel("Rules Library");
+        JLabel ruleLibLabel = new JLabel(
+            Translator.localize("label.rules-library"));
         c.gridx = 0; c.gridy = 3;
         c.gridwidth = 1;
         c.weightx = 1.0;  c.weighty = 0.0;
+        c.ipadx = 3;      c.ipady = 3;
+        c.insets = new Insets(10, 0, 0, 0);
         gb.setConstraints(ruleLibLabel, c);
         _configPanel.add(ruleLibLabel);
         
-        _addRuleButton.setMargin(new Insets(0, 0, 0, 0));
-        _removeRuleButton.setMargin(new Insets(0, 0, 0, 0));
+        _addRuleButton.setMargin(new Insets(2, 2, 2, 2));
+        _removeRuleButton.setMargin(new Insets(2, 2, 2, 2));
         JPanel xferButtons = new JPanel();
         xferButtons.setLayout(new BoxLayout(xferButtons, BoxLayout.Y_AXIS));
         xferButtons.add(_addRuleButton);
@@ -239,19 +222,23 @@ public class PerspectiveConfigurator extends JDialog{
         
         c.gridx = 2;      c.gridy = 4;
         c.weightx = 0.0;  c.weighty = 0.0;
+        c.insets = new Insets(0, 3, 0, 5);
         gb.setConstraints(xferButtons, c);
         _configPanel.add(xferButtons);
         
-        JLabel rulesLabel = new JLabel("Selected Rules");
+        JLabel rulesLabel = new JLabel(
+            Translator.localize("label.selected-rules"));
         c.gridx = 3;      c.gridy = 3;
         c.gridwidth = 1;
         c.weightx = 1.0;
+        c.insets = new Insets(10, 0, 0, 0);
         gb.setConstraints(rulesLabel, c);
         _configPanel.add(rulesLabel);
         
         c.gridx = 0;      c.gridy = 4;
         c.weighty = 1.0;
         c.gridwidth = 2;  c.gridheight = 2;
+        c.insets = new Insets(0, 0, 0, 0);
         JScrollPane ruleLibScroll =
         new JScrollPane(_ruleLibraryList,
         JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -269,8 +256,6 @@ public class PerspectiveConfigurator extends JDialog{
         _configPanel.add(rulesScroll);
         
         // add action listeners:
-        _okButton.addActionListener(new OkListener());
-        _cancelButton.addActionListener(new CancelListener());
         _newPerspectiveButton
         .addActionListener(new NewPerspectiveListener());
         _removePerspectiveButton
@@ -310,15 +295,6 @@ public class PerspectiveConfigurator extends JDialog{
             }
             
             PerspectiveManager.getInstance().saveUserPerspectives();
-            setVisible(false);
-            dispose();
-        }
-    }
-    
-    class CancelListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            setVisible(false);
-            dispose();
         }
     }
     
