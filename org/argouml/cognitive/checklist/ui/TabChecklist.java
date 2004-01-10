c045624627c8d7b509d9c6ea9193af05002b40e7
Linus Tolke <linus@tigris.org>
2004-01-10 14:15:48 +0000

    The Checklists are now working and enabled by default. This is also the first attempt at restructuring the contents i.e. using the i18n in a straight-forward way.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@5587 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/cognitive/checklist/ui/TabChecklist.java
+++ org/argouml/cognitive/checklist/ui/TabChecklist.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -34,6 +34,7 @@ import java.beans.VetoableChangeListener;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -51,6 +52,7 @@ import org.argouml.kernel.DelayedChangeNotify;
 import org.argouml.kernel.DelayedVChangeListener;
 import org.argouml.model.ModelFacade;
 import org.argouml.model.uml.UmlModelEventPump;
+import org.argouml.i18n.Translator;
 import org.argouml.ui.LookAndFeelMgr;
 import org.argouml.ui.TabSpawnable;
 import org.argouml.ui.targetmanager.TargetEvent;
@@ -100,10 +102,11 @@ public class TabChecklist extends TabSpawnable
 	_table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
 	_table.sizeColumnsToFit(-1);
 
-	//JScrollPane sp = JTable.createScrollPaneForTable(_table);
 	JScrollPane sp = new JScrollPane(_table);
 
 	setLayout(new BorderLayout());
+	add(new JLabel(Translator.localize("tab.checklist.warning")), 
+	    BorderLayout.NORTH);
 	add(sp, BorderLayout.CENTER);
     }
     
--- org/argouml/uml/cognitive/checklist/Init.java
+++ org/argouml/uml/cognitive/checklist/Init.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2001 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -30,26 +30,45 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import org.argouml.cognitive.checklist.CheckManager;
+import org.argouml.cognitive.checklist.Checklist;
+import org.argouml.cognitive.checklist.CheckItem;
 import org.argouml.model.ModelFacade;
+import org.argouml.i18n.Translator;
 
-/** Registers Checklists for different kinds of model elements. If you
- *  add a new checklist, a line must be added here.
+
+/**
+ * Registers Checklists for different kinds of model elements. If you
+ * add a new checklist, a line must be added here.
  *
- *  @see org.argouml.cognitive.checklist.CheckManager */
-
+ * @see org.argouml.cognitive.checklist.CheckManager
+ */
 public class Init {
 
+    /**
+     * @deprecated as of 0.15.3. Visibility changed to private.
+     */
     protected static Logger cat =
 	Logger.getLogger(Init.class);
 
-    /** static initializer, register all appropriate critics */
+    private static Logger _Log =
+	Logger.getLogger(Init.class);
+
+    /** 
+     * static initializer, register all appropriate critics<p>
+     *
+     * When the checklists are converted to the new format, this function
+     * no longer needs Locale as argument.
+     */
     public static void init(Locale locale) {
+	createChecklists();
+
 	ResourceBundle bundle =
 	    ResourceBundle
 	    .getBundle("org.argouml.i18n.UMLCognitiveResourceBundle", locale);
-	addChecklist(bundle, (Class)ModelFacade.CLASS, "ChClass");
+
+	// addChecklist(bundle, (Class)ModelFacade.CLASS, "ChClass");
 	addChecklist(bundle, (Class)ModelFacade.INTERFACE, "ChInterface");
-	addChecklist(bundle, (Class)ModelFacade.ATTRIBUTE, "ChAttribute");
+	// addChecklist(bundle, (Class)ModelFacade.ATTRIBUTE, "ChAttribute");
 	addChecklist(bundle, (Class)ModelFacade.OPERATION, "ChOperation");
 	addChecklist(bundle, (Class)ModelFacade.ASSOCIATION, "ChAssociation");
 	addChecklist(bundle, (Class)ModelFacade.ASSOCIATION_CLASS, "ChAssociation");
@@ -59,6 +78,10 @@ public class Init {
 	addChecklist(bundle, (Class)ModelFacade.ACTOR, "ChActor");
     }
 
+    /**
+     * @deprecated will not be used when all check lists are moved to the
+     * new format.
+     */
     private static void addChecklist(ResourceBundle bundle,
 				     Class cls, String key) {
         try {
@@ -67,9 +90,143 @@ public class Init {
             CheckManager.register(cls, checklist);
         }
         catch (MissingResourceException e) {
-            cat.error(e);
+            _Log.error(e);
             e.printStackTrace();
         }
     }
+
+
+    /**
+     * Creat a check item (an UMLCheckItem) corresponding to the given key.
+     * and add it to the given checklist.
+     *
+     * @param category to add the item to.
+     * @param key to get the localized string.
+     * @param checklist to add the item to.
+     */
+    private static void newCheckItem(String category, String key, 
+				     Checklist checklist) {
+	CheckItem checkitem = new UMLCheckItem(category, 
+					       Translator.localize(key));
+	checklist.addItem(checkitem);
+    }
+
+    /**
+     * Create all check lists.<p>
+     *
+     * This is the new implementation and a change from the old implementation
+     * in that the source code (i.e. this function) contains knowledge on
+     * what to check.<p>
+     *
+     * The purpose of this is to allow the ArgoUML project to jointly develop
+     * the knowledge database and let the internationalizations be just 
+     * internationalizations.<p>
+     *
+     * When adding a new check list or an item to an existing check list you
+     * will have to:<ol>
+     * <li>Add the lines in this function.
+     * <li>Add the tags for the new items to the i18n property file 
+     *     (checklist.properties).
+     * <li>Notify the localization teams.
+     * </ol>
+     */
+    private static void createChecklists() {
+	//
+	// This function will, by design, contain long lists of i18n keys.
+	// It might be tempting to shorten these by constructing them from
+	// the parts but please then consider that we might loose the
+	// possibility to build tools to statically check the property files 
+	// against this file.
+	//
+	Checklist cl;
+	String cat;
+
+	// Class
+	cl = new Checklist();
+
+	cat = Translator.localize("checklist.class.naming");
+	newCheckItem(cat, "checklist.class.naming.describe-clearly", cl);
+	newCheckItem(cat, "checklist.class.naming.is-noun", cl);
+	newCheckItem(cat, "checklist.class.naming.misinterpret", cl);
+
+	cat = Translator.localize("checklist.class.encoding");
+	newCheckItem(cat, "checklist.class.encoding.convert-to-attribute", cl);
+	newCheckItem(cat, "checklist.class.encoding.do-just-one-thing", cl);
+	newCheckItem(cat, "checklist.class.encoding.break-into-parts", cl);
+
+	cat = Translator.localize("checklist.class.value");
+	newCheckItem(cat, "checklist.class.value.start-with-meaningful-values",
+		     cl);
+	newCheckItem(cat, "checklist.class.value.convert-to-invariant", cl);
+	newCheckItem(cat,
+		     "checklist.class.value.establish-invariant-in-constructors",
+		     cl);
+	newCheckItem(cat, "checklist.class.value.maintain-invariant", cl);
+
+	cat = Translator.localize("checklist.class.location");
+	newCheckItem(cat, "checklist.class.location.move-somewhere", cl);
+	newCheckItem(cat, "checklist.class.location.planned-subclasses", cl);
+	newCheckItem(cat, "checklist.class.location.eliminate-class", cl);
+	newCheckItem(cat,
+		     "checklist.class.location.eliminates-or-affects-something-else",
+		     cl);
+
+	cat = Translator.localize("checklist.class.updates");
+        newCheckItem(cat, "checklist.class.updates.reasons-for-update", cl);
+	newCheckItem(cat, "checklist.class.updates.affects-something-else", cl);
+
+	CheckManager.register((Class) ModelFacade.CLASS, cl);
+
+
+	// Attribute
+	cl = new Checklist();
+
+	cat = Translator.localize("checklist.attribute.naming");
+	newCheckItem(cat, "checklist.attribute.naming.describe-clearly", cl);
+	newCheckItem(cat, "checklist.attribute.naming.is-noun", cl);
+	newCheckItem(cat, "checklist.attribute.naming.misinterpret", cl);
+
+	cat = Translator.localize("checklist.attribute.encoding");
+	newCheckItem(cat, "checklist.attribute.encoding.is-too-restrictive",
+		     cl);
+	newCheckItem(cat,
+		     "checklist.attribute.encoding.allow-impossible-values",
+		     cl);
+	newCheckItem(cat, "checklist.attribute.encoding.combine-with-other",
+		     cl);
+	newCheckItem(cat, "checklist.attribute.encoding.break-into-parts", cl);
+	newCheckItem(cat, "checklist.attribute.encoding.is-computable", cl);
+
+	cat = Translator.localize("checklist.attribute.value");
+	newCheckItem(cat, "checklist.attribute.value.default-value", cl);
+	newCheckItem(cat, "checklist.attribute.value.correct-default-value",
+		     cl);
+	newCheckItem(cat, "checklist.attribute.value.is-correctness-checkable",
+		     cl);
+
+	cat = Translator.localize("checklist.attribute.location");
+	newCheckItem(cat, "checklist.attribute.location.move-somewhere", cl);
+	newCheckItem(cat, "checklist.attribute.location.move-up-hierarchy", cl);
+	newCheckItem(cat, "checklist.attribute.location.include-all", cl);
+	newCheckItem(cat, "checklist.attribute.location.could-be-eliminated",
+		     cl);
+	newCheckItem(cat,
+		     "checklist.attribute.location.eliminates-or-affects-something-else",
+		     cl);
+
+	cat = Translator.localize("checklist.attribute.updates");
+        newCheckItem(cat, "checklist.attribute.updates.reasons-for-update", cl);
+	newCheckItem(cat, "checklist.attribute.updates.affects-something-else",
+		     cl);
+	newCheckItem(cat,
+		     "checklist.attribute.updates.exists-method-for-update",
+		     cl);
+	newCheckItem(cat,
+		     "checklist.attribute.updates.exists-method-for-specific-value",
+		     cl);
+
+	CheckManager.register((Class) ModelFacade.ATTRIBUTE, cl);
+
+    }	
 
 } /* end class Init */
