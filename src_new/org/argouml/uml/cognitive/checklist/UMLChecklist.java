191a8b8dd41f0a62fbfad901984c1f9c629a3b50
Linus Tolke <linus@tigris.org>
2004-01-11 22:25:33 +0000

    Finished the conversion of the checklists (for the default language).
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5591 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/cognitive/checklist/UMLChecklist.java
+++ src_new/org/argouml/uml/cognitive/checklist/UMLChecklist.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2001 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -27,8 +27,11 @@ package org.argouml.uml.cognitive.checklist;
 import org.argouml.cognitive.checklist.CheckItem;
 import org.argouml.cognitive.checklist.Checklist;
 
-/** Exacly like Checklist, but makes UMLCheckItems by default. */
-
+/**
+ * Exacly like Checklist, but makes UMLCheckItems by default. 
+ *
+ * @deprecated as of 0.15.3. No longer needed.
+ */
 public class UMLChecklist extends Checklist {
 
     /**
