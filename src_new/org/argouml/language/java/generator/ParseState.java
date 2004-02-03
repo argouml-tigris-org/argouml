32a346655c7d238125c319d01bc6d4f0bff6a37e
Linus Tolke <linus@tigris.org>
2004-02-03 07:07:37 +0000

    Fixed some javadoc comments.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5671 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/language/java/generator/ParseState.java
+++ src_new/org/argouml/language/java/generator/ParseState.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2001 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -51,9 +51,9 @@ public class ParseState
     private Object mClassifier;
 
     /**
-       Create a new parse state.
-
-       @param The namespace the classifier belongs to.
+     * Create a new parse state.
+     *
+     * @param handle is the namespace the classifier belongs to.
      */
     public ParseState(Object handle) {
         if (ModelFacade.isAClassifier(handle)) {
--- src_new/org/argouml/uml/ui/PropPanel.java
+++ src_new/org/argouml/uml/ui/PropPanel.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -480,7 +480,7 @@ abstract public class PropPanel
 
     /**
      * @deprecated As of ArgoUml version 0.13.5,replaced by
-     *             {@link org.argouml.ui.targetmanager.TargetManager.getInstance().navigateBack();
+     *             {@link org.argouml.ui.targetmanager.TargetManager.getInstance().navigateBack()}
      *
      */
     public void navigateBackAction() {
