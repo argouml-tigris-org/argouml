efed893e3b61f3f7545171040d66c39a3a0150d2
Michael Stockman <d00mst@tigris.org>
2004-02-08 11:22:37 +0000

    Removed a call to deprecated.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5695 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/ActionEdgesDisplay.java
+++ src_new/org/argouml/uml/ui/ActionEdgesDisplay.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2001 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -24,6 +24,7 @@
 
 package org.argouml.uml.ui;
 
+import org.argouml.kernel.ProjectManager;
 import org.argouml.ui.*;
 import org.tigris.gef.base.*;
 import org.tigris.gef.graph.*;
@@ -69,8 +70,8 @@ public class ActionEdgesDisplay extends UMLAction {
     // main methods
 
     public void actionPerformed(ActionEvent ae) {
-        ProjectBrowser pb = ProjectBrowser.getInstance();
-        ArgoDiagram d = pb.getActiveDiagram();
+        ArgoDiagram d = ProjectManager.getManager()
+                .getCurrentProject().getActiveDiagram();
         Editor ce = Globals.curEditor();
         MutableGraphModel mgm = (MutableGraphModel) ce.getGraphModel();
 
