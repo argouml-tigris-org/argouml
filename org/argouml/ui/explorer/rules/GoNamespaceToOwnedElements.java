f5a4bdc21dd4a517b99e807615c874121fbc9e6c
Michael Stockman <d00mst@tigris.org>
2004-04-02 23:38:55 +0000

    Don't show StateMachines out of context, eg Attributes may be contexts but are not Namespaces -> the StateMachine will be in some namespace which is not the Attribute.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@5861 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/rules/GoNamespaceToOwnedElements.java
+++ org/argouml/ui/explorer/rules/GoNamespaceToOwnedElements.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -44,10 +44,12 @@ public class GoNamespaceToOwnedElements extends AbstractPerspectiveRule{
         Iterator it = ownedElements.iterator();
         Collection ret = new ArrayList();
         while (it.hasNext()) {
-        	Object o = it.next();
-        	if (!ModelFacade.isACollaboration(o)) {
-        		ret.add(o);
-        	}
+	    Object o = it.next();
+	    if (ModelFacade.isACollaboration(o))
+		continue;
+	    if (ModelFacade.isAStateMachine(o) && ModelFacade.getContext(o) != parent)
+		continue;
+	    ret.add(o);
         }
         return ret;
     }
