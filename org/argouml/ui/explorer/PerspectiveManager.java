73c48f8fbfd14124bf3102c33aee0d9dac3378f1
Michael Stockman <d00mst@tigris.org>
2004-04-02 23:35:54 +0000

    Slightly inconsistent indention. StateMachineToTop removed from package perspective - see issue 1220
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@5860 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/PerspectiveManager.java
+++ org/argouml/ui/explorer/PerspectiveManager.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2001 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -251,12 +251,11 @@ public class PerspectiveManager {
         packagePerspective.addRule(new GoStateToDoActivity());
         packagePerspective.addRule(new GoStateToEntry());
         packagePerspective.addRule(new GoStateToExit());
-		packagePerspective.addRule(new GoClassifierToSequenceDiagram());
-		packagePerspective.addRule(new GoOperationToSequenceDiagram());
-		packagePerspective.addRule(new GoClassifierToInstance());
-                packagePerspective.addRule(new GoStateMachineToTop());
-                packagePerspective.addRule(new GoStateToIncomingTrans());
-                packagePerspective.addRule(new GoStateToOutgoingTrans());
+	packagePerspective.addRule(new GoClassifierToSequenceDiagram());
+	packagePerspective.addRule(new GoOperationToSequenceDiagram());
+	packagePerspective.addRule(new GoClassifierToInstance());
+	packagePerspective.addRule(new GoStateToIncomingTrans());
+	packagePerspective.addRule(new GoStateToOutgoingTrans());
         
         ExplorerPerspective diagramPerspective = 
             new ExplorerPerspective("combobox.item.diagram-centric");
