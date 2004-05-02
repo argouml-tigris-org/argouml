8a2b9c8009056cc7cbfbcbc25d2f5b4bb420d899
Michiel van der Wulp <mvw@tigris.org>
2004-05-02 13:20:24 +0000

    Removed some unused imports.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@5956 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/rules/GoModelToCollaboration.java
+++ org/argouml/ui/explorer/rules/GoModelToCollaboration.java
@@ -26,7 +26,6 @@ package org.argouml.ui.explorer.rules;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
