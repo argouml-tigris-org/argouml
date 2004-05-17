f3f0e9aa5a6b8d97aad896508f55018c9bf7f9c5
Markus Klink <mkl@tigris.org>
2004-05-17 15:57:07 +0000

    Create a replacement for deprecated checkbox implementation of UMLTaggedBooleanProperty.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@5979 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/TabDocumentation.java
+++ src_new/org/argouml/uml/ui/TabDocumentation.java
@@ -3,14 +3,14 @@
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
+// and this paragraph appear in all copies. This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -36,36 +36,43 @@ import org.tigris.gef.presentation.Fig;
 
 /**
  * This the tab in the details pane for documentation.
- *
- * <p>This prop panel now uses the new (0.13.*) event implementation.
- *
- * <p>questions regarding where the data is stored i.e. author etc.
- * edit>settings... and setting 'global' author does not affect the author
- * field in the documentation tab
- * <p>Enabling output from the documentation fields when generating code as embedded
- * in javadocs and for html/ diagram creation is considered important by users
- *
- * <p>When importing sources, already saved javadoc statements
- * are not automatically added to the documenation Jtext window.
- * When Adding notes to classes the notes are not included in
- * the documentation text window.
- * <p>Since: field is not validated for real date. change
- * to DateField?
- *
- * <p>Note all fields in the TabDocumentation are
- * added automatically to the tagged value tab
- * view.
- *
- * <p>refactored by: raphael-langerhorst@gmx.at; 5th April 03
- *      changes: uses LabelledLayout instead of GridBagLayout
- *              uses the new event pump introduced late 2002 by Jaap
- *
- * <p>UMLModelElementTaggedValueDocument is used to access the tagged values 
- * of an MModelElement
+ * 
+ * <p>
+ * This prop panel now uses the new (0.13.*) event implementation.
+ * 
+ * <p>
+ * questions regarding where the data is stored i.e. author etc.
+ * edit>settings... and setting 'global' author does not affect the author field
+ * in the documentation tab
+ * <p>
+ * Enabling output from the documentation fields when generating code as
+ * embedded in javadocs and for html/ diagram creation is considered important
+ * by users
+ * 
+ * <p>
+ * When importing sources, already saved javadoc statements are not
+ * automatically added to the documenation Jtext window. When Adding notes to
+ * classes the notes are not included in the documentation text window.
+ * <p>
+ * Since: field is not validated for real date. change to DateField?
+ * 
+ * <p>
+ * Note all fields in the TabDocumentation are added automatically to the tagged
+ * value tab view.
+ * 
+ * <p>
+ * refactored by: raphael-langerhorst@gmx.at; 5th April 03 changes: uses
+ * LabelledLayout instead of GridBagLayout uses the new event pump introduced
+ * late 2002 by Jaap
+ * 
+ * <p>
+ * UMLModelElementTaggedValueDocument is used to access the tagged values of an
+ * MModelElement
  */
 public class TabDocumentation extends PropPanel {
 
-    ////////////////////////////////////////////////////////////////
+    private static String _orientation = Configuration.getString(Configuration
+            .makeKey("layout", "tabdocumentation"));
 
     /**
      * Construct new documentation tab
@@ -73,46 +80,32 @@ public class TabDocumentation extends PropPanel {
     public TabDocumentation() {
 
         //uses the new LabelledLayout;
-        //TODO: the title of the prop panel is localized using the localisation of documentation
+        //TODO: the title of the prop panel is localized using the
+        // localisation of documentation
         //- should this change? (Raphael)
-        // MVW: I changed label.documentation into tab.documentation 
-        // to get rid of  ":" in the tab. 
-        super(
-	      Translator.localize("tab.documentation"),
-	      (Configuration.getString(
-                    Configuration.makeKey("layout", "tabdocumentation"))
-                    .equals("West") ||
-	       Configuration.getString(
-                    Configuration.makeKey("layout", "tabdocumentation"))
-                    .equals("East"))
-	      ? Vertical.getInstance() : Horizontal.getInstance());
+        // MVW: I changed label.documentation into tab.documentation
+        // to get rid of ":" in the tab.
 
-	// Change this to call labelled layout constructor:
-        //        super("tab.documentation", null, 2); 
+        super(Translator.localize("tab.documentation"), (_orientation
+                .equals("West") || _orientation.equals("East")) ? Vertical
+                .getInstance() : Horizontal.getInstance());
 
-        addField(
-		 Translator.localize("label.author"),
-		 new UMLTextField2(new UMLModelElementTaggedValueDocument("author")));
+        addField(Translator.localize("label.author"), new UMLTextField2(
+                new UMLModelElementTaggedValueDocument("author")));
 
-        //unknown where this information is stored; it does not go to myproject.argo (xml file)
-        addField(
-		 Translator.localize("label.version"),
-		 new UMLTextField2(new UMLModelElementTaggedValueDocument("version")));
+        //unknown where this information is stored; it does not go to
+        // myproject.argo (xml file)
+        addField(Translator.localize("label.version"), new UMLTextField2(
+                new UMLModelElementTaggedValueDocument("version")));
 
-        addField(
-		 Translator.localize("label.since"),
-		 new UMLTextField2(new UMLModelElementTaggedValueDocument("since")));
+        addField(Translator.localize("label.since"), new UMLTextField2(
+                new UMLModelElementTaggedValueDocument("since")));
 
-        //TODO: change UMLCheckBox to UMLCheckBox2 (Raphael)
-        addField(
-		 Translator.localize("label.deprecated"),
-		 new UMLCheckBox(
-				 "",
-				 this,
-				 new UMLTaggedBooleanProperty("deprecated")));
+        addField(Translator.localize("label.deprecated"),
+                new UMLDeprecatedCheckBox());
 
-        UMLTextArea2 _see =
-            new UMLTextArea2(new UMLModelElementTaggedValueDocument("see"));
+        UMLTextArea2 _see = new UMLTextArea2(
+                new UMLModelElementTaggedValueDocument("see"));
         _see.setRows(2);
         _see.setLineWrap(true);
         _see.setWrapStyleWord(true);
@@ -123,23 +116,22 @@ public class TabDocumentation extends PropPanel {
         //make new column with LabelledLayout
         add(LabelledLayout.getSeperator());
 
-        UMLTextArea2 _doc =
-            new UMLTextArea2(new UMLModelElementTaggedValueDocument("documentation"));
+        UMLTextArea2 _doc = new UMLTextArea2(
+                new UMLModelElementTaggedValueDocument("documentation"));
 
         _doc.setRows(2);
         _doc.setLineWrap(true);
         _doc.setWrapStyleWord(true);
         JScrollPane spDocs = new JScrollPane();
         spDocs.getViewport().add(_doc);
-        addField(
-		 Translator.localize("label.documentation"),
-		 spDocs);
+        addField(Translator.localize("label.documentation"), spDocs);
     }
 
     /**
-     * Checks if the tabprops should be enabled. Returns true if the target returned
-     * by getTarget is a modelelement or if that target shows up as Fig on the
-     * active diagram and has a modelelement as owner.
+     * Checks if the tabprops should be enabled. Returns true if the target
+     * returned by getTarget is a modelelement or if that target shows up as Fig
+     * on the active diagram and has a modelelement as owner.
+     * 
      * @return true if this tab should be enabled, otherwise false.
      */
     public boolean shouldBeEnabled() {
