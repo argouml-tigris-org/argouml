e551941ad463f8f804527e970e5c9f7a29048b97
Markus Klink <mkl@tigris.org>
2004-05-21 10:13:05 +0000

    implemented a cache system so that subsequent iterations through the factory methods in makeNode can be avoided. Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6005 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ui/CmdCreateNode.java
+++ src_new/org/argouml/ui/CmdCreateNode.java
@@ -3,14 +3,14 @@
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
+// and this paragraph appear in all copies. This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -31,46 +31,51 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
-import org.apache.log4j.Logger;
-
 import javax.swing.Action;
 
+import org.apache.log4j.Logger;
 import org.argouml.i18n.Translator;
 import org.argouml.model.uml.UmlFactory;
 
-/** 
- * Command to create nodes with the appropriate modelelement. The
- * modelelement is initialized via the build methods on the uml
- * factories. If there is no no-parameter build method, the create
- * method corresponding to the modelelement is used.
- *
+/**
+ * Command to create nodes with the appropriate modelelement. The modelelement
+ * is initialized via the build methods on the uml factories. If there is no
+ * no-parameter build method, the create method corresponding to the
+ * modelelement is used.
+ * 
  * @see org.argouml.model.uml.foundation.core.CoreFactory
  * @author jaap.branderhorst@xs4all.nl
  */
 public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
 
-    
-    protected static Logger cat = Logger.getLogger(CmdCreateNode.class);
-    
+    private static Logger LOG = Logger.getLogger(CmdCreateNode.class);
+
+    private static Hashtable cache = new Hashtable();
+
+    private static Object[] emptyParam = new Object[] {};
+
     private static Vector factoryMethods = new Vector();
     static {
         Method[] methodArray = UmlFactory.class.getMethods();
         for (int i = 0; i < methodArray.length; i++) {
             if (methodArray[i].getName().startsWith("get")
-                && !methodArray[i].getName().equals("getFactory")
-                && !methodArray[i].getName().equals("getClass")) {
+                    && !methodArray[i].getName().equals("getFactory")
+                    && !methodArray[i].getName().equals("getClass")) {
                 factoryMethods.add(methodArray[i]);
             }
         }
     }
-    
+
     private static final String ACTION_PREFIX_KEY = "action.new";
-    
+
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param args
+     *            a hastable of arguments
      * @param resource
      * @param name
+     *            the tooltip name
      */
     public CmdCreateNode(Hashtable args, String resource, String name) {
         super(args, resource, name);
@@ -79,6 +84,7 @@ public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
 
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param args
      * @param name
      */
@@ -89,9 +95,13 @@ public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
 
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param nodeClass
+     *            the class for which to create a node, and which to create
+     *            itself
      * @param resource
      * @param name
+     *            the tooltip name
      */
     public CmdCreateNode(Class nodeClass, String resource, String name) {
         super(nodeClass, resource, name);
@@ -100,44 +110,62 @@ public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
 
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param nodeClass
+     *            the class for which to create a node, and which to create
+     *            itself
      * @param name
+     *            the tooltip name
      */
     public CmdCreateNode(Object nodeClass, String name) {
-        super((Class)nodeClass, name);
+        super((Class) nodeClass, name);
         putToolTip(name);
     }
 
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param nodeClass
+     *            the class for which to create a node, and which to create
+     *            itself
      * @param sticky
      * @param resource
      * @param name
+     *            the tooltip name
      */
-    public CmdCreateNode(
-			 Class nodeClass,
-			 boolean sticky,
-			 String resource,
-			 String name) {
+    public CmdCreateNode(Class nodeClass, boolean sticky, String resource,
+            String name) {
         super(nodeClass, sticky, resource, name);
         putToolTip(name);
     }
 
     /**
      * Constructor for CmdCreateNode.
+     * 
      * @param nodeClass
+     *            the class for which to create a node, and which to create
+     *            itself
      * @param sticky
      * @param name
+     *            the tooltip name
      */
     public CmdCreateNode(Object nodeClass, boolean sticky, String name) {
-        super((Class)nodeClass, sticky, name);
+        super((Class) nodeClass, sticky, name);
         putToolTip(name);
     }
 
     /**
-     * Creates a modelelement using the uml model factories.
+     * Creates a modelelement using the uml model factories. If it finds a
+     * suitable match it will but the factory and method of this factory in a
+     * cache, so that subsequent lookups do not have to iterate through all
+     * methods again. If no match is found it will delegate to
+     * <code>super.makeNode()</code>.
+     * 
+     * @return an object which represents in most cases a particular UML
+     *         Element.
+     * 
      * @see org.tigris.gef.graph.GraphFactory#makeNode()
+     * @see org.tigris.gef.base.CmdCreateNode#makeNode()
      */
     public Object makeNode() {
         // here we should implement usage of the factories
@@ -146,55 +174,61 @@ public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
         // factories
 
         try {
+            Object[] cachedParams = (Object[]) cache
+                    .get(_args.get("className"));
+            if (cachedParams != null) {
+                LOG.debug("Using method and factory from cache");
+                return ((Method) cachedParams[1]).invoke(cachedParams[0],
+                        emptyParam);
+            }
             Iterator it = factoryMethods.iterator();
             while (it.hasNext()) {
-                Object factory =
-                    ((Method) it.next())
-		    .invoke(UmlFactory.getFactory(), new Object[] {
-		    });
-                List createMethods =
-                    Arrays.asList(factory.getClass().getMethods());
+                Object factory = ((Method) it.next()).invoke(UmlFactory
+                        .getFactory(), new Object[] {});
+                List createMethods = Arrays.asList(factory.getClass()
+                        .getMethods());
                 Iterator it2 = createMethods.iterator();
                 String classname = getCreateClassName();
                 while (it2.hasNext()) {
                     Method method = (Method) it2.next();
                     String methodname = method.getName();
                     if (methodname.endsWith(classname)
-                        && (methodname
-			    .substring(0, methodname.lastIndexOf(classname))
-			    .equals("build"))
-                        && method.getParameterTypes().length == 0)
-		    {
-			return method.invoke(factory, new Object[] {
-			});
-		    }
+                            && (methodname.substring(0, methodname
+                                    .lastIndexOf(classname)).equals("build"))
+                            && method.getParameterTypes().length == 0) {
+                        LOG.debug("Using method: " + method);
+                        Object[] params = new Object[] { factory, method};
+                        cache.put(_args.get("className"), params);
+                        return method.invoke(factory, emptyParam);
+                    }
                 }
                 it2 = createMethods.iterator();
                 while (it2.hasNext()) {
                     Method method = (Method) it2.next();
                     String methodname = method.getName();
                     if (methodname.endsWith(classname)
-                        && (methodname
-			    .substring(0, methodname.lastIndexOf(classname))
-			    .equals("create")))
-		    {
-                        return method.invoke(factory, new Object[] {
-                        });
+                            && (methodname.substring(0, methodname
+                                    .lastIndexOf(classname)).equals("create"))) {
+                        LOG.debug("Using method: " + method);
+                        Object[] params = new Object[] { factory, method};
+                        cache.put(_args.get("className"), params);
+                        return method.invoke(factory, emptyParam);
                     }
                 }
 
             }
         } catch (IllegalAccessException e2) {
-            cat.error(e2);
+            LOG.error(e2);
         } catch (InvocationTargetException e3) {
-            cat.error(e3);
+            LOG.error(e3);
         }
-
+        LOG.debug("delegating to super.makeNode");
         return super.makeNode();
     }
 
     /**
      * returns the name of the uml modelelement without impl, M or the fullname
+     * 
      * @return String
      */
     private String getCreateClassName() {
@@ -210,8 +244,8 @@ public class CmdCreateNode extends org.tigris.gef.base.CmdCreateNode {
      * Adds tooltip text to the Action.
      */
     private void putToolTip(String name) {
-        putValue(Action.SHORT_DESCRIPTION, 
-		Translator.localize(ACTION_PREFIX_KEY) + " " 
-		+ Translator.localize(name));
+        putValue(Action.SHORT_DESCRIPTION, Translator
+                .localize(ACTION_PREFIX_KEY)
+                + " " + Translator.localize(name));
     }
 }
