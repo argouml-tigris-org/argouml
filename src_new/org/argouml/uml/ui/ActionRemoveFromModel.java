138af066b5254481c69100dbaeedcc0bbf4f2192
Michiel van der Wulp <mvw@tigris.org>
2004-06-12 06:22:46 +0000

    Solved Issue 2655.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6063 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/ActionRemoveFromModel.java
+++ src_new/org/argouml/uml/ui/ActionRemoveFromModel.java
@@ -37,6 +37,7 @@ import org.argouml.i18n.Translator;
 import org.argouml.kernel.Project;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.ModelFacade;
+import org.argouml.model.uml.behavioralelements.statemachines.StateMachinesHelper;
 import org.argouml.ui.ProjectBrowser;
 import org.argouml.ui.targetmanager.TargetManager;
 import org.argouml.uml.diagram.ui.UMLDiagram;
@@ -118,6 +119,11 @@ public class ActionRemoveFromModel extends UMLChangeAction {
 			     .getModel())) {
             return false;
         }
+        if (StateMachinesHelper.getHelper().isTopState(target))
+            /* we can not delete a "top" state, 
+             * it comes and goes with the statemachine. Issue 2655.
+             */
+            return false;
         return target != null;
     }
 
