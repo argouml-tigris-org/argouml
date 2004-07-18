65bbd3246c5b5930dc8d0d1dfd7baec37760cef6
Markus Klink <mkl@tigris.org>
2004-07-18 14:55:24 +0000

    fixed style issues Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6215 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/SelectionNodeClarifiers.java
+++ org/argouml/uml/diagram/ui/SelectionNodeClarifiers.java
@@ -3,14 +3,14 @@
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
+// and this paragraph appear in all copies. This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -40,16 +40,18 @@ public class SelectionNodeClarifiers extends SelectionResize {
     // constructors
 
     /** Construct a new SelectionNodeClarifiers for the given Fig */
-    public SelectionNodeClarifiers(Fig f) { super(f); }
-
-    /** Paint the handles at the four corners and midway along each edge
-     * of the bounding box.  */
-    public void paint(Graphics g) {
-	((FigNodeModelElement) _content).paintClarifiers(g);
-	super.paint(g);
+    public SelectionNodeClarifiers(Fig f) {
+        super(f);
     }
 
-  
-  
+    /**
+     * Paint the handles at the four corners and midway along each edge of the
+     * bounding box.
+     */
+    public void paint(Graphics g) {
+        ((FigNodeModelElement) _content).paintClarifiers(g);
+        super.paint(g);
+    }
+
 } /* end class SelectionNodeClarifiers */
 
