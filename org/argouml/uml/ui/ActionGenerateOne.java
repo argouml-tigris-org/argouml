ffad16e5f34c9f918676eee7cb1408e44e7cced0
Michael Stockman <d00mst@tigris.org>
2004-07-20 01:37:22 +0000

    Renamed variables named 'enum' in comments (should they ever become code again).
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6236 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/ActionGenerateOne.java
+++ org/argouml/uml/ui/ActionGenerateOne.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -72,10 +72,10 @@ public class ActionGenerateOne extends UMLAction {
         /*
         Editor ce = org.tigris.gef.base.Globals.curEditor();
         Vector sels = ce.getSelectionManager().getFigs();
-        java.util.Enumeration enum = sels.elements();
+        Enumeration figs = sels.elements();
         Vector classes = new Vector();
-        while (enum.hasMoreElements()) {
-        Fig f = (Fig)enum.nextElement();
+        while (figs.hasMoreElements()) {
+        Fig f = (Fig) figs.nextElement();
         Object owner = f.getOwner();
         if (!(owner instanceof MClass) && !(owner instanceof MInterface))
         continue;
@@ -114,9 +114,9 @@ public class ActionGenerateOne extends UMLAction {
         Editor ce = org.tigris.gef.base.Globals.curEditor();
         if(ce != null) {
             Vector sels = ce.getSelectionManager().getFigs();
-            java.util.Enumeration enum = sels.elements();
-            while (enum.hasMoreElements()) {
-        	Fig f = (Fig) enum.nextElement();
+            java.util.Enumeration figs = sels.elements();
+            while (figs.hasMoreElements()) {
+        	Fig f = (Fig) figs.nextElement();
         	Object owner = f.getOwner();
         	if (!(owner instanceof MClass) && !(owner instanceof MInterface))
         	    continue;
