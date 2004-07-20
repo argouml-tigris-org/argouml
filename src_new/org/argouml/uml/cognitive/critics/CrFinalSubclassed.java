709aa4cdff558e321423d2b1edf008b25c4ec904
Michael Stockman <d00mst@tigris.org>
2004-07-20 00:38:33 +0000

    Renamed variables named 'enum'. Adapted style.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6233 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/cognitive/critics/CrFinalSubclassed.java
+++ src_new/org/argouml/uml/cognitive/critics/CrFinalSubclassed.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2003 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -22,8 +22,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-
-
 // File: CrFinalSubclassed.java
 // Classes: CrFinalSubclassed
 // Original Author: jrobbins@ics.uci.edu
@@ -36,20 +34,16 @@ import org.argouml.cognitive.Designer;
 import org.argouml.cognitive.critics.Critic;
 import org.argouml.model.ModelFacade;
 
-
 // Use model through ModelFacade
 
 /** Well-formedness rule [2] for MGeneralizableElement. See page 31 of UML 1.1
  *  Semantics. OMG document ad/97-08-04. 
  *  In UML 1.3 it is rule [2] in section 2.5.3.18 page 2-54.
  */
-
-
 public class CrFinalSubclassed extends CrUML {
 
     public CrFinalSubclassed() {
 	setHeadline("Remove final keyword or remove subclasses");
-
 	addSupportedDecision(CrUML.decINHERITANCE);
 	setKnowledgeTypes(Critic.KT_SEMANTICS);
 	addTrigger("specialization");
@@ -57,11 +51,16 @@ public class CrFinalSubclassed extends CrUML {
     }
 
     public boolean predicate2(Object dm, Designer dsgr) {
-	if (!(ModelFacade.isAGeneralizableElement(dm))) return NO_PROBLEM;
-	if (!(ModelFacade.isLeaf(dm))) return NO_PROBLEM;
-	Iterator enum = ModelFacade.getSpecializations(dm).iterator();
-	if (enum.hasNext()) return PROBLEM_FOUND;
-	return NO_PROBLEM;
+	if (!ModelFacade.isAGeneralizableElement(dm)) {
+	    return NO_PROBLEM;
+	}
+
+	if (!ModelFacade.isLeaf(dm)) {
+	    return NO_PROBLEM;
+	}
+
+	Iterator specs = ModelFacade.getSpecializations(dm).iterator();
+	return specs.hasNext() ? PROBLEM_FOUND : NO_PROBLEM;
     }
 
 } /* end class CrFinalSubclassed.java */
