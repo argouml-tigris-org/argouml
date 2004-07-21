7d97eedb292ce5def3df1af3e7c51ed92d3de48d
Markus Klink <mkl@tigris.org>
2004-07-21 18:35:34 +0000

    one step forward to make the todo list by offender update correctly. It was not even implementing the ToDoListListener Interface.
    Now why does it still not update?
    Issue number:  2435
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6248 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/cognitive/ui/ToDoByOffender.java
+++ src_new/org/argouml/cognitive/ui/ToDoByOffender.java
@@ -3,14 +3,14 @@
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies.  This software program and
+// and this paragraph appear in all copies. This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -26,127 +26,127 @@ package org.argouml.cognitive.ui;
 
 import java.util.Enumeration;
 import java.util.Vector;
+
 import org.apache.log4j.Logger;
-
-
 import org.argouml.cognitive.Designer;
 import org.argouml.cognitive.ToDoItem;
 import org.argouml.cognitive.ToDoListEvent;
+import org.argouml.cognitive.ToDoListListener;
 import org.tigris.gef.util.VectorSet;
 
-public class ToDoByOffender extends ToDoPerspective {
-    protected static Logger cat = 
-        Logger.getLogger(ToDoByOffender.class);
+public class ToDoByOffender extends ToDoPerspective implements ToDoListListener {
+
+    protected static Logger cat = Logger.getLogger(ToDoByOffender.class);
 
     public ToDoByOffender() {
-	super("combobox.todo-perspective-offender");
-	addSubTreeModel(new GoListToOffenderToItem());
+        super("combobox.todo-perspective-offender");
+        addSubTreeModel(new GoListToOffenderToItem());
     }
 
     ////////////////////////////////////////////////////////////////
     // ToDoListListener implementation
 
     public void toDoItemsChanged(ToDoListEvent tde) {
-	cat.debug("toDoItemsChanged");
-	Vector items = tde.getToDoItems();
-	int nItems = items.size();
-	Object path[] = new Object[2];
-	path[0] = Designer.TheDesigner.getToDoList();
+        cat.debug("toDoItemsChanged");
+        Vector items = tde.getToDoItems();
+        int nItems = items.size();
+        Object path[] = new Object[2];
+        path[0] = Designer.TheDesigner.getToDoList();
 
-	VectorSet allOffenders =
-	    Designer.TheDesigner.getToDoList().getOffenders();
-	Enumeration elems = allOffenders.elements();
-	while (elems.hasMoreElements()) {
-	    Object off = elems.nextElement();
-	    path[1] = off;
-	    int nMatchingItems = 0;
-	    for (int i = 0; i < nItems; i++) {
-		ToDoItem item = (ToDoItem) items.elementAt(i);
-		VectorSet offenders = item.getOffenders();
-		if (!offenders.contains(off)) continue;
-		nMatchingItems++;
-	    }
-	    if (nMatchingItems == 0) continue;
-	    int childIndices[] = new int[nMatchingItems];
-	    Object children[] = new Object[nMatchingItems];
-	    nMatchingItems = 0;
-	    for (int i = 0; i < nItems; i++) {
-		ToDoItem item = (ToDoItem) items.elementAt(i);
-		VectorSet offenders = item.getOffenders();
-		if (!offenders.contains(off)) continue;
-		childIndices[nMatchingItems] = getIndexOfChild(off, item);
-		children[nMatchingItems] = item;
-		nMatchingItems++;
-	    }
-	    fireTreeNodesChanged(this, path, childIndices, children);
-	}
+        VectorSet allOffenders = Designer.TheDesigner.getToDoList()
+                .getOffenders();
+        Enumeration elems = allOffenders.elements();
+        while (elems.hasMoreElements()) {
+            Object off = elems.nextElement();
+            path[1] = off;
+            int nMatchingItems = 0;
+            for (int i = 0; i < nItems; i++) {
+                ToDoItem item = (ToDoItem) items.elementAt(i);
+                VectorSet offenders = item.getOffenders();
+                if (!offenders.contains(off)) continue;
+                nMatchingItems++;
+            }
+            if (nMatchingItems == 0) continue;
+            int childIndices[] = new int[nMatchingItems];
+            Object children[] = new Object[nMatchingItems];
+            nMatchingItems = 0;
+            for (int i = 0; i < nItems; i++) {
+                ToDoItem item = (ToDoItem) items.elementAt(i);
+                VectorSet offenders = item.getOffenders();
+                if (!offenders.contains(off)) continue;
+                childIndices[nMatchingItems] = getIndexOfChild(off, item);
+                children[nMatchingItems] = item;
+                nMatchingItems++;
+            }
+            fireTreeNodesChanged(this, path, childIndices, children);
+        }
     }
 
     public void toDoItemsAdded(ToDoListEvent tde) {
-	cat.debug("toDoItemAdded");
-	Vector items = tde.getToDoItems();
-	int nItems = items.size();
-	Object path[] = new Object[2];
-	path[0] = Designer.TheDesigner.getToDoList();
+        cat.debug("toDoItemAdded");
+        Vector items = tde.getToDoItems();
+        int nItems = items.size();
+        Object path[] = new Object[2];
+        path[0] = Designer.TheDesigner.getToDoList();
 
-	VectorSet allOffenders =
-	    Designer.TheDesigner.getToDoList().getOffenders();
-	Enumeration elems = allOffenders.elements();
-	while (elems.hasMoreElements()) {
-	    Object off = elems.nextElement();
-	    path[1] = off;
-	    int nMatchingItems = 0;
-	    for (int i = 0; i < nItems; i++) {
-		ToDoItem item = (ToDoItem) items.elementAt(i);
-		VectorSet offenders = item.getOffenders();
-		if (!offenders.contains(off)) continue;
-		nMatchingItems++;
-	    }
-	    if (nMatchingItems == 0) continue;
-	    int childIndices[] = new int[nMatchingItems];
-	    Object children[] = new Object[nMatchingItems];
-	    nMatchingItems = 0;
-	    for (int i = 0; i < nItems; i++) {
-		ToDoItem item = (ToDoItem) items.elementAt(i);
-		VectorSet offenders = item.getOffenders();
-		if (!offenders.contains(off)) continue;
-		childIndices[nMatchingItems] = getIndexOfChild(off, item);
-		children[nMatchingItems] = item;
-		nMatchingItems++;
-	    }
-	    fireTreeNodesInserted(this, path, childIndices, children);
-	}
+        VectorSet allOffenders = Designer.TheDesigner.getToDoList()
+                .getOffenders();
+        Enumeration elems = allOffenders.elements();
+        while (elems.hasMoreElements()) {
+            Object off = elems.nextElement();
+            path[1] = off;
+            int nMatchingItems = 0;
+            for (int i = 0; i < nItems; i++) {
+                ToDoItem item = (ToDoItem) items.elementAt(i);
+                VectorSet offenders = item.getOffenders();
+                if (!offenders.contains(off)) continue;
+                nMatchingItems++;
+            }
+            if (nMatchingItems == 0) continue;
+            int childIndices[] = new int[nMatchingItems];
+            Object children[] = new Object[nMatchingItems];
+            nMatchingItems = 0;
+            for (int i = 0; i < nItems; i++) {
+                ToDoItem item = (ToDoItem) items.elementAt(i);
+                VectorSet offenders = item.getOffenders();
+                if (!offenders.contains(off)) continue;
+                childIndices[nMatchingItems] = getIndexOfChild(off, item);
+                children[nMatchingItems] = item;
+                nMatchingItems++;
+            }
+            fireTreeNodesInserted(this, path, childIndices, children);
+        }
     }
 
     public void toDoItemsRemoved(ToDoListEvent tde) {
-	cat.debug("toDoItemRemoved");
-	Vector items = tde.getToDoItems();
-	int nItems = items.size();
-	Object path[] = new Object[2];
-	path[0] = Designer.TheDesigner.getToDoList();
+        cat.debug("toDoItemRemoved");
+        Vector items = tde.getToDoItems();
+        int nItems = items.size();
+        Object path[] = new Object[2];
+        path[0] = Designer.TheDesigner.getToDoList();
 
-	VectorSet allOffenders =
-	    Designer.TheDesigner.getToDoList().getOffenders();
-	Enumeration elems = allOffenders.elements();
-	while (elems.hasMoreElements()) {
-	    Object off = elems.nextElement();
-	    //       boolean anyInOff = false;
-	    //       for (int i = 0; i < nItems; i++) {
-	    // 	ToDoItem item = (ToDoItem) items.elementAt(i);
-	    // 	VectorSet offenders = item.getOffenders();
-	    // 	if (offenders.contains(off)) anyInOff = true;
-	    //       }
-	    //       if (!anyInOff) continue;
+        VectorSet allOffenders = Designer.TheDesigner.getToDoList()
+                .getOffenders();
+        Enumeration elems = allOffenders.elements();
+        while (elems.hasMoreElements()) {
+            Object off = elems.nextElement();
+            //       boolean anyInOff = false;
+            //       for (int i = 0; i < nItems; i++) {
+            // 	ToDoItem item = (ToDoItem) items.elementAt(i);
+            // 	VectorSet offenders = item.getOffenders();
+            // 	if (offenders.contains(off)) anyInOff = true;
+            //       }
+            //       if (!anyInOff) continue;
 
-	    cat.debug("toDoItemRemoved updating PriorityNode");
-	    path[1] = off;
-	    //fireTreeNodesChanged(this, path, childIndices, children);
-	    fireTreeStructureChanged(path);
-	}
+            cat.debug("toDoItemRemoved updating PriorityNode");
+            path[1] = off;
+            //fireTreeNodesChanged(this, path, childIndices, children);
+            fireTreeStructureChanged(path);
+        }
     }
 
-    public void toDoListChanged(ToDoListEvent tde) { }  
+    public void toDoListChanged(ToDoListEvent tde) {
+    }
 
 } /* end class ToDoByOffender */
-
 
