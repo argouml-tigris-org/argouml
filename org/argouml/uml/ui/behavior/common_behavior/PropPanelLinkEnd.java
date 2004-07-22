e3fd8f91d109d3f3c61d0706e6a40d282d3430f6
Markus Klink <mkl@tigris.org>
2004-07-22 11:13:04 +0000

    Property Panel for LinkEnds Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6252 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/ui/behavior/common_behavior/PropPanelLinkEnd.java
@@ -0,0 +1,55 @@
+// $Id$
+// Copyright (c) 1996-2004 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+// File: PropPanelLinkEnd.java
+// Classes: PropPanelLinkEnd
+
+package org.argouml.uml.ui.behavior.common_behavior;
+
+import org.argouml.i18n.Translator;
+import org.argouml.uml.ui.UMLComboBoxNavigator;
+import org.argouml.uml.ui.foundation.core.PropPanelModelElement;
+import org.argouml.util.ConfigLoader;
+
+/**
+ * A basic property panel for link ends. TODO: uses the associationEnd icon
+ * 
+ * @author mkl
+ *  
+ */
+public class PropPanelLinkEnd extends PropPanelModelElement {
+
+    public PropPanelLinkEnd() {
+        super("Linkend", _assocEndIcon, ConfigLoader.getTabPropsOrientation());
+        addField(Translator.localize("UMLMenu", "label.name"),
+                getNameTextField());
+        addField(Translator.localize("UMLMenu", "label.stereotype"),
+                new UMLComboBoxNavigator(this, Translator.localize("UMLMenu",
+                        "tooltip.nav-stereo"), getStereotypeComboBox()));
+        addField(Translator.localize("UMLMenu", "label.namespace"),
+                getNamespaceScroll());
+        addSeperator();
+    }
+
+} /* end class PropPanelLinkEnd */
