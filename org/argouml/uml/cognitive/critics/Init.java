5a7d7a57030ca39d03ceea0979316657500aa0b9
Markus Klink <mkl@tigris.org>
2004-07-22 15:36:08 +0000

    changed signature of critic attributes Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6256 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/cognitive/critics/Init.java
+++ org/argouml/uml/cognitive/critics/Init.java
@@ -56,218 +56,205 @@ import org.argouml.model.ModelFacade;
 public class Init {
 
     // UML specific
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crAssocNameConflict = new CrAssocNameConflict();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crAttrNameConflict = new CrAttrNameConflict();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crOperNameConflict = new CrOperNameConflict();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crCircularAssocClass = new CrCircularAssocClass();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crCircularInheritance = new CrCircularInheritance();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crCircularComposition = new CrCircularComposition();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crCrossNamespaceAssoc = new CrCrossNamespaceAssoc();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crDupParamName = new CrDupParamName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crDupRoleNames = new CrDupRoleNames();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crFinalSubclassed = new CrFinalSubclassed();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crIllegalGeneralization =
+
+    private static Critic crAssocNameConflict = new CrAssocNameConflict();
+
+    private static Critic crAttrNameConflict = new CrAttrNameConflict();
+
+    private static Critic crOperNameConflict = new CrOperNameConflict();
+
+    private static Critic crCircularAssocClass = new CrCircularAssocClass();
+
+    private static Critic crCircularInheritance = new CrCircularInheritance();
+
+    private static Critic crCircularComposition = new CrCircularComposition();
+
+    private static Critic crCrossNamespaceAssoc = new CrCrossNamespaceAssoc();
+
+    private static Critic crDupParamName = new CrDupParamName();
+
+    private static Critic crDupRoleNames = new CrDupRoleNames();
+
+    private static Critic crFinalSubclassed = new CrFinalSubclassed();
+
+    private static Critic crIllegalGeneralization =
 	new CrIllegalGeneralization();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crAlreadyRealizes = new CrAlreadyRealizes();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInterfaceAllPublic = new CrInterfaceAllPublic();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInterfaceOperOnly = new CrInterfaceOperOnly();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMultipleAgg = new CrMultipleAgg();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNWayAgg = new CrNWayAgg();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNavFromInterface = new CrNavFromInterface();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crUnnavigableAssoc = new CrUnnavigableAssoc();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNameConflictAC = new CrNameConflictAC();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMissingClassName = new CrMissingClassName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMissingAttrName = new CrMissingAttrName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMissingOperName = new CrMissingOperName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMissingStateName = new CrMissingStateName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoInstanceVariables = new CrNoInstanceVariables();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoAssociations = new CrNoAssociations();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNonAggDataType = new CrNonAggDataType();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crOppEndConflict = new CrOppEndConflict();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crUselessAbstract = new CrUselessAbstract();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crUselessInterface = new CrUselessInterface();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crDisambigClassName = new CrDisambigClassName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crDisambigStateName = new CrDisambigStateName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crConflictingComposites =
+
+    private static Critic crAlreadyRealizes = new CrAlreadyRealizes();
+
+    private static Critic crInterfaceAllPublic = new CrInterfaceAllPublic();
+
+    private static Critic crInterfaceOperOnly = new CrInterfaceOperOnly();
+
+    private static Critic crMultipleAgg = new CrMultipleAgg();
+
+    private static Critic crNWayAgg = new CrNWayAgg();
+
+    private static Critic crNavFromInterface = new CrNavFromInterface();
+
+    private static Critic crUnnavigableAssoc = new CrUnnavigableAssoc();
+
+    private static Critic crNameConflictAC = new CrNameConflictAC();
+
+    private static Critic crMissingClassName = new CrMissingClassName();
+
+    private static Critic crMissingAttrName = new CrMissingAttrName();
+
+    private static Critic crMissingOperName = new CrMissingOperName();
+
+    private static Critic crMissingStateName = new CrMissingStateName();
+
+    private static Critic crNoInstanceVariables = new CrNoInstanceVariables();
+
+    private static Critic crNoAssociations = new CrNoAssociations();
+
+    private static Critic crNonAggDataType = new CrNonAggDataType();
+
+    private static Critic crOppEndConflict = new CrOppEndConflict();
+
+    private static Critic crUselessAbstract = new CrUselessAbstract();
+
+    private static Critic crUselessInterface = new CrUselessInterface();
+
+    private static Critic crDisambigClassName = new CrDisambigClassName();
+
+    private static Critic crDisambigStateName = new CrDisambigStateName();
+
+    private static Critic crConflictingComposites =
 	new CrConflictingComposites();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyAssoc = new CrTooManyAssoc();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyAttr = new CrTooManyAttr();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyOper = new CrTooManyOper();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyStates = new CrTooManyStates();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyTransitions = new CrTooManyTransitions();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crTooManyClasses = new CrTooManyClasses();
+    private static Critic crTooManyAssoc = new CrTooManyAssoc();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoTransitions = new CrNoTransitions();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoIncomingTransitions =
+    private static Critic crTooManyAttr = new CrTooManyAttr();
+
+    private static Critic crTooManyOper = new CrTooManyOper();
+
+    private static Critic crTooManyStates = new CrTooManyStates();
+
+    private static Critic crTooManyTransitions = new CrTooManyTransitions();
+
+    private static Critic crTooManyClasses = new CrTooManyClasses();
+
+    private static Critic crNoTransitions = new CrNoTransitions();
+
+    private static Critic crNoIncomingTransitions =
 	new CrNoIncomingTransitions();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoOutgoingTransitions =
+
+    private static Critic crNoOutgoingTransitions =
 	new CrNoOutgoingTransitions();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMultipleInitialStates =
+
+    private static Critic crMultipleInitialStates =
 	new CrMultipleInitialStates();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoInitialState = new CrNoInitialState();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoTriggerOrGuard = new CrNoTriggerOrGuard();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoGuard = new CrNoGuard();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInvalidFork = new CrInvalidFork();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInvalidJoin = new CrInvalidJoin();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInvalidBranch = new CrInvalidBranch();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crEmptyPackage = new CrEmptyPackage();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNoOperations = new CrNoOperations();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crConstructorNeeded = new CrConstructorNeeded();
+    private static Critic crNoInitialState = new CrNoInitialState();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNameConfusion = new CrNameConfusion();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMergeClasses = new CrMergeClasses();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crSubclassReference = new CrSubclassReference();
+    private static Critic crNoTriggerOrGuard = new CrNoTriggerOrGuard();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crComponentWithoutNode = new CrComponentWithoutNode();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crCompInstanceWithoutNode =
+    private static Critic crNoGuard = new CrNoGuard();
+
+    private static Critic crInvalidFork = new CrInvalidFork();
+
+    private static Critic crInvalidJoin = new CrInvalidJoin();
+
+    private static Critic crInvalidBranch = new CrInvalidBranch();
+
+    private static Critic crEmptyPackage = new CrEmptyPackage();
+
+    private static Critic crNoOperations = new CrNoOperations();
+
+    private static Critic crConstructorNeeded = new CrConstructorNeeded();
+
+    private static Critic crNameConfusion = new CrNameConfusion();
+
+    private static Critic crMergeClasses = new CrMergeClasses();
+
+    private static Critic crSubclassReference = new CrSubclassReference();
+
+    private static Critic crComponentWithoutNode = new CrComponentWithoutNode();
+
+    private static Critic crCompInstanceWithoutNode =
 	new CrCompInstanceWithoutNode();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crClassWithoutComponent =
+
+    private static Critic crClassWithoutComponent =
 	new CrClassWithoutComponent();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInterfaceWithoutComponent =
+
+    private static Critic crInterfaceWithoutComponent =
 	new CrInterfaceWithoutComponent();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crObjectWithoutComponent =
+
+    private static Critic crObjectWithoutComponent =
 	new CrObjectWithoutComponent();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNodeInsideElement = new CrNodeInsideElement();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNodeInstanceInsideElement =
+
+    private static Critic crNodeInsideElement = new CrNodeInsideElement();
+
+    private static Critic crNodeInstanceInsideElement =
 	new CrNodeInstanceInsideElement();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crWrongLinkEnds = new CrWrongLinkEnds();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crInstanceWithoutClassifier =
+
+    private static Critic crWrongLinkEnds = new CrWrongLinkEnds();
+
+    private static Critic crInstanceWithoutClassifier =
 	new CrInstanceWithoutClassifier();
 
-    // public static Critic crCallWithoutReturn = new CrCallWithoutReturn();
-    // public static Critic crReturnWithoutCall = new CrReturnWithoutCall(); 
-    // public static Critic crSeqInstanceWithoutClassifier =
+    // private static Critic crCallWithoutReturn = new CrCallWithoutReturn();
+    // private static Critic crReturnWithoutCall = new CrReturnWithoutCall(); 
+    // private static Critic crSeqInstanceWithoutClassifier =
 	// new CrSeqInstanceWithoutClassifier();
-    // public static Critic crStimulusWithWrongPosition =
+    // private static Critic crStimulusWithWrongPosition =
 	// new CrStimulusWithWrongPosition();
 
     // from UML 1.1 Semantics spec
 
     // common coding conventions
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic
+    private static Critic
 	crUnconventionalOperName = new CrUnconventionalOperName();
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic
+    private static Critic
 	crUnconventionalAttrName = new CrUnconventionalAttrName(); 
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic
+    private static Critic
 	crUnconventionalClassName = new CrUnconventionalClassName(); 
 
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic
+    private static Critic
 	crUnconventionalPackName = new CrUnconventionalPackName(); 
 
     // Java specific
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crClassMustBeAbstract = new CrClassMustBeAbstract();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crReservedName = new CrReservedName();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMultiInherit = new CrMultipleInheritance();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crMultiRealization = new CrMultipleRealization();
+    private static Critic crClassMustBeAbstract = new CrClassMustBeAbstract();
+
+    private static Critic crReservedName = new CrReservedName();
+
+    private static Critic crMultiInherit = new CrMultipleInheritance();
+
+    private static Critic crMultiRealization = new CrMultipleRealization();
+
     // code generation
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crIllegalName = new CrIllegalName();
+    private static Critic crIllegalName = new CrIllegalName();
 
     // Pattern specific
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crConsiderSingleton = new CrConsiderSingleton();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crSingletonViolatedMSA =
+    private static Critic crConsiderSingleton = new CrConsiderSingleton();
+
+    private static Critic crSingletonViolatedMSA =
 	new CrSingletonViolatedMissingStaticAttr();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crSingletonViolatedOPC =
+
+    private static Critic crSingletonViolatedOPC =
 	new CrSingletonViolatedOnlyPrivateConstructors();
 
     // Presentation critics
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crNodesOverlap = new CrNodesOverlap();
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static Critic crZeroLengthEdge = new CrZeroLengthEdge();
+    private static Critic crNodesOverlap = new CrNodesOverlap();
+
+    private static Critic crZeroLengthEdge = new CrZeroLengthEdge();
 
 
     // Compound critics
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static CompoundCritic clsNaming =
+    private static CompoundCritic clsNaming =
 	new CompoundCritic(crMissingClassName, crDisambigClassName);
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static CompoundCritic noTrans1 =
+
+    private static CompoundCritic noTrans1 =
 	new CompoundCritic(crNoTransitions, crNoIncomingTransitions);
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static CompoundCritic noTrans2 =
+    
+    private static CompoundCritic noTrans2 =
 	new CompoundCritic(crNoTransitions, crNoOutgoingTransitions);
+
     // only classes with name need a constructor
-    /** @deprecated as of 0.15.3 to be made private. */
-    public static CompoundCritic crCompoundConstructorNeeded =
+    private static CompoundCritic crCompoundConstructorNeeded =
         new CompoundCritic(crMissingClassName, crConstructorNeeded);
 
     /** static initializer, register all appropriate critics */
