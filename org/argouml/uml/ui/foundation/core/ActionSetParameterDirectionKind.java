cbe6eaa54d687c063d7400e5c806fd5486ccbe31
Markus Klink <mkl@tigris.org>
2004-07-25 09:14:37 +0000

    revised PropPanelParameter Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6289 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/ui/foundation/core/ActionSetParameterDirectionKind.java
@@ -0,0 +1,87 @@
+// $Id$
+// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.ui.foundation.core;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JRadioButton;
+
+import org.argouml.i18n.Translator;
+import org.argouml.model.ModelFacade;
+import org.argouml.uml.ui.UMLChangeAction;
+import org.argouml.uml.ui.UMLRadioButtonPanel;
+
+/**
+ * An action to set the concurrency of an operation.
+ * 
+ * @author mkl
+ *  
+ */
+public class ActionSetParameterDirectionKind extends UMLChangeAction {
+
+    public static final ActionSetParameterDirectionKind SINGLETON = new ActionSetParameterDirectionKind();
+
+    public final static String IN_COMMAND = "in";
+
+    public final static String OUT_COMMAND = "out";
+
+    public final static String INOUT_COMMAND = "inout";
+
+    public final static String RETURN_COMMAND = "return";
+
+    /**
+     * Constructor for ActionSetElementOwnershipSpecification.
+     */
+    protected ActionSetParameterDirectionKind() {
+        super(Translator.localize("Set"), true, NO_ICON);
+    }
+
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e) {
+        super.actionPerformed(e);
+        if (e.getSource() instanceof JRadioButton) {
+            JRadioButton source = (JRadioButton) e.getSource();
+            String actionCommand = source.getActionCommand();
+            Object target = ((UMLRadioButtonPanel) source.getParent())
+                    .getTarget();
+            if (org.argouml.model.ModelFacade.isAParameter(target)) {
+                Object m = /* (MModelElement) */target;
+                Object kind = null;
+                if (actionCommand.equals(IN_COMMAND)) {
+                    kind = ModelFacade.IN_PARAMETERDIRECTIONKIND;
+                } else if (actionCommand.equals(OUT_COMMAND)) {
+                    kind = ModelFacade.OUT_PARAMETERDIRECTIONKIND;
+                } else if (actionCommand.equals(INOUT_COMMAND)) {
+                    kind = ModelFacade.INOUT_PARAMETERDIRECTIONKIND;
+                } else {
+                    kind = ModelFacade.RETURN_PARAMETERDIRECTIONKIND;
+                }
+                ModelFacade.setKind(m, kind);
+            }
+        }
+    }
+}
