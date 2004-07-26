9133c63a3d3eb38ceec78afc161bbe335ee014dc
Michiel van der Wulp <mvw@tigris.org>
2004-07-26 18:16:37 +0000

    Style issues. Privatized stuff.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6322 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/state/ui/FigForkState.java
+++ org/argouml/uml/diagram/state/ui/FigForkState.java
@@ -45,105 +45,147 @@ public class FigForkState extends FigStateVertex {
     ////////////////////////////////////////////////////////////////
     // constants
 
-    public static final int MARGIN = 2;
+    private static final int MARGIN = 2;
 
-    public static final int X = 10;
+    private static final int X = 10;
 
-    public static final int Y = 10;
+    private static final int Y = 10;
 
-    public static final int WIDTH = 80;
+    private static final int WIDTH = 80;
 
-    public static final int HEIGHT = 9;
+    private static final int HEIGHT = 9;
 
     ////////////////////////////////////////////////////////////////
     // instance variables
 
-    FigRect _head;
+    private FigRect bigPort;
+    private FigRect head;
 
     ////////////////////////////////////////////////////////////////
     // constructors
 
+    /**
+     * The main constructor.
+     */
     public FigForkState() {
-        _bigPort = new FigRect(X, Y, WIDTH, HEIGHT, Color.cyan, Color.cyan);
-        _head = new FigRect(X, Y, WIDTH, HEIGHT, Color.black, Color.black);
+        bigPort = new FigRect(X, Y, WIDTH, HEIGHT, Color.cyan, Color.cyan);
+        head = new FigRect(X, Y, WIDTH, HEIGHT, Color.black, Color.black);
         // add Figs to the FigNode in back-to-front order
-        addFig(_bigPort);
-        addFig(_head);
+        addFig(bigPort);
+        addFig(head);
 
         setBlinkPorts(false); //make port invisble unless mouse enters
         Rectangle r = getBounds();
     }
 
+    /**
+     * The constructor which links the Fig into the existing UML element 
+     * @param gm ignored
+     * @param node the UML element
+     */
     public FigForkState(GraphModel gm, Object node) {
         this();
         setOwner(node);
     }
 
+    /**
+     * @see java.lang.Object#clone()
+     */
     public Object clone() {
         FigForkState figClone = (FigForkState) super.clone();
         Iterator it = figClone.getFigs(null).iterator();
-        figClone._bigPort = (FigRect) it.next();
-        figClone._head = (FigRect) it.next();
+        figClone.bigPort = (FigRect) it.next();
+        figClone.head = (FigRect) it.next();
         return figClone;
     }
 
     ////////////////////////////////////////////////////////////////
     // Fig accessors
 
-    /** Initial states are fixed size. */
-    //public boolean isResizable() { return false; }
-    /* Override setBounds to keep shapes looking right */
+    /** Override setBounds to keep shapes looking right 
+     * @see org.tigris.gef.presentation.Fig#setBounds(int, int, int, int)
+     */
     public void setBounds(int x, int y, int w, int h) {
         Rectangle oldBounds = getBounds();
         if (w > h)
             h = 9;
         else
             w = 9;
-        _bigPort.setBounds(x, y, w, h);
-        _head.setBounds(x, y, w, h);
+        bigPort.setBounds(x, y, w, h);
+        head.setBounds(x, y, w, h);
 
         calcBounds(); //_x = x; _y = y; _w = w; _h = h;
         updateEdges();
         firePropChange("bounds", oldBounds, getBounds());
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#setLineColor(java.awt.Color)
+     */
     public void setLineColor(Color col) {
-        _head.setLineColor(col);
+        head.setLineColor(col);
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#getLineColor()
+     */
     public Color getLineColor() {
-        return _head.getLineColor();
+        return head.getLineColor();
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#setFillColor(java.awt.Color)
+     */
     public void setFillColor(Color col) {
-        _head.setFillColor(col);
+        head.setFillColor(col);
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#getFillColor()
+     */
     public Color getFillColor() {
-        return _head.getFillColor();
+        return head.getFillColor();
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#setFilled(boolean)
+     */
     public void setFilled(boolean f) {
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#getFilled()
+     */
     public boolean getFilled() {
         return true;
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#setLineWidth(int)
+     */
     public void setLineWidth(int w) {
-        _head.setLineWidth(w);
+        head.setLineWidth(w);
     }
 
+    /**
+     * @see org.tigris.gef.presentation.Fig#getLineWidth()
+     */
     public int getLineWidth() {
-        return _head.getLineWidth();
+        return head.getLineWidth();
     }
 
     ////////////////////////////////////////////////////////////////
     // Event handlers
 
+    /**
+     * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+     */
     public void mouseClicked(MouseEvent me) {
     }
 
+    /**
+     * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+     */
     public void keyPressed(KeyEvent ke) {
     }
 
