336c877a0568436e3592c57912a59fe22bcae106
Bob Tarling <bobtarling@tigris.org>
2004-08-05 19:45:58 +0000

    Specialist figs for diaplying names, stereotypes and the combination of the two.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6404 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src_new/org/argouml/uml/diagram/ui/FigNameCompartment.java
@@ -0,0 +1,35 @@
+package org.argouml.uml.diagram.ui;
+
+import org.argouml.uml.diagram.static_structure.ui.FigCompartment;
+
+/**
+ * A specialist for displaying the model element name and stereotype.
+ * @author Bob Tarling
+ */
+public class FigNameCompartment extends FigCompartment {
+    
+    private FigStereotype stereotype;
+    private FigName name;
+    public FigNameCompartment(int x, int y, int w, int h, boolean expandOnly) {
+        stereotype = new FigStereotype(x, y, w, h/2 ,expandOnly);
+        name = new FigName(x, y+h/2, w, h/2 ,expandOnly);
+        addFig(stereotype);
+        addFig(name);
+    }
+    
+    public void setStereotype(String stereotype) {
+        this.stereotype.setText(stereotype);
+    }
+    
+    public String getStereotype() {
+        return stereotype.getText();
+    }
+    
+    public void setName(String name) {
+        this.name.setText(name);
+    }
+    
+    public String getName() {
+        return name.getText();
+    }
+}
