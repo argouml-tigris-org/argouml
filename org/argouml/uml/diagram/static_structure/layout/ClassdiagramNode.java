f7d91111450fd51acb778cf009242bd3402cf831
Markus Klink <mkl@tigris.org>
2004-08-06 19:56:02 +0000

    much improved layout for classes associated to the inheritance tree. Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6412 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/static_structure/layout/ClassdiagramNode.java
+++ org/argouml/uml/diagram/static_structure/layout/ClassdiagramNode.java
@@ -24,36 +24,36 @@
 
 package org.argouml.uml.diagram.static_structure.layout;
 
-import java.awt.*;
-import java.util.*;
-import org.argouml.uml.diagram.layout.*;
-import org.argouml.uml.diagram.static_structure.ui.*;
-import org.tigris.gef.presentation.*;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.util.Vector;
 
-
+import org.argouml.uml.diagram.layout.LayoutedNode;
+import org.argouml.uml.diagram.static_structure.ui.FigPackage;
+import org.tigris.gef.presentation.Fig;
+import org.tigris.gef.presentation.FigNode;
 
 /**
- * This class represents a node in the classdiagram
- * (a class, interface or package).
+ * This class represents a node in the classdiagram (a class, interface or
+ * package).
  */
 class ClassdiagramNode implements LayoutedNode {
 
-    // Operations
-
     /**
      * Operation ClassdiagramNode creates a new ClassdiagramNode.
-     *
-     * @param figure represents the figure in the diagram, that
-     *               peers this layout node.
+     * 
+     * @param figure
+     *            represents the figure in the diagram, that peers this layout
+     *            node.
      */
     public ClassdiagramNode(FigNode figure) {
-	setFigure(figure);
+        setFigure(figure);
     }
 
     /**
-     * Operation getSize returns the size of the figure associated
-     * with this layout node.
-     *
+     * Operation getSize returns the size of the figure associated with this
+     * layout node.
+     * 
      * @return The size of the associated figure.
      */
     public Dimension getSize() {
@@ -61,9 +61,9 @@ class ClassdiagramNode implements LayoutedNode {
     }
 
     /**
-     * Operation getLocation returns the location of the associated
-     * figure in the diagram.
-     *
+     * Operation getLocation returns the location of the associated figure in
+     * the diagram.
+     * 
      * @return The location of the associated figure.
      */
     public Point getLocation() {
@@ -71,56 +71,58 @@ class ClassdiagramNode implements LayoutedNode {
     }
 
     /**
-     * Operation setLocation set the new location of the
-     * associated figure in the diagram.
-     *
-     * @param newLocation represents the new location for this figure.
+     * Operation setLocation set the new location of the associated figure in
+     * the diagram.
+     * 
+     * @param newLocation
+     *            represents the new location for this figure.
      */
     public void setLocation(Point newLocation) {
         Point oldLocation = getFigure().getLocation();
-        
+
         getFigure().setLocation(newLocation);
         int xTrans = newLocation.x - oldLocation.x;
         int yTrans = newLocation.y - oldLocation.y;
         Vector enclosingFigs = getFigure().getEnclosedFigs();
         if (enclosingFigs != null) {
             for (int i = 0; i < enclosingFigs.size(); i++)
-                ((Fig)(enclosingFigs.elementAt(i))).translate(xTrans,  yTrans);
+                ((Fig) (enclosingFigs.elementAt(i))).translate(xTrans, yTrans);
         }
     }
 
     /**
      * Check if this node is associated with a package.
-     *
-     * @return true, if this node is associated with a package,
-     *         false otherwise.
+     * 
+     * @return true, if this node is associated with a package, false otherwise.
      */
     boolean isPackage() {
-	return (getFigure() instanceof FigPackage);
+        return (getFigure() instanceof FigPackage);
+    }
+
+    public int getRealRank() {
+        return _rank;
     }
 
     /**
      * Compute or just return the rank of this node.
-     *
+     * 
      * @return The rank for this node.
      */
     public int getRank() {
-        
+
         if (_rank == NORANK) {
             // If the rank was not computed yet, compute it now.
-            if (getUplinks().size() == 0) {  // If there are no uplinks,
-                _rank = 0;  // place the node in the 1st row.
-            } else {  // Otherwise compute the max rank of the uplinks + 1
+            if (getUplinks().size() == 0) { // If there are no uplinks,
+                _rank = 0; // place the node in the 1st row.
+            } else { // Otherwise compute the max rank of the uplinks + 1
                 for (int i = 0; i < getUplinks().size(); i++) {
-                    
+
                     // avoid recursing, causing stack overflow error, if the
                     // uplinks are cyclic.
-                    if (getUplink(i) == this){
-                        return _rank;
-                    }
-                    
+                    if (getUplink(i) == this) { return _rank; }
+
                     if (getUplink(i).getRank() + 1 > _rank) {
-                        
+
                         _rank = getUplink(i).getRank() + 1;
                     }
                 }
@@ -131,141 +133,172 @@ class ClassdiagramNode implements LayoutedNode {
 
     /**
      * Operation setRank changes the value of the attribute _rank.
-     *
-     * @param new_rank represents the new value of _rank.	
+     * 
+     * @param new_rank
+     *            represents the new value of _rank.
      */
-    public void setRank(int new_rank) { _rank = new_rank; }
+    public void setRank(int new_rank) {
+        _rank = new_rank;
+    }
 
     /**
      * Add a constant to the rank of this node.
-     *
-     * @param n The value to add.
+     * 
+     * @param n
+     *            The value to add.
      */
     public void addRank(int n) {
-	setRank(n + getRank());
+        setRank(n + getRank());
     }
 
     /**
      * Operation getColumn returns the value of the attribute _column.
-     *
+     * 
      * @return The value of the attribute _column.
      */
-    public int getColumn() { return _column; }
+    public int getColumn() {
+        return _column;
+    }
 
     /**
      * Operation setColumn changes the value of the attribute _column.
-     *
-     * @param new_column represents the new value of _column.	
+     * 
+     * @param new_column
+     *            represents the new value of _column.
      */
-    public void setColumn(int new_column) { _column = new_column; }
+    public void setColumn(int new_column) {
+        _column = new_column;
+    }
 
     /**
      * Operation getUplinks returns the value of the attribute _uplinks.
-     *
+     * 
      * @return The value of the attribute _uplinks.
      */
-    public Vector getUplinks() { return _uplinks; }
+    public Vector getUplinks() {
+        return _uplinks;
+    }
 
     /**
      * Get the uplink for a given index.
-     *
-     * @param The index of this uplink.
+     * 
+     * @param The
+     *            index of this uplink.
      * @return The ClassdiagramNode for this uplink.
      */
     public ClassdiagramNode getUplink(int i) {
-	return (ClassdiagramNode) (_uplinks.elementAt(i)); 
+        return (ClassdiagramNode) (_uplinks.elementAt(i));
     }
 
     /**
      * Add an uplink to this node.
-     *
-     * @param new_uplink represents the new uplinks.	
+     * 
+     * @param new_uplink
+     *            represents the new uplinks.
      */
     public void addUplink(ClassdiagramNode new_uplink) {
-	_uplinks.add(new_uplink);
+        _uplinks.add(new_uplink);
     }
 
     /**
      * Operation getDownlinks returns the value of the attribute _downlinks.
-     *
+     * 
      * @return The value of the attribute _downlinks.
      */
-    public Vector getDownlinks() { return _downlinks; }
+    public Vector getDownlinks() {
+        return _downlinks;
+    }
 
     /**
      * Get the downlink for a given index.
-     *
-     * @param The index of this downlink.
+     * 
+     * @param The
+     *            index of this downlink.
      * @return The ClassdiagramNode of this downlink.
      */
     public ClassdiagramNode getDownlink(int i) {
-	return (ClassdiagramNode) (_downlinks.elementAt(i));
+        return (ClassdiagramNode) (_downlinks.elementAt(i));
     }
 
     /**
      * Operation setDownlinks changes the value of the attribute _downlinks.
-     *
-     * @param new_downlinks represents the new value of _downlinks.	
+     * 
+     * @param new_downlinks
+     *            represents the new value of _downlinks.
      */
     public void addDownlink(ClassdiagramNode new_downlink) {
-	_downlinks.add(new_downlink);
+        _downlinks.add(new_downlink);
     }
 
     /**
      * Operation getFigure returns the value of the attribute _figure.
-     *
+     * 
      * @return The value of the attribute _figure.
      */
-    public FigNode getFigure() { return _figure; }
+    public FigNode getFigure() {
+        return _figure;
+    }
 
     /**
      * Operation setFigure changes the value of the attribute _figure.
-     *
-     * @param new_figure represents the new value of _figure.	
+     * 
+     * @param new_figure
+     *            represents the new value of _figure.
      */
-    public void setFigure(FigNode new_figure) { _figure = new_figure; }
+    public void setFigure(FigNode new_figure) {
+        _figure = new_figure;
+    }
 
     /**
      * Return the weight of this node.
-     *
+     * 
      * @return The weight of this node.
      */
-    public float getWeight() { return _weight; }
+    public float getWeight() {
+        return _weight;
+    }
 
     /**
      * Set a new weight for this node.
-     *
-     * @param weight The new weight of this node.
+     * 
+     * @param weight
+     *            The new weight of this node.
      */
-    public void setWeight( float weight) { _weight = weight; }
-
-    /** a node is movable when it hasn't got any up- or downlinks, but sidelinks
-     *
-     * @return whether the node is movable
-     */
-    boolean isMovable() {
-        return (_downlinks.size() == 0
-		&& _uplinks.size() == 0
-		&& _sidelinks.size() > 0);
+    public void setWeight(float weight) {
+        _weight = weight;
     }
 
-    /** A placementhint gives an indication where it might be feasible to
-     *  place this node. It is used by the layouter, and there
-     *  is no guarantee that it will be used.
-     *  @param hint x coordinate of the desired placement
+    /**
+     * a node is movable when it hasn't got any up- or downlinks.
+     * 
+     * @return whether the node is movable
+     */
+    public boolean isMovable() {
+        return (_downlinks.size() == 0 && _uplinks.size() == 0);
+    }
+
+    /**
+     * A placementhint gives an indication where it might be feasible to place
+     * this node. It is used by the layouter, and there is no guarantee that it
+     * will be used.
+     * 
+     * @param hint
+     *            x coordinate of the desired placement
      */
     public void setPlacementHint(int hint) {
         placementHint = hint;
     }
 
     /** get the current placementhint. */
-    public int getPlacementHint() { return placementHint; }
+    public int getPlacementHint() {
+        return placementHint;
+    }
 
     // Attributes
 
     /**
-     * Constant to be used as an initializer when this node has
-     * no rank assigned yet.
+     * Constant to be used as an initializer when this node has no rank assigned
+     * yet.
      */
     public static int NORANK = -1;
 
@@ -275,8 +308,8 @@ class ClassdiagramNode implements LayoutedNode {
     private int _rank = NORANK;
 
     /**
-     * Constant to be used as an initializer when this node is
-     * not placed at an column.
+     * Constant to be used as an initializer when this node is not placed at an
+     * column.
      */
     public int NOCOLUMN = -1;
 
@@ -286,58 +319,44 @@ class ClassdiagramNode implements LayoutedNode {
     private int _column = NOCOLUMN;
 
     /**
-     * Attribute _uplinks represents the links I consider as
-     * an 'uplink'. An uplink is a link going to a superclass or
-     * a interface that this class implements. Figures that are 
-     * usually placed above this figure.
+     * Attribute _uplinks represents the links I consider as an 'uplink'. An
+     * uplink is a link going to a superclass or a interface that this class
+     * implements. Figures that are usually placed above this figure.
      */
     private Vector _uplinks = new Vector();
 
     /**
-     * Attribute _downlinks represents the links I consider as
-     * an 'downlink'. The opposite of an uplink. See explanation
-     * above.
+     * Attribute _downlinks represents the links I consider as an 'downlink'.
+     * The opposite of an uplink. See explanation above.
      */
     private Vector _downlinks = new Vector();
-    
-    /** Sidelinks are basically associations. We need to know if a node
-     * has sidelinks in order to determine if we can move him around freely
-     * after ranking.
-     * @see isMovable() 
-     */
-    private Vector _sidelinks = new Vector();
-    
 
     /**
-     * Attribute _figure represents the figure, that this
-     * ClassdiagramNode represents during the layout process.
+     * Attribute _figure represents the figure, that this ClassdiagramNode
+     * represents during the layout process.
      */
     private FigNode _figure = null;
 
     /**
-     * This attributes stores the 'weight' of this node.
-     * This is a computed attribute that is used during the horizontal
-     * placement process. It's based on the position of the 'uplinked'
-     * objects. The actual purpose is to minimize the number of link
-     * crossings in the diagram. Since we don't compute the actual number
-     * of link crossings, we look where our uplinked objects are, and then
-     * try to place our object in a way, that we can expect to have a
-     * minimal number of crossings.
+     * This attributes stores the 'weight' of this node. This is a computed
+     * attribute that is used during the horizontal placement process. It's
+     * based on the position of the 'uplinked' objects. The actual purpose is to
+     * minimize the number of link crossings in the diagram. Since we don't
+     * compute the actual number of link crossings, we look where our uplinked
+     * objects are, and then try to place our object in a way, that we can
+     * expect to have a minimal number of crossings.
      */
     private float _weight = 1;
-    
-    
-    /** a Node is movable when it has associations and no up- or
-     * downlinks.  The logic behind this is, that we would like to
-     * move nodes which were skipped during the rankking process to a
-     * position where they are as close as possible to the
-     * respective(s) nodes they share an association to
+
+    /**
+     * a Node is movable when it has associations and no up- or downlinks. The
+     * logic behind this is, that we would like to move nodes which were skipped
+     * during the rankking process to a position where they are as close as
+     * possible to the respective(s) nodes they share an association to
      */
     private boolean _movable = false;
 
     private int placementHint = -1;
-  
+
 }
-
-
 
