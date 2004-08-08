b99026bca172aea9766f19eda19dff006293f352
Michiel van der Wulp <mvw@tigris.org>
2004-08-08 08:24:39 +0000

    Style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6426 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/application/api/ArgoModule.java
+++ src_new/org/argouml/application/api/ArgoModule.java
@@ -35,7 +35,16 @@ import java.util.Vector;
  * @since 0.9.4
  */
 public interface ArgoModule { 
+    /**
+     * External modules are supposed to be located at 
+     * <code>MODULEFILENAME</code>.
+     */
     public static final String MODULEFILENAME = ".argo.modules";
+    
+    /**
+     * Or, alternatively, external modules may be located at 
+     * <code>MODULEFILENAME_ALTERNATE</code>
+     */
     public static final String MODULEFILENAME_ALTERNATE = "argo.modules";
 
     /** 
@@ -101,6 +110,8 @@ public interface ArgoModule {
      * 
      * @param popUpActions Vector of actions
      * @param context which the actions are valid for
+     *
+     * @return Vector containing pop-up actions
      */
     public Vector getModulePopUpActions(Vector popUpActions, Object context);
 
--- src_new/org/argouml/application/api/ArgoSingletonModule.java
+++ src_new/org/argouml/application/api/ArgoSingletonModule.java
@@ -68,6 +68,10 @@ public interface ArgoSingletonModule extends ArgoModule {
      */
     public void activateSingleton();
 
+    /**
+     * TODO: Document this.
+     * @return class which identifies the singleton
+     */
     public Class getSingletonType();
 
 } /* end interface ArgoSingletonModule */
