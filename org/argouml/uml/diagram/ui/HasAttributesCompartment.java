f809534d73205acc635175673e6632228c899096
Bob Tarling <bobtarling@tigris.org>
2004-08-16 09:18:32 +0000

    Forgot to add these new interfaces that should be implemented by figs with relevant compartments
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6498 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/diagram/ui/HasAttributesCompartment.java
@@ -0,0 +1,9 @@
+package org.argouml.uml.diagram.ui;
+
+/**
+ * @author Bob Tarling
+ */
+public interface HasAttributesCompartment {
+	public boolean isAttributesVisible();
+    public void setAttributesVisible(boolean visible);
+}
---
+++ org/argouml/uml/diagram/ui/HasFeaturesCompartment.java
@@ -0,0 +1,7 @@
+package org.argouml.uml.diagram.ui;
+
+/**
+ * @author Bob Tarling
+ */
+public abstract interface HasFeaturesCompartment {
+}
