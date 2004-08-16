485f8d04869a6cc37a4f689f6ee2739193f3fe53
Bob Tarling <bobtarling@tigris.org>
2004-08-16 09:18:32 +0000

    Forgot to add these new interfaces that should be implemented by figs with relevant compartments
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@6498 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src_new/org/argouml/uml/diagram/ui/HasOperationsCompartment.java
@@ -0,0 +1,9 @@
+package org.argouml.uml.diagram.ui;
+
+/**
+ * @author Bob Tarling
+ */
+public interface HasOperationsCompartment {
+	public boolean isOperationsVisible();
+    public void setOperationsVisible(boolean visible);
+}
