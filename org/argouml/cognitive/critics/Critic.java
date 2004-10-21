d0e141e1f59bb3d8a4e266f53ef43fa768a449e3
Markus Klink <mkl@tigris.org>
2004-10-21 11:38:30 +0000

    removed catch of generic Exception. Replaced by specific catch clauses. Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@6896 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/cognitive/critics/Critic.java
+++ org/argouml/cognitive/critics/Critic.java
@@ -49,7 +49,6 @@ import org.argouml.cognitive.Poster;
 import org.argouml.cognitive.ToDoItem;
 import org.argouml.i18n.Translator;
 import org.argouml.kernel.Wizard;
-import org.argouml.kernel.WizardItem;
 import org.tigris.gef.util.VectorSet;
 
 /** "Abstract" base class for design critics.  Each subclass should define
@@ -694,30 +693,33 @@ public class Critic implements Poster, Serializable {
     ////////////////////////////////////////////////////////////////
     // corrective automations, wizards
 
-    /** Create a new Wizard to help the user fix the identified problem.
-     *  This version assumes subclasses override getWizClass to return
-     *  the appropriate Class of wizard.  Critic subclasses that need to
-     *  initialize their wizard might override this to call
-     *  super.makeWizard() and then work with the result.
-     *  
-     * @param item the todo item
+    /**
+     * Create a new Wizard to help the user fix the identified problem. This
+     * version assumes subclasses override getWizClass to return the appropriate
+     * Class of wizard. Critic subclasses that need to initialize their wizard
+     * might override this to call super.makeWizard() and then work with the
+     * result.
+     * 
+     * @param item
+     *            the todo item
      * @return the wizard
      */
     public Wizard makeWizard(ToDoItem item) {
-	Class wizClass = getWizardClass(item);
-	// if wizClass is not a subclass of Wizard, print a warning
-	if (wizClass != null) {
-	    try {
-		Wizard w = (Wizard) wizClass.newInstance();
-		w.setToDoItem((WizardItem)item);
-		initWizard(w);
-		return w;
-	    }
-	    catch (Exception ex) {
-		LOG.error("Could not make wizard: " + item, ex);
-	    }
-	}
-	return null;
+        Class wizClass = getWizardClass(item);
+        // if wizClass is not a subclass of Wizard, print a warning
+        if (wizClass != null) {
+            try {
+                Wizard w = (Wizard) wizClass.newInstance();
+                w.setToDoItem(item);
+                initWizard(w);
+                return w;
+            } catch (IllegalAccessException illEx) {
+                LOG.error("Could not access wizard: ", illEx);
+            } catch (InstantiationException instEx) {
+                LOG.error("Could not instantiate wizard: ", instEx);
+            }
+        }
+        return null;
     }
 
     /** Return the Class of wizard that can fix the problem identifed by
