f02e37e1cd316d309b2dff659182dac52b4c465e
Michiel van der Wulp <mvw@tigris.org>
2004-11-13 22:37:09 +0000

    Style issues. Added some tests for the recent problems with deleting dagrams - which fail ...
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@7046 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/kernel/GUITestProject.java
+++ tests/org/argouml/kernel/GUITestProject.java
@@ -53,6 +53,10 @@ public class GUITestProject extends TestCase {
     
     
     
+    /**
+     * Test deleting a package that contains a Class diagram.
+     * The diagram should be deleted, too.
+     */
     public void testDeletePackageWithClassDiagram() {
         Project p = ProjectManager.getManager().getCurrentProject();
         assertEquals(2, p.getDiagrams().size());
@@ -77,6 +81,10 @@ public class GUITestProject extends TestCase {
         assertEquals(sizeMembers, p.getMembers().size());
     }
     
+    /**
+     * Test deleting a class that contains a Statechart diagram.
+     * The diagram should be deleted, too.
+     */
     public void testDeleteClassWithStateDiagram() {
         Project p = ProjectManager.getManager().getCurrentProject();
         assertEquals(2, p.getDiagrams().size());
@@ -100,11 +108,82 @@ public class GUITestProject extends TestCase {
         assertEquals(sizeMembers + 1, p.getMembers().size());
         p.moveToTrash(aClass);
         assertTrue("Statemachine not in trash", p.isInTrash(machine));
-        assertTrue("Class not int trash", p.isInTrash(aClass));
+        assertTrue("Class not in trash", p.isInTrash(aClass));
         assertEquals(sizeDiagrams, p.getDiagrams().size());
         assertEquals(sizeMembers, p.getMembers().size());
     }
     
+    /**
+     * Test deleting a package that contains a Class with Statechart diagram.
+     * The diagram should be deleted, too.
+     */ 
+    /*
+    public void testDeletePackageWithStateDiagram() {
+        Project p = ProjectManager.getManager().getCurrentProject();
+        assertEquals(2, p.getDiagrams().size());
+        
+        int sizeMembers = p.getMembers().size();
+        int sizeDiagrams = p.getDiagrams().size();
+        
+        // test with a class and class diagram
+        Object package1 = ModelManagementFactory.getFactory().buildPackage(
+                "test1", null);
+        Object aClass = CoreFactory.getFactory().buildClass(package1);
+        
+        
+        // try with Statediagram
+        Object machine = StateMachinesFactory.getFactory().buildStateMachine(
+                aClass);
+        UMLStateDiagram d = new UMLStateDiagram(ModelFacade
+                .getNamespace(machine), machine);
+        p.addMember(d);
+        assertEquals(sizeDiagrams + 1, p.getDiagrams().size());
+        assertEquals(sizeMembers + 1, p.getMembers().size());
+        p.moveToTrash(package1);
+        assertTrue("Statemachine not in trash", p.isInTrash(machine));
+        assertTrue("Class not in trash", p.isInTrash(aClass));
+        assertEquals(sizeDiagrams, p.getDiagrams().size());
+        assertEquals(sizeMembers, p.getMembers().size());
+    }*/
+
+    /**
+     * Test deleting an operation that contains a statechart diagram.
+     * The diagram should be deleted, too.
+     */
+    /*
+    public void testDeleteOperationWithStateDiagram() {
+        Project p = ProjectManager.getManager().getCurrentProject();
+        assertEquals(2, p.getDiagrams().size());
+        
+        int sizeMembers = p.getMembers().size();
+        int sizeDiagrams = p.getDiagrams().size();
+        
+        // test with a class and class diagram
+        Object package1 = ModelManagementFactory.getFactory().buildPackage(
+                "test1", null);
+        Object aClass = CoreFactory.getFactory().buildClass(package1);
+        Object oper = CoreFactory.getFactory().buildOperation(aClass);
+        
+        
+        // try with Statediagram
+        Object machine = StateMachinesFactory.getFactory().buildStateMachine(
+                oper);
+        UMLStateDiagram d = new UMLStateDiagram(ModelFacade
+                .getNamespace(machine), machine);
+        p.addMember(d);
+        assertEquals(sizeDiagrams + 1, p.getDiagrams().size());
+        assertEquals(sizeMembers + 1, p.getMembers().size());
+        p.moveToTrash(oper);
+        assertTrue("Statemachine not in trash", p.isInTrash(machine));
+        assertTrue("Operation not in trash", p.isInTrash(oper));
+        assertEquals(sizeDiagrams, p.getDiagrams().size());
+        assertEquals(sizeMembers, p.getMembers().size());
+    }
+    */
+
+    /**
+     * Test deleting a statechart diagram directly.
+     */
     public void testDeleteStateDiagram() {
         Project p = ProjectManager.getManager().getCurrentProject();
         assertEquals(2, p.getDiagrams().size());
