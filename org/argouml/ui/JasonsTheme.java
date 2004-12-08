9a9263904fbd605ac988f0b535bd305705184228
Linus Tolke <linus@tigris.org>
2004-12-08 19:42:31 +0000

    Style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@7161 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/JasonsTheme.java
+++ org/argouml/ui/JasonsTheme.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-99 The Regents of the University of California. All
+// Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -32,20 +32,19 @@ import javax.swing.plaf.metal.MetalTheme;
 /**
  * This class defines a variation on the default Metal Theme.
  */
-
 public class JasonsTheme extends MetalTheme {
 
     private final ColorUIResource primary1 = new ColorUIResource(102, 102, 153);
     private final ColorUIResource primary2 = new ColorUIResource(153, 153, 204);
     private final ColorUIResource primary3 = new ColorUIResource(204, 204, 255);
-  
+
     private final ColorUIResource secondary1 =
 	new ColorUIResource(102, 102, 102);
     private final ColorUIResource secondary2 =
 	new ColorUIResource(153, 153, 153);
     private final ColorUIResource secondary3 =
 	new ColorUIResource(204, 204, 204);
-  
+
     private final FontUIResource controlFont =
 	new FontUIResource("SansSerif", Font.BOLD, 10);
     private final FontUIResource systemFont =
@@ -66,7 +65,8 @@ public class JasonsTheme extends MetalTheme {
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getPrimary1()
      */
-    protected ColorUIResource getPrimary1() { return primary1; } 
+    protected ColorUIResource getPrimary1() { return primary1; }
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getPrimary2()
      */
@@ -75,58 +75,50 @@ public class JasonsTheme extends MetalTheme {
      * @see javax.swing.plaf.metal.MetalTheme#getPrimary3()
      */
     protected ColorUIResource getPrimary3() { return primary3; }
-  
+
     // these are gray in Metal Default Theme
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getSecondary1()
      */
     protected ColorUIResource getSecondary1() { return secondary1; }
-    
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getSecondary2()
      */
     protected ColorUIResource getSecondary2() { return secondary2; }
-    
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getSecondary3()
      */
     protected ColorUIResource getSecondary3() { return secondary3; }
-  
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getControlTextFont()
      */
     public FontUIResource getControlTextFont() { return controlFont; }
-    
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getSystemTextFont()
      */
     public FontUIResource getSystemTextFont() { return systemFont; }
-    
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getUserTextFont()
      */
     public FontUIResource getUserTextFont() { return userFont; }
-    
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getMenuTextFont()
      */
     public FontUIResource getMenuTextFont() { return controlFont; }
-    
-    /**
-     * TODO: Should this be getTextHighlightColor?
-     * @return
-     */
-    public FontUIResource getEmphasisTextFont() { return windowTitleFont; }
-  
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getSubTextFont()
      */
     public FontUIResource getSubTextFont() { return smallFont; }
-  
+
     /**
      * @see javax.swing.plaf.metal.MetalTheme#getWindowTitleFont()
      */
     public FontUIResource getWindowTitleFont() { return windowTitleFont; }
-
-
 }
