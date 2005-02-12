4fbc3e6ce211b366271b7e98cf24c440a7b60790
Michiel van der Wulp <mvw@tigris.org>
2005-02-12 20:47:39 +0000

    Added a residence-centric explorer perspective. To see it working, press "Restore Defaults" in the "Configure Perspectives" dialog.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@7724 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/rules/GoComponentToResidentModelElement.java
+++ org/argouml/ui/explorer/rules/GoComponentToResidentModelElement.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -24,29 +24,51 @@
 
 package org.argouml.ui.explorer.rules;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.argouml.i18n.Translator;
+import org.argouml.model.Model;
 
 /**
+ * Rule for Component -> Resident Element.
+ * 
+ * @author mvw@tigris.org
  *
- * @author  alexb
  */
-public abstract class AbstractPerspectiveRule implements PerspectiveRule {
+public class GoComponentToResidentModelElement extends AbstractPerspectiveRule {
 
     /**
      * @see org.argouml.ui.explorer.rules.PerspectiveRule#getRuleName()
      */
-    public abstract String getRuleName();
+    public String getRuleName() {
+        return Translator.localize ("misc.component.resident.modelelement");
+    }
 
     /**
      * @see org.argouml.ui.explorer.rules.PerspectiveRule#getChildren(java.lang.Object)
      */
-    public abstract Collection getChildren(Object parent);
+    public Collection getChildren(Object parent) {
+        if (Model.getFacade().isAComponent(parent)) {
+            // First get the collection of ElementResidence
+            Iterator eri = 
+                Model.getFacade().getResidentElements(parent).iterator();
+            Collection result = new ArrayList();
+            while (eri.hasNext()) {
+                result.add(Model.getFacade().getResident(eri.next()));
+            }
+            return result;
+        }
+        return null;
+    }
 
     /**
-     * @see java.lang.Object#toString()
+     * @see org.argouml.ui.explorer.rules.PerspectiveRule#getDependencies(java.lang.Object)
      */
-    public String toString() {
-        return getRuleName();
+    public Set getDependencies(Object parent) {
+        return null;
     }
 
 }
