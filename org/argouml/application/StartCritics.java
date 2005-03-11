58f2d81274063d26c7d0c05db6730aa36b4c9418
Markus Klink <mkl@tigris.org>
2005-03-11 10:02:58 +0000

    Issue number: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@7916 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/application/StartCritics.java
+++ org/argouml/application/StartCritics.java
@@ -32,8 +32,8 @@ import org.argouml.cognitive.Designer;
 import org.argouml.kernel.Project;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.Model;
+import org.argouml.uml.cognitive.UMLDecision;
 import org.argouml.uml.cognitive.critics.ChildGenUML;
-import org.argouml.uml.cognitive.critics.CrUML;
 
 /**
  * StartCritics is a thread which helps to start the critiquing thread.
@@ -54,8 +54,7 @@ public class StartCritics implements Runnable {
         Project p = ProjectManager.getManager().getCurrentProject();
         Designer.addListener(ProjectManager.getManager());
         // set the icon for this poster
-        dsgr.setClarifier(ResourceLoaderWrapper.
-            lookupIconResource("PostItD0"));
+        dsgr.setClarifier(ResourceLoaderWrapper.lookupIconResource("PostItD0"));
         dsgr.setDesignerName(Configuration.getString(Argo.KEY_USER_FULLNAME));
         Configuration.addListener(Argo.KEY_USER_FULLNAME, dsgr); //MVW
         dsgr.spawnCritiquer(p);
@@ -66,16 +65,25 @@ public class StartCritics implements Runnable {
             Model.getPump().addModelEventListener(dsgr, o);
         }
         LOG.info("spawned critiquing thread");
-
-        // should be in logon wizard?
-        dsgr.startConsidering(CrUML.DEC_INHERITANCE);
-        dsgr.startConsidering(CrUML.DEC_CONTAINMENT);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.CLASS_SELECTION);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.BEHAVIOR);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.NAMING);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.STORAGE);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.INHERITANCE);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.CONTAINMENT);
+        dsgr.getDecisionModel()
+                .startConsidering(UMLDecision.PLANNED_EXTENSIONS);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.STATE_MACHINES);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.PATTERNS);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.RELATIONSHIPS);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.INSTANCIATION);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.MODULARITY);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.EXPECTED_USAGE);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.METHODS);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.CODE_GEN);
+        dsgr.getDecisionModel().startConsidering(UMLDecision.STEREOTYPES);
         Designer.setUserWorking(true);
     }
 
 } /* end class StartCritics */
-
-
-
-
 
