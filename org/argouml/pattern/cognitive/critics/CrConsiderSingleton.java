5a07ac1ba03ce49d905b7a242079d02ce8c47ac4
Markus Klink <mkl@tigris.org>
2005-03-11 09:48:36 +0000

    Issue number: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@7915 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/pattern/cognitive/critics/CrConsiderSingleton.java
+++ org/argouml/pattern/cognitive/critics/CrConsiderSingleton.java
@@ -29,6 +29,7 @@ import java.util.Iterator;
 import org.argouml.cognitive.Designer;
 import org.argouml.cognitive.ToDoItem;
 import org.argouml.model.Model;
+import org.argouml.uml.cognitive.UMLDecision;
 import org.argouml.uml.cognitive.critics.CrUML;
 
 /**
@@ -56,7 +57,7 @@ public class CrConsiderSingleton extends CrUML {
      */
     public CrConsiderSingleton() {
         setupHeadAndDesc();
-        addSupportedDecision(CrUML.DEC_PATTERNS);
+        addSupportedDecision(UMLDecision.PATTERNS);
         setPriority(ToDoItem.LOW_PRIORITY);
 
         // These may not actually make any difference at present (the code
