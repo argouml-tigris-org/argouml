f17349d06b7cbbeef78dc4959a8088041ca2e366
Michiel van der Wulp <mvw@tigris.org>
2005-04-24 14:26:32 +0000

    Style issues. Improved commenting.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8058 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/TabStyle.java
+++ src_new/org/argouml/uml/ui/TabStyle.java
@@ -82,7 +82,7 @@ import org.tigris.gef.presentation.Fig;
  *
  * According the decision taken in issue 502, this tab is renamed "Presentation"
  * for the user. And the Presentation tab shall contain presentation options,
- * and no semantic UML properties (which belong inthe "Properties" panel).
+ * and no semantic UML properties (which belong in the "Properties" panel).
  * In contrast, the diagram pop-up menu for a model element
  * may access both presentation options as well as semantic UML properties. <p>
  *
@@ -132,7 +132,7 @@ public class TabStyle extends TabSpawnable implements TabFigTarget,
     }
 
     /**
-     * construct a default stylepanel with basenames <code>StylePanel</code>
+     * Construct a default stylepanel with basenames <code>StylePanel</code>
      * and <code>SP</code>, resulting in the lookup order described above.
      */
     public TabStyle() {
@@ -186,10 +186,9 @@ public class TabStyle extends TabSpawnable implements TabFigTarget,
                 Collection col = p.findFigsForMember(t);
                 if (col == null || col.isEmpty()) {
                     return;
-                } else {
-                    t = col.iterator().next();
-                    if (!(t instanceof Fig)) return;
                 }
+                t = col.iterator().next();
+                if (!(t instanceof Fig)) return;
             } else {
                 return;
             }
