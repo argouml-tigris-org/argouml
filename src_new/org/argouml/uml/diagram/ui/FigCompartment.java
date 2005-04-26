9ae2f618e6d9188cfe1ef0f7ad1655ffa2109a2c
Bob Tarling <bobtarling@tigris.org>
2005-04-26 15:12:32 +0000

    Remove references to deprecated variables and default to standard GEF code.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8069 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/ui/FigCompartment.java
+++ src_new/org/argouml/uml/diagram/ui/FigCompartment.java
@@ -31,25 +31,4 @@ import org.tigris.gef.presentation.FigGroup;
  */
 public abstract class FigCompartment extends FigGroup {
 
-    /**
-     * @see org.tigris.gef.presentation.Fig#getLineWidth()
-     */
-    public int getLineWidth() {
-        // For the moment we access deprecated variables in GEF
-        // in order to get over a nasty
-        // side effect in the getLineWidth() method of GEF.
-        // This will be resolved in GEF 0.10.9
-        return _lineWidth;
-    }
-
-    /**
-     * @see org.tigris.gef.presentation.Fig#setLineWidth(int)
-     */
-    public void setLineWidth(int w) {
-        // For the moment we access deprecated variables in GEF
-        // in order to get over a nasty
-        // side effect in the setLineWidth(int) method of GEF.
-        // This will be resolved in GEF 0.10.9
-        _lineWidth = w;
-    }
 }
