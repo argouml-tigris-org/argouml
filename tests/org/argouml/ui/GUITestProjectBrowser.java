20a690ac4c3e3616ce7f272ec7bfbb7ad37177cf
Linus Tolke <linus@tigris.org>
2005-06-06 20:07:21 +0000

    Fixed a test case after having changed the way the tabs' titles are localized.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8308 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/ui/GUITestProjectBrowser.java
+++ tests/org/argouml/ui/GUITestProjectBrowser.java
@@ -26,6 +26,7 @@ package org.argouml.ui;
 
 import junit.framework.TestCase;
 
+import org.argouml.i18n.Translator;
 import org.argouml.kernel.Project;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.Model;
@@ -65,8 +66,8 @@ public class GUITestProjectBrowser extends TestCase {
 	assertNotNull(pb.getStatusBar());
 	assertNotNull(pb.getJMenuBar());
 	assertNotNull(pb.getEditorPane());
-	assertNotNull(pb.getNamedTab("tab.properties"));
-	assertNotNull(pb.getNamedTab("tab.source"));
+	assertNotNull(pb.getNamedTab(Translator.localize("tab.properties")));
+	assertNotNull(pb.getNamedTab(Translator.localize("tab.source")));
 	assertNotNull(pb.getTodoPane());
     }
 
@@ -84,9 +85,8 @@ public class GUITestProjectBrowser extends TestCase {
      * Test the existance of public static members.
      */
     public void compileTestPublicStaticMembers() {
-	int r =
-	    ProjectBrowser.DEFAULT_COMPONENTWIDTH
-	        + ProjectBrowser.DEFAULT_COMPONENTHEIGHT;
+	new Integer(ProjectBrowser.DEFAULT_COMPONENTWIDTH
+	        + ProjectBrowser.DEFAULT_COMPONENTHEIGHT);
     }
 
     /**
@@ -130,13 +130,5 @@ public class GUITestProjectBrowser extends TestCase {
 	p.moveToTrash(package2);
 	assertEquals("The target is not reset to the first diagram",
             p.getDiagrams().get(0), tm.getTarget());
-    }
-
-    /**
-     * Test the existance of deprecated methods.
-     */
-    public void compileExistDeprecated() {
-	ProjectBrowser pb = ProjectBrowser.getInstance();
-	pb.getTarget();
     }
 }
