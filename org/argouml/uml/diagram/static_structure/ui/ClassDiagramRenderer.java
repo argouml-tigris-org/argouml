2ec11c23d239bb95c5ba18a5c50faff13e4b4ab0
Bob Tarling <bobtarling@tigris.org>
2005-06-14 19:20:24 +0000

    Do some extra validation
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@8342 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/static_structure/ui/ClassDiagramRenderer.java
+++ org/argouml/uml/diagram/static_structure/ui/ClassDiagramRenderer.java
@@ -89,6 +89,9 @@ public class ClassDiagramRenderer extends UmlDiagramRenderer {
      */
     public FigNode getFigNodeFor(GraphModel gm, Layer lay,
 				 Object node, Map styleAttributes) {
+        if (node == null) {
+            throw new IllegalArgumentException("A node must be supplied");
+        }
         if (Model.getFacade().isAClass(node)) {
             return new FigClass(gm, node);
         } else if (Model.getFacade().isAInterface(node)) {
@@ -107,7 +110,9 @@ public class ClassDiagramRenderer extends UmlDiagramRenderer {
             return new FigNodeAssociation(gm, node);
         }
         LOG.error("TODO: ClassDiagramRenderer getFigNodeFor " + node);
-        return null;
+        throw new IllegalArgumentException(
+                "Node is not a recognised type. Received "
+                + node.getClass().getName());
     }
 
     /**
