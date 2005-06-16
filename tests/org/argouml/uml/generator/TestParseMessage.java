dfa2eeb6fd938a389b1de9ec32ba8320e53c137b
Bob Tarling <bobtarling@tigris.org>
2005-06-16 10:37:30 +0000

    Combine some tests from GUITest??? to Test???
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8351 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/uml/generator/TestParseMessage.java
+++ tests/org/argouml/uml/generator/TestParseMessage.java
@@ -24,35 +24,391 @@
 
 package org.argouml.uml.generator;
 
+import java.text.ParseException;
+import java.util.Iterator;
+
 import junit.framework.TestCase;
 
-import java.text.ParseException;
-import ru.novosoft.uml.behavior.collaborations.*;
+import org.argouml.model.Model;
+
+import ru.novosoft.uml.behavior.collaborations.MMessage;
+import ru.novosoft.uml.behavior.collaborations.MMessageImpl;
 
 /**
- * Non-gui tests of ParserDisplay.parseMessage.
+ * Test class to test the parsing of a message.<p>
+ *
+ * For some reason these tests require the ProjectBrowser loaded when running.
+ * For that reason they cannot be run in Headless mode.
  */
 public class TestParseMessage extends TestCase {
     /**
-     * @see junit.framework.TestCase#TestCase()
+     * @see junit.framework.TestCase#TestCase(String)
      */
     public TestParseMessage(String str) {
-	super(str);
+        super(str);
     }
-
+    
     /**
      * Static test of the signature.
      *
      * @throws ParseException if the parsing fails.
      */
-    public void compileTestParseMessage()
-	throws ParseException
-    {
-	ParserDisplay.SINGLETON.parseMessage(new MMessageImpl(), "hej");
+    public void compileTestParseMessage() throws ParseException {
+        ParserDisplay.SINGLETON.parseMessage(new MMessageImpl(), "hej");
     }
 
     /**
      * Dummy test.
      */
     public void testDummy() { }
+
+    /**
+     * Test the parsing of a message.
+     *
+     * @throws ParseException if the parsing was in error.
+     */
+    public void testParseMessage() throws ParseException {
+        Object coll = Model.getCollaborationsFactory().createCollaboration();
+        Object inter = Model.getCollaborationsFactory().buildInteraction(coll);
+
+        Object cl1 = Model.getCollaborationsFactory().createClassifierRole();
+        Object cl2 = Model.getCollaborationsFactory().createClassifierRole();
+        Object cl3 = Model.getCollaborationsFactory().createClassifierRole();
+        Object cl4 = Model.getCollaborationsFactory().createClassifierRole();
+        Object cl5 = Model.getCollaborationsFactory().createClassifierRole();
+        Model.getCoreHelper().setNamespace(cl1, coll);
+        Model.getCoreHelper().setNamespace(cl2, coll);
+        Model.getCoreHelper().setNamespace(cl3, coll);
+        Model.getCoreHelper().setNamespace(cl4, coll);
+        Model.getCoreHelper().setNamespace(cl5, coll);
+
+        Object r1to2 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl1, cl2);
+        Object r2to3 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl2, cl3);
+        Object r3to4 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl3, cl4);
+        Object r4to5 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl4, cl5);
+        Object r3to1 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl3, cl1);
+        Object r5to3 =
+            Model.getCollaborationsFactory().buildAssociationRole(cl5, cl3);
+
+        /* START TESTING STUFF */
+
+        MMessage m1 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r1to2);
+        assertTrue(m1.getSender() == cl1);
+        assertTrue(m1.getReceiver() == cl2);
+        assertTrue(m1.getInteraction() == inter);
+        assertNull(m1.getActivator());
+        assertTrue(
+            m1.getAction() == null || m1.getAction().getRecurrence() == null);
+        assertTrue(m1.getPredecessors().size() == 0);
+        m1.setName("m1");
+
+        MMessage m2 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r2to3);
+        assertTrue(m2.getSender() == cl2);
+        assertTrue(m2.getReceiver() == cl3);
+        assertTrue(m2.getActivator() == m1);
+        assertTrue(
+            m2.getAction() == null || m2.getAction().getRecurrence() == null);
+        assertTrue(m2.getPredecessors().size() == 0);
+        m2.setName("m2");
+
+        MMessage m3 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r2to3);
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(
+            m3.getPredecessors().iterator().next() == m2
+                && m3.getPredecessors().size() == 1);
+        m3.setName("m3");
+
+        /* TRY MOVING IN A SIMPLE MANER */
+
+        trySimpleMoving(m1, m2, m3);
+
+        /* TRY SOME ERRORS */
+
+        trySomeErrors(m3);
+
+        /* TRY SOME MORE COMPLEX MOVING */
+
+        trySomeMoreComplexMoving(m1, m2, m3);
+
+        /* TRY SOME MORE ERRORS */
+
+        checkParseException(m1, " 1.1 : ");
+        checkParseException(m2, " 1.1.1 : ");
+
+        /* TRY GUARD/ITERATOR SYNTAX */
+
+        tryGuardAndIteratorSyntax(m3);
+
+        /* TRY SOME GUARD/ITERATOR ERRORS */
+
+        trySomeGuardAndIteratorErrors(m3);
+
+        /* TRY THE ACTIONS */
+
+        tryTheActions(m3);
+
+        /* TRY SOME ACTION ERRORS */
+
+        trySomeActionErrors(m3);
+
+        /* TRY THE PREDECESSORS */
+
+        checkParseException(m3, "1.2.1 / 1.2 :");
+
+        MMessage m4 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r3to4);
+        MMessage m5 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r4to5);
+        MMessage m6 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r5to3);
+        MMessage m7 =
+            (MMessage) Model.getCollaborationsFactory()
+            	.buildMessage(inter, r3to1);
+
+        checkParseException(m6, "1.2.2 :");
+
+        parseMessage(m7, "2:");
+        assertTrue(m7.getSender() == cl1);
+        assertTrue(m7.getReceiver() == cl3);
+        assertNull(m7.getActivator());
+        assertTrue(m7.getPredecessors().iterator().next() == m1);
+        assertTrue(m7.getPredecessors().size() == 1);
+
+        parseMessage(m7, "1.2.1.1.1.1:");
+        assertTrue(m7.getSender() == cl3);
+        assertTrue(m7.getReceiver() == cl1);
+        assertTrue(m7.getActivator() == m6);
+        assertTrue(m7.getPredecessors().size() == 0);
+
+        /* TRY PREDECESSORS */
+        tryPredecessors(m1, m3, m4, m5, m7);
+
+        /* TRY SOME PREDECESSOR ERRORS */
+        trySomePredecessorErrors(m2, m3);
+    }
+
+    /**
+     * @param m1 message to be tested
+     * @param m3 message to be tested
+     * @param m4 message to be tested
+     * @param m5 message to be tested
+     * @param m7 message to be tested
+     * @throws ParseException if the parser found a syntax error
+     */
+    private void tryPredecessors(MMessage m1, MMessage m3, MMessage m4,
+            MMessage m5, MMessage m7) throws ParseException {
+        MMessage m;
+        Iterator it;
+        assertNull(m1.getActivator());
+        assertTrue(m1.getPredecessors().size() == 0);
+        assertTrue(m4.getActivator() == m3);
+        assertTrue(m4.getPredecessors().size() == 0);
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(m3.getPredecessors().size() == 1);
+
+        parseMessage(m7, "1.2.1 / 1.2.1.1.1.1:");
+        assertTrue(m7.getPredecessors().iterator().next() == m4);
+        assertTrue(m7.getPredecessors().size() == 1);
+
+        parseMessage(m7, "1.2.1, 1.2.1.1 / 1.2.1.1.1.1:");
+        boolean pre1 = false;
+        boolean pre2 = false;
+        it = m7.getPredecessors().iterator();
+        m = (MMessage) it.next();
+        if (m == m4) {
+            pre1 = true;
+        } else if (m == m5) {
+            pre2 = true;
+        } else {
+            assertTrue("Strange message found", false);
+        }
+        m = (MMessage) it.next();
+        if (m == m4) {
+            pre1 = true;
+        } else if (m == m5) {
+            pre2 = true;
+        } else {
+            assertTrue("Strange message found", false);
+        }
+        assertTrue(pre1);
+        assertTrue(pre2);
+        assertTrue(!it.hasNext());
+    }
+
+    /**
+     * @param m2 message to be tested
+     * @param m3 message to be tested
+     */
+    private void trySomePredecessorErrors(MMessage m2, MMessage m3) {
+        checkParseException(m2, "1.2 / 1.1 :");
+        checkParseException(m2, "1.2.1 / 1.1 :");
+        checkParseException(m3, "1.2.1 / 1.2 :");
+    }
+
+    /**
+     * @param m3 message to be tested
+     */
+    private void trySomeActionErrors(MMessage m3) {
+        checkParseException(m3, "1.2 : func() ()");
+        checkParseException(m3, "1.2 : func() foo()");
+        checkParseException(m3, "1.2 : func(), foo()");
+        checkParseException(m3, "1.2 : var() = func()");
+        checkParseException(m3, "1.2 : var = func(), foo()");
+        checkParseException(m3, "1.2 : func() foo()");
+        checkParseException(m3, "1.2 : var = ()");
+        checkParseException(m3, "1.2 : var = () foo");
+        checkParseException(m3, "1.2 : var = (foo(), bar())");
+        checkParseException(m3, "1.2 : func(");
+        checkParseException(m3, "1.2 : func(a, b");
+    }
+
+    /**
+     * @param m3 message to be tested
+     * @throws ParseException if the parser found a syntax error
+     */
+    private void tryTheActions(MMessage m3) throws ParseException {
+        parseMessage(m3, " 1.2 : func() ");
+        assertEquals("func", m3.getAction().getScript().getBody());
+        parseMessage(m3, " 1.2 ");
+        assertEquals("func", m3.getAction().getScript().getBody());
+        parseMessage(m3, " 1.2 : ");
+        assertEquals("", m3.getAction().getScript().getBody());
+
+        parseMessage(m3, " 1.2 : var := func() ");
+        assertEquals("var := func", m3.getAction().getScript().getBody());
+
+        parseMessage(m3, " 1.2 : var = func() ");
+        assertEquals("var := func", m3.getAction().getScript().getBody());
+
+        parseMessage(m3, "1.2:var2:=func2()");
+        assertEquals("var2 := func2", m3.getAction().getScript().getBody());
+
+        parseMessage(m3, " 1.2 : var, var2, var3 := func() ");
+        assertEquals(
+            "var, var2, var3 := func",
+            m3.getAction().getScript().getBody());
+
+        parseMessage(m3, "1.2 : load_the_accumulating_taxes");
+    }
+
+    /**
+     * @param m3 message to be tested
+     */
+    private void trySomeGuardAndIteratorErrors(MMessage m3) {
+        checkParseException(m3, " [x < 5] 1.2 [x > 6] : ");
+        checkParseException(m3, " 1 [x < 5] / 1.2 : ");
+        checkParseException(m3, " 1 * / 1.2 : ");
+        checkParseException(m3, " 1 // / 1.2 : ");
+        checkParseException(m3, " 1 , 2 [x < 5] / 1.2 : ");
+        checkParseException(m3, " 1 , 2 * / 1.2 : ");
+        checkParseException(m3, " 1 , 2 // / 1.2 : ");
+        checkParseException(m3, "/ 1.2 , 2 : ");
+        checkParseException(m3, "/1.2,2:");
+    }
+
+    /**
+     * @param m3 message to be tested
+     * @throws ParseException if the parser found a syntax error
+     */
+    private void tryGuardAndIteratorSyntax(MMessage m3) throws ParseException {
+        parseMessage(m3, " 1.2 [ x < 5 ] : ");
+        assertNotNull(m3.getAction());
+        assertNotNull(m3.getAction().getRecurrence());
+        assertTrue("[x < 5]".equals(m3.getAction().getRecurrence().getBody()));
+
+        parseMessage(m3, " 1.2 * [ i = 1..10 ] : ");
+        assertEquals("*[i = 1..10]", m3.getAction().getRecurrence().getBody());
+
+        parseMessage(m3, " 1.2 *// : ");
+        assertEquals("*[i = 1..10]", m3.getAction().getRecurrence().getBody());
+
+        parseMessage(m3, " * // [i=1..] 1.2 : ");
+        assertEquals("*//[i=1..]", m3.getAction().getRecurrence().getBody());
+        parseMessage(m3, " 1.2 : ");
+        assertEquals("*//[i=1..]", m3.getAction().getRecurrence().getBody());
+    }
+
+    /**
+     * @param m1 message to be tested
+     * @param m2 message to be tested
+     * @param m3 message to be tested
+     * @throws ParseException if the parser found a syntax error
+     */
+    private void trySomeMoreComplexMoving(MMessage m1, MMessage m2,
+            MMessage m3) throws ParseException {
+        parseMessage(m3, " 1.1.1 : ");
+        assertTrue(m3.getActivator() == m2);
+        assertTrue(m3.getPredecessors().size() == 0);
+
+        parseMessage(m3, " / 1..2 : ");
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(m2.getPredecessors().size() == 0);
+        assertTrue(
+            m3.getPredecessors().iterator().next() == m2
+                && m3.getPredecessors().size() == 1);
+        parseMessage(m3, "");
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(
+            m3.getPredecessors().iterator().next() == m2
+                && m3.getPredecessors().size() == 1);
+    }
+
+    /**
+     * @param m3 message to be tested
+     */
+    private void trySomeErrors(MMessage m3) {
+        checkParseException(m3, " 2.1 : ");
+        checkParseException(m3, " 1.2 : 1.2 :");
+        checkParseException(m3, " / / 1.2 : ");
+    }
+
+    /**
+     * @param m1 message to be tested
+     * @param m2 message to be tested
+     * @param m3 message to be tested
+     * @throws ParseException if the parser found a syntax error
+     */
+    private void trySimpleMoving(MMessage m1, MMessage m2, MMessage m3)
+        throws ParseException {
+        parseMessage(m3, " \t1.1 : ");
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(m3.getPredecessors().size() == 0);
+        assertTrue(
+            m2.getPredecessors().iterator().next() == m3
+                && m2.getPredecessors().size() == 1);
+
+        parseMessage(m3, " / 1.2\t: ");
+        assertTrue(m3.getActivator() == m1);
+        assertTrue(m2.getPredecessors().size() == 0);
+        assertTrue(
+            m3.getPredecessors().iterator().next() == m2
+                && m3.getPredecessors().size() == 1);
+    }
+
+    private void parseMessage(MMessage m, String s) throws ParseException {
+        ParserDisplay.SINGLETON.parseMessage(m, s);
+    }
+
+    private void checkParseException(MMessage m, String s) {
+        try {
+            ParserDisplay.SINGLETON.parseMessage(m, s);
+            assertTrue("Didn't throw for \"" + s + "\" in " + m, false);
+        } catch (ParseException pe) {
+            // The expected exception is thrown.
+        }
+    }
 }
