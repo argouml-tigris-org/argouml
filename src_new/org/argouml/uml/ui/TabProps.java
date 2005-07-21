70ad524c65dd6d0cf9007b3e7227ab663d6cfba6
Markus Klink <mkl@tigris.org>
2005-07-21 06:50:54 +0000

    fixed a number of issues regarding the factory.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8616 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/TabProps.java
+++ src_new/org/argouml/uml/ui/TabProps.java
@@ -379,6 +379,8 @@ public class TabProps
      * @return A new prop panel to display any model element of the given type
      */
     private TabModelTarget createPropPanel(Object modelElement) {
+        
+       
 
         // Create prop panels for diagrams
         if (modelElement instanceof UMLActivityDiagram) {
@@ -402,6 +404,14 @@ public class TabProps
         if (modelElement instanceof UMLUseCaseDiagram) {
             return new PropPanelUMLUseCaseDiagram();
         }
+        
+        // if (1 == 1) return null;
+        
+        if (Model.getFacade().isASubmachineState(modelElement)) { 
+            return new PropPanelSubmachineState();}
+
+        if (Model.getFacade().isASubactivityState(modelElement)) { 
+                return new PropPanelSubactivityState();}
         
         // Create prop panels for model elements
         if (Model.getFacade().isAAbstraction(modelElement)) { 
@@ -514,10 +524,6 @@ public class TabProps
                 return new PropPanelStimulus();}
         if (Model.getFacade().isAStubState(modelElement)) { 
                 return new PropPanelStubState();}
-        if (Model.getFacade().isASubactivityState(modelElement)) { 
-                return new PropPanelSubactivityState();}
-        if (Model.getFacade().isASubmachineState(modelElement)) { 
-                return new PropPanelSubmachineState();}
         if (Model.getFacade().isASubsystem(modelElement)) { 
                 return new PropPanelSubsystem();}
         if (Model.getFacade().isASynchState(modelElement)) { 
