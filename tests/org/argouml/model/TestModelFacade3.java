097aa029b5a0de97c410a34589350bcc1d987c92
Markus Klink <mkl@tigris.org>
2005-07-22 07:51:16 +0000

    fixed failure for getTipString as it is not delivering an IllegalArgumentException
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8637 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/model/TestModelFacade3.java
+++ tests/org/argouml/model/TestModelFacade3.java
@@ -22,7 +22,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-
 package org.argouml.model;
 
 import java.lang.reflect.InvocationTargetException;
@@ -34,140 +33,152 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
 /**
- * This TestCase dynamically generates a suite of tests to throw
- * arguments at each method in the Model subsystem Facade to check 
- * whether they throw an IllegalArgumentException.
+ * This TestCase dynamically generates a suite of tests to throw arguments at
+ * each method in the Model subsystem Facade to check whether they throw an
+ * IllegalArgumentException.
  */
 public class TestModelFacade3 extends TestCase {
 
-    private Method methodToTest = null;
-    private Facade facade = null;
+        private Method methodToTest = null;
 
-    /**
-     * Creates one of the test cases.
-     *
-     * @param method to be tested.
-     */
-    public TestModelFacade3(Method method) {
-	super(method.toString());
-	methodToTest = method;
-    }
+        private Facade facade = null;
 
-    /**
-     * @return the test suite
-     */
-    public static Test suite() {
-	TestSuite suite =
-	    new TestSuite("Tests for "
-			  + TestModelFacade3.class.getPackage().getName());
-
-        Method methods[] = Facade.class.getDeclaredMethods();
-
-	for (int i = 0; i < methods.length; i++) {
-            suite.addTest(new TestModelFacade3(methods[i]));
+        /**
+         * Creates one of the test cases.
+         * 
+         * @param method
+         *                to be tested.
+         */
+        public TestModelFacade3(Method method) {
+                super(method.toString());
+                methodToTest = method;
         }
 
-        return suite;
-    }
+        /**
+         * @return the test suite
+         */
+        public static Test suite() {
+                TestSuite suite = new TestSuite("Tests for "
+                                + TestModelFacade3.class.getPackage().getName());
 
-    /**
-     * This method checks whether a test should be executed for a given
-     * method.
-     *
-     * @return true if method is not eligible for testing (e.g. helper methods)
-     */
-    protected boolean methodForbidden() {
-        return methodToTest.getName().equals("getClassNull")
-	    || methodToTest.getName().startsWith("isA")
-	    || methodToTest.getName().equals("create");
-    }
+                Method methods[] = Facade.class.getDeclaredMethods();
 
-    /**
-     * @see junit.framework.TestCase#runTest()
-     */
-    protected void runTest() throws Throwable {
-        if (!methodForbidden()) {
-            if (methodToTest.getParameterTypes() != null
-		&& methodToTest.getParameterTypes().length >= 1
-		&& Modifier.isPublic(methodToTest.getModifiers())) {
-                testOneOrMoreParameters();
-            }
-        }
-    }
-
-    /**
-     * testOneOrMoreParameters checks whether a public method in the
-     * Model subsystem Facade throws an IllegalArgumentException when
-     * invoked with stupid arguments. Stupid arguments are arguments 
-     * which are not from the UML domain, such as a plain Object.
-     */
-    public void testOneOrMoreParameters() {
-        int nrParameters = methodToTest.getParameterTypes().length;
-        Class[] params = methodToTest.getParameterTypes();
-        Object[] foo = new Object[nrParameters ];
-
-        // set up all parameters. Some methods are invoked with
-        // primitives or collections, so we need to create them
-        // accordingly
-        for (int i = 0; i < nrParameters; i++) {
-            try {
-                if (params[i].isPrimitive()) {
-                    String primitiveName = params[i].getName();
-                    if (primitiveName.equals("int")) {
-                        foo[i] = new Integer(0);
-                    }
-                    if (primitiveName.equals("boolean")) {
-                        foo[i] = new Boolean(true);
-                    }
-                    if (primitiveName.equals("short")) {
-                        foo[i] = new Short("0");
-                    }
-                } else if (params[i].getName().equals("java.util.Collection")) {
-                    foo[i] = new ArrayList();
-                } else {
-                    // this call could easily fall if there is e.g. no
-                    // public default constructor. If it fails tweak the
-                    // if/else tree above to accomodate the parameter
-                    // or check if we need to test the particular method
-                    // at all.
-                    foo[i] = params[i].newInstance();
+                for (int i = 0; i < methods.length; i++) {
+                        suite.addTest(new TestModelFacade3(methods[i]));
                 }
-            } catch (InstantiationException e) {
-                fail("Cannot create an instance of : "
-		     + params[i].getName() + ", required for "
-		     + methodToTest.getName() + ". Check if "
-		     + "test needs reworking.");
-            } catch (IllegalAccessException il) {
-                fail("Illegal Access to : " + params[i].getName());
-            }
+
+                return suite;
         }
 
-
-        try {
-            methodToTest.invoke(facade,  foo);
-            fail(methodToTest.getName()
-		 + " does not deliver an IllegalArgumentException");
-        } catch (InvocationTargetException e) {
-            if (e.getTargetException() instanceof IllegalArgumentException) {
-                return;
-            }
-            fail("Test failed for " + methodToTest.toString()
-		 + " because of: " + e.getTargetException());
-        } catch (Exception e) {
-            fail("Test failed for " + methodToTest.toString()
-		 + " because of: " + e.toString());
+        /**
+         * This method checks whether a test should be executed for a given
+         * method.
+         * 
+         * @return true if method is not eligible for testing (e.g. helper
+         *         methods)
+         */
+        protected boolean methodForbidden() {
+                return methodToTest.getName().equals("getClassNull")
+                                || methodToTest.getName().startsWith("isA")
+                                || methodToTest.getName().equals("create")
+                                || methodToTest.getName().equals("getTipString");
         }
 
-    }
+        /**
+         * @see junit.framework.TestCase#runTest()
+         */
+        protected void runTest() throws Throwable {
+                if (!methodForbidden()) {
+                        if (methodToTest.getParameterTypes() != null
+                                        && methodToTest.getParameterTypes().length >= 1
+                                        && Modifier.isPublic(methodToTest
+                                                        .getModifiers())) {
+                                testOneOrMoreParameters();
+                        }
+                }
+        }
 
-    /**
-     * @see junit.framework.TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        super.setUp();
-        facade = Model.getFacade();
-        assertNotNull("Cound not get Facade", facade);
-    }
+        /**
+         * testOneOrMoreParameters checks whether a public method in the Model
+         * subsystem Facade throws an IllegalArgumentException when invoked with
+         * stupid arguments. Stupid arguments are arguments which are not from
+         * the UML domain, such as a plain Object.
+         */
+        public void testOneOrMoreParameters() {
+                int nrParameters = methodToTest.getParameterTypes().length;
+                Class[] params = methodToTest.getParameterTypes();
+                Object[] foo = new Object[nrParameters];
+
+                // set up all parameters. Some methods are invoked with
+                // primitives or collections, so we need to create them
+                // accordingly
+                for (int i = 0; i < nrParameters; i++) {
+                        try {
+                                if (params[i].isPrimitive()) {
+                                        String primitiveName = params[i]
+                                                        .getName();
+                                        if (primitiveName.equals("int")) {
+                                                foo[i] = new Integer(0);
+                                        }
+                                        if (primitiveName.equals("boolean")) {
+                                                foo[i] = new Boolean(true);
+                                        }
+                                        if (primitiveName.equals("short")) {
+                                                foo[i] = new Short("0");
+                                        }
+                                } else if (params[i].getName().equals(
+                                                "java.util.Collection")) {
+                                        foo[i] = new ArrayList();
+                                } else {
+                                        // this call could easily fall if there
+                                        // is e.g. no
+                                        // public default constructor. If it
+                                        // fails tweak the
+                                        // if/else tree above to accomodate the
+                                        // parameter
+                                        // or check if we need to test the
+                                        // particular method
+                                        // at all.
+                                        foo[i] = params[i].newInstance();
+                                }
+                        } catch (InstantiationException e) {
+                                fail("Cannot create an instance of : "
+                                                + params[i].getName()
+                                                + ", required for "
+                                                + methodToTest.getName()
+                                                + ". Check if "
+                                                + "test needs reworking.");
+                        } catch (IllegalAccessException il) {
+                                fail("Illegal Access to : "
+                                                + params[i].getName());
+                        }
+                }
+
+                try {
+                        methodToTest.invoke(facade, foo);
+                        fail(methodToTest.getName()
+                                        + " does not deliver an IllegalArgumentException");
+                } catch (InvocationTargetException e) {
+                        if (e.getTargetException() instanceof IllegalArgumentException) {
+                                return;
+                        }
+                        fail("Test failed for " + methodToTest.toString()
+                                        + " because of: "
+                                        + e.getTargetException());
+                } catch (Exception e) {
+                        fail("Test failed for " + methodToTest.toString()
+                                        + " because of: " + e.toString());
+                }
+
+        }
+
+        /**
+         * @see junit.framework.TestCase#setUp()
+         */
+        protected void setUp() throws Exception {
+                super.setUp();
+                facade = Model.getFacade();
+                assertNotNull("Cound not get Facade", facade);
+        }
 }
