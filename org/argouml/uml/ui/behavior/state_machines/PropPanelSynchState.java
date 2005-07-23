fc35403baaf0891b34058cb362a635b87b9a1f29
Michiel van der Wulp <mvw@tigris.org>
2005-07-23 16:57:43 +0000

    Corrected a typo.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@8661 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/behavior/state_machines/PropPanelSynchState.java
+++ org/argouml/uml/ui/behavior/state_machines/PropPanelSynchState.java
@@ -25,8 +25,8 @@
 package org.argouml.uml.ui.behavior.state_machines;
 
 import org.argouml.i18n.Translator;
-import org.argouml.util.ConfigLoader;
 import org.argouml.uml.ui.UMLTextField2;
+import org.argouml.util.ConfigLoader;
 
 /**
  * The properties panel for a SynchState.
@@ -42,8 +42,9 @@ public class PropPanelSynchState extends PropPanelStateVertex {
      * The constructor.
      */
     public PropPanelSynchState() {
-        super("Synch State", lookupIcon("SyncState"),
-                ConfigLoader.getTabPropsOrientation());
+        super("Synch State", 
+            lookupIcon("SynchState"),
+            ConfigLoader.getTabPropsOrientation());
 
         addField(Translator.localize("label.name"),
                 getNameTextField());
