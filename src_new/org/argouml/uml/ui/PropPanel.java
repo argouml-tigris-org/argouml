ea8751890d118ec5c35d085221f9d4587041275e
Markus Klink <mkl@tigris.org>
2005-09-08 18:10:50 +0000

    introduce method to add actions to the buttonpanel
    Issue number:  3260
    Obtained from:
    Submitted by:
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: Issue number:
    CVS:   If this change addresses one or more issues,
    CVS:   then enter the issue number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to the project by someone else; i.e.,
    CVS:   they sent us a patch or a set of diffs, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8834 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/PropPanel.java
+++ src_new/org/argouml/uml/ui/PropPanel.java
@@ -30,20 +30,23 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.util.Iterator;
 
+import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.event.EventListenerList;
 
+import org.apache.log4j.Logger;
 import org.argouml.application.helpers.ResourceLoaderWrapper;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.ContainerDispatcher;
 import org.argouml.model.Model;
-import org.argouml.ui.LookAndFeelMgr;
 import org.argouml.ui.AbstractArgoJPanel;
+import org.argouml.ui.LookAndFeelMgr;
 import org.argouml.ui.targetmanager.TargetEvent;
 import org.argouml.ui.targetmanager.TargetListener;
 import org.argouml.ui.targetmanager.TargetManager;
@@ -56,24 +59,27 @@ import org.tigris.swidgets.Vertical;
 import org.tigris.toolbar.ToolBar;
 
 /**
- * This abstract class provides the basic layout and event dispatching
- * support for all Property Panels.<p>
- *
- * The property panel is {@link org.tigris.swidgets.LabelledLayout} layed out
- * as a number (specified in the constructor) of equally sized panels
- * that split the available space.  Each panel has a column of
- * "captions" and matching column of "fields" which are laid out
- * indepently from the other panels.<p>
- *
- * The Properties panels for UML Model Elements are structured in an
- * inheritance hierarchy that matches the UML 1.3 metamodel.
+ * This abstract class provides the basic layout and event dispatching support
+ * for all Property Panels.
+ * <p>
+ * The property panel is {@link org.tigris.swidgets.LabelledLayout} layed out as
+ * a number (specified in the constructor) of equally sized panels that split
+ * the available space. Each panel has a column of "captions" and matching
+ * column of "fields" which are laid out indepently from the other panels.
+ * <p>
+ * The Properties panels for UML Model Elements are structured in an inheritance
+ * hierarchy that matches the UML 1.3 metamodel.
  */
-public abstract class PropPanel
-    extends AbstractArgoJPanel
-    implements TabModelTarget, UMLUserInterfaceContainer {
-    ////////////////////////////////////////////////////////////////
-    // instance vars
+public abstract class PropPanel extends AbstractArgoJPanel implements
+        TabModelTarget, UMLUserInterfaceContainer {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = Logger.getLogger(PropPanel.class);
+
     private Object target;
+
     private Object modelElement;
 
     private EventListenerList listenerList;
@@ -85,28 +91,31 @@ public abstract class PropPanel
     private Font smallFont = LookAndFeelMgr.getInstance().getSmallFont();
 
     // TODO: This should request a ComponentDispatcher from the model component
-    ContainerDispatcher componentDispatcher = Model.createContainerDispatcher(this);
-    
+    ContainerDispatcher componentDispatcher = Model
+            .createContainerDispatcher(this);
+
     /**
-     * Construct new PropPanel using LabelledLayout.<p>
-     *
-     * @param icon The icon to display for the panel
-     * @param title The title of the panel
-     *
-     * @param orientation the orientation
+     * Construct new PropPanel using LabelledLayout.
+     * <p>
+     * 
+     * @param icon
+     *            The icon to display for the panel
+     * @param title
+     *            The title of the panel
+     * @param orientation
+     *            the orientation
      */
     public PropPanel(String title, ImageIcon icon, Orientation orientation) {
         super(title);
         setOrientation(orientation);
         buttonPanel = new ToolBar();
-        buttonPanel.putClientProperty("JToolBar.isRollover",  Boolean.TRUE);
+        buttonPanel.putClientProperty("JToolBar.isRollover", Boolean.TRUE);
         buttonPanel.setFloatable(false);
 
         setLayout(new LabelledLayout(orientation == Vertical.getInstance()));
 
         if (icon != null) {
-            titleLabel =
-                new JLabel(title, icon, SwingConstants.LEFT);
+            titleLabel = new JLabel(title, icon, SwingConstants.LEFT);
         } else {
             titleLabel = new JLabel(title);
         }
@@ -117,17 +126,21 @@ public abstract class PropPanel
 
     /**
      * Constructs a new Proppanel without an icon. If there is an icon it's
-     * updated at runtime via settarget.<p>
-     *
-     * @param title the title
-     * @param orientation the orientation
+     * updated at runtime via settarget.
+     * <p>
+     * 
+     * @param title
+     *            the title
+     * @param orientation
+     *            the orientation
      */
     public PropPanel(String title, Orientation orientation) {
-        this (title, null, orientation);
+        this(title, null, orientation);
     }
 
     /**
      * Get the ComponentDispatcher for thie prop panel.
+     * 
      * @return the ComponentDispatcher
      */
     public ContainerDispatcher getContainerDispatcher() {
@@ -136,7 +149,7 @@ public abstract class PropPanel
 
     /**
      * Set the orientation of the panel.
-     *
+     * 
      * @see org.tigris.swidgets.Orientable#setOrientation(org.tigris.swidgets.Orientation)
      */
     public void setOrientation(Orientation orientation) {
@@ -144,18 +157,29 @@ public abstract class PropPanel
     }
 
     /**
-     * @param button the button to be added to the button panel
+     * @param button
+     *            the button to be added to the button panel
      */
     protected void addButton(Component button) {
-        button.setFocusable(false); //Buttons shall not go in the tabbing loop
+        button.setFocusable(false); // Buttons shall not go in the tabbing loop
         buttonPanel.add(button);
+    }
+    
+    protected void addAction(Action action) {
+        JButton button = new JButton(action);
+        button.setText("");
+        button.setFocusable(false);
+        buttonPanel.add(button);   
     }
 
     /**
-     * Add a component with the specified label.<p>
-     *
-     * @param label the label for the component
-     * @param component the component
+     * Add a component with the specified label.
+     * <p>
+     * 
+     * @param label
+     *            the label for the component
+     * @param component
+     *            the component
      * @return the label added
      */
     public JLabel addField(String label, Component component) {
@@ -169,17 +193,19 @@ public abstract class PropPanel
     }
 
     /**
-     * Add a component with the specified label positioned after
-     * another component.
-     *
-     * @param label the label for the component
-     * @param component the component
-     *
-     * @param afterComponent the component before
+     * Add a component with the specified label positioned after another
+     * component.
+     * 
+     * @param label
+     *            the label for the component
+     * @param component
+     *            the component
+     * @param afterComponent
+     *            the component before
      * @return the newly added label
      */
     public JLabel addFieldAfter(String label, Component component,
-				Component afterComponent) {
+            Component afterComponent) {
         int nComponent = this.getComponentCount();
         for (int i = 0; i < nComponent; ++i) {
             if (getComponent(i) == afterComponent) {
@@ -196,17 +222,20 @@ public abstract class PropPanel
     }
 
     /**
-     * Add a component with the specified label positioned before
-     * another component.<p>
-     *
-     * @param label the label for the component
-     * @param component the component
-     * @param beforeComponent the component
-     *
+     * Add a component with the specified label positioned before another
+     * component.
+     * <p>
+     * 
+     * @param label
+     *            the label for the component
+     * @param component
+     *            the component
+     * @param beforeComponent
+     *            the component
      * @return the newly added component
      */
     public JLabel addFieldBefore(String label, Component component,
-				 Component beforeComponent) {
+            Component beforeComponent) {
         int nComponent = this.getComponentCount();
         for (int i = 0; i < nComponent; ++i) {
             if (getComponent(i) == beforeComponent) {
@@ -230,14 +259,18 @@ public abstract class PropPanel
     }
 
     /**
-     * Set the target to be associated with a particular property panel.<p>
-     *
-     * This involves resetting the third party listeners.<p>
-     * @deprecated As Of Argouml version 0.13.5,
-     *             This will change visibility from release 0.16
-     * @param t  The object to be set as a target.
+     * Set the target to be associated with a particular property panel.
+     * <p>
+     * This involves resetting the third party listeners.
+     * <p>
+     * 
+     * @deprecated As Of Argouml version 0.13.5, This will change visibility
+     *             from release 0.16
+     * @param t
+     *            The object to be set as a target.
      */
     public void setTarget(Object t) {
+        LOG.debug("setTarget called with " + t + " as parameter (not target!)");
         t = (t instanceof Fig) ? ((Fig) t).getOwner() : t;
 
         // If the target has changed notify the third party listener if it
@@ -260,22 +293,20 @@ public abstract class PropPanel
 
             // This will add a new MElement listener after update is complete
 
-            dispatch =
-                new UMLChangeDispatch(this,
-				      UMLChangeDispatch.TARGET_CHANGED_ADD);
+            dispatch = new UMLChangeDispatch(this,
+                    UMLChangeDispatch.TARGET_CHANGED_ADD);
 
         } else {
-            dispatch =
-                new UMLChangeDispatch(this, UMLChangeDispatch.TARGET_REASSERTED);
+            dispatch = new UMLChangeDispatch(this,
+                    UMLChangeDispatch.TARGET_REASSERTED);
 
         }
         SwingUtilities.invokeLater(dispatch);
 
         // update the titleLabel
-        //MVW: This overrules the icon set initiallly... Why do we need this?
+        // MVW: This overrules the icon set initiallly... Why do we need this?
         if (titleLabel != null) {
-            Icon icon =
-                ResourceLoaderWrapper.getInstance().lookupIcon(t);
+            Icon icon = ResourceLoaderWrapper.getInstance().lookupIcon(t);
             if (icon != null) {
                 titleLabel.setIcon(icon);
             }
@@ -285,10 +316,11 @@ public abstract class PropPanel
     /**
      * Builds a eventlistenerlist of all targetlisteners that are part of this
      * container and its children.
-     *
-     * @param container the container to search for targetlisteners
-     * @return an EventListenerList with all TargetListeners on this
-     * container and its children.
+     * 
+     * @param container
+     *            the container to search for targetlisteners
+     * @return an EventListenerList with all TargetListeners on this container
+     *         and its children.
      */
     private EventListenerList registrateTargetListeners(Container container) {
         Component[] components = container.getComponents();
@@ -299,12 +331,11 @@ public abstract class PropPanel
             }
             if (components[i] instanceof TargettableModelView) {
                 list.add(TargetListener.class,
-			 ((TargettableModelView) components[i])
-			     .getTargettableModel());
+                        ((TargettableModelView) components[i])
+                                .getTargettableModel());
             }
             if (components[i] instanceof Container) {
-                EventListenerList list2 =
-		    registrateTargetListeners((Container) components[i]);
+                EventListenerList list2 = registrateTargetListeners((Container) components[i]);
                 Object[] objects = list2.getListenerList();
                 for (int j = 1; j < objects.length; j += 2) {
                     list.add(TargetListener.class, (TargetListener) objects[j]);
@@ -320,7 +351,6 @@ public abstract class PropPanel
     public final Object getTarget() {
         return target;
     }
-
 
     /**
      * @see org.argouml.ui.TabTarget#refresh()
@@ -338,10 +368,9 @@ public abstract class PropPanel
     }
 
     /**
-     * This method can be overriden in derived Panels where the
-     * appropriate namespace for display may not be the same as
-     * the namespace of the target.
-     *
+     * This method can be overriden in derived Panels where the appropriate
+     * namespace for display may not be the same as the namespace of the target.
+     * 
      * @return the namespace
      */
     protected Object getDisplayNamespace() {
@@ -370,14 +399,14 @@ public abstract class PropPanel
     /**
      * @see org.argouml.uml.ui.UMLUserInterfaceContainer#formatElement(java.lang.Object)
      */
-    public String formatElement(/*MModelElement*/Object element) {
+    public String formatElement(/* MModelElement */Object element) {
         return getProfile().formatElement(element, getDisplayNamespace());
     }
 
     /**
      * @see org.argouml.uml.ui.UMLUserInterfaceContainer#formatNamespace(java.lang.Object)
      */
-    public String formatNamespace(/*MNamespace*/Object namespace) {
+    public String formatNamespace(/* MNamespace */Object namespace) {
         return getProfile().formatElement(namespace, null);
     }
 
@@ -395,8 +424,8 @@ public abstract class PropPanel
     public void removeElement() {
         Object theTarget = getTarget();
         if (Model.getFacade().isABase(theTarget)) {
-            Object newTarget =
-                Model.getFacade().getModelElementContainer(theTarget);
+            Object newTarget = Model.getFacade().getModelElementContainer(
+                    theTarget);
             Object base = theTarget;
             TargetManager.getInstance().setTarget(base);
             ActionEvent event = new ActionEvent(this, 1, "delete");
@@ -408,18 +437,15 @@ public abstract class PropPanel
     }
 
     /**
-     * Check whether this element can be deleted.
-     * Currently it only checks whether we delete the main model.
-     * ArgoUML does not like that.
-     *
+     * Check whether this element can be deleted. Currently it only checks
+     * whether we delete the main model. ArgoUML does not like that.
+     * 
      * @since 0.13.2
      * @return whether this element can be deleted
      */
     public boolean isRemovableElement() {
-        return ((getTarget() != null)
-		&& (getTarget()
-		    != (ProjectManager.getManager().getCurrentProject()
-			.getModel())));
+        return ((getTarget() != null) && (getTarget() != (ProjectManager
+                .getManager().getCurrentProject().getModel())));
     }
 
     /**
@@ -427,8 +453,8 @@ public abstract class PropPanel
      */
     public void targetAdded(TargetEvent e) {
         // we can neglect this, the TabProps allways selects the first target
-	// in a set of targets. The first target can only be
-	// changed in a targetRemoved or a TargetSet event
+        // in a set of targets. The first target can only be
+        // changed in a targetRemoved or a TargetSet event
         if (listenerList == null) {
             listenerList = registrateTargetListeners(this);
         }
@@ -444,7 +470,6 @@ public abstract class PropPanel
         fireTargetRemoved(e);
         // setTarget(e.getNewTarget());
 
-
     }
 
     /**
@@ -457,47 +482,48 @@ public abstract class PropPanel
     }
 
     private void fireTargetSet(TargetEvent targetEvent) {
-      if ( listenerList==null)
-        listenerList=registrateTargetListeners( this);
-	//          Guaranteed to return a non-null array
-	Object[] listeners = listenerList.getListenerList();
-	for (int i = listeners.length - 2; i >= 0; i -= 2) {
-	    if (listeners[i] == TargetListener.class) {
-		// Lazily create the event:
-		((TargetListener) listeners[i + 1]).targetSet(targetEvent);
-	    }
-	}
+        if (listenerList == null)
+            listenerList = registrateTargetListeners(this);
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TargetListener.class) {
+                // Lazily create the event:
+                ((TargetListener) listeners[i + 1]).targetSet(targetEvent);
+            }
+        }
     }
 
     private void fireTargetAdded(TargetEvent targetEvent) {
-        if ( listenerList==null)
-          listenerList=registrateTargetListeners( this);
-	// Guaranteed to return a non-null array
-	Object[] listeners = listenerList.getListenerList();
+        if (listenerList == null)
+            listenerList = registrateTargetListeners(this);
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
 
-	for (int i = listeners.length - 2; i >= 0; i -= 2) {
-	    if (listeners[i] == TargetListener.class) {
-		// Lazily create the event:
-		((TargetListener) listeners[i + 1]).targetAdded(targetEvent);
-	    }
-	}
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TargetListener.class) {
+                // Lazily create the event:
+                ((TargetListener) listeners[i + 1]).targetAdded(targetEvent);
+            }
+        }
     }
 
     private void fireTargetRemoved(TargetEvent targetEvent) {
-      if ( listenerList==null)
-        listenerList=registrateTargetListeners( this);
-	// Guaranteed to return a non-null array
-	Object[] listeners = listenerList.getListenerList();
-	for (int i = listeners.length - 2; i >= 0; i -= 2) {
-	    if (listeners[i] == TargetListener.class) {
-		// Lazily create the event:
-		((TargetListener) listeners[i + 1]).targetRemoved(targetEvent);
-	    }
-	}
+        if (listenerList == null)
+            listenerList = registrateTargetListeners(this);
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TargetListener.class) {
+                // Lazily create the event:
+                ((TargetListener) listeners[i + 1]).targetRemoved(targetEvent);
+            }
+        }
     }
 
     /**
-     * @param theTitleLabel the title of the panel shown at the top
+     * @param theTitleLabel
+     *            the title of the panel shown at the top
      */
     protected void setTitleLabel(JLabel theTitleLabel) {
         this.titleLabel = theTitleLabel;
@@ -512,8 +538,9 @@ public abstract class PropPanel
 
     /**
      * Look up an icon.
-     *
-     * @param name the resource name.
+     * 
+     * @param name
+     *            the resource name.
      * @return an ImageIcon corresponding to the given resource name
      */
     protected static ImageIcon lookupIcon(String name) {
