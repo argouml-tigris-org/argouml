15d6234f502468ec8a4f2c2ff152ae8ea49c3022
Linus Tolke <linus@tigris.org>
2005-09-13 17:07:19 +0000

    Patched so that it will work easier on MacOS.
    Contributed by Ludovic Maitre.
    
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8866 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/build.sh
+++ src_new/build.sh
@@ -1,17 +1,30 @@
 #! /bin/sh
 # $Id$
 #
- 
+
+# OS specific support.
+darwin=false
+
+case "`uname`" in
+		Darwin*) darwin=true;;
+esac
+
 # +-------------------------------------------------------------------------+
 # | Verify and Set Required Environment Variables                           |
 # +-------------------------------------------------------------------------+
 if [ "$JAVA_HOME" = "" ] ; then
-	echo "***************************************************************"
-	echo "  ERROR: JAVA_HOME environment variable not found."
-	echo ""
-	echo "  Please set JAVA_HOME to the Java JDK installation directory."
-	echo "***************************************************************"
-	exit 1
+	if $darwin; then
+		# Set Java Home automatically
+        JAVA_HOME=/Library/Java/Home
+		export JAVA_HOME
+	else
+		echo "***************************************************************"
+		echo "  ERROR: JAVA_HOME environment variable not found."
+		echo ""
+		echo "  Please set JAVA_HOME to the Java JDK installation directory."
+		echo "***************************************************************"
+		exit 1
+	fi
 fi
 
 #
