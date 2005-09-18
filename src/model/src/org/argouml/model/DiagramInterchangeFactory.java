0cd6f8d4e6f5121b2d788b51b01988a50d13bda8
Linus Tolke <linus@tigris.org>
2005-09-18 21:37:05 +0000

    Style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@8882 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/model/src/org/argouml/model/DiagramInterchangeFactory.java
+++ src/model/src/org/argouml/model/DiagramInterchangeFactory.java
@@ -1,3 +1,27 @@
+// $Id$
+// Copyright (c) 2005 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
 package org.argouml.model;
 
 import java.util.Collection;
@@ -5,177 +29,185 @@ import java.util.List;
 
 public interface DiagramInterchangeFactory {
 
-	public abstract List getModelDiagram();
+    List getModelDiagram();
 
-	public abstract void setModelDiagram(List diagrams);
+    void setModelDiagram(List diagrams);
 
-	/**
-	 * Return true of the given ModelElement is the owner of the diagram.
-	 */
-	public abstract boolean isDiagramOwner(Object modelElement, Object diagram);
+    /**
+     * Return true of the given ModelElement is the owner of the diagram.
+     */
+    boolean isDiagramOwner(Object modelElement, Object diagram);
 
-	/**
-	 * Return true of the given ModelElement is the owner of the diagram.
-	 */
-	public abstract Object getDiagramElementOwner(Object diagram);
+    /**
+     * Return true of the given ModelElement is the owner of the diagram.
+     */
+    Object getDiagramElementOwner(Object diagram);
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createDiagram();
+    /**
+     *
+     * @return
+     */
+    Object createDiagram();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createDiagramLink();
+    /**
+     *
+     * @return
+     */
+    Object createDiagramLink();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createEllipse();
+    /**
+     * 
+     * @return
+     */
+    Object createEllipse();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createGraphConnector();
+    /**
+     * 
+     * @return
+     */
+    Object createGraphConnector();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createGraphEdge();
+    /**
+     *
+     * @return
+     */
+    Object createGraphEdge();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createGraphNode();
+    /**
+     *
+     * @return
+     */
+    Object createGraphNode();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createImage();
+    /**
+     * 
+     * @return
+     */
+    Object createImage();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createPolyline();
+    /**
+     * 
+     * @return
+     */
+    Object createPolyline();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createProperty();
+    /**
+     * 
+     * @return
+     */
+    Object createProperty();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createReference();
+    /**
+     * 
+     * @return
+     */
+    Object createReference();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createSimpleSemanticModelElement();
+    /**
+     * 
+     * @return
+     */
+    Object createSimpleSemanticModelElement();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createTextElement();
+    /**
+     * 
+     * @return
+     */
+    Object createTextElement();
 
-	/**
-	 * 
-	 * @return
-	 */
-	public abstract Object createUml1SemanticModelBridge();
+    /**
+     * 
+     * @return
+     */
+    Object createUml1SemanticModelBridge();
 
-	/**
-	 * Creates an instance of BezierPoint structure type.
-	 * @param base
-	 * @param control1
-	 * @param control2
-	 * @return
-	 */
-	public abstract Object createBezierPoint(Object base, Object control1,
-			Object control2);
+    /**
+     * Creates an instance of BezierPoint structure type.
+     * @param base
+     * @param control1
+     * @param control2
+     * @return
+     */
+    Object createBezierPoint(Object base, Object control1,
+            Object control2);
 
-	/**
-	 * Creates an instance of Dimension structure type.
-	 * @param width
-	 * @param height
-	 * @return
-	 */
-	public abstract Object createDimension(double width, double height);
+    /**
+     * Creates an instance of Dimension structure type.
+     * @param width
+     * @param height
+     * @return
+     */
+    Object createDimension(double width, double height);
 
-	/**
-	 * Creates an instance of Point structure type.
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public abstract Object createPoint(double x, double y);
+    /**
+     * Creates an instance of Point structure type.
+     * @param x
+     * @param y
+     * @return
+     */
+    Object createPoint(double x, double y);
 
-	/**
-	 * 
-	 * @param modelElement
-	 * @param diagram
-	 * @return
-	 */
-	public abstract Object buildUml1SemanticModelBridge(Object modelElement,
-			Object diagramOrGraphElement);
+    /**
+     *
+     * @param modelElement
+     * @param diagram
+     * @return
+     */
+    Object buildUml1SemanticModelBridge(Object modelElement,
+            Object diagramOrGraphElement);
 
-	public abstract Object buildSimpleSemanticModelElement(Object grafElement,
-			Object diagram, String presentation, String typeInfo);
+    Object buildSimpleSemanticModelElement(Object grafElement,
+            Object diagram, String presentation, String typeInfo);
 
-	/**
-	 * Build a diagram
-	 * @param model
-	 * @return A new diagram
-	 */
-	public abstract Object buildDiagram(Object model);
+    /**
+     * Build a diagram.
+     *
+     * @param model
+     * @return A new diagram
+     */
+    Object buildDiagram(Object model);
 
-	public abstract Object buildGraphNode(Object parentGraphElement,
-			Object modelElement);
+    Object buildGraphNode(Object parentGraphElement,
+            Object modelElement);
 
-	public abstract Object buildProperty(String key, String value);
+    Object buildProperty(String key, String value);
 
-	// ADiagramElementProperty delegate methods
+    // ADiagramElementProperty delegate methods
 
-	/* (non-Javadoc)
-	 * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#add(org.omg.uml.diagraminterchange.DiagramElement, org.omg.uml.diagraminterchange.Property)
-	 */
-	public abstract boolean addProperty(Object arg0, Object arg1);
+    /**
+     * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#add(
+     *         org.omg.uml.diagraminterchange.DiagramElement,
+     *         org.omg.uml.diagraminterchange.Property)
+     */
+    boolean addProperty(Object arg0, Object arg1);
 
-	/* (non-Javadoc)
-	 * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#exists(org.omg.uml.diagraminterchange.DiagramElement, org.omg.uml.diagraminterchange.Property)
-	 */
-	public abstract boolean existsProperty(Object arg0, Object arg1);
+    /**
+     * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#exists(
+     *         org.omg.uml.diagraminterchange.DiagramElement,
+     *         org.omg.uml.diagraminterchange.Property)
+     */
+    boolean existsProperty(Object arg0, Object arg1);
 
-	/* (non-Javadoc)
-	 * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#getProperty(org.omg.uml.diagraminterchange.DiagramElement)
-	 */
-	public abstract Collection getProperties(Object arg0);
+    /**
+     * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#getProperty(
+     *         org.omg.uml.diagraminterchange.DiagramElement)
+     */
+    Collection getProperties(Object arg0);
 
-	/* (non-Javadoc)
-	 * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#remove(org.omg.uml.diagraminterchange.DiagramElement, org.omg.uml.diagraminterchange.Property)
-	 */
-	public abstract boolean removeProperty(Object arg0, Object arg1);
+    /**
+     * @see org.omg.uml.diagraminterchange.ADiagramElementProperty#remove(
+     *         org.omg.uml.diagraminterchange.DiagramElement,
+     *         org.omg.uml.diagraminterchange.Property)
+     */
+    boolean removeProperty(Object arg0, Object arg1);
 
-	//easy interface to properties
-	public abstract boolean hasProperty(Object diagramElement,
-			String propertyName);
+    //easy interface to properties
+    boolean hasProperty(Object diagramElement,
+            String propertyName);
 
-	public abstract void setProperty(Object diagramElement, String key,
-			String value);
+    void setProperty(Object diagramElement, String key,
+            String value);
 
-	public abstract String getProperty(Object diagramElement, String key);
+    String getProperty(Object diagramElement, String key);
 
 }
