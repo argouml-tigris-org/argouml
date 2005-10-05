d9eec460e4d5eb34bbd100bd71de7be1beb123a0
Bob Tarling <bobtarling@tigris.org>
2005-10-05 17:36:26 +0000

    Add a temporoary utility to indicate if we're still running NSUML
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@8954 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ProfileJava.java
+++ org/argouml/uml/ProfileJava.java
@@ -29,6 +29,7 @@ import java.io.InputStream;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
+import org.argouml.kernel.NsumlEnabler;
 import org.argouml.model.Model;
 import org.argouml.model.UmlException;
 import org.argouml.model.XmiReader;
@@ -293,18 +294,10 @@ public class ProfileJava extends Profile {
         //
         String modelFileName = System.getProperty("argo.defaultModel");
         
-        // if we use the NSUML implementation use the file default.xmi
-        String nsumlImpl = "org.argouml.model.uml.NSUMLModelImplementation";
-        String mdrImpl = "org.argouml.model.mdr.MDRModelImplementation";
-        
         if (modelFileName == null) {
-            //TODO: Remove the ",nsumlImpl" when we will switch the 
-            //default metamodel to UML 1.4/MDR.
-            if (nsumlImpl.equals(System.getProperty(
-                    "argouml.model.implementation", nsumlImpl))) {
+            if (NsumlEnabler.isNsuml()) {
                 modelFileName = "/org/argouml/default.xmi";           		
-            } else if (mdrImpl.equals(System.getProperty(
-                "argouml.model.implementation"))) {
+            } else {
                 modelFileName = "/org/argouml/model/mdr/mof/default-uml14.xmi";
             }
         }
