4424f7c81451809260623e4b80f13ba4f8cd3db1
Markus Klink <mkl@tigris.org>
2005-10-10 12:47:43 +0000

    remove anon classes
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@8985 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/behavior/use_cases/PropPanelUseCase.java
+++ org/argouml/uml/ui/behavior/use_cases/PropPanelUseCase.java
@@ -110,41 +110,6 @@ public class PropPanelUseCase extends PropPanelClassifier {
 
 
     /**
-     * <p>Invoked by the "Add use case" toolbar button to create a new use case
-     *   property panel in the same namespace as the current use case.</p>
-     *
-     * <p>This code uses getFactory and adds the use case explicitly to the
-     *   namespace. Extended to actually navigate to the new use case.</p>
-     */
-    private class ActionNewUseCase extends AbstractActionNewModelElement {
-
-        /**
-         * The constructor.
-         */
-        public ActionNewUseCase() {
-            super("button.new-usecase");
-            putValue(Action.NAME, Translator.localize("button.new-usecase"));
-        }
-
-        /**
-         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-         */
-        public void actionPerformed(ActionEvent e) {
-            Object target = TargetManager.getInstance().getModelTarget();
-            if (Model.getFacade().isAUseCase(target)) {
-                Object ns = Model.getFacade().getNamespace(target);
-                if (ns != null) {
-                    Object useCase = Model.getUseCasesFactory()
-                        .createUseCase();
-                    Model.getCoreHelper().addOwnedElement(ns, useCase);
-                    TargetManager.getInstance().setTarget(useCase);
-                    super.actionPerformed(e);
-                }
-            }
-        }
-    }
-
-    /**
      * <p>Invoked by the "New Extension Point" toolbar button to create a new
      *   extension point for this use case in the same namespace as the current
      *   use case.</p>
