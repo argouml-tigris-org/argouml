df914d53f0aa1e9f238b5e30e4ccd6504959aa4b
Bob Tarling <bobtarling@tigris.org>
2005-10-12 15:39:11 +0000

    A stereotype should be allowed to take the full width of the stereotype container. That way the centering function of the stereotype allows it to position its text correctly.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@9030 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/ui/FigStereotypesCompartment.java
+++ src_new/org/argouml/uml/diagram/ui/FigStereotypesCompartment.java
@@ -31,6 +31,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.argouml.application.api.Notation;
 import org.argouml.application.api.NotationContext;
+import org.argouml.kernel.SingleStereotypeEnabler;
 import org.argouml.language.helpers.NotationHelper;
 import org.argouml.model.Model;
 import org.argouml.uml.diagram.static_structure.ui.FigFeature;
@@ -172,6 +173,30 @@ public class FigStereotypesCompartment extends FigCompartment {
                     removeFig((Fig) figs.get(i));
                 }
             }
+        }
+    }
+    
+    protected void setBoundsImpl(int x, int y, int w, int h) {
+        if (SingleStereotypeEnabler.isEnabled()) {
+            super.setBoundsImpl(x, y, w, h);
+        } else {
+            int n = getFigs().size() - 1;
+            int newH = h;
+            
+            Iterator figs = iterator();
+            Fig fig;
+            int fw;
+            int yy = y;
+            while (figs.hasNext()) {
+                fig = (Fig) figs.next();
+                if (fig != getBigPort()) {
+                    fw = w - 2;
+                    fig.setBounds(x + 1, yy + 1, fw, fig.getMinimumSize().height);
+                    yy += fig.getMinimumSize().height;
+                }
+            }
+            getBigPort().setBounds(x, y, w, newH);
+            calcBounds();
         }
     }
     
