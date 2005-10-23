cf2fca751e1a620855d8c5b5524d6066a160497e
Ludovic Maître <rastaman@tigris.org>
2005-10-23 15:53:16 +0000

    Allow to set the context of an ActivityGraph or a StateMachine via a combobox.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9114 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/ui/behavior/state_machines/UMLStateMachineContextComboBoxModel.java
@@ -0,0 +1,125 @@
+// $Id$
+// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.ui.behavior.state_machines;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.argouml.kernel.Project;
+import org.argouml.kernel.ProjectManager;
+import org.argouml.kernel.SingleStereotypeEnabler;
+import org.argouml.model.Model;
+import org.argouml.uml.ui.UMLComboBoxModel2;
+import org.argouml.uml.ui.UMLModelElementListModel2;
+
+/**
+ * Listmodel for the context of a statemachine.
+ *
+ * @since Dec 6, 2002
+ * @author jaap.branderhorst@xs4all.nl
+ */
+public class UMLStateMachineContextComboBoxModel
+    extends  UMLComboBoxModel2  {
+
+    /**
+     * Constructor for UMLStateMachineContextListModel.
+     */
+    public UMLStateMachineContextComboBoxModel() {
+        super("context",false);
+    }
+
+    /**
+     * @see org.argouml.uml.ui.UMLModelElementListModel2#buildModelList()
+     */
+    protected void buildModelList() {
+        Set paths = new HashSet();
+        Set elements = new TreeSet(new Comparator() {
+            public int compare(Object o1, Object o2) {
+                try {
+                    String name1 = Model.getFacade().getName(o1);
+                    String name2 = Model.getFacade().getName(o2);
+                    name1 = (name1 != null ? name1 : "");
+                    name2 = (name2 != null ? name2 : "");
+
+                    return name1.compareTo(name2);
+                } catch (Exception e) {
+                    throw new ClassCastException(e.getMessage());
+                }
+            }
+        });
+        Project p = ProjectManager.getManager().getCurrentProject();
+        Iterator it = p.getUserDefinedModels().iterator();
+
+        while (it.hasNext()) {
+            Object model = /* (MModel) */it.next();
+
+            addAllUniqueModelElementsFrom(elements, paths, Model
+                    .getModelManagementHelper().getAllModelElementsOfKind(
+                            model, Model.getMetaTypes().getClassifier()));
+        }
+
+        addAll(elements);
+        //removeAllElements();
+        //addElement(Model.getFacade().getContext(getTarget()));
+    }
+
+    /**
+     * @see org.argouml.uml.ui.UMLComboBoxModel2#isValidElement(Object)
+     */
+    protected boolean isValidElement(Object element) {
+        return Model.getFacade().isAClass(element)
+                || Model.getFacade().isAUseCase(element);
+    }
+    
+    /**
+     * Helper method for buildModelList.
+     * <p>
+     * Adds those elements from source that do not have the same path as any
+     * path in paths to elements, and its path to paths. Thus elements will
+     * never contain two objects with the same path, unless they are added by
+     * other means.
+     */
+    private static void addAllUniqueModelElementsFrom(Set elements, Set paths,
+            Collection source) {
+        Iterator it2 = source.iterator();
+
+        while (it2.hasNext()) {
+            Object obj = it2.next();
+            Object path = Model.getModelManagementHelper().getPath(obj);
+            if (!paths.contains(path)) {
+                paths.add(path);
+                elements.add(obj);
+            }
+        }
+    }
+
+    protected Object getSelectedModelElement() {
+        return Model.getFacade().getContext(getTarget());
+    }
+}
