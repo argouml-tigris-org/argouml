620c95589b5f815183b75d5fcc2bc2fca0a6ded1
Ludovic Maître <rastaman@tigris.org>
2005-10-27 13:04:03 +0000

    Update edition of TaggedValues for UML 1.4. Please test before this evening.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@9158 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/TabTaggedValues.java
+++ src_new/org/argouml/uml/ui/TabTaggedValues.java
@@ -26,37 +26,25 @@ package org.argouml.uml.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.VetoableChangeListener;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Vector;
 
-import javax.swing.AbstractAction;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
-import javax.swing.SwingUtilities;
-import javax.swing.event.TableModelEvent;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableColumn;
 
-import org.argouml.application.helpers.ResourceLoaderWrapper;
+import org.apache.log4j.Logger;
 import org.argouml.i18n.Translator;
-import org.argouml.kernel.DelayedChangeNotify;
-import org.argouml.kernel.DelayedVChangeListener;
 import org.argouml.kernel.NsumlEnabler;
 import org.argouml.model.Model;
-import org.argouml.ui.LookAndFeelMgr;
 import org.argouml.ui.AbstractArgoJPanel;
+import org.argouml.ui.LookAndFeelMgr;
 import org.argouml.ui.targetmanager.TargetEvent;
+import org.argouml.uml.ui.foundation.extension_mechanisms.UMLTagDefinitionComboBoxModel;
 import org.tigris.gef.presentation.Fig;
 import org.tigris.toolbar.ToolBar;
 
@@ -66,15 +54,23 @@ import org.tigris.toolbar.ToolBar;
 public class TabTaggedValues extends AbstractArgoJPanel
     implements TabModelTarget {
 
+    private Logger LOG = Logger.getLogger(TabTaggedValues.class);
+    
+    static boolean UML_14 = Model.getMetaTypes().getTagDefinition()!=null;
+    
     ////////////////////////////////////////////////////////////////
     // instance variables
     private Object target;
-    private TableModelTaggedValues tableModel = null;
+    private TabTaggedValuesModel tableModel = null;
     private boolean shouldBeEnabled = false;
     private JTable table = new JTable(10, 2);
     private JLabel titleLabel;
     private JToolBar buttonPanel;
 
+    private UMLComboBox2 tagDefinitionsComboBox;
+ 
+    private UMLComboBoxModel2 tagDefinitionsComboBoxModel;
+    
     /**
      * The constructor.
      */
@@ -89,10 +85,22 @@ public class TabTaggedValues extends AbstractArgoJPanel
         b.setToolTipText(Translator.localize("button.delete"));
         b.setAction(new ActionRemoveTaggedValue(this));
 
-        tableModel = new TableModelTaggedValues(this);
+        tableModel = new TabTaggedValuesModel(this);
         table.setModel(tableModel);
-
         table.setRowSelectionAllowed(false);
+        if (UML_14) {
+            LOG.info("In UML 1.4 mode");
+            tagDefinitionsComboBoxModel = new UMLTagDefinitionComboBoxModel();
+            tagDefinitionsComboBox = new UMLComboBox2(tagDefinitionsComboBoxModel);
+            tagDefinitionsComboBox.setEditable(true);
+            tagDefinitionsComboBox.setRenderer(new UMLListCellRenderer2(false));
+            table.setDefaultEditor((Class)Model.getMetaTypes().getTagDefinition(), 
+                new DefaultCellEditor(tagDefinitionsComboBox));
+            table.setDefaultRenderer((Class)Model.getMetaTypes().getTagDefinition(),
+                    new UMLTableCellRenderer());
+        } else {
+            LOG.info("Not in UML 1.4 mode");
+        }
         JScrollPane sp = new JScrollPane(table);
         Font labelFont = LookAndFeelMgr.getInstance().getSmallFont();
         table.setFont(labelFont);
@@ -158,6 +166,10 @@ public class TabTaggedValues extends AbstractArgoJPanel
         //valCol.setMinWidth(250);
         //valCol.setWidth(550);
         //valCol.setPreferredWidth(550);
+
+        if (UML_14)
+            tagDefinitionsComboBoxModel.setTarget(t);
+        
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         
         tableModel.setTarget(target);
@@ -223,13 +235,12 @@ public class TabTaggedValues extends AbstractArgoJPanel
      */
     public void targetSet(TargetEvent e) {
         setTarget(e.getNewTarget());
-
     }
 
     /**
      * @return Returns the tableModel.
      */
-    protected TableModelTaggedValues getTableModel() {
+    protected TabTaggedValuesModel getTableModel() {
         return tableModel;
     }
     /**
@@ -240,241 +251,3 @@ public class TabTaggedValues extends AbstractArgoJPanel
     }
 } /* end class TabTaggedValues */
 
-
-
-
-/**
- * The model for the table with the tagged values.
- *
- */
-class TableModelTaggedValues extends AbstractTableModel
-    implements VetoableChangeListener,
-	       DelayedVChangeListener,
-	       PropertyChangeListener {
-
-    ////////////////
-    // instance varables
-    private Object target;		// ModelElement
-    private TabTaggedValues tab = null;
-
-    ////////////////
-    // constructor
-    /**
-     * The constructor.
-     *
-     * @param t the tab
-     */
-    public TableModelTaggedValues(TabTaggedValues t) {
-        tab = t;
-    }
-
-    ////////////////
-    // accessors
-    /**
-     * @param t the target modelelement
-     */
-    public void setTarget(Object t) {
-
-        if (!Model.getFacade().isAModelElement(t)) {
-            throw new IllegalArgumentException();
-        }
-
-	if (target != null) {
-	    Model.getPump().removeModelEventListener(this, target);
-	}
-	target = t;
-	Model.getPump().addModelEventListener(this, t);
-	fireTableDataChanged();
-	tab.resizeColumns();
-    }
-
-    ////////////////
-    // TableModel implemetation
-    /**
-     * @see javax.swing.table.TableModel#getColumnCount()
-     */
-    public int getColumnCount() { return 2; }
-
-    /**
-     * @see javax.swing.table.TableModel#getColumnName(int)
-     */
-    public String  getColumnName(int c) {
-	if (c == 0) {
-	    return Translator.localize("label.taggedvaluespane.tag");
-	}
-	if (c == 1) {
-	    return Translator.localize("label.taggedvaluespane.value");
-	}
-	return "XXX";
-    }
-
-    /**
-     * @see javax.swing.table.TableModel#getColumnClass(int)
-     */
-    public Class getColumnClass(int c) {
-	return String.class;
-    }
-
-    /**
-     * @see javax.swing.table.TableModel#isCellEditable(int, int)
-     */
-    public boolean isCellEditable(int row, int col) {
-	return true;
-    }
-
-    /**
-     * @see javax.swing.table.TableModel#getRowCount()
-     */
-    public int getRowCount() {
-	if (target == null || Model.getUmlFactory().isRemoved(target)) {
-	    return 0;
-	}
-	Collection tvs = Model.getFacade().getTaggedValuesCollection(target);
-	//if (tvs == null) return 1;
-	return tvs.size() + 1;
-    }
-
-    /**
-     * @see javax.swing.table.TableModel#getValueAt(int, int)
-     */
-    public Object getValueAt(int row, int col) {
-	Vector tvs =
-	    new Vector(Model.getFacade().getTaggedValuesCollection(target));
-	//if (tvs == null) return "";
-	if (row == tvs.size()) {
-	    return ""; //blank line allows addition
-	}
-	Object tv = tvs.elementAt(row);
-	if (col == 0) {
-	    String n = Model.getFacade().getTagOfTag(tv);
-	    if (n == null) {
-	        return "";
-	    }
-	    return n;
-	}
-	if (col == 1) {
-	    String be = Model.getFacade().getValueOfTag(tv);
-	    if (be == null) {
-	        return "";
-	    }
-	    return be;
-	}
-	return "TV-" + row * 2 + col; // for debugging
-    }
-
-    /**
-     * @see javax.swing.table.TableModel#setValueAt(java.lang.Object, int, int)
-     */
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-	TableModelEvent mEvent = null;
-
-	if (columnIndex != 0 && columnIndex != 1) {
-	    return;
-	}
-	if (!(aValue instanceof String)) {
-	    return;
-	}
-        if (aValue == null) {
-            aValue = "";
-        }
-
-	Vector tvs =
-	    new Vector(Model.getFacade().getTaggedValuesCollection(target));
-	if (tvs.size() <= rowIndex) {
-	    Object tv =
-		Model.getExtensionMechanismsFactory().createTaggedValue();
-	    if (columnIndex == 0) {
-	        Model.getExtensionMechanismsHelper().setTag(tv, aValue);
-	    }
-	    if (columnIndex == 1) {
-		Model.getExtensionMechanismsHelper().setTag(tv, "");
-		Model.getCommonBehaviorHelper().setValue(tv, aValue);
-	    }
-	    tvs.addElement(tv);
-
-	    mEvent =
-		new TableModelEvent(this, tvs.size(), tvs.size(),
-				    TableModelEvent.ALL_COLUMNS,
-				    TableModelEvent.INSERT);
-	} else if ("".equals(aValue) && columnIndex == 0) {
-	    tvs.removeElementAt(rowIndex);
-	    mEvent =
-		new TableModelEvent(this, rowIndex, rowIndex,
-				    TableModelEvent.ALL_COLUMNS,
-				    TableModelEvent.DELETE);
-	} else {
-	    Object tv = tvs.elementAt(rowIndex);
-	    if (columnIndex == 0) {
-	        Model.getExtensionMechanismsHelper().setTag(tv, aValue);
-	    }
-	    if (columnIndex == 1)  {
-                Model.getCommonBehaviorHelper().setValue(tv, aValue);
-            }
-	    mEvent = new TableModelEvent(this, rowIndex);
-	}
-	Model.getCoreHelper().setTaggedValues(target, tvs);
-	if (mEvent != null) {
-	    fireTableChanged(mEvent);
-	}
-	tab.resizeColumns();
-    }
-
-    ////////////////
-    // event handlers
-
-    /**
-     * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
-     */
-    public void propertyChange(PropertyChangeEvent evt) {
-//      TODO: How to implement this ("mee" was a MElementEvent)?
-        //if ("taggedValue".equals(mee.getName()))
-        fireTableChanged(new TableModelEvent(this));
-    }
-
-    /**
-     * @see java.beans.VetoableChangeListener#vetoableChange(java.beans.PropertyChangeEvent)
-     */
-    public void vetoableChange(PropertyChangeEvent pce) {
-	DelayedChangeNotify delayedNotify = new DelayedChangeNotify(this, pce);
-	SwingUtilities.invokeLater(delayedNotify);
-    }
-
-    /**
-     * @see org.argouml.kernel.DelayedVChangeListener#delayedVetoableChange(java.beans.PropertyChangeEvent)
-     */
-    public void delayedVetoableChange(PropertyChangeEvent pce) {
-	fireTableDataChanged();
-	tab.resizeColumns();
-    }
-
-} /* end class TableModelTaggedValues */
-
-
-class ActionRemoveTaggedValue extends AbstractAction {
-
-    private TabTaggedValues tab;
-
-    /**
-     * The constructor.
-     */
-    public ActionRemoveTaggedValue(TabTaggedValues tabtv) {
-        super("", ResourceLoaderWrapper.lookupIconResource("Delete"));
-        tab = tabtv;
-    }
-
-    /**
-     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
-    public void actionPerformed(ActionEvent e) {
-        TableModelTaggedValues model = tab.getTableModel();
-        JTable table = tab.getTable();
-        int row = table.getSelectedRow();
-        List c = new ArrayList(
-                Model.getFacade().getTaggedValuesCollection(tab.getTarget()));
-        if ((row != -1) && (c.size() > row)) {
-            c.remove(row);
-            Model.getCoreHelper().setTaggedValues(tab.getTarget(), c);
-            model.fireTableChanged(new TableModelEvent(model));
-        }
-    }
-}
---
+++ src_new/org/argouml/uml/ui/TabTaggedValuesModel.java
@@ -0,0 +1,235 @@
+package org.argouml.uml.ui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.VetoableChangeListener;
+import java.util.Collection;
+import java.util.Vector;
+
+import javax.swing.SwingUtilities;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.AbstractTableModel;
+
+import org.apache.log4j.Logger;
+import org.argouml.i18n.Translator;
+import org.argouml.kernel.DelayedChangeNotify;
+import org.argouml.kernel.DelayedVChangeListener;
+import org.argouml.model.Model;
+
+/**
+ * The model for the table with the tagged values. Implementation for UML 1.4
+ * and TagDefinitions.
+ */
+public class TabTaggedValuesModel extends AbstractTableModel implements
+        VetoableChangeListener, DelayedVChangeListener, PropertyChangeListener {
+
+    private Logger LOG = Logger.getLogger(TabTaggedValuesModel.class);
+    
+    // //////////////
+    // instance varables
+    private Object target;
+
+    // ModelElement
+    private TabTaggedValues tab = null;
+
+    // //////////////
+    // constructor
+    /**
+     * The constructor.
+     * 
+     * @param t
+     *            the tab
+     */
+    public TabTaggedValuesModel(TabTaggedValues t) {
+        tab = t;
+    }
+
+    // //////////////
+    // accessors
+    /**
+     * @param t
+     *            the target modelelement
+     */
+    public void setTarget(Object t) {
+        LOG.info("Set target to "+t);
+        if (!Model.getFacade().isAModelElement(t)) {
+            throw new IllegalArgumentException();
+        }
+        if (target != t) {
+            if (target != null)
+                Model.getPump().removeModelEventListener(this, target);            
+            target = t;
+            if (t != null)
+                Model.getPump().addModelEventListener(this, t);
+        }
+        //always fire changes in the case something has changed in the composition
+        //of the taggedValues collection.
+        fireTableDataChanged();
+        tab.resizeColumns();
+    }
+
+    // //////////////
+    // TableModel implementation
+    /**
+     * @see javax.swing.table.TableModel#getColumnCount()
+     */
+    public int getColumnCount() {
+        return 2;
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#getColumnName(int)
+     */
+    public String getColumnName(int c) {
+        if (c == 0) {
+            return Translator.localize("label.taggedvaluespane.tag");
+        }
+        if (c == 1) {
+            return Translator.localize("label.taggedvaluespane.value");
+        }
+        return "XXX";
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#getColumnClass(int)
+     */
+    public Class getColumnClass(int c) {
+        if (!TabTaggedValues.UML_14)
+            return String.class;
+        if (c == 0)
+            return (Class) Model.getMetaTypes().getTagDefinition();
+        if (c == 1)
+            return String.class;
+        return null;
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#isCellEditable(int, int)
+     */
+    public boolean isCellEditable(int row, int col) {
+        return true;
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#getRowCount()
+     */
+    public int getRowCount() {
+        if (target == null || Model.getUmlFactory().isRemoved(target)) {
+            return 0;
+        }
+        Collection tvs = Model.getFacade().getTaggedValuesCollection(target);
+        // if (tvs == null) return 1;
+        return tvs.size() + 1;
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#getValueAt(int, int)
+     */
+    public Object getValueAt(int row, int col) {
+        Vector tvs = new Vector(Model.getFacade().getTaggedValuesCollection(
+                target));
+        // if (tvs == null) return "";
+        if (row == tvs.size()) {
+            return ""; // blank line allows addition
+        }
+        Object tv = tvs.elementAt(row);
+        if (col == 0) {
+            Object n = tab.UML_14?Model.getFacade().getTagDefinition(tv):Model.getFacade().getTagOfTag(tv);
+            if (n == null) {
+                return "";
+            }
+            return n;
+        }
+        if (col == 1) {
+            String be = Model.getFacade().getValueOfTag(tv);
+            if (be == null) {
+                return "";
+            }
+            return be;
+        }
+        return "TV-" + row * 2 + col; // for debugging
+    }
+
+    /**
+     * @see javax.swing.table.TableModel#setValueAt(java.lang.Object, int, int)
+     */
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        TableModelEvent mEvent = null;
+
+        if (columnIndex != 0 && columnIndex != 1) {
+            return;
+        }
+        
+        //if (!(aValue instanceof String)) {
+        //    return;
+        //}
+        if (columnIndex == 1 && aValue == null) {
+            aValue = "";
+        }
+
+        Vector tvs = new Vector(Model.getFacade().getTaggedValuesCollection(
+                target));
+        if (tvs.size() <= rowIndex) {
+            Object tv = Model.getExtensionMechanismsFactory()
+                    .createTaggedValue();
+            if (columnIndex == 0) {
+                Model.getExtensionMechanismsHelper().setTag(tv, aValue);
+            }
+            if (columnIndex == 1) {
+                Model.getExtensionMechanismsHelper().setTag(tv, "");
+                Model.getCommonBehaviorHelper().setValue(tv, aValue);
+            }
+            tvs.addElement(tv);
+
+            mEvent = new TableModelEvent(this, tvs.size(), tvs.size(),
+                    TableModelEvent.ALL_COLUMNS, TableModelEvent.INSERT);
+        } else if ("".equals(aValue) && columnIndex == 0) {
+            tvs.removeElementAt(rowIndex);
+            mEvent = new TableModelEvent(this, rowIndex, rowIndex,
+                    TableModelEvent.ALL_COLUMNS, TableModelEvent.DELETE);
+        } else {
+            Object tv = tvs.elementAt(rowIndex);
+            if (columnIndex == 0) {
+                    Model.getExtensionMechanismsHelper().setTag(tv, aValue);
+            }
+            if (columnIndex == 1) {
+                Model.getCommonBehaviorHelper().setValue(tv, aValue);
+            }
+            mEvent = new TableModelEvent(this, rowIndex);
+        }
+        Model.getCoreHelper().setTaggedValues(target, tvs);
+        if (mEvent != null) {
+            fireTableChanged(mEvent);
+        }
+        tab.resizeColumns();
+    }
+
+    // //////////////
+    // event handlers
+
+    /**
+     * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO: How to implement this ("mee" was a MElementEvent)?
+        // if ("taggedValue".equals(mee.getName()))
+        fireTableChanged(new TableModelEvent(this));
+    }
+
+    /**
+     * @see java.beans.VetoableChangeListener#vetoableChange(java.beans.PropertyChangeEvent)
+     */
+    public void vetoableChange(PropertyChangeEvent pce) {
+        DelayedChangeNotify delayedNotify = new DelayedChangeNotify(this, pce);
+        SwingUtilities.invokeLater(delayedNotify);
+    }
+
+    /**
+     * @see org.argouml.kernel.DelayedVChangeListener#delayedVetoableChange(java.beans.PropertyChangeEvent)
+     */
+    public void delayedVetoableChange(PropertyChangeEvent pce) {
+        fireTableDataChanged();
+        tab.resizeColumns();
+    }
+
+} /* end class TableModelTaggedValues */
