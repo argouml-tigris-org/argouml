e52644ee56de3c2f3630d5c5ec76ed6fa96e9cbc
Ludovic Maître <rastaman@tigris.org>
2005-10-28 19:43:38 +0000

    Call .equals() method on Model.getDirectionKind().getXXXParameter() instead of variable  to avoid null pointer exception when the variable is null in generateAttributeParameterModifier. Should have no impact.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@9187 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- modules/cpp/src/org/argouml/language/cpp/generator/GeneratorCpp.java
+++ modules/cpp/src/org/argouml/language/cpp/generator/GeneratorCpp.java
@@ -1273,10 +1273,10 @@ public class GeneratorCpp extends Generator2
             return "*";
         } else if (def.length() == 0) {
             if (Model.getFacade().isAParameter(attr)
-                    && (Model.getFacade().getKind(attr).equals(
-                        Model.getDirectionKind().getOutParameter())
-                    || Model.getFacade().getKind(attr).equals(
-                            Model.getDirectionKind().getInOutParameter()))) {
+                    && (Model.getDirectionKind().getOutParameter().equals(
+                            Model.getFacade().getKind(attr)))
+                    || Model.getDirectionKind().getInOutParameter().equals(
+                            Model.getFacade().getKind(attr))) {
                 // out or inout parameters are defaulted to reference if
                 // not specified else
                 return "&";
