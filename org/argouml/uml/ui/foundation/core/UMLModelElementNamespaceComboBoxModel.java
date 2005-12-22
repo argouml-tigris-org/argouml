d7fa70e4c0e658727ad74106916c2b11474fb48e
Michiel van der Wulp <mvw@tigris.org>
2005-12-22 11:55:04 +0000

    Fix for issue 3832: Exception. Regretfully, this reopens issue 3780.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9543 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
+++ org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
@@ -70,15 +70,17 @@ public class UMLModelElementNamespaceComboBoxModel extends UMLComboBoxModel2 {
         Object t = getTarget();
         Collection c = Model.getCoreHelper().getAllPossibleNamespaces(t, model);
 
-        /* These next 2 lines for the case that the current namespace 
+        /* These next lines for the case that the current namespace 
          * is not a valid one... Which ofcourse should not happen, 
          * but it does - see the project attached to issue 3772. */
         /* TODO: Enhance the isValidNamespace function so
          * that this never happens. */
         if (t != null) {
             Object namespace = Model.getFacade().getNamespace(t);
-            if (!c.contains(namespace)) c.add(namespace);
-            LOG.warn("The current namespace is not a valid one!");
+            if (!c.contains(namespace)) { 
+                c.add(namespace);
+                LOG.warn("The current namespace is not a valid one!");
+            }
         }
         setElements(c);
     }
@@ -104,7 +106,8 @@ public class UMLModelElementNamespaceComboBoxModel extends UMLComboBoxModel2 {
         if (t != null 
                 && evt.getSource() == t
                 && evt.getNewValue() != null) {
-            setTarget(t);
+//            setTarget(t); // this fixes issue 3780, but causes issue 3832.
+            buildModelList();
         }
     }
 }
