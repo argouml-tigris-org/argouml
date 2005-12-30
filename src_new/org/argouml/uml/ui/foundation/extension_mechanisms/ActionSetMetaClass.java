d8edafc220111a9e57e4c39b454568cf44591de5
Michiel van der Wulp <mvw@tigris.org>
2005-12-30 16:06:01 +0000

    Fix for issue 3839: Incomplete baseclass combobox.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@9553 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/foundation/extension_mechanisms/ActionSetMetaClass.java
+++ src_new/org/argouml/uml/ui/foundation/extension_mechanisms/ActionSetMetaClass.java
@@ -31,9 +31,9 @@ import org.argouml.uml.ui.UMLAction;
 import org.argouml.uml.ui.UMLComboBox2;
 
 /**
- *
+ * Action to set the baseclass of a stereotype.
+ * 
  * @author mkl
- *
  */
 public class ActionSetMetaClass extends UMLAction {
 
@@ -61,23 +61,21 @@ public class ActionSetMetaClass extends UMLAction {
         Object stereo = null;
         if (source instanceof UMLComboBox2) {
             UMLComboBox2 combo = (UMLComboBox2) source;
-            newBase = combo.getSelectedItem();
-            Object o = combo.getTarget();
-            if (Model.getFacade().isAStereotype(o)) {
-                stereo = /* (String) */o;
-                o = combo.getSelectedItem();
-
-                newBase = /* (MUseCase) */o;
+            stereo = combo.getTarget();
+            if (Model.getFacade().isAStereotype(stereo)) {
                 oldBase = Model.getFacade().getBaseClass(stereo);
-                if (newBase != oldBase) {
-                    Model.getExtensionMechanismsHelper().setBaseClass(
-                            stereo,
-                            newBase);
-                } else {
-                    if (o != null && o.equals("")) {
+                newBase = combo.getSelectedItem();
+                if (newBase != null) { // TODO: How come this happens?
+                    if (newBase != oldBase) {
                         Model.getExtensionMechanismsHelper().setBaseClass(
                                 stereo,
-                                "ModelElement");
+                                newBase);
+                    } else {
+                        if (newBase != null && newBase.equals("")) {
+                            Model.getExtensionMechanismsHelper().setBaseClass(
+                                    stereo,
+                            "ModelElement");
+                        }
                     }
                 }
             }
