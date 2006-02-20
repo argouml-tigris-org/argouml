ec314122aa7fbd9b715dd548c3774b4958cff8b5
Michiel van der Wulp <mvw@tigris.org>
2006-02-20 17:59:00 +0000

    Fixed the New Parameter button on the parameter proppanel. Merged the 2 implementations of the action that creates the parameter. Changed the default type of the new parameter from "void" to "int".
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9835 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/foundation/core/ActionNewParameter.java
+++ org/argouml/uml/ui/foundation/core/ActionNewParameter.java
@@ -1,16 +1,16 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 2004-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies. This software program and
+// and this paragraph appear in all copies.  This software program and
 // documentation are copyrighted by The Regents of the University of
 // California. The software program and documentation are supplied "AS
 // IS", without any accompanying services from The Regents. The Regents
 // does not warrant that the operation of the program will be
 // uninterrupted or error-free. The end-user understands that the program
 // was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
 // UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 // SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
 // ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
@@ -27,44 +27,52 @@ package org.argouml.uml.ui.foundation.core;
 import java.awt.event.ActionEvent;
 import java.util.Collection;
 
+import javax.swing.Action;
+
+import org.argouml.i18n.Translator;
+import org.argouml.kernel.Project;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.Model;
 import org.argouml.ui.targetmanager.TargetManager;
 import org.argouml.uml.ui.AbstractActionNewModelElement;
 
+
 /**
- * Action to add a parameter to the selected target.
+ * Action to create a new parameter, either for the currently
+ * selected element (operation, event), or "next to" an existing parameter
+ * (that is actually selected by the user).
  *
- * @since Juli 11, 2004
- * @author jaap.branderhorst@xs4all.nl
+ * @author Michiel
  */
 public class ActionNewParameter extends AbstractActionNewModelElement {
+
     /**
      * The constructor.
      */
     public ActionNewParameter() {
         super("button.new-parameter");
+        putValue(Action.NAME, Translator.localize("button.new-parameter"));
     }
 
     /**
-     * On event, a parameter is build and added to the target.
-     *
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent e) {
-        super.actionPerformed(e);
-        //Object target =  getTarget(); // it is not set anywhere, pity...
-        Object target =  TargetManager.getInstance().getModelTarget();
-        Object model =
-            ProjectManager.getManager().getCurrentProject().getModel();
-        Object voidType =
-            ProjectManager.getManager().getCurrentProject().findType("void");
-        Collection propertyChangeListeners =
-            ProjectManager.getManager().getCurrentProject()
-            	.findFigsForMember(target);
-        Object param =
-            Model.getCoreFactory().buildParameter(target, model,
-                voidType, propertyChangeListeners);
-        TargetManager.getInstance().setTarget(param);
+        Object target = TargetManager.getInstance().getModelTarget();
+        if (Model.getFacade().isAParameter(target)) {
+            target = Model.getFacade().getModelElementContainer(target);
+        }
+        if (target != null) {
+            super.actionPerformed(e);
+            Project currentProject =
+                ProjectManager.getManager().getCurrentProject();
+            Object model = currentProject.getModel();
+            Object intType = currentProject.findType("int");
+            Collection propertyChangeListeners =
+                currentProject.findFigsForMember(target);
+            TargetManager.getInstance().setTarget(
+                Model.getCoreFactory().buildParameter(
+                    target, model, intType, propertyChangeListeners));
+        }
     }
 }
