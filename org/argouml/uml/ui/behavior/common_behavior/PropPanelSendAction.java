e72d54a4730f37e34a9bdbb12f3dc91eae34973e
Michiel van der Wulp <mvw@tigris.org>
2006-02-22 21:55:16 +0000

    Added TODO. Style.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9858 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/behavior/common_behavior/PropPanelSendAction.java
+++ org/argouml/uml/ui/behavior/common_behavior/PropPanelSendAction.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -28,8 +28,8 @@ package org.argouml.uml.ui.behavior.common_behavior;
 /**
  * The properties panel for a SendAction.
  *
- * TODO: this property panel needs refactoring to remove dependency on
- *       old gui components.
+ * TODO: this property panel needs a field to select the Signal, 
+ * similar to the one on the PropPanelCreateAction.
  */
 public class PropPanelSendAction extends PropPanelAction {
 
