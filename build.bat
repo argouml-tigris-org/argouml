f54c2fd5860cf0457d3ca89b1d6435864163b2a8
Tom Morris <tfmorris@tigris.org>
2006-02-26 20:26:38 +0000

    Allow argouml-mdr build to be called from main argouml build.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9898 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- build.bat
+++ build.bat
@@ -4,8 +4,14 @@ rem
 rem build.bat always calls the version of ant distributed with ArgoUML
 rem
 
-set SAVE_ANT_HOME=%ANT_HOME%
+setlocal
 set ANT_HOME=..\tools\ant-1.6.2
+
+:: Convert relative path to absolute
+pushd %ANT_HOME%
+set ANT_HOME=%CD%
+popd
+
 call %ANT_HOME%\bin\ant %1 %2 %3 %4 %5 %6 %7 %8 %9
-set ANT_HOME=%SAVE_ANT_HOME%
-set SAVE_ANT_HOME=
+
+endlocal
