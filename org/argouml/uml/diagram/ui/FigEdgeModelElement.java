c82cbe2bb387d96a25513c7fe556144069fd2963
Michiel van der Wulp <mvw@tigris.org>
2006-02-27 19:16:42 +0000

    Style.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9903 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/FigEdgeModelElement.java
+++ org/argouml/uml/diagram/ui/FigEdgeModelElement.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -239,7 +239,8 @@ public abstract class FigEdgeModelElement
         if (item != null
             && Globals.curEditor().getSelectionManager().containsFig(this)) {
             tip = item.getHeadline();
-        } else if (getOwner() != null && Model.getFacade().isABase(getOwner())) {
+        } else if (getOwner() != null 
+                && Model.getFacade().isAModelElement(getOwner())) {
             tip = Model.getFacade().getTipString(getOwner());
         } else {
             tip = toString();
