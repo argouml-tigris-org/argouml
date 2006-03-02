489a75ab9e30b3de4d9b983c51c0a2f16b80336a
Tom Morris <tfmorris@tigris.org>
2006-03-02 06:56:18 +0000

    Clean up style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9921 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/notation/uml/ObjectFlowStateStateNotationUml.java
+++ org/argouml/uml/notation/uml/ObjectFlowStateStateNotationUml.java
@@ -38,7 +38,7 @@ import org.argouml.uml.notation.ObjectFlowStateStateNotation;
 
 /**
  * @author Michiel
- *
+ * 
  */
 public class ObjectFlowStateStateNotationUml extends
         ObjectFlowStateStateNotation {
@@ -46,7 +46,8 @@ public class ObjectFlowStateStateNotationUml extends
     /**
      * The constructor.
      * 
-     * @param objectflowstate the ObjectFlowState represented by this notation
+     * @param objectflowstate
+     *            the ObjectFlowState represented by this notation
      */
     public ObjectFlowStateStateNotationUml(Object objectflowstate) {
         super(objectflowstate);
@@ -65,49 +66,50 @@ public class ObjectFlowStateStateNotationUml extends
     public String parse(String text) {
         try {
             parseObjectFlowState2(myObjectFlowState, text);
-    } catch (ParseException pe) {
-        String msg = "statusmsg.bar.error.parsing.objectflowstate";
-        Object[] args = {
-            pe.getLocalizedMessage(),
-            new Integer(pe.getErrorOffset()),
-        };
-        ProjectBrowser.getInstance().getStatusBar().showStatus(
-                Translator.messageFormat(msg, args));
-    }
-    return toString();
+        } catch (ParseException pe) {
+            String msg = "statusmsg.bar.error.parsing.objectflowstate";
+            Object[] args = {pe.getLocalizedMessage(),
+                             new Integer(pe.getErrorOffset()), };
+            ProjectBrowser.getInstance().getStatusBar().showStatus(
+                    Translator.messageFormat(msg, args));
+        }
+        return toString();
     }
 
     /**
      * Do the actual parsing.
      * 
-     * @param objectFlowState the given element to be altered
-     * @param s the new string
+     * @param objectFlowState
+     *            the given element to be altered
+     * @param s
+     *            the new string
      * @return the altered ObjectFlowState
-     * @throws ParseException when the given text was rejected 
+     * @throws ParseException
+     *             when the given text was rejected
      */
-    protected Object parseObjectFlowState2(Object objectFlowState, String s) 
-        throws ParseException {
+    protected Object parseObjectFlowState2(Object objectFlowState, String s)
+            throws ParseException {
         s = s.trim();
         /* Let's not be picky about the brackets - just remove them: */
         if (s.startsWith("[")) s = s.substring(1);
         if (s.endsWith("]")) s = s.substring(0, s.length() - 1);
         s = s.trim();
-        Object c =
-            Model.getFacade().getType(objectFlowState); // get the classifier
+        Object c = Model.getFacade().getType(objectFlowState); // get the
+                                                                // classifier
         if (c != null) {
             if (Model.getFacade().isAClassifierInState(c)) {
                 Object classifier = Model.getFacade().getType(c);
                 if ((s == null) || "".equals(s)) {
                     // the State of a ClassifierInState is removed,
                     // so let's reduce it to a Classifier.
-                    Model.getCoreHelper().setType(objectFlowState,
-                            classifier);
+                    Model.getCoreHelper().setType(objectFlowState, classifier);
                     delete(c);
                     Model.getCoreHelper().setType(objectFlowState, classifier);
-                    return objectFlowState; // the model is changed - our job is done
+                    return objectFlowState; // the model is changed - job done
                 }
-                Collection states = new ArrayList(Model.getFacade().getInStates(c));
-                Collection statesToBeRemoved = new ArrayList(states); // UML states
+                Collection states = new ArrayList(Model.getFacade()
+                        .getInStates(c));
+                Collection statesToBeRemoved = new ArrayList(states);
                 Collection namesToBeAdded = new ArrayList(); // Strings
                 StringTokenizer tokenizer = new StringTokenizer(s, ",");
                 while (tokenizer.hasMoreTokens()) {
@@ -129,46 +131,48 @@ public class ObjectFlowStateStateNotationUml extends
                 Iterator i = namesToBeAdded.iterator();
                 while (i.hasNext()) {
                     String name = (String) i.next();
-                    /* Now we have to see if any state in any statemachine 
-                     * of classifier is named [name]. If so, then we only have to 
-                     * link the state to c. */
-                    Object state =
-                        Model.getActivityGraphsHelper().findStateByName(classifier, name);
+                    /*
+                     * Now we have to see if any state in any statemachine of
+                     * classifier is named [name]. If so, then we only have to
+                     * link the state to c.
+                     */
+                    Object state = Model.getActivityGraphsHelper()
+                            .findStateByName(classifier, name);
                     if (state != null) {
                         states.add(state);
                         // the model is changed - our job is done
                     } else {
                         // no state named s is found, so we have to
                         // reject the user's input
-                        throw new ParseException(
-                                "State with name " + s + " not found", 0);
+                        throw new ParseException("State with name " + s
+                                + " not found", 0);
                     }
                 }
 
                 /* Finally, do the adaptations: */
                 Model.getActivityGraphsHelper().setInStates(c, states);
-                
+
             } else { // then c is a "normal" Classifier
                 Collection statesToBeAdded = new ArrayList(); // UML states
 
                 StringTokenizer tokenizer = new StringTokenizer(s, ",");
                 while (tokenizer.hasMoreTokens()) {
                     String nextToken = tokenizer.nextToken().trim();
-                    Object state =
-                        Model.getActivityGraphsHelper().findStateByName(c, nextToken);
+                    Object state = Model.getActivityGraphsHelper()
+                            .findStateByName(c, nextToken);
                     if (state != null) {
                         statesToBeAdded.add(state);
                     } else {
                         // no state with the given name is found, so we have to
                         // reject the complete user's input
-                        throw new ParseException(
-                                "State with name " + s + " not found", 0);
+                        throw new ParseException("State with name " + s
+                                + " not found", 0);
                     }
                 }
-                
+
                 // let's create a new ClassifierInState with the correct links
                 Object cis = Model.getActivityGraphsFactory()
-                    .buildClassifierInState(c, statesToBeAdded);
+                        .buildClassifierInState(c, statesToBeAdded);
                 Model.getCoreHelper().setType(myObjectFlowState, cis);
                 // the model is changed - our job is done
             }
@@ -182,7 +186,7 @@ public class ObjectFlowStateStateNotationUml extends
 
     /**
      * This deletes modelelements, and swallows null without barking.
-     *
+     * 
      * @author mvw@tigris.org
      * @param obj
      *            the modelelement to be deleted
