33c4f878b6c77666bd3daee04fdb879230eb48b4
Michiel van der Wulp <mvw@tigris.org>
2006-03-03 18:40:14 +0000

    Style.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@9929 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/behavior/state_machines/UMLStateDeferrableEventList.java
+++ org/argouml/uml/ui/behavior/state_machines/UMLStateDeferrableEventList.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -49,7 +49,9 @@ public class UMLStateDeferrableEventList extends UMLMutableLinkedList {
      * @see org.argouml.uml.ui.UMLMutableLinkedList#getPopupMenu()
      */
     public JPopupMenu getPopupMenu() {
-        return new PopupMenuNewEvent(ActionNewEvent.Roles.DEFERRABLE_EVENT, this);
+        JPopupMenu menu = new PopupMenuNewEvent(
+                ActionNewEvent.Roles.DEFERRABLE_EVENT, this); 
+        return menu;
     }
 
 
