266a04a0b612b7f6e9d23b7640505038d5c1ba1a
Linus Tolke <linus@tigris.org>
2006-03-12 10:05:35 +0000

    Style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10051 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/rules/GoStereotypeToTagDefinition.java
+++ org/argouml/ui/explorer/rules/GoStereotypeToTagDefinition.java
@@ -27,10 +27,14 @@ package org.argouml.ui.explorer.rules;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.argouml.model.Model;
 
+/**
+ * Rule for Stereotype -> TagDefinition.
+ */
 public class GoStereotypeToTagDefinition extends AbstractPerspectiveRule {
 
     /**
@@ -45,7 +49,7 @@ public class GoStereotypeToTagDefinition extends AbstractPerspectiveRule {
      */
     public Collection getChildren(Object parent) {
         if (Model.getFacade().isAStereotype(parent)) {
-            ArrayList list = new ArrayList();
+            List list = new ArrayList();
 
             if (Model.getFacade().getTagDefinitions(parent) != null
                     && Model.getFacade().getTagDefinitions(parent).size() > 0) {
