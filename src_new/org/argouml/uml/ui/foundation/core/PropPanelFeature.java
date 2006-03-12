6278bffeae0038292d97a329120009fe817dd0f4
Linus Tolke <linus@tigris.org>
2006-03-12 13:13:18 +0000

    Style issues.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10058 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/foundation/core/PropPanelFeature.java
+++ src_new/org/argouml/uml/ui/foundation/core/PropPanelFeature.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -30,7 +30,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import org.argouml.i18n.Translator;
-import org.argouml.uml.ui.UMLComboBox2;
 import org.argouml.uml.ui.UMLLinkedList;
 import org.tigris.swidgets.Orientation;
 
@@ -43,8 +42,6 @@ import org.tigris.swidgets.Orientation;
 public abstract class PropPanelFeature extends PropPanelModelElement {
 
     private UMLFeatureOwnerScopeCheckBox ownerScopeCheckbox;
-
-    private UMLComboBox2 ownerComboBox;
 
     private JScrollPane ownerScroll;
 
@@ -107,7 +104,8 @@ public abstract class PropPanelFeature extends PropPanelModelElement {
      */
     protected JPanel getVisibilityPanel() {
         if (visibilityPanel == null) {
-            visibilityPanel = new UMLModelElementVisibilityRadioButtonPanel(
+            visibilityPanel =
+                new UMLModelElementVisibilityRadioButtonPanel(
                     Translator.localize("label.visibility"), true);
         }
         return visibilityPanel;
