5a328086ec3b83de7852c56addc344fce9d843c0
Michiel van der Wulp <mvw@tigris.org>
2006-03-13 19:45:35 +0000

    Let's not forget the tooltips...
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10068 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/ActionRemoveFromDiagram.java
+++ org/argouml/uml/diagram/ui/ActionRemoveFromDiagram.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -45,6 +45,11 @@ import org.tigris.gef.presentation.Fig;
  */
 public class ActionRemoveFromDiagram extends AbstractAction {
 
+    /**
+     * The constructor.
+     * 
+     * @param name the localised (!) name
+     */
     public ActionRemoveFromDiagram(String name) {
         super(name, ResourceLoaderWrapper.lookupIcon("RemoveFromDiagram"));
         String localMnemonic =
@@ -53,6 +58,8 @@ public class ActionRemoveFromDiagram extends AbstractAction {
             putValue(Action.MNEMONIC_KEY,
 		     new Integer(localMnemonic.charAt(0)));
         }
+        // Set the tooltip string:
+        putValue(Action.SHORT_DESCRIPTION, name);
     }
 
     /**
