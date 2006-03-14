003ce4b50515cc748c164f5dde1319d2af8d8c8e
Linus Tolke <linus@tigris.org>
2006-03-14 21:58:35 +0000

    Removed branch that is never entered.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10089 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/cognitive/critics/CrComponentInstanceWithoutClassifier.java
+++ src_new/org/argouml/uml/cognitive/critics/CrComponentInstanceWithoutClassifier.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -129,20 +129,14 @@ public class CrComponentInstanceWithoutClassifier extends CrUML {
 		    offs.addElement(deploymentDiagram);
 		}
 		offs.addElement(figComponentInstance);
-	    } else if (figComponentInstance.getEnclosingFig() != null
-		     && ((Model.getFacade().getNodeInstance(
-		                         figComponentInstance.getOwner()))
-			 == null)) {
-	        // TODO: Can we remove this? We will never arrive here anyway.
-		if (offs == null) {
-		    offs = new ListSet();
-		    offs.addElement(deploymentDiagram);
-		}
-		offs.addElement(figComponentInstance);
 	    }
 	}
 
 	return offs;
     }
 
+    /**
+     * The UID.
+     */
+    private static final long serialVersionUID = -2178052428128671983L;
 } /* end class CrComponentInstanceWithoutClassifier.java */
