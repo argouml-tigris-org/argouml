d5cbf8d21d2751f1a8019adc0776f1aaa8f8d5a4
Tom Morris <tfmorris@tigris.org>
2006-03-24 22:26:49 +0000

    Deprecate getOwner().  It was never implemented.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10220 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/model/src/org/argouml/model/UmlHelper.java
+++ src/model/src/org/argouml/model/UmlHelper.java
@@ -45,9 +45,14 @@ public interface UmlHelper {
      * Returns the owner of some modelelement object. In most cases this will be
      * the owning namespace but in some cases it will be null (the root model)
      * or for instance the owning class with an attribute.
-     *
-     * @param handle the modelelement
+     * 
+     * @param handle
+     *            the modelelement
      * @return Object the owner
+     * @deprecated for 0.21 by tfmorris - use getModelElementContainer - this
+     *             was never implemented for MDR, so any use would throw a run
+     *             time exception. Will be removed quickly because of this.
+     * @see Facade#getModelElementContainer(Object)
      */
     Object getOwner(Object handle);
 
