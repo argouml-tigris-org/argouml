ed51471e912d6fba9b40a8fa879aceb933404bb5
Bob Tarling <bobtarling@tigris.org>
2006-03-25 17:40:43 +0000

    Remove exceptions - thanks Markus
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10234 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/FigEdgeModelElement.java
+++ org/argouml/uml/diagram/ui/FigEdgeModelElement.java
@@ -232,7 +232,6 @@ public abstract class FigEdgeModelElement
      */
     public void setItemUID(ItemUID newId) {
         itemUid = newId;
-        throw new UnsupportedOperationException("Trying to prove this is ever called");
     }
 
     /**
