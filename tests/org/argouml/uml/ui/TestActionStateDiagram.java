05c398bfeebd2613399a0b9b7ae60a94220d6514
Michiel van der Wulp <mvw@tigris.org>
2006-04-01 16:30:56 +0000

    Make this compile and run without error. Still work to do to make it more useful.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10302 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/uml/ui/TestActionStateDiagram.java
+++ tests/org/argouml/uml/ui/TestActionStateDiagram.java
@@ -25,18 +25,42 @@
 package org.argouml.uml.ui;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import junit.framework.TestCase;
+
+import org.argouml.kernel.ProjectManager;
 import org.argouml.model.Model;
 import org.argouml.ui.targetmanager.TargetManager;
+import org.argouml.uml.diagram.UMLMutableGraphSupport;
+import org.argouml.uml.diagram.ui.UMLDiagram;
+import org.tigris.gef.undo.UndoableAction;
 
 /**
+ * TODO: This needs more work. 
+ * Test e.g. creation when an operation is selected.
  *
  * @author jaap.branderhorst@xs4all.nl
  * @since Jan 9, 2003
  */
-public class TestActionStateDiagram
-    extends AbstractTestActionAddDiagram {
+public class TestActionStateDiagram extends TestCase {
+
+    /**
+     * The action to be tested.
+     */
+    private UndoableAction action;
+
+    /**
+     * The namespace a created diagram should have.
+     */
+    private Object ns;
+
+    /**
+     * A list with namespaces that should be valid for the diagram to be
+     * created.
+     */
+    private List validNamespaces;
 
     /**
      * Constructor.
@@ -50,7 +74,7 @@ public class TestActionStateDiagram
     /**
      * @see org.argouml.uml.ui.AbstractTestActionAddDiagram#getAction()
      */
-    protected ActionAddDiagram getAction() {
+    protected UndoableAction getAction() {
         return new ActionStateDiagram();
     }
 
@@ -76,6 +100,9 @@ public class TestActionStateDiagram
         rl.add(Model.getMetaTypes().getDataType());
         rl.add(Model.getMetaTypes().getNode());
         rl.add(Model.getMetaTypes().getComponent());
+        
+        // TODO: this should fail, but it doesn't:
+        rl.add(Model.getMetaTypes().getTransition()); 
         return rl;
     }
 
@@ -83,9 +110,85 @@ public class TestActionStateDiagram
      * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() {
-        super.setUp();
-        TargetManager.getInstance().setTarget(
-                Model.getCoreFactory().createClass());
+        action = getAction();
+        ns = getNamespace();
+        validNamespaces = getValidNamespaceClasses();
+        
+        TargetManager.getInstance().setTarget(ns);
+    }
+    
+    public void testCreateDiagram() {
+        Model.getPump().flushModelEvents();
+        action.actionPerformed(null);
+        Object d = TargetManager.getInstance().getTarget();
+        assertTrue("No diagram generated", d instanceof UMLDiagram);
+        Model.getPump().flushModelEvents();
+        UMLDiagram diagram = (UMLDiagram) d;
+        assertNotNull(
+                      "The diagram has no namespace",
+                      diagram.getNamespace());
+        assertNotNull(
+                      "The diagram has no graphmodel",
+                      diagram.getGraphModel());
+        assertTrue("The graphmodel of the diagram is not a "
+                   + "UMLMutableGraphSupport",
+                   diagram.getGraphModel() instanceof UMLMutableGraphSupport);
+        assertNotNull("The diagram has no name", diagram.getName());
+    }
+    
+    /**
+     * Tests if two diagrams created have different names.
+     */
+    public void testDifferentNames() {
+        action.actionPerformed(null);
+        Object d = TargetManager.getInstance().getTarget();
+        assertTrue("No diagram generated", d instanceof UMLDiagram);
+        Model.getPump().flushModelEvents();
+        UMLDiagram diagram1 = (UMLDiagram) d;
+        // This next line is needed to register the diagram in the project,
+        // since creating a next diagram will need the new name to be compared
+        // with existing diagrams in the project, to validate
+        // there are no duplicates.
+        ProjectManager.getManager().getCurrentProject().addMember(diagram1);
+        
+        TargetManager.getInstance().setTarget(ns);
+        action.actionPerformed(null);
+        d = TargetManager.getInstance().getTarget();
+        assertTrue("No diagram generated", d instanceof UMLDiagram);
+        Model.getPump().flushModelEvents();
+        UMLDiagram diagram2 = (UMLDiagram) d;
+
+        Model.getPump().flushModelEvents();
+        assertTrue(
+                   "The created diagrams have the same name",
+                   !(diagram1.getName().equals(diagram2.getName())));
+    }
+    
+    /**
+     * Tests if the list with namespaces defined in getValidNamespaceClasses
+     * contains only valid namespaces.
+     * 
+     * TODO: This test does not test anything, really!
+     */
+    public void testValidNamespaces() {
+        Iterator it = validNamespaces.iterator();
+        while (it.hasNext()) {
+            Object type = it.next();
+
+            Object o = Model.getUmlFactory().buildNode(type);
+            String objDesc = "" + o;
+            if (o != null) {
+                objDesc += " (" + o.getClass() + ")";
+            }
+            TargetManager.getInstance().setTarget(o);
+            Model.getPump().flushModelEvents();
+            action.actionPerformed(null);
+            Object d = TargetManager.getInstance().getTarget();
+            assertTrue(
+                    objDesc
+                    + " is not valid namespace for the diagram",
+                    d instanceof UMLDiagram);
+        }
     }
 
 }
