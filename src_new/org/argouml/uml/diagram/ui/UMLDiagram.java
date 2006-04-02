42accc80e878d561f283de696e619cd6251de4be
Michiel van der Wulp <mvw@tigris.org>
2006-04-02 08:57:04 +0000

    Do not unregister non-existent listener. Reinstate Tom's remark.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10308 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/ui/UMLDiagram.java
+++ src_new/org/argouml/uml/diagram/ui/UMLDiagram.java
@@ -249,13 +249,15 @@ public abstract class UMLDiagram
             LOG.error(ns);
             throw new IllegalArgumentException("Given object not a namespace");
         }
-        if (namespace != ns) {
+        if ((namespace != null) && (namespace != ns)) {
             Model.getPump().removeModelEventListener(this, namespace);
         }
         namespace = ns;
-        // add the diagram as a listener to the namspace so
+        // Add the diagram as a listener to the namespace so
         // that when the namespace is removed the diagram is deleted also.
-        /* Listening only to "remove" events does not work... */
+        /* Listening only to "remove" events does not work... 
+         * TODO: Check if this works now with new event pump - tfm 
+         */
         Model.getPump().addModelEventListener(this, namespace);
     }
 
