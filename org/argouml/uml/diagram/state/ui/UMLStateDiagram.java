a677cb0bfe1d12481f4d03033e1f96e01c48da83
Michiel van der Wulp <mvw@tigris.org>
2006-04-02 08:59:33 +0000

    Issue 2225: Make it possible to change the namespace of a StateMachine, and have the Statechart diagram follow this change.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10310 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/state/ui/UMLStateDiagram.java
+++ org/argouml/uml/diagram/state/ui/UMLStateDiagram.java
@@ -24,6 +24,7 @@
 
 package org.argouml.uml.diagram.state.ui;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyVetoException;
 
 import javax.swing.Action;
@@ -34,6 +35,7 @@ import org.argouml.kernel.ProjectManager;
 import org.argouml.model.Model;
 import org.argouml.ui.CmdCreateNode;
 import org.argouml.ui.CmdSetMode;
+import org.argouml.uml.diagram.UMLMutableGraphSupport;
 import org.argouml.uml.diagram.state.StateDiagramGraphModel;
 import org.argouml.uml.diagram.ui.RadioAction;
 import org.argouml.uml.diagram.ui.UMLDiagram;
@@ -234,9 +236,27 @@ public class UMLStateDiagram extends UMLDiagram {
         lay.setGraphEdgeRenderer(rend);
         setLayer(lay);
 
-        /* TODO: Listen to machine namespace changes, 
+        /* Listen to machine namespace changes, 
          * to adapt the namespace of the diagram. */
-//        Model.getPump().addModelEventListener(this, machine, "namespace");
+        Model.getPump().addModelEventListener(this, theStateMachine, "namespace");
+    }
+
+    /**
+     * @see org.argouml.uml.diagram.ui.UMLDiagram#propertyChange(java.beans.PropertyChangeEvent)
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO Auto-generated method stub
+        super.propertyChange(evt);
+        if (evt.getSource() == theStateMachine) {
+            Object newNamespace = 
+                Model.getFacade().getNamespace(theStateMachine);
+            if (getNamespace() != newNamespace) {
+                /* The namespace of the statemachine is changed! */
+                setNamespace(newNamespace);
+                ((UMLMutableGraphSupport)getGraphModel())
+                                .setHomeModel(newNamespace);
+            }
+        }
     }
 
     /**
