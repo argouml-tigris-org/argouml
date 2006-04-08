0940826bb030344863575a18ebabb3c5ad620342
Tom Morris <tfmorris@tigris.org>
2006-04-08 23:54:21 +0000

    Issue 2472 - Force keyboard focus to always be on the OK key during window open.  Split Settings dialog class into a separate file.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10357 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/ui/SettingsDialog.java
@@ -0,0 +1,246 @@
+// $Id$
+// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.ui;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+
+import org.apache.log4j.Logger;
+import org.argouml.application.api.Argo;
+import org.argouml.application.api.PluggableSettingsTab;
+import org.argouml.application.api.SettingsTabPanel;
+import org.argouml.i18n.Translator;
+
+/**
+ * Action for starting the Argo settings window.
+ * 
+ * @author Thomas N
+ * @author Thierry Lach
+ * @since 0.9.4
+ */
+public class SettingsDialog extends ArgoDialog implements WindowListener {
+
+    /**
+     * The serial version.
+     */
+    private static final long serialVersionUID = -8233301947357843703L;
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = Logger.getLogger(SettingsDialog.class);
+
+    private JButton applyButton;
+
+    private JTabbedPane tabs;
+    
+    boolean doingShow = false;
+    
+    boolean windowOpen = false;
+
+    /**
+     * Constructor to build new settings dialog
+     */
+    SettingsDialog() {
+        super(ProjectBrowser.getInstance(), 
+                Translator.localize("dialog.settings"),
+                ArgoDialog.OK_CANCEL_OPTION, 
+                true);
+
+
+        tabs = new JTabbedPane();
+        
+        applyButton = new JButton(Translator.localize("button.apply"));
+        String mnemonic = Translator.localize("button.apply.mnemonic");
+        if (mnemonic != null && mnemonic.length() > 0) {
+            applyButton.setMnemonic(mnemonic.charAt(0));
+        }
+        applyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleSave();
+            }
+        });
+        addButton(applyButton);
+        
+        List list = Argo.getPlugins(PluggableSettingsTab.class);
+        ListIterator iterator = list.listIterator();
+        try {
+            while (iterator.hasNext()) {
+                Object o = iterator.next();
+                SettingsTabPanel stp = 
+                    ((PluggableSettingsTab) o).getSettingsTabPanel();
+                tabs.addTab(Translator.localize(stp.getTabKey()), stp
+                        .getTabPanel());
+            }
+        } catch (Exception exception) {
+            LOG.error("got an Exception in ActionSettings", exception);
+        }
+        
+        // Increase width to accommodate all tabs on one row.
+        final int minimumWidth = 480;
+        tabs.setPreferredSize(new Dimension(Math.max(tabs
+                .getPreferredSize().width, minimumWidth), tabs
+                .getPreferredSize().height));
+        
+        tabs.setTabPlacement(SwingConstants.LEFT);
+        setContent(tabs);
+        addWindowListener(this);
+        
+    }
+
+    /**
+     * @see java.awt.Component#show()
+     */
+    public void show() {
+        // If a recursive call from setVisible(), just return
+        if (doingShow) {
+            return;
+        }
+        doingShow = true;
+        handleRefresh();
+        super.show();
+        toFront();
+        setVisible(true);
+        doingShow = false;
+        // windowOpen state will be changed when window is activated
+    }
+    
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(
+     *      java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent ev) {
+        super.actionPerformed(ev);
+        if (ev.getSource() == getOkButton()) {
+            handleSave();
+        } else if (ev.getSource() == getCancelButton()) {
+            handleCancel();
+        }
+    }
+
+    /*
+     * Called when the user has pressed Save. Performs "Save" in all Tabs.
+     */
+    private void handleSave() {
+        for (int i = 0; i < tabs.getComponentCount(); i++) {
+            Object o = tabs.getComponent(i);
+            if (o instanceof SettingsTabPanel) {
+                ((SettingsTabPanel) o).handleSettingsTabSave();
+            }
+        }
+        windowOpen = false;
+    }
+
+    /*
+     * Called when the user has pressed Cancel. Performs "Cancel" in all Tabs.
+     */
+    private void handleCancel() {
+        for (int i = 0; i < tabs.getComponentCount(); i++) {
+            Object o = tabs.getComponent(i);
+            if (o instanceof SettingsTabPanel) {
+                ((SettingsTabPanel) o).handleSettingsTabCancel();
+            }
+        }
+        windowOpen = false;
+    }
+
+    /**
+     * Perform "Refresh" in all Tabs.
+     */
+    private void handleRefresh() {
+        for (int i = 0; i < tabs.getComponentCount(); i++) {
+            Object o = tabs.getComponent(i);
+            if (o instanceof SettingsTabPanel) {
+                ((SettingsTabPanel) o).handleSettingsTabRefresh();
+            }
+        }
+    }
+    
+    private void handleOpen() {
+        // We only request focus the first time we become visible
+        if (!windowOpen) {
+            getOkButton().requestFocusInWindow();
+            windowOpen = true;
+        }        
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowActivated(java.awt.event.WindowEvent)
+     */
+    public void windowActivated(WindowEvent e) {
+        handleOpen();
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowClosed(java.awt.event.WindowEvent)
+     */
+    public void windowClosed(WindowEvent e) {
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowDeactivated(java.awt.event.WindowEvent)
+     */
+    public void windowDeactivated(WindowEvent e) {
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowDeiconified(java.awt.event.WindowEvent)
+     */
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowIconified(java.awt.event.WindowEvent)
+     */
+    public void windowIconified(WindowEvent e) {
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowOpened(java.awt.event.WindowEvent)
+     */
+    public void windowOpened(WindowEvent e) {
+        handleOpen();
+    }
+
+    /**
+     * @see java.awt.event.WindowListener#windowClosing(java.awt.event.WindowEvent)
+     */
+    public void windowClosing(WindowEvent e) {
+        // Handle the same as an explicit cancel
+        handleCancel();
+    }
+
+}
+
+
