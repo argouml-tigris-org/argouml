7e64f2f5c0fac400de3e55b480d71dee381e26f5
Tom Morris <tfmorris@tigris.org>
2006-04-08 23:33:23 +0000

    Remove duplicate $Id lines
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10356 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/UMLListCellRenderer2.java
+++ org/argouml/uml/ui/UMLListCellRenderer2.java
@@ -22,7 +22,6 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-// $Id$
 package org.argouml.uml.ui;
 
 import java.awt.Component;
