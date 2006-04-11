a30ad1003ef4c35203f435dfd183fd0c9c42d367
Tom Morris <tfmorris@tigris.org>
2006-04-11 17:08:09 +0000

    Create a second logger for FigAssociation to conform to project rules.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10377 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/FigEdgeModelElement.java
+++ org/argouml/uml/diagram/ui/FigEdgeModelElement.java
@@ -102,7 +102,7 @@ public abstract class FigEdgeModelElement
         ArgoNotationEventListener,
         Highlightable {
 
-    protected static final Logger LOG =
+    private static final Logger LOG =
         Logger.getLogger(FigEdgeModelElement.class);
 
     private DiElement diElement = null;
