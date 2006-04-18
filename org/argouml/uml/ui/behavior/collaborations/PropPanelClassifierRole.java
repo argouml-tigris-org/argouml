c2401f1bd9dd07f2dbc3c973e48a6768e060b57b
Michiel van der Wulp <mvw@tigris.org>
2006-04-18 21:06:56 +0000

    Style.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10430 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/behavior/collaborations/PropPanelClassifierRole.java
+++ org/argouml/uml/ui/behavior/collaborations/PropPanelClassifierRole.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -68,11 +68,13 @@ public class PropPanelClassifierRole extends PropPanelClassifier {
 
 	addField(Translator.localize("label.name"),
 	    getNameTextField());
-	addField(Translator.localize("label.namespace"),
+
+        addField(Translator.localize("label.namespace"),
 	    getNamespaceScroll());
 
 	addField(Translator.localize("label.multiplicity"),
             getMultiplicityComboBox());
+
 	JList baseList =
 	    new UMLMutableLinkedList(new UMLClassifierRoleBaseListModel(),
 		ActionAddClassifierRoleBase.SINGLETON,
