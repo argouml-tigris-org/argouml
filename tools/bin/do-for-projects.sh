d736ab1914d7e8f0c3b7d08a4a70ba1a72bb540b
Linus Tolke <linus@tigris.org>
2006-04-20 20:10:52 +0000

    Updated the list of projects.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10448 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tools/bin/do-for-projects.sh
+++ tools/bin/do-for-projects.sh
@@ -3,16 +3,15 @@
 
 # Do the same thing for each project involved in the release.
 
-CHECKOUT_PROJECTS="argouml \
+PROJECTS="CHECKOUT_PROJECTS="argouml \
+    argouml-classfile \
+    argouml-cpp \
     argouml-csharp \
-    argouml-de argouml-es argouml-en-gb argouml-fr argouml-nb argouml-ru \
+    argouml-idl \
+    argouml-php \
+    argouml-de argouml-es argouml-en-gb argouml-fr argouml-nb \
+    argouml-pt argouml-ru \
     argouml-i18n-zh"
-
-PROJECTS="$CHECKOUT_PROJECTS \
-    argouml/modules/cpp \
-    argouml/modules/classfile \
-    argouml/modules/idl \
-    argouml/modules/php"
 
 case $1 in
 --checkout)
