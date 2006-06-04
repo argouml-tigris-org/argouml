36c2bac3444565b0e6cb3bb81b68194a295ac8c2
Michiel van der Wulp <mvw@tigris.org>
2006-06-04 18:49:58 +0000

    Issue 4242: Fixed according patches supplied by Andrea.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10672 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ tests/org/argouml/persistence/TestPersistenceManager.java
@@ -0,0 +1,81 @@
+// $Id$
+// Copyright (c) 2006 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.persistence;
+
+import junit.framework.TestCase;
+
+/**
+ * Test class for PersistenceManager. Actually, the only tested method is 
+ * getPersisterFromFileName().
+ *
+ * @since Jun 2, 2006
+ * @author andrea.nironi@gmail.com
+ */
+public class TestPersistenceManager extends TestCase {
+
+    /**
+     * Constructor for TestPersistenceManager.
+     * @param arg0 is the name of the test case.
+     */
+    public TestPersistenceManager(String arg0) {
+        super(arg0);
+    }
+    
+    /**
+     * Test if the getPersisterFromFileName returns a correct implementation.
+     * 
+     */
+    public void testGetPersisterFromFileName() {
+        PersistenceManager persistence = PersistenceManager.getInstance();
+        AbstractFilePersister testPersister;
+        
+        // test an unknown file
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown.foo");
+        assertNull(testPersister);
+        
+        // test project readable files
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown." + new ZargoFilePersister().getExtension());
+        assertNotNull(testPersister);
+        
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown." + new XmiFilePersister().getExtension());
+        assertNotNull(testPersister);
+        
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown." + new XmlFilePersister().getExtension());
+        assertNotNull(testPersister);
+        
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown." + new UmlFilePersister().getExtension());
+        assertNotNull(testPersister);
+        
+        testPersister = persistence.getPersisterFromFileName(
+                "unknown." + new ZipFilePersister().getExtension());
+        assertNotNull(testPersister);
+    }
+    
+}
