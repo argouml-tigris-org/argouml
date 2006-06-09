a9110e96b426998da071f2eba31424493f7e5a21
Michiel van der Wulp <mvw@tigris.org>
2006-06-09 16:41:22 +0000

    Fix for the 2nd problem in issue 4222.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10711 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/UMLComboBoxModel2.java
+++ org/argouml/uml/ui/UMLComboBoxModel2.java
@@ -39,6 +39,7 @@ import org.argouml.model.AddAssociationEvent;
 import org.argouml.model.AssociationChangeEvent;
 import org.argouml.model.AttributeChangeEvent;
 import org.argouml.model.DeleteInstanceEvent;
+import org.argouml.model.InvalidElementException;
 import org.argouml.model.Model;
 import org.argouml.model.RemoveAssociationEvent;
 import org.argouml.ui.targetmanager.TargetEvent;
@@ -199,6 +200,20 @@ public abstract class UMLComboBoxModel2 extends AbstractListModel
      * target of the proppanel is changed.
      */
     protected abstract void buildModelList();
+
+    /**
+     * @param obj an UML object
+     * @return its name or "" (if it was not named or deleted)
+     */
+    protected String getName(Object obj) {
+        try {
+            Object n = Model.getFacade().getName(obj);
+            String name = (n != null ? (String) n : "");
+            return name;
+        } catch (InvalidElementException e) {
+            return "";
+        }
+    }
 
     /**
      * Utility method to change all elements in the list with modelelements
