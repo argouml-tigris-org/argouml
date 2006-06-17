a125beada72718729300e7e9eddb114c7aa5b0cc
Michiel van der Wulp <mvw@tigris.org>
2006-06-17 15:30:29 +0000

    To be able to test notation for issue 4279 for edges, I added Java notation for an Association.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10785 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ org/argouml/uml/notation/java/AssociationEndNameNotationJava.java
@@ -0,0 +1,90 @@
+// $Id$
+// Copyright (c) 2006 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.notation.java;
+
+import org.argouml.model.Model;
+import org.argouml.ui.ProjectBrowser;
+import org.argouml.uml.notation.AssociationEndNameNotation;
+import org.argouml.uml.notation.uml.NotationUtilityUml;
+
+/**
+ * The Java notation for an associationend name (i.e. the  role).
+ * 
+ * @author michiel
+ */
+public class AssociationEndNameNotationJava extends AssociationEndNameNotation {
+
+    /**
+     * The constructor.
+     *
+     * @param assocEnd the Java associationEnd
+     */
+    public AssociationEndNameNotationJava(Object assocEnd) {
+        super(assocEnd);
+    }
+
+    /**
+     * @see org.argouml.notation.NotationProvider4#getParsingHelp()
+     */
+    public String getParsingHelp() {
+//        return "parsing.help.fig-association-end-name";
+        return "Parsing in Java not yet supported";
+    }
+
+    /**
+     * @see org.argouml.notation.NotationProvider4#parse(java.lang.String)
+     */
+    public String parse(String text) {
+        ProjectBrowser.getInstance().getStatusBar().showStatus(
+            "Parsing in Java not yet supported");
+        return toString();
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        String name = Model.getFacade().getName(myAssociationEnd);
+        if (name == null) {
+            name = "";
+        }
+
+        Object visi = Model.getFacade().getVisibility(myAssociationEnd);
+        String visibility = "";
+        if (visi != null) {
+            visibility = NotationUtilityJava.generateVisibility(visi);
+        }
+        if (name.length() < 1) {
+            visibility = "";
+            //this is the temporary solution for issue 1011
+        }
+
+        String stereoString = 
+            NotationUtilityUml.generateStereotype(myAssociationEnd);
+
+        return stereoString + visibility + name;
+    }
+    
+}
