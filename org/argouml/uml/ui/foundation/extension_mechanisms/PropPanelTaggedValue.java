aed653119ddec8d4fcb335ba2d5327575da2d661
Michiel van der Wulp <mvw@tigris.org>
2006-07-02 07:19:24 +0000

    A step towards solving issue 4031: Incomplete TagDefinition-TaggedValue implementation.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@10847 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/ui/foundation/extension_mechanisms/PropPanelTaggedValue.java
+++ org/argouml/uml/ui/foundation/extension_mechanisms/PropPanelTaggedValue.java
@@ -24,10 +24,24 @@
 
 package org.argouml.uml.ui.foundation.extension_mechanisms;
 
+import java.awt.AWTEvent;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+
 import org.argouml.i18n.Translator;
+import org.argouml.kernel.Project;
+import org.argouml.kernel.ProjectManager;
+import org.argouml.model.Model;
 import org.argouml.uml.ui.ActionNavigateContainerElement;
+import org.argouml.uml.ui.UMLComboBox2;
+import org.argouml.uml.ui.UMLComboBoxModel2;
+import org.argouml.uml.ui.UMLComboBoxNavigator;
 import org.argouml.uml.ui.foundation.core.PropPanelModelElement;
 import org.argouml.util.ConfigLoader;
+import org.tigris.gef.undo.UndoableAction;
 
 /**
  * The properties panel for a TaggedValue. <p>
@@ -43,7 +57,9 @@ import org.argouml.util.ConfigLoader;
  * @author michiel
  */
 public class PropPanelTaggedValue extends PropPanelModelElement {
-
+    
+    private JComponent modelElementSelector;
+    
     /**
      * The constructor.
      */
@@ -52,10 +68,111 @@ public class PropPanelTaggedValue extends PropPanelModelElement {
 
         addField(Translator.localize("label.name"),
                 getNameTextField());
+        addField(Translator.localize("label.modelelement"),
+                getModelElementSelector());
 
         addAction(new ActionNavigateContainerElement());
         addAction(new ActionNewTagDefinition());
         addAction(getDeleteAction());
     }
 
+    /**
+     * Returns the modelelement selecter. 
+     * This is a component which allows the
+     * user to select a single item as the modelelement.
+     *
+     * @return the modelelement selecter
+     */
+    protected JComponent getModelElementSelector() {
+        if (modelElementSelector == null) {
+            modelElementSelector = new Box(BoxLayout.X_AXIS);
+            modelElementSelector.add(new UMLComboBoxNavigator(this,
+                    Translator.localize("label.modelelement.navigate.tooltip"),
+                    new UMLComboBox2(
+                            new UMLTaggedValueModelElementComboBoxModel(),
+                            new ActionSetTaggedValueModelElement())
+            ));
+        }
+        return modelElementSelector;
+    }
+    
+    class ActionSetTaggedValueModelElement extends UndoableAction {
+
+        /**
+         * Construct this action.
+         */
+        public ActionSetTaggedValueModelElement() {
+            super();
+        }
+
+        /**
+         * @see org.tigris.gef.undo.UndoableAction#actionPerformed(java.awt.event.ActionEvent)
+         */
+        public void actionPerformed(ActionEvent e) {
+            super.actionPerformed(e);
+            Object source = e.getSource();
+            if (source instanceof UMLComboBox2
+                    && e.getModifiers() == AWTEvent.MOUSE_EVENT_MASK) {
+                UMLComboBox2 combo = (UMLComboBox2) source;
+                Object o = combo.getSelectedItem();
+                final Object taggedValue = combo.getTarget();
+                if (Model.getFacade().isAModelElement(o)
+                        && Model.getFacade().isATaggedValue(taggedValue)) {
+                    Object oldME = Model.getFacade().getModelElement(taggedValue);
+                    Model.getExtensionMechanismsHelper()
+                        .removeTaggedValue(oldME, taggedValue);
+                    Model.getExtensionMechanismsHelper()
+                        .addTaggedValue(o, taggedValue);
+                }
+            }
+        }
+        
+    }
+    
+    class UMLTaggedValueModelElementComboBoxModel 
+        extends UMLComboBoxModel2 {
+
+        /**
+         * Constructor for UMLModelElementStereotypeComboBoxModel.
+         */
+        public UMLTaggedValueModelElementComboBoxModel() {
+            super("modelelement", true); // ??
+//            Model.getPump().addClassModelEventListener(
+//                this,
+//                Model.getMetaTypes().getNamespace(),
+//                "ownedElement");
+        }
+
+        /**
+         * @see org.argouml.uml.ui.UMLComboBoxModel2#buildModelList()
+         */
+        protected void buildModelList() {
+            Object elem = getTarget();
+            Project p = ProjectManager.getManager().getCurrentProject();
+            Object model = p.getRoot();
+            setElements(Model.getModelManagementHelper()
+                .getAllModelElementsOfKindWithModel(model, 
+                        Model.getMetaTypes().getModelElement()));
+        }
+
+        /**
+         * @see org.argouml.uml.ui.UMLComboBoxModel2#getSelectedModelElement()
+         */
+        protected Object getSelectedModelElement() {
+            Object me = null;
+            if (getTarget() != null 
+                    && Model.getFacade().isATaggedValue(getTarget())) {
+                me = Model.getFacade().getModelElement(getTarget());
+            }
+            return me;
+        }
+
+        /**
+         * @see org.argouml.uml.ui.UMLComboBoxModel2#isValidElement(java.lang.Object)
+         */
+        protected boolean isValidElement(Object element) {
+            return Model.getFacade().isAModelElement(element);
+        }
+        
+    }
 }
