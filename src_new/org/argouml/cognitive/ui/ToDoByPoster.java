f7d65e6411171d378f404627bbed67f7b767425b
Michiel van der Wulp <mvw@tigris.org>
2006-07-09 19:29:36 +0000

    Fix for issue 1884, as supplied by Andrea. view todo-items by poster: selected item deselected by argo
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@10892 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/cognitive/ui/ToDoByPoster.java
+++ src_new/org/argouml/cognitive/ui/ToDoByPoster.java
@@ -139,21 +139,29 @@ public class ToDoByPoster extends ToDoPerspective
      */
     public void toDoItemsRemoved(ToDoListEvent tde) {
 	LOG.debug("toDoItemRemoved");
+        Vector items = tde.getToDoItems();
+        int nItems = items.size();
+        
 	ToDoList list = Designer.theDesigner().getToDoList(); //source?
 	Object[] path = new Object[2];
 	path[0] = Designer.theDesigner().getToDoList();
 
 
 	Enumeration elems = list.getPosters().elements();
-	while (elems.hasMoreElements()) {
+ 	while (elems.hasMoreElements()) {
 	    Poster p = (Poster) elems.nextElement();
-	    //       boolean anyInPoster = false;
-	    //       for (int i = 0; i < nItems; i++) {
-	    // 	ToDoItem item = (ToDoItem) items.elementAt(i);
-	    // 	Poster post = item.getPoster();
-	    // 	if (post == p) anyInPoster = true;
-	    //       }
-	    //       if (!anyInPoster) continue;
+            boolean anyInPoster = false;
+            for (int i = 0; i < nItems; i++) {
+                ToDoItem item = (ToDoItem) items.elementAt(i);
+                Poster post = item.getPoster();
+                if (post == p) { 
+                    anyInPoster = true;
+                    break;
+                }
+            }
+            if (!anyInPoster) { 
+                continue;
+            }
 	    path[1] = p;
 	    fireTreeStructureChanged(path);
 	}
