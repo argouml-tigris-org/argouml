846dee0e89ae6009daa1d0619bf64cdded4bb473
Markus Klink <mkl@tigris.org>
2006-08-20 11:55:58 +0000

    new implementation of CrAssocNameConflict
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@11066 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/cognitive/critics/CrAssocNameConflict.java
+++ src_new/org/argouml/uml/cognitive/critics/CrAssocNameConflict.java
@@ -25,46 +25,126 @@
 package org.argouml.uml.cognitive.critics;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.argouml.cognitive.Designer;
+import org.argouml.cognitive.ListSet;
+import org.argouml.cognitive.ToDoItem;
 import org.argouml.cognitive.critics.Critic;
 import org.argouml.model.Model;
 import org.argouml.uml.cognitive.UMLDecision;
+import org.argouml.uml.cognitive.UMLToDoItem;
 
 /**
- * Well-formedness rule [2] for MNamespace. See page 33 of UML 1.1
- * Semantics. OMG document ad/97-08-04.
- *
- * @author jrobbins
+ * Well-formedness rule [2] for MNamespace. See page 2-64 of UML 1.1 Semantics.
+ * OMG document 1.4.1
+ * 
+ * @author mkl
  */
 public class CrAssocNameConflict extends CrUML {
 
     /**
      * The constructor.
-     *
+     * 
      */
     public CrAssocNameConflict() {
         setupHeadAndDesc();
-	addSupportedDecision(UMLDecision.NAMING);
-	setKnowledgeTypes(Critic.KT_SYNTAX);
-	// no good trigger
+        addSupportedDecision(UMLDecision.NAMING);
+        setKnowledgeTypes(Critic.KT_SYNTAX);
+        // no good trigger
     }
 
     /**
      * @see org.argouml.uml.cognitive.critics.CrUML#predicate2(
-     * java.lang.Object, org.argouml.cognitive.Designer)
+     *      java.lang.Object, org.argouml.cognitive.Designer)
      */
     public boolean predicate2(Object dm, Designer dsgr) {
-	if (!(Model.getFacade().isANamespace(dm))) return NO_PROBLEM;
-	Collection oes = Model.getFacade().getOwnedElements(dm);
-	if (oes == null) return NO_PROBLEM;
-	Iterator elems = oes.iterator();
-	while (elems.hasNext()) {
-	    if (!Model.getFacade().isAAssociation(elems.next())) continue;
-	    // TODO: not implemented yet
-	}
-	return NO_PROBLEM;
+        return computeOffenders(dm).size() > 1;
     }
 
+    /**
+     * @see org.argouml.cognitive.critics.Critic#toDoItem( java.lang.Object,
+     *      org.argouml.cognitive.Designer)
+     */
+    public ToDoItem toDoItem(Object dm, Designer dsgr) {
+        ListSet offs = computeOffenders(dm);
+        return new UMLToDoItem(this, offs, dsgr);
+    }
+
+    /**
+     * @param dm
+     *            the object to check
+     * @return the set of offenders
+     */
+    protected ListSet computeOffenders(Object dm) {
+        ListSet offenderResult = new ListSet();
+        if (Model.getFacade().isANamespace(dm)) {
+            Iterator it = Model.getFacade().getOwnedElements(dm).iterator();
+            HashMap names = new HashMap();
+            while (it.hasNext()) {
+                Object name1Object = it.next();
+
+                if (!Model.getFacade().isAAssociation(name1Object))
+                    continue;
+                String name = Model.getFacade().getName(name1Object);
+                Collection typ1 = getAllTypes(name1Object);
+                if (name == null)
+                    continue;
+                if ("".equals(name))
+                    continue;
+                if (names.containsKey(name)) {
+                    Object offender = names.get(name);
+                    Collection typ2 = getAllTypes(offender);
+                    if (typ1.containsAll(typ2) && typ2.containsAll(typ1)) {
+                        if (!offenderResult.contains(offender))
+                            offenderResult.addElement(offender);
+                        offenderResult.addElement(name1Object);
+                    }
+                }
+                names.put(name, name1Object);
+            }
+        }
+        return offenderResult;
+    }
+
+    /**
+     * @see org.argouml.cognitive.Poster#stillValid(
+     *      org.argouml.cognitive.ToDoItem, org.argouml.cognitive.Designer)
+     */
+    public boolean stillValid(ToDoItem i, Designer dsgr) {
+        if (!isActive())
+            return false;
+        ListSet offs = i.getOffenders();
+
+        // first element is e.g. the class, but we need to have its namespace
+        // to recompute the offenders.
+        Object f = offs.firstElement();
+        Object ns = Model.getFacade().getNamespace(f);
+        if (!predicate(ns, dsgr))
+            return false;
+        ListSet newOffs = computeOffenders(ns);
+        boolean res = offs.equals(newOffs);
+        return res;
+    }
+
+    public Collection getAllTypes(Object assoc) {
+        Set list = new HashSet();
+        if (assoc == null)
+            return list;
+        Collection assocEnds = Model.getFacade().getConnections(assoc);
+        if (assocEnds == null)
+            return list;
+        Iterator iterator = assocEnds.iterator();
+        while (iterator.hasNext()) {
+            Object element = (Object) iterator.next();
+            if (Model.getFacade().isAAssociationEnd(element)) {
+                Object type = Model.getFacade().getType(element);
+                list.add(type);
+            }
+        }
+        return list;
+    }
 } /* end class CrAssocNameConflict.java */
