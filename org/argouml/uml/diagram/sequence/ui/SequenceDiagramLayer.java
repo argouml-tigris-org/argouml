47d131cf3754ff2a16c6f3465813742da02ae4a5
Bob Tarling <bobtarling@tigris.org>
2006-09-17 22:44:11 +0000

    Make removing a FigMessage safer
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@11195 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/sequence/ui/SequenceDiagramLayer.java
+++ org/argouml/uml/diagram/sequence/ui/SequenceDiagramLayer.java
@@ -294,10 +294,10 @@ public class SequenceDiagramLayer extends LayerPerspectiveMutable {
             FigMessagePort source = (FigMessagePort) fm.getSourcePortFig();
             FigMessagePort dest = (FigMessagePort) fm.getDestPortFig();
             
-            removeFigMessagePort(source);
-            removeFigMessagePort(dest);
-            updateNodeStates(source);
-            if (fm.getSourceFigNode() != fm.getDestFigNode()) {
+            if (source != null) removeFigMessagePort(source);
+            if (dest != null) removeFigMessagePort(dest);
+            if (source != null) updateNodeStates(source);
+            if (dest != null && fm.getSourceFigNode() != fm.getDestFigNode()) {
                 updateNodeStates(dest);
             }
         }
