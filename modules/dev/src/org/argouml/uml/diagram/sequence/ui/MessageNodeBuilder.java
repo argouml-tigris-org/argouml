a1a0bda6a4a7be3fcb3857cc2077d2e5e7f7175b
Bob Tarling <bobtarling@tigris.org>
2006-10-01 15:21:38 +0000

    Remove activations when a message is removed
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@11273 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- modules/dev/src/org/argouml/uml/diagram/sequence/ui/MessageNodeBuilder.java
+++ modules/dev/src/org/argouml/uml/diagram/sequence/ui/MessageNodeBuilder.java
@@ -25,6 +25,8 @@
 package org.argouml.uml.diagram.sequence.ui;
 
 import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.argouml.uml.diagram.sequence.CallerListNodeBuilder;
 import org.argouml.uml.diagram.sequence.MessageNode;
 
 public class MessageNodeBuilder {
@@ -35,13 +37,18 @@ public class MessageNodeBuilder {
         int nodeCount = fcr.getNodeCount();
         for (int i = 0; i < nodeCount; ++i) {
             MessageNode mn = fcr.getNode(i);
-            String descr = "MessageNode y=" + fcr.getYCoordinate(mn);
+            String descr =
+            	"MessageNode y=" + 
+            	fcr.getYCoordinate(mn) + 
+            	" " + 
+            	mn.getState();
             FigMessagePort fmp = mn.getFigMessagePort();
             if (fmp != null) {
                 descr += " FigMessagePort registered";
             }
             DefaultMutableTreeNode tn = new DefaultMutableTreeNode(descr);
             treeNode.add(tn);
+            CallerListNodeBuilder.addNodeTree(tn, mn);
         }
     }
 }
