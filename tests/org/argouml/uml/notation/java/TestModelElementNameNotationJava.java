2a7e1565131ec211b1fe730d2ad28ad7a7174e18
Michiel van der Wulp <mvw@tigris.org>
2007-01-03 16:46:24 +0000

    Fix for issue 4551 as supplied by Luis Sergio Oliveira (with junit-test).
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@11705 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ tests/org/argouml/uml/notation/java/TestModelElementNameNotationJava.java
@@ -0,0 +1,85 @@
+// $Id$
+// Copyright (c) 2007 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.notation.java;
+
+import java.util.HashMap;
+
+import org.argouml.kernel.ProjectManager;
+import org.argouml.model.Model;
+import org.argouml.uml.notation.ModelElementNameNotation;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the ModelElementNameNotationJava class.
+ * 
+ * @author Luis Sergio Oliveira (euluis)
+ */
+public class TestModelElementNameNotationJava extends TestCase {
+    private Object theClass;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        theClass = Model.getCoreFactory().buildClass("TheClass", getModel());
+    }
+
+    public void testToStringForRealization() {
+        Object theInterface = Model.getCoreFactory().buildInterface(
+                "TheInterface", getModel());
+        Object realization = Model.getCoreFactory().buildRealization(theClass,
+                theInterface, getModel());
+        assertToStringForUnnamedRelationshipIsEmpty(realization);
+        assertToStringForNamedRelationshipEqualsItsName(realization);
+    }
+
+    private void assertToStringForUnnamedRelationshipIsEmpty(
+            Object relationship) {
+        ModelElementNameNotation notation = new ModelElementNameNotationJava(
+                relationship);
+        assertEquals("", notation.toString(relationship, new HashMap()));
+    }
+
+    private void assertToStringForNamedRelationshipEqualsItsName(
+            Object relationship) {
+        ModelElementNameNotation notation = new ModelElementNameNotationJava(
+                relationship);
+        Model.getCoreHelper().setName(relationship, "relationshipName");
+        assertEquals(Model.getFacade().getName(relationship), notation.toString(
+                relationship, new HashMap()));
+    }
+
+    public void testToStringForGeneralization() {
+        Object theBaseClass = Model.getCoreFactory().buildClass("TheBaseClass",
+                getModel());
+        Object generalization = Model.getCoreFactory().buildGeneralization(
+                theClass, theBaseClass);
+        assertToStringForUnnamedRelationshipIsEmpty(generalization);
+        assertToStringForNamedRelationshipEqualsItsName(generalization);
+    }
+
+    private Object getModel() {
+        return ProjectManager.getManager().getCurrentProject().getModel();
+    }
+}
