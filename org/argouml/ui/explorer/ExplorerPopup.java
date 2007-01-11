91161d064d72776c08a552d6cbb7ff8ec23a34cb
Tom Morris <tfmorris@tigris.org>
2007-01-11 01:59:08 +0000

    Remove stray import
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@11805 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/ui/explorer/ExplorerPopup.java
+++ org/argouml/ui/explorer/ExplorerPopup.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -69,8 +69,6 @@ import org.argouml.uml.ui.ActionStateDiagram;
 import org.argouml.uml.ui.ActionUseCaseDiagram;
 import org.tigris.gef.base.Diagram;
 import org.tigris.gef.graph.MutableGraphModel;
-
-import com.sun.corba.se.internal.iiop.messages.Message;
 
 /**
  * PopUp for extra functionality for the Explorer.
