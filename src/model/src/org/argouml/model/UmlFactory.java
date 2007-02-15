ec2c1107b3a5f09e7fc3f66d48d25cce4c25be99
Tom Morris <tfmorris@tigris.org>
2007-02-15 19:58:54 +0000

    Issue 4632: change implementation to make WFR checking optional Adjust valid connection array to better match UML spec.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12054 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/model/src/org/argouml/model/UmlFactory.java
+++ src/model/src/org/argouml/model/UmlFactory.java
@@ -81,11 +81,11 @@ public interface UmlFactory {
      * Checks if some type of UML model element is valid to
      * connect two other existing UML model elements.<p>
      * 
-     * This only works for UML elements. If a diagram contains
-     * elements of another type then it is the responsibility
-     * of the diagram to filter those out before calling this
-     * method.
-     *
+     * This version of the method may also enforce well-formedness rules from
+     * the UML spec.  To control use behavior use 
+     * {@link #isConnectionValid(Object, Object, Object, boolean)}.<p>
+     * 
+     * @deprecated for 0.25.1 by tfmorris use 4 arg version
      * @param connectionType  the UML object type of the connection
      * @param fromElement     the UML object type of the "from"
      * @param toElement       the UML object type of the "to"
@@ -93,7 +93,23 @@ public interface UmlFactory {
      */
     boolean isConnectionValid(Object connectionType, Object fromElement,
             Object toElement);
-
+    
+    /**
+     * Checks if the given type of UML model element is valid to connect two
+     * other existing UML model elements, optionally checking UML
+     * well-formedness rules in addition to basic metamodel validity.
+     * <p>
+     * 
+     * @param connectionType
+     *            the UML object type of the connection
+     * @param fromElement
+     *            the UML object type of the "from"
+     * @param toElement
+     *            the UML object type of the "to"
+     * @return true if valid
+     */
+    boolean isConnectionValid(Object connectionType, Object fromElement,
+            Object toElement, boolean checkWFR);
     /**
      * Delete a model element. This will do a a 'cascading deletes' 
      * which recursively deletes any model elements which would no
