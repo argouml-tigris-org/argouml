5aec24a6b8ae38e72c46cdebbf3a4e46f6af8b09
Michiel van der Wulp <mvw@tigris.org>
2007-02-16 09:07:56 +0000

    Make the pop-up menu on the Enumeration fig work, and give the item "Add EnumerationLiteral" an icon. Added a junit test for the presence of the icon.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@12073 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/application/helpers/ResourceLoaderWrapper.java
+++ org/argouml/application/helpers/ResourceLoaderWrapper.java
@@ -361,6 +361,7 @@ public final class ResourceLoaderWrapper {
         images.put("button.new-dependency", "Dependency");
         images.put("button.new-destroyaction", "DestroyAction");
         images.put("button.new-enumeration", "Enumeration");
+        images.put("button.new-enumeration-literal", "EnumerationLiteral");
         images.put("button.new-extension-point", "New Extension Point");
         images.put("button.new-extend", "Extend");
     }
