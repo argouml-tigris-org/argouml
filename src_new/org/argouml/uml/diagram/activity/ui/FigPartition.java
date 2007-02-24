7935e245c2345a63c8380a7964bb7fa9b2aa3ed9
Bob Tarling <bobtarling@tigris.org>
2007-02-24 18:51:04 +0000

    Provide specialist Selection class for FigPartition
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12134 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/diagram/activity/ui/FigPartition.java
+++ src_new/org/argouml/uml/diagram/activity/ui/FigPartition.java
@@ -26,17 +26,23 @@ package org.argouml.uml.diagram.activity.ui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.argouml.model.Model;
 import org.argouml.uml.diagram.ui.FigNodeModelElement;
+import org.tigris.gef.base.Globals;
 import org.tigris.gef.base.LayerPerspective;
+import org.tigris.gef.base.Selection;
 import org.tigris.gef.graph.GraphModel;
 import org.tigris.gef.presentation.Fig;
 import org.tigris.gef.presentation.FigLine;
 import org.tigris.gef.presentation.FigRect;
 import org.tigris.gef.presentation.FigText;
+import org.tigris.gef.presentation.Handle;
 
 /**
  * This class represents a Partition or Swimlane for Activity diagrams.
@@ -208,6 +214,7 @@ public class FigPartition extends FigNodeModelElement {
      * @see org.tigris.gef.presentation.Fig#setBoundsImpl(int, int, int, int)
      */
     protected void setBoundsImpl(int x, int y, int w, int h) {
+	
         if (getNameFig() == null) {
             return;
         }
@@ -225,6 +232,266 @@ public class FigPartition extends FigNodeModelElement {
         calcBounds(); //_x = x; _y = y; _w = w; _h = h;
         updateEdges();
     }
+    
+    public Selection makeSelection() {
+	return new SelectionPartition(this);
+    }
+    
+    /**
+     * A specialist Selection class for FigPartitions.
+     * This ensures that all swimlanes are the same length (ie height).
+     * TODO: Make sure that all swimlanes appear side by side (UML spec
+     * states "separated from neighboring swimlanes by vertical solid
+     * lines on both sides".
+     * TODO: Allow drag of the west and east edge to resize both the selected
+     * Fig and the fig connected to that side.
+     * TODO: Show NorthWest and SouthWest handle only on leftmost swimlane.
+     * TODO: Show NorthEast and SouthEast handle only on rightmost swimlane.
+     * @author Bob
+     */
+    private class SelectionPartition extends Selection {
 
+        private int cx;
+        private int cy;
+        private int cw;
+        private int ch;
+        
+        /**
+         * Construct a new SelectionPartition for the given partition
+         */
+        public SelectionPartition(FigPartition f) {
+            super(f);
+        }
+        
+        /** Return a handle ID for the handle under the mouse, or -1 if
+         *  none. Needs-More-Work: in the future, return a Handle instance or
+         *  null. <p>
+         *  <pre>
+         *   0-------1-------2
+         *   |               |
+         *   3               4
+         *   |               |
+         *   5-------6-------7
+         * </pre>
+         */
+        public void hitHandle(Rectangle r, Handle h) {
+            if (getContent().isResizable()) {
+        
+                updateHandleBox();
+                Rectangle testRect = new Rectangle(0, 0, 0, 0);
+                testRect.setBounds(
+                    cx - HAND_SIZE / 2,
+                    cy - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    ch + HAND_SIZE / 2);
+                boolean leftEdge = r.intersects(testRect);
+                testRect.setBounds(
+                    cx + cw - HAND_SIZE / 2,
+                    cy - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    ch + HAND_SIZE / 2);
+                boolean rightEdge = r.intersects(testRect);
+                testRect.setBounds(
+                    cx - HAND_SIZE / 2,
+                    cy - HAND_SIZE / 2,
+                    cw + HAND_SIZE / 2,
+                    HAND_SIZE);
+                boolean topEdge = r.intersects(testRect);
+                testRect.setBounds(
+                    cx - HAND_SIZE / 2,
+                    cy + ch - HAND_SIZE / 2,
+                    cw + HAND_SIZE / 2,
+                    HAND_SIZE);
+                boolean bottomEdge = r.intersects(testRect);
+                // TODO: midpoints for side handles
+                if (leftEdge && topEdge) {
+                    h.index = Handle.NORTHWEST;
+                    h.instructions = "Resize top left";
+                } else if (rightEdge && topEdge) {
+                    h.index = Handle.NORTHEAST;
+                    h.instructions = "Resize top right";
+                } else if (leftEdge && bottomEdge) {
+                    h.index = Handle.SOUTHWEST;
+                    h.instructions = "Resize bottom left";
+                } else if (rightEdge && bottomEdge) {
+                    h.index = Handle.SOUTHEAST;
+                    h.instructions = "Resize bottom right";
+                }
+                // TODO: side handles
+                else {
+                    h.index = -1;
+                    h.instructions = "Move object(s)";
+                }
+            } else {
+                h.index = -1;
+                h.instructions = "Move object(s)";
+            }
+        
+        }
+        
+        /** Update the private variables cx etc. that represent the rectangle on
+        	  whose corners handles are to be drawn.*/
+        private void updateHandleBox() {
+            final Rectangle cRect = getContent().getHandleBox();
+            cx = cRect.x;
+            cy = cRect.y;
+            cw = cRect.width;
+            ch = cRect.height;
+        }
+        
+        /** Paint the handles at the four corners and midway along each edge
+         * of the bounding box.  */
+        public void paint(Graphics g) {
+            final Fig fig = getContent();
+            if (getContent().isResizable()) {
+        
+                updateHandleBox();
+                g.setColor(Globals.getPrefs().handleColorFor(fig));
+                g.fillRect(
+                    cx - HAND_SIZE / 2,
+                    cy - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    HAND_SIZE);
+                g.fillRect(
+                    cx + cw - HAND_SIZE / 2,
+                    cy - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    HAND_SIZE);
+                g.fillRect(
+                    cx - HAND_SIZE / 2,
+                    cy + ch - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    HAND_SIZE);
+                g.fillRect(
+                    cx + cw - HAND_SIZE / 2,
+                    cy + ch - HAND_SIZE / 2,
+                    HAND_SIZE,
+                    HAND_SIZE);
+            } else {
+                final int x = fig.getX();
+                final int y = fig.getY();
+                final int w = fig.getWidth();
+                final int h = fig.getHeight();
+                g.setColor(Globals.getPrefs().handleColorFor(fig));
+                g.drawRect(
+                    x - BORDER_WIDTH,
+                    y - BORDER_WIDTH,
+                    w + BORDER_WIDTH * 2 - 1,
+                    h + BORDER_WIDTH * 2 - 1);
+                g.drawRect(
+                    x - BORDER_WIDTH - 1,
+                    y - BORDER_WIDTH - 1,
+                    w + BORDER_WIDTH * 2 + 2 - 1,
+                    h + BORDER_WIDTH * 2 + 2 - 1);
+                g.fillRect(x - HAND_SIZE, y - HAND_SIZE, HAND_SIZE, HAND_SIZE);
+                g.fillRect(x + w, y - HAND_SIZE, HAND_SIZE, HAND_SIZE);
+                g.fillRect(x - HAND_SIZE, y + h, HAND_SIZE, HAND_SIZE);
+                g.fillRect(x + w, y + h, HAND_SIZE, HAND_SIZE);
+            }
+        }
+        
+        /**
+         * Change some attribute of the selected Fig when the user drags one
+         * of its handles. Needs-More-Work: someday I might implement resizing
+         * that maintains the aspect ratio.
+         */
+        public void dragHandle(int mX, int mY, int anX, int anY, Handle hand) {
+            
+            final Fig fig = getContent();
+            final List partitions = new ArrayList();
+        
+            Iterator it = fig.getLayer().getContents().iterator();
+            while (it.hasNext()) {
+        	Object o = it.next();
+        	if (o instanceof FigPartition) {
+        	    partitions.add(o);
+        	}
+            }
+
+            updateHandleBox();
+        
+            final int x = cx;
+            final int y = cy;
+            final int w = cw;
+            final int h = ch;
+            int newX = x, newY = y, newWidth = w, newHeight = h;
+            Dimension minSize = fig.getMinimumSize();
+            int minWidth = minSize.width, minHeight = minSize.height;
+            switch (hand.index) {
+            case -1 :
+                fig.translate(anX - mX, anY - mY);
+                return;
+            case Handle.NORTHWEST :
+                newWidth = x + w - mX;
+                newWidth = (newWidth < minWidth) ? minWidth : newWidth;
+                newHeight = y + h - mY;
+                newHeight = (newHeight < minHeight) ? minHeight : newHeight;
+                newX = x + w - newWidth;
+                newY = y + h - newHeight;
+                fig.setHandleBox(newX, newY, newWidth, newHeight);
+                if ((newX + newWidth) != (x + w)) {
+                    newX += (newX + newWidth) - (x + w);
+                }
+                if ((newY + newHeight) != (y + h)) {
+                    newY += (newY + newHeight) - (y + h);
+                }
+                setHandleBox(partitions, newX, newY, newWidth, newHeight);
+                return;
+            case Handle.NORTH :
+                break;
+            case Handle.NORTHEAST :
+                newWidth = mX - x;
+                newWidth = (newWidth < minWidth) ? minWidth : newWidth;
+                newHeight = y + h - mY;
+                newHeight = (newHeight < minHeight) ? minHeight : newHeight;
+                newY = y + h - newHeight;
+                fig.setHandleBox(newX, newY, newWidth, newHeight);
+                if ((newY + newHeight) != (y + h)) {
+                    newY += (newY + newHeight) - (y + h);
+                }
+                setHandleBox(partitions, newX, newY, newWidth, newHeight);
+                break;
+            case Handle.WEST :
+                break;
+            case Handle.EAST :
+                break;
+            case Handle.SOUTHWEST :
+                newWidth = x + w - mX;
+                newWidth = (newWidth < minWidth) ? minWidth : newWidth;
+                newHeight = mY - y;
+                newHeight = (newHeight < minHeight) ? minHeight : newHeight;
+                newX = x + w - newWidth;
+                fig.setHandleBox(newX, newY, newWidth, newHeight);
+                if ((newX + newWidth) != (x + w)) {
+                    newX += (newX + newWidth) - (x + w);
+                }
+                setHandleBox(partitions, newX, newY, newWidth, newHeight);
+                break;
+            case Handle.SOUTH :
+                break;
+            case Handle.SOUTHEAST :
+                newWidth = mX - x;
+                newWidth = (newWidth < minWidth) ? minWidth : newWidth;
+                newHeight = mY - y;
+                newHeight = (newHeight < minHeight) ? minHeight : newHeight;
+                setHandleBox(partitions, newX, newY, newWidth, newHeight);
+                break;
+            }
+        }
+        
+        private void setHandleBox(
+        	List partitions, int x, int y, int width, int height) {
+            Iterator it = partitions.iterator();
+            while (it.hasNext()) {
+        	Fig f = (Fig) it.next();
+        	if (f == getContent()) {
+                    f.setHandleBox(x, y, width, height);
+        	} else {
+                    f.setHandleBox(f.getX(), y, f.getWidth(), height);
+        	}
+            }
+            
+        }
+    }
 }
 
