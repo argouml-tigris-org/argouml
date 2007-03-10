b63e6918fc3f9c4dae1967d8e0d0398657344786
Thomas Neustupny <thn@tigris.org>
2007-03-10 08:08:06 +0000

    fix for issue 4686 and added new java RE options in the java tab, still not functional
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@12249 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/reveng/ConfigPanelExtension.java
+++ org/argouml/uml/reveng/ConfigPanelExtension.java
@@ -29,10 +29,15 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 
+import org.argouml.application.api.Argo;
+import org.argouml.application.api.Configuration;
+import org.argouml.application.api.ConfigurationKey;
 import org.argouml.i18n.Translator;
 
 /**
@@ -40,12 +45,33 @@ import org.argouml.i18n.Translator;
  */
 public class ConfigPanelExtension extends JPanel {
 
+    /**
+     * Key for RE general settings: level of import detail. Values:
+     * 0: classifiers only
+     * 1: classifiers plus feature specifications
+     * 2: full import
+     */
+    public static final ConfigurationKey KEY_IMPORT_EXTENDED_MODEL_ATTR =
+        Configuration.makeKey("import", "extended", "java", "model", "attributes");
+
+    /**
+     * Key for RE general settings: level of import detail. Values:
+     * 0: classifiers only
+     * 1: classifiers plus feature specifications
+     * 2: full import
+     */
+    public static final ConfigurationKey KEY_IMPORT_EXTENDED_MODEL_ARRAYS =
+        Configuration.makeKey("import", "extended", "java", "model", "arrays");
+
     private JPanel configPanel;
 
     private JRadioButton attribute;
 
     private JRadioButton datatype;
 
+    private JCheckBox modelcollections, modelorderedcollections;
+
+    private JTextField collectionlist, orderedcollectionlist;
 
     /**
      * Construct a new default configuration extension panel.
@@ -59,42 +85,84 @@ public class ConfigPanelExtension extends JPanel {
             new JLabel(
                     Translator.localize("action.import-java-attr-model"));
         configPanel.add(attributeLabel1,
-                createGridBagConstraints(true, false));
+                createGridBagConstraints(true, false, false));
         ButtonGroup group1 = new ButtonGroup();
         attribute =
             new JRadioButton(
                     Translator.localize("action.import-java-UML-attr"));
-        attribute.setSelected(true);
         group1.add(attribute);
         configPanel.add(attribute,
-                createGridBagConstraints(false, false));
+                createGridBagConstraints(false, false, false));
         JRadioButton association =
             new JRadioButton(
                     Translator.localize("action.import-java-UML-assoc"));
         group1.add(association);
         configPanel.add(association,
-                createGridBagConstraints(false, true));
+                createGridBagConstraints(false, true, false));
+        String modelattr =
+            Configuration.getString(KEY_IMPORT_EXTENDED_MODEL_ATTR);
+        if ("1".equals(modelattr)) {
+            association.setSelected(true);
+        } else {
+            attribute.setSelected(true);
+        }
         
         JLabel attributeLabel2 =
             new JLabel(
                     Translator.localize("action.import-java-array-model"));
         configPanel.add(attributeLabel2,
-                createGridBagConstraints(true, false));
+                createGridBagConstraints(true, false, false));
         ButtonGroup group2 = new ButtonGroup();
         datatype =
             new JRadioButton(
                     Translator.localize(
                             "action.import-java-array-model-datatype"));
-        datatype.setSelected(true);
         group2.add(datatype);
         configPanel.add(datatype,
-                createGridBagConstraints(false, false));
+                createGridBagConstraints(false, false, false));
         JRadioButton multi =
             new JRadioButton(
                     Translator.localize(
                     "action.import-java-array-model-multi"));
         group2.add(multi);
         configPanel.add(multi,
+                createGridBagConstraints(false, true, false));
+        String modelarrays =
+            Configuration.getString(KEY_IMPORT_EXTENDED_MODEL_ARRAYS);
+        if ("1".equals(modelarrays)) {
+            multi.setSelected(true);
+        } else {
+            datatype.setSelected(true);
+        }
+
+        modelcollections =
+            new JCheckBox(Translator.localize(
+                    "action.import-option-model-collections"), false);
+        configPanel.add(modelcollections,
+                createGridBagConstraints(true, false, false));
+
+        collectionlist = new JTextField();
+        configPanel.add(collectionlist,
+                createGridBagConstraints(false, false, true));
+        JLabel listLabel =
+            new JLabel(
+                    Translator.localize("action.import-comma-separated-names"));
+        configPanel.add(listLabel,
+                createGridBagConstraints(false, true, false));
+
+        modelorderedcollections =
+            new JCheckBox(Translator.localize(
+                    "action.import-option-model-ordered-collections"), false);
+        configPanel.add(modelorderedcollections,
+                createGridBagConstraints(true, false, false));
+
+        orderedcollectionlist = new JTextField();
+        configPanel.add(orderedcollectionlist,
+                createGridBagConstraints(false, false, true));
+        listLabel =
+            new JLabel(
+                    Translator.localize("action.import-comma-separated-names"));
+        configPanel.add(listLabel,
                 createGridBagConstraintsFinal());
 
         // TODO: Get the list of extended settings from the current
@@ -110,7 +178,7 @@ public class ConfigPanelExtension extends JPanel {
      * @return
      */
     private GridBagConstraints createGridBagConstraints(boolean topInset,
-            boolean bottomInset) {
+            boolean bottomInset, boolean fill) {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = GridBagConstraints.RELATIVE;
         gbc.gridy = GridBagConstraints.RELATIVE;
@@ -119,7 +187,7 @@ public class ConfigPanelExtension extends JPanel {
         gbc.weightx = 1.0;
         gbc.weighty = 0.0;
         gbc.anchor = GridBagConstraints.NORTHWEST;
-        gbc.fill = GridBagConstraints.NONE;
+        gbc.fill = fill ? GridBagConstraints.HORIZONTAL : GridBagConstraints.NONE;
         gbc.insets = 
             new Insets(
                     topInset ? 5 : 0, 
@@ -136,7 +204,7 @@ public class ConfigPanelExtension extends JPanel {
      * @return the GridBagConstraints object
      */
     private GridBagConstraints createGridBagConstraintsFinal() {
-        GridBagConstraints gbc = createGridBagConstraints(false, true);
+        GridBagConstraints gbc = createGridBagConstraints(false, true, false);
         gbc.gridheight = GridBagConstraints.REMAINDER;
         gbc.weighty = 1.0;
         return gbc;
@@ -156,4 +224,10 @@ public class ConfigPanelExtension extends JPanel {
         return datatype;
     }
 
+    public void disposeDialog() {
+        Configuration.setString(KEY_IMPORT_EXTENDED_MODEL_ATTR,
+                String.valueOf(getAttribute().isSelected() ? "0" : "1"));
+        Configuration.setString(KEY_IMPORT_EXTENDED_MODEL_ARRAYS,
+                String.valueOf(getDatatype().isSelected() ? "0" : "1"));
+    }
 }
