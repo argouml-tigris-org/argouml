1bc92a9be60e798be399e2015f18556700d8a55c
Tom Morris <tfmorris@tigris.org>
2007-03-29 00:46:27 +0000

    Refactor the two independent OCLEvaluators and merge bug fixes which had only been made to one or the other.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12302 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ocl/CriticOclEvaluator.java
+++ src_new/org/argouml/ocl/CriticOclEvaluator.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -24,26 +24,33 @@
 
 package org.argouml.ocl;
 
-import java.util.Iterator;
-
-
-import org.argouml.i18n.Translator;
-import org.argouml.model.Model;
-import org.tigris.gef.base.Diagram;
 import org.tigris.gef.ocl.ExpansionException;
 
 
 /**
- * CriticOclEvaluator is responsible for evaluating simple OCL expressions
- * used in the critiques.<p>
- *
+ * CriticOclEvaluator is singleton version of OCLEvaluator which is used for
+ * evaluating simple OCL expressions used in the critiques.<p>
+ * 
+ * Implementation History: As best I was able to reconstruct the history in
+ * early 2007, OclEvaluator was originally implemented as a singleton, but that
+ * was changed in September, 2004 because of some perceived problems, but then
+ * it was discovered that the Critic subsystem depended on the singleton
+ * pattern, so an earlier version of OclEvaluator was reintroduced with a new
+ * name. Because of the large amount of duplicate code between the two classes,
+ * this was refactored in March, 2007 to use an instance of the main
+ * OclEvaluator class.
+ * 
  * @stereotype singleton
+ * @deprecated for 0.25.2 by tfmorris - use {@link OCLEvaluator}
  */
-public class CriticOclEvaluator extends org.tigris.gef.ocl.OCLEvaluator {
+public class CriticOclEvaluator {
 
     private static final CriticOclEvaluator INSTANCE =
         new CriticOclEvaluator();
 
+    private static final OCLEvaluator EVALUATOR =
+        new OCLEvaluator();
+    
     private CriticOclEvaluator() {
         // no instantiations
     }
@@ -56,95 +63,24 @@ public class CriticOclEvaluator extends org.tigris.gef.ocl.OCLEvaluator {
     }
 
     /*
-     * @see org.tigris.gef.ocl.OCLEvaluator#evalToString(
-     * java.lang.Object, java.lang.String)
+     * @see OCLEvaluator#evalToString(java.lang.Object, java.lang.String)
      */
     public synchronized String evalToString(Object self, String expr)
         throws ExpansionException {
-        String res = null;
-        if (GET_NAME_EXPR_1.equals(expr)
-                && Model.getFacade().isAModelElement(self)) {
-            res = Model.getFacade().getName(self);
-            if (res == null || "".equals(res)) {
-                res = Translator.localize("misc.name.anon");
-            }
-        }
-        if (GET_NAME_EXPR_2.equals(expr)
-                && Model.getFacade().isAModelElement(self)) {
-            res = Model.getFacade().getName(self);
-            if (res == null || "".equals(res)) {
-                res = Translator.localize("misc.name.anon");
-            }
-        }
-        if (GET_OWNER_EXPR.equals(expr) && Model.getFacade().isAFeature(self)) {
-            Object owner = Model.getFacade().getOwner(self);
-            if (owner != null) {
-                res = Model.getFacade().getName(owner);
-                if (res == null || "".equals(res)) {
-                    res = Translator.localize("misc.name.anon");
-                }
-            }
-        }
-        if (GET_NAME_EXPR_1.equals(expr) && self instanceof Diagram) {
-            res = ((Diagram) self).getName();
-            if (res == null || "".equals(res)) {
-                res = Translator.localize("misc.name.anon");
-            }
-        }
-        if (GET_NAME_EXPR_2.equals(expr) && self instanceof Diagram) {
-            res = ((Diagram) self).getName();
-            if (res == null || "".equals(res)) {
-                res = Translator.localize("misc.name.anon");
-            }
-        }
-    /*
-        if (GET_OWNER_EXPR.equals(expr) && self instanceof Diagram) {
-            res = ((Diagram)self).getOwner().getName();
-            if (res == null || "".equals(res)) {
-                res = Translator.localize("misc.name.anon");
-            }
-        }
-    */
-        if (res == null) {
-            res = evalToString(self, expr, ", ");
-        }
-        return res;
+        
+        return EVALUATOR.evalToString(self, expr);
     }
 
     /*
-     * @see org.tigris.gef.ocl.OCLEvaluator#evalToString(
-     * java.lang.Object, java.lang.String, java.lang.String)
+     * @see OCLEvaluator#evalToString(java.lang.Object, java.lang.String, java.lang.String)
      */
     public synchronized String evalToString(
             Object self,
             String expr,
-            String sep) throws ExpansionException {
-        _scratchBindings.put("self", self);
-        java.util.List values = eval(_scratchBindings, expr);
-        _strBuf.setLength(0);
-        Iterator iter = values.iterator();
-        while (iter.hasNext()) {
-            Object v = iter.next();
-            if (Model.getFacade().isAModelElement(v)) {
-                v = Model.getFacade().getName(v);
-                if ("".equals(v)) {
-                    v = Translator.localize("misc.name.anon");
-                }
-            }
-            if (Model.getFacade().isAExpression(v)) {
-                v = Model.getFacade().getBody(v);
-                if ("".equals(v)) {
-                    v = "(unspecified)";
-                }
-            }
-            if (!"".equals(v)) {
-                _strBuf.append(v);
-                if (iter.hasNext()) {
-                    _strBuf.append(sep);
-                }
-            }
-        }
-        return _strBuf.toString();
+            String sep)
+    	throws ExpansionException {
+        
+        return EVALUATOR.evalToString(self, expr, sep);
     }
 
-}  // end of OCLEvaluator
+}
