b8b6cc31b5aa09a6f2fd3736a3e7ead01e238721
Bob Tarling <bobtarling@tigris.org>
2007-04-13 19:42:23 +0000

    Fig aren't invisible in ArgoUML
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@12386 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/deployment/ui/FigComponent.java
+++ org/argouml/uml/diagram/deployment/ui/FigComponent.java
@@ -281,21 +281,15 @@ public class FigComponent extends FigNodeModelElement {
                     }
                 }
             }
-        } else {
-            if (isVisible() 
-                    // If we are not visible most likely we're being deleted.
-                    // TODO: This indicates a more fundamental problem that should
-                    // be investigated - tfm - 20061230
-                    && encloser == null && getEnclosingFig() != null) {
-                Object encloserOwner = getEnclosingFig().getOwner();
-                if (Model.getFacade().isANode(encloserOwner)
-                        && (Model.getFacade().getDeploymentLocations(comp).
-                                contains(encloserOwner))) {
-                    Model.getCoreHelper()
-                            .removeDeploymentLocation(comp, encloserOwner);
-                }
-                super.setEnclosingFig(encloser);
+        } else if (encloser == null && getEnclosingFig() != null) {
+            Object encloserOwner = getEnclosingFig().getOwner();
+            if (Model.getFacade().isANode(encloserOwner)
+                    && (Model.getFacade().getDeploymentLocations(comp).
+                            contains(encloserOwner))) {
+                Model.getCoreHelper()
+                        .removeDeploymentLocation(comp, encloserOwner);
             }
+            super.setEnclosingFig(encloser);
         }
     }
 
