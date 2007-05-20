6e7b40567245a4b3a2596133933de8c2d9b78065
Michiel van der Wulp <mvw@tigris.org>
2007-05-20 14:09:51 +0000

    Removed (cyclic) dependency from org.argouml.ui to org.argouml.moduleloader, by moving the field with the paths to the modules from one settings panel to another.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12630 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tests/org/argouml/TestDependencies.java
+++ tests/org/argouml/TestDependencies.java
@@ -141,6 +141,7 @@ public class TestDependencies extends TestCase {
             {"org.argouml.language.java.generator", "org.argouml.notation"},
             {"org.argouml.notation", "org.argouml.notation.ui"},
             {"org.argouml.ui.targetmanager", "org.argouml.ui"},
+            {"org.argouml.ui", "org.argouml.moduleloader"},
         };
         for (int i = 0; i < dep.length; i++) {
             suite.addTest(new CheckNoDependency(jdepend, dep[i]));
