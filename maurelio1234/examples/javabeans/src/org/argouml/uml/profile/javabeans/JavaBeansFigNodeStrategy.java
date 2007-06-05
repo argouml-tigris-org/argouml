7872e261f96170edb8dcaf5344524b5f8d981fe2
Marcos Aurélio <maurelio1234@tigris.org>
2007-06-05 07:56:56 +0000

    week 1: displaying icons for stereotyped classifiers
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/gsoc2007@12756 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- maurelio1234/examples/javabeans/src/org/argouml/uml/profile/javabeans/JavaBeansFigNodeStrategy.java
+++ maurelio1234/examples/javabeans/src/org/argouml/uml/profile/javabeans/JavaBeansFigNodeStrategy.java
@@ -1,21 +1,40 @@
 package org.argouml.uml.profile.javabeans;
 
-import java.util.logging.Logger;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.BufferedInputStream;
+import java.io.IOException;
 
 import org.argouml.model.Model;
 import org.argouml.uml.profile.FigNodeStrategy;
-import org.tigris.gef.presentation.Fig;
-import org.tigris.gef.presentation.FigRect;
+import org.tigris.gef.presentation.FigImage;
 
 public class JavaBeansFigNodeStrategy implements FigNodeStrategy {
 
-    public Fig[] getExtraFiguresForNode(Object element) {
-	Fig ret = null;
-//	if (Model.getExtensionMechanismsHelper().hasStereoType(element, "Bean")) {
-	    ret = new FigRect(10, 10, 16, 16);
-//	}
+    private FigImage icon = null;
+    
+    public JavaBeansFigNodeStrategy() {
+	BufferedInputStream bis = new BufferedInputStream(this.getClass()
+		.getResourceAsStream("bean.GIF"));
+	byte[] buf = new byte[10000];
+	try {
+	    bis.read(buf);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	Image img = Toolkit.getDefaultToolkit().createImage(buf);
+	icon = new FigImage(0, 0, img);
+    }
+    
+    public FigImage getIconForStereotype(Object stereotype) {
+	FigImage ret = null;
+	String s = Model.getFacade().getName(stereotype);
+	
+	if ("Bean".equals(s)) {
+	    ret = icon; 
+	}
 	    
-	return new Fig[] { ret };
+	return ret;
     }
     
 }
