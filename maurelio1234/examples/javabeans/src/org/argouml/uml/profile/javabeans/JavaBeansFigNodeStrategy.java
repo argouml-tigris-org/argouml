f8d86a8ae63c4b2deeff102e6ea94763e06c6f51
Marcos Aurélio <maurelio1234@tigris.org>
2007-06-07 12:15:57 +0000

    figures work only for classes
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/gsoc2007@12769 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- maurelio1234/examples/javabeans/src/org/argouml/uml/profile/javabeans/JavaBeansFigNodeStrategy.java
+++ maurelio1234/examples/javabeans/src/org/argouml/uml/profile/javabeans/JavaBeansFigNodeStrategy.java
@@ -10,23 +10,32 @@ import org.argouml.uml.profile.FigNodeStrategy;
 
 public class JavaBeansFigNodeStrategy implements FigNodeStrategy {
 
-    private Image icon = null;
+    private Image iconBean = null;
+    private Image iconRay  = null;
     
     public JavaBeansFigNodeStrategy() {
+	iconBean = readImage("bean.GIF");
+	iconRay  = readImage("raio.gif");
+    }
+
+    private final byte[] buf = new byte[10000];
+    
+    private Image readImage(String fileName) {
 	BufferedInputStream bis = new BufferedInputStream(this.getClass()
-		.getResourceAsStream("bean.GIF"));
-	byte[] buf = new byte[10000];
+		.getResourceAsStream(fileName));
 	try {
 	    bis.read(buf);
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-	icon = Toolkit.getDefaultToolkit().createImage(buf);
+	return Toolkit.getDefaultToolkit().createImage(buf);
     }
     
     public Image getIconForStereotype(Object stereotype) {
 	if ("Bean".equals(Model.getFacade().getName(stereotype))) {
-	    return icon;
+	    return iconBean;
+	} else 	if ("Event".equals(Model.getFacade().getName(stereotype))) {
+	    return iconRay;
 	} else {
 	    return null;
 	}
