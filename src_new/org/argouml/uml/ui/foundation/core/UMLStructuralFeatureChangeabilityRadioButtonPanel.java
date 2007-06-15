89616a7a8b5f583e6f6b96474cdb7f45224ba96e
Tom Morris <tfmorris@tigris.org>
2007-06-15 06:02:38 +0000

    Indentation - no functionaly change
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12843 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/foundation/core/UMLStructuralFeatureChangeabilityRadioButtonPanel.java
+++ src_new/org/argouml/uml/ui/foundation/core/UMLStructuralFeatureChangeabilityRadioButtonPanel.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -80,7 +80,7 @@ public class UMLStructuralFeatureChangeabilityRadioButtonPanel
             if (kind == null) {
                 setSelected(null);
             } else if (kind.equals(
-                            Model.getChangeableKind().getAddOnly())) {
+                    Model.getChangeableKind().getAddOnly())) {
                 setSelected(ActionSetChangeability.ADDONLY_COMMAND);
             } else if (kind.equals(
                     Model.getChangeableKind().getChangeable())) {
