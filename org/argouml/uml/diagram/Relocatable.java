d23833846ebc5e9e8a9dfe7bc49105aa50288cd2
Michiel van der Wulp <mvw@tigris.org>
2007-06-27 15:36:24 +0000

    Remove (cyclic) dependency from UMLDiagram to org.argouml.ui.explorer.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@12910 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/Relocatable.java
+++ org/argouml/uml/diagram/Relocatable.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 2005-2006 The Regents of the University of California. All
+// Copyright (c) 2005-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -22,7 +22,7 @@
 // CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
 // UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-package org.argouml.ui.explorer;
+package org.argouml.uml.diagram;
 
 /**
  * Interface which is used by the Explorer to determine if a diagram can
