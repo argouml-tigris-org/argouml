dbd5c2a1f93a9155be071fc4c06751955e4127e2
Tom Morris <tfmorris@tigris.org>
2007-07-05 05:09:02 +0000

    Add collection types
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@12984 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/model/src/org/argouml/model/XmiReader.java
+++ src/model/src/org/argouml/model/XmiReader.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -72,7 +72,7 @@ public interface XmiReader {
     /**
      * @return the map
      */
-    Map getXMIUUIDToObjectMap();
+    Map<String, Object> getXMIUUIDToObjectMap();
     
     /**
      * Set the list of element names for which errors should be ignored when
