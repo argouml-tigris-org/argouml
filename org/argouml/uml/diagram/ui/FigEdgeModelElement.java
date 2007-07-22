5172eae38bd815bdbe10e2c90822d8c3c90e4a67
Michiel van der Wulp <mvw@tigris.org>
2007-07-22 18:19:42 +0000

    Prepare to remove this...
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk/src_new@13106 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- org/argouml/uml/diagram/ui/FigEdgeModelElement.java
+++ org/argouml/uml/diagram/ui/FigEdgeModelElement.java
@@ -639,6 +639,7 @@ public abstract class FigEdgeModelElement
     
     /**
      * TODO: Remove this - nobody should need this - Michiel.
+     * @deprecated by MVW in V0.25.4. Not used.
      * @return a i18 key that represents a help string
      *         giving an explanation to the user of the syntax
      */
