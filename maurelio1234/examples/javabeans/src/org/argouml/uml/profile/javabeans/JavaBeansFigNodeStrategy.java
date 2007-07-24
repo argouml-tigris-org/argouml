d34437a39ee9ccea2cf89f0dcfc5d622721ac765
Marcos Aurélio <maurelio1234@tigris.org>
2007-07-24 18:38:51 +0000

    solving big icon rendering problem
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/gsoc2007@13135 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ maurelio1234/examples/javabeans/src/org/argouml/uml/profile/javabeans/JavaBeansFigNodeStrategy.java
@@ -0,0 +1,56 @@
+package org.argouml.uml.profile.javabeans;
+
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+
+import org.apache.log4j.Logger;
+import org.argouml.model.Model;
+import org.argouml.uml.profile.FigNodeStrategy;
+
+/**
+ * Sample JavaBeans Profile 
+ * 
+ * @author Marcos Aurélio
+ *
+ */
+public class JavaBeansFigNodeStrategy implements FigNodeStrategy {
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = Logger.getLogger(JavaBeansFigNodeStrategy.class);
+
+    private static  final Image iconBean = readImage("bean.GIF", 2904);
+    private static final Image iconRay  = readImage("event.gif", 243);
+
+
+    private static Image readImage(String fileName, int fileSize) {
+		BufferedInputStream bis = new BufferedInputStream(
+				JavaBeansFigNodeStrategy.class.getResourceAsStream(fileName));
+
+		byte[] buf = new byte[fileSize];
+		try {
+			bis.read(buf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		LOG.info("IMAGE READ: " + fileName);
+		return new ImageIcon(buf).getImage();//Toolkit.getDefaultToolkit().createImage(buf);
+	}
+    
+    /**
+     * @see org.argouml.uml.profile.FigNodeStrategy#getIconForStereotype(java.lang.Object)
+     */
+    public Image getIconForStereotype(Object stereotype) {
+	if ("Bean".equals(Model.getFacade().getName(stereotype))) {
+	    return iconBean;
+	} else 	if ("Event".equals(Model.getFacade().getName(stereotype))) {
+	    return iconRay;
+	} else {
+	    return null;
+	}
+    }
+    
+}
