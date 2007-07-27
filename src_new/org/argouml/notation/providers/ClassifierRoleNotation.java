2c6694219ff969af391b346a1de1c3216b48225e
Michiel van der Wulp <mvw@tigris.org>
2007-07-27 08:25:39 +0000

    Made the ClassifierRole notation update when the Class name changes.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@13160 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/notation/providers/ClassifierRoleNotation.java
+++ src_new/org/argouml/notation/providers/ClassifierRoleNotation.java
@@ -24,6 +24,9 @@
 
 package org.argouml.notation.providers;
 
+import java.beans.PropertyChangeListener;
+import java.util.Collection;
+
 import org.argouml.model.Model;
 import org.argouml.notation.NotationProvider;
 
@@ -44,6 +47,15 @@ public abstract class ClassifierRoleNotation extends NotationProvider {
     public ClassifierRoleNotation(Object classifierRole) {
         if (!Model.getFacade().isAClassifierRole(classifierRole)) {
             throw new IllegalArgumentException("This is not a ClassifierRole.");
+        }
+    }
+
+    @Override
+    public void initialiseListener(PropertyChangeListener listener, Object modelElement) {
+        super.initialiseListener(listener, modelElement);
+        Collection classifiers = Model.getFacade().getBases(modelElement);
+        for (Object c : classifiers) {
+            addElementListener(listener, c, "name");
         }
     }
 
