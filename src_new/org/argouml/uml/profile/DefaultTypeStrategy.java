3b49f723c370834b67c29a8a7846a13477fafda6
Tom Morris <tfmorris@tigris.org>
2007-09-26 20:40:27 +0000

    Merge profile package from Marcus Aurelio's GSoC branch
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@13599 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src_new/org/argouml/uml/profile/DefaultTypeStrategy.java
@@ -0,0 +1,48 @@
+// $Id: eclipse-argo-codetemplates.xml 11347 2006-10-26 22:37:44Z linus $
+// Copyright (c) 2007 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.profile;
+
+/**
+ * Using this strategy profiles can provide the default used when creating new 
+ * parameters, operations and attributes.
+ *
+ * @author maurelio1234
+ */
+public interface DefaultTypeStrategy {
+    /**
+     * @return the default type used in new attributes
+     */
+    public Object getDefaultAttributeType();
+
+    /**
+     * @return the default type used in new parameters
+     */
+    public Object getDefaultParameterType();
+
+    /**
+     * @return the default type used in new operations
+     */
+    public Object getDefaultReturnType();
+}
---
+++ src_new/org/argouml/uml/profile/ProfileConfiguration.java
@@ -0,0 +1,332 @@
+// $Id: ProfileConfiguration.java 13298 2007-08-12 19:40:57Z maurelio1234 $
+// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+package org.argouml.uml.profile;
+
+import java.awt.Image;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
+import org.argouml.kernel.AbstractProjectMember;
+import org.argouml.kernel.Project;
+import org.argouml.model.Model;
+import org.argouml.ui.explorer.ExplorerEventAdaptor;
+/**
+ *   This class captures represents the unique access point for the 
+ *   configurability allowed by the use of profiles. 
+ *
+ *   @author maurelio1234
+ */
+public class ProfileConfiguration extends AbstractProjectMember {
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = Logger
+            .getLogger(ProfileConfiguration.class);
+
+    private FormatingStrategy formatingStrategy;
+    private DefaultTypeStrategy defaultTypeStrategy;
+    private Vector	      figNodeStrategies = new Vector();
+    
+    private Vector<Profile> profiles = new Vector<Profile>();
+    private Vector<Object> profileModels = new Vector<Object>();
+    
+    /**
+     * The extension used in serialization and returned by {@link #getType()}
+     */
+    public static final String EXTENSION = "profile";
+        
+    /**
+     * The default constructor for this class. Sets the Java profile as the 
+     * default one and its formating strategy as the default one.
+     * 
+     * @param project the project that contains this configuration
+     */
+    public ProfileConfiguration(Project project) {
+	super(EXTENSION, project);
+	
+        Iterator it = ProfileManagerImpl.getInstance().getDefaultProfiles().iterator();
+        
+        while (it.hasNext()) {
+            Profile p = (Profile) it.next();
+            addProfile(p);
+        }
+
+        updateStrategies();
+    }
+    
+    private void updateStrategies() {
+        for (Profile profile : profiles) {
+            activateFormatingStrategy(profile);                
+            activateDefaultTypeStrategy(profile);                
+        }
+    }
+
+    /**
+     * @return the current formating strategy
+     */
+    public FormatingStrategy getFormatingStrategy() {
+        return formatingStrategy;
+    }
+
+    /**
+     * @return the current default type strategy
+     */
+    public DefaultTypeStrategy getDefaultTypeStrategy() {
+        return defaultTypeStrategy;
+    }
+    
+    /**
+     * Updates the current strategy to the strategy provided by the 
+     * passed profile. The profile should have been previously registered.
+     * 
+     * @param profile the profile providing the current default type strategy
+     */
+    public void activateDefaultTypeStrategy(Profile p) {
+        if (p != null && p.getDefaultTypeStrategy() != null
+                && getProfiles().contains(p)) {
+            this.defaultTypeStrategy = p.getDefaultTypeStrategy();
+        }
+    }
+    
+    /**
+     * Updates the current strategy to the strategy provided by the 
+     * passed profile. The profile should have been previously registered.
+     * 
+     * @param profile the profile providing the current formating strategy
+     */
+    public void activateFormatingStrategy(Profile profile) {
+	if (profile != null && profile.getFormatingStrategy() != null
+		&& getProfiles().contains(profile)) {
+	    this.formatingStrategy = profile.getFormatingStrategy();
+	}
+    }
+
+    /**
+     * @return the list of applied profiles
+     */
+    public Vector<Profile> getProfiles() {
+        return profiles;
+    }
+    
+    /**
+     * Applies a new profile to this configuration
+     * 
+     * @param p the profile to be applied
+     */
+    @SuppressWarnings("unchecked")
+    public void addProfile(Profile p) {
+        if (!profiles.contains(p)) {
+            profiles.add(p);
+            try {
+                profileModels.addAll(p.getProfilePackages());
+            } catch (ProfileException e) {
+            }
+            
+            FigNodeStrategy fns = p.getFigureStrategy();
+            if (fns != null) {
+                figNodeStrategies.add(fns);
+            }
+
+            for (Profile dependency : p.getDependencies()) {
+                addProfile(dependency);
+            }
+
+            updateStrategies();
+            ExplorerEventAdaptor.getInstance().structureChanged();
+        }
+    }
+        
+    /**
+     * @return the list of models of the currently applied profile.
+     */
+    private Vector getProfileModels() {
+        return profileModels;
+    }
+
+    /**
+     * Removes the passed profile from the configuration. 
+     * 
+     * @param p the profile to be removed
+     */
+    public void removeProfile(Profile p) {
+        profiles.remove(p);
+        try {
+            profileModels.removeAll(p.getProfilePackages());
+        } catch (ProfileException e) {
+            LOG.error("Exception", e);
+        }
+
+        FigNodeStrategy fns = p.getFigureStrategy();
+        if (fns != null) {
+            figNodeStrategies.remove(fns);
+        }
+
+        if (formatingStrategy == p.getFormatingStrategy()) {
+            formatingStrategy = null;
+        }
+
+        Vector<Profile> markForRemoval = new Vector<Profile>();
+        for (Profile profile : profiles) {
+            if (profile.getDependencies().contains(p)) {
+                markForRemoval.add(profile);
+            }
+        }
+
+        for (Profile profile : markForRemoval) {
+            removeProfile(profile);
+        }
+
+        updateStrategies();        
+        ExplorerEventAdaptor.getInstance().structureChanged();
+    }
+    
+    private FigNodeStrategy compositeFigNodeStrategy = new FigNodeStrategy() {
+
+	public Image getIconForStereotype(Object element) {
+	    Iterator it = figNodeStrategies.iterator();
+
+	    while (it.hasNext()) {
+		FigNodeStrategy strat = (FigNodeStrategy) it.next();
+		Image extra = strat.getIconForStereotype(element);
+
+		if (extra != null) {
+		    return extra;
+		}
+	    }
+	    return null;
+	}
+	
+    };
+    
+    /**
+     * @return the current FigNodeStrategy
+     */
+    public FigNodeStrategy getFigNodeStrategy() {
+	return compositeFigNodeStrategy; 
+    }
+
+    /**
+     * @return the extension for this project member
+     * @see org.argouml.kernel.AbstractProjectMember#getType()
+     */
+    public String getType() {
+	return EXTENSION;
+    }
+
+    /**
+     * Objects of this class are always consistent, there's no need 
+     * to repair them.
+     * 
+     * @return the empty string.
+     * @see org.argouml.kernel.ProjectMember#repair()
+     */
+    public String repair() {
+	return "";
+    }
+
+    /**
+     * @return the "Profile Configuration" string
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return "Profile Configuration";
+    }
+
+    /**
+     * @param string
+     * @param obj 
+     * @return
+     */
+    @SuppressWarnings("deprecation")
+    public Object findStereotypeForObject(String string, Object obj) {
+        Iterator iter = null;
+        
+        for (Object model : profileModels) {
+            iter = Model.getFacade().getOwnedElements(model).iterator();
+
+            while (iter.hasNext()) {
+                Object stereo = iter.next();
+                if (!Model.getFacade().isAStereotype(stereo)
+                        || !string.equals(Model.getFacade().getName(stereo))) {
+                    continue;
+                }
+
+                if (Model.getExtensionMechanismsHelper().isValidStereoType(obj,
+                        stereo)) {
+                    return Model.getModelManagementHelper()
+                            .getCorrespondingElement(stereo,
+                                    Model.getFacade().getModel(obj));
+                }
+            }            
+        }
+
+        return null;
+    }
+
+    /**
+     * @param name
+     * @return
+     */
+    public Object findType(String name) {
+        Object result = null;
+        Iterator it = getProfileModels().iterator();
+        while (result == null && it.hasNext()) {
+            result = ModelUtils.findTypeInModel(name, it.next());
+        }
+        
+        return result;
+    }
+
+    /**
+     * @param metaType
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public Collection findByMetaType(Object metaType) {
+        Set elements = new HashSet();
+
+        Iterator it = getProfileModels().iterator();
+        while (it.hasNext()) {
+            Object model = it.next();
+            elements.addAll(Model.getModelManagementHelper()
+                    .getAllModelElementsOfKind(model, metaType));
+        }
+        return elements;
+    }
+
+    /**
+     * @param modelElement
+     * @return
+     */
+    public Collection findAllStereotypesForModelElement(Object modelElement) {
+        return Model.getExtensionMechanismsHelper().getAllPossibleStereotypes(
+                getProfileModels(), modelElement);
+    }
+}
