a555e898c1f6382f228b67f5139ef3236e0e4433
Tom Morris <tfmorris@tigris.org>
2007-11-22 06:33:31 +0000

    Localize
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@13806 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/uml/ui/AbstractActionRemoveElement.java
+++ src_new/org/argouml/uml/ui/AbstractActionRemoveElement.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -52,7 +52,7 @@ public class AbstractActionRemoveElement extends UndoableAction {
      * Constructor for AbstractActionRemoveElement.
      */
     protected AbstractActionRemoveElement() {
-        this(Translator.localize("Delete From Model"));
+        this(Translator.localize("menu.popup.remove"));
     }
 
     /**
@@ -108,6 +108,7 @@ public class AbstractActionRemoveElement extends UndoableAction {
     /*
      * @see javax.swing.Action#isEnabled()
      */
+    @Override
     public boolean isEnabled() {
         return getObjectToRemove() != null && getTarget() != null;
     }
