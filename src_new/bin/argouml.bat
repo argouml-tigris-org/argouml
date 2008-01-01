dbbd3fe5baa71d9cdc89188ce15d7d40e49a7f8d
Linus Tolke <linus@tigris.org>
2008-01-01 20:56:29 +0000

    Updated the argouml.bat according to Dave Thompsons first attachment. Issue 4370.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14018 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/bin/argouml.bat
+++ src_new/bin/argouml.bat
@@ -1,2 +1,4 @@
 @echo off
-java -cp @argojarfile@ org.argouml.application.Main %1 %2 %3 %4 %5 %6 %7 %8
+java -cp argouml.jar org.argouml.application.Main %1 %2 %3 %4 %5 %6 %7 %8
+
+if errorlevel 1 pause
