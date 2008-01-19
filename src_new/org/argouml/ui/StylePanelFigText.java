4973acd98523b8bdde3385c52193e99b6281e732
Michiel van der Wulp <mvw@tigris.org>
2008-01-19 10:30:06 +0000

    Fix a NullPointer exception you may get when executing File->New.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14056 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/ui/StylePanelFigText.java
+++ src_new/org/argouml/ui/StylePanelFigText.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -146,6 +146,9 @@ public class StylePanelFigText extends StylePanelFig {
     public void refresh() {
         super.refresh();
         FigText ft = (FigText) getPanelTarget();
+        if (ft == null) {
+            return;
+        }
         String fontName = ft.getFontFamily();
         int size = ft.getFontSize();
         String styleName = STYLES[0];
