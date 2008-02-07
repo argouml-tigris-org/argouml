66b0b19af68e9273c2c7ddef69a4d9faeaf5960e
Michiel van der Wulp <mvw@tigris.org>
2008-02-07 08:39:57 +0000

    Added comment.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14085 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src_new/org/argouml/persistence/OldZargoFilePersister.java
+++ src_new/org/argouml/persistence/OldZargoFilePersister.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2007 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -161,6 +161,7 @@ class OldZargoFilePersister extends ZargoFilePersister {
                     String name = projectMember.getZipName();
                     String originalName = name;
                     while (names.contains(name)) {
+                        /* Issue 4806 explains why we need this! */
                         name = ++counter + originalName;
                     }
                     names.add(name);
