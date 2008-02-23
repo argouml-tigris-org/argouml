c9ae807954377068716a4e2a7e5506da7248b87f
Michiel van der Wulp <mvw@tigris.org>
2008-02-23 19:58:48 +0000

    Prevent an exception when right-clicking in the HomeModel field of a diagram properties panel.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14147 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/app/src/org/argouml/uml/ui/UMLList2.java
+++ src/app/src/org/argouml/uml/ui/UMLList2.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -133,9 +133,11 @@ public abstract class UMLList2
         Point point = event.getPoint();
         int index = locationToIndex(point);
         JPopupMenu popup = new JPopupMenu();
-        if (((UMLModelElementListModel2) getModel())
-                .buildPopup(popup, index)) {
-            popup.show(this, point.x, point.y);
+        ListModel lm = getModel(); 
+        if (lm instanceof UMLModelElementListModel2) {
+            if (((UMLModelElementListModel2) lm).buildPopup(popup, index)) {
+                popup.show(this, point.x, point.y);
+            }
         }
     }
 }
