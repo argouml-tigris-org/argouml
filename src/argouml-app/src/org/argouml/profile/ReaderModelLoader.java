bda80e546250337a0bad607b9941f4cc3524a94d
Bob Tarling <bobtarling@tigris.org>
2008-04-21 01:48:01 +0000

    Rethrow wrapped exception and log later together in one chain
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14428 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/profile/ReaderModelLoader.java
+++ src/argouml-app/src/org/argouml/profile/ReaderModelLoader.java
@@ -93,8 +93,7 @@ public class ReaderModelLoader implements ProfileModelLoader {
                 Collection elements = xmiReader.parse(inputSource, true);
                 return elements;
             } catch (UmlException e) {
-                LOG.error("Exception while loading profile ", e);
-                throw new ProfileException("Invalid XMI data!");
+                throw new ProfileException("Invalid XMI data!", e);
             }
         }
         LOG.error("Profile not found");
