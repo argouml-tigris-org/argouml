77816f3f779165adea30158d7b5b31e2376f34c4
Tom Morris <tfmorris@tigris.org>
2008-04-22 05:27:33 +0000

    Add @deprecated annotations
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14433 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/ui/AbstractArgoJPanel.java
+++ src/argouml-app/src/org/argouml/ui/AbstractArgoJPanel.java
@@ -25,18 +25,24 @@
 package org.argouml.ui;
 
 /**
- * This exists purely for the backwards compatibility of modules.
- * Use {@link #org.argouml.application.api.AbstractArgoJPanel} if backwards compatibility to
- * ArgoUML 0.24 and before is not required.
- * @deprecated by Bob Tarling in 0.25.5 I would recommend to keep for at least
- * two major releases.
+ * This exists purely for the backwards compatibility of modules. New code
+ * should use {@link org.argouml.application.api.AbstractArgoJPanel}.
+ * 
+ * @deprecated by Bob Tarling in 0.25.5. Use
+ *             {@link org.argouml.application.api.AbstractArgoJPanel}.
+ *             <p>
+ *             Because this is a widely used class it is recommended to retain
+ *             the deprecated class for at least two major releases (ie until
+ *             after 0.28 is released using the current numbering scheme).
  */
+@Deprecated
 public abstract class AbstractArgoJPanel
 extends org.argouml.application.api.AbstractArgoJPanel {
     /**
      * The constructor.
      *
      */
+    @Deprecated
     public AbstractArgoJPanel() {
         super();
     }
@@ -46,6 +52,7 @@ extends org.argouml.application.api.AbstractArgoJPanel {
      *
      * @param title The name as a localized string.
      */
+    @Deprecated
     public AbstractArgoJPanel(String title) {
         super(title);
     }
@@ -56,6 +63,7 @@ extends org.argouml.application.api.AbstractArgoJPanel {
      * @param title The name (a localized string).
      * @param t if true, remove tab from parent JTabbedPane
      */
+    @Deprecated
     public AbstractArgoJPanel(String title, boolean t) {
         super(title, t);
     }
