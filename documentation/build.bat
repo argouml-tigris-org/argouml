b0474c9337f26a3c7e60083669766172cf42214b
Linus Tolke <linus@tigris.org>
2008-05-05 18:46:06 +0000

    Allow for both checkout layouts.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14628 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- documentation/build.bat
+++ documentation/build.bat
@@ -5,7 +5,21 @@ rem build.bat always calls the version of ant distributed with ArgoUML
 rem
 
 setlocal
+echo Searching for tools dir...
+
+set ANT_HOME=..\tools\apache-ant-1.7.0
+echo trying %ANT_HOME%
+if exist %ANT_HOME% goto antpathok
+
 set ANT_HOME=..\argouml-core-tools\apache-ant-1.7.0
+echo trying %ANT_HOME%
+if exist %ANT_HOME% goto antpathok
+
+echo Could not find tools dir.
+goto endfail
+
+:antpathok
+echo Ant Found
 
 :: Convert relative path to absolute
 pushd %ANT_HOME%
--- documentation/build.sh
+++ documentation/build.sh
@@ -6,7 +6,16 @@
 #
  
 # Always use the ant that comes with ArgoUML
-ANT_HOME=../argouml-core-tools/apache-ant-1.7.0
+if [ -e `pwd`/../argouml-core-tools ] ; then
+    ANT_HOME=`pwd`/../argouml-core-tools/apache-ant-1.7.0
+elif [ -e `pwd`/../tools ] ; then
+    ANT_HOME=`pwd`/../tools/apache-ant-1.7.0
+else
+    echo "***************************************************************"
+    echo "  ERROR: tools directory not found."
+    echo "***************************************************************"
+    exit 1
+fi
 
 echo ANT_HOME is: $ANT_HOME
 echo
