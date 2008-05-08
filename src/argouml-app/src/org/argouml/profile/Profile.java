8861fa27e4aa313bdc2379f9d409616720e35629
Bob Tarling <bobtarling@tigris.org>
2008-05-08 00:00:15 +0000

    Remove deprecated methods that have never made a stable release
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14662 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/profile/Profile.java
+++ src/argouml-app/src/org/argouml/profile/Profile.java
@@ -81,25 +81,6 @@ public abstract class Profile {
     public abstract String getDisplayName();
     
     /**
-     * @return the UML Model that contains the profile model
-     * @deprecated by maurelio1234. Use {@link #getProfilePackages()} instead.
-     */
-    @Deprecated
-    public Object getModel() {
-        Collection c = null;
-        try {
-            c = getProfilePackages();
-        } catch (ProfileException e) {
-        }
-        
-        if (c != null) {
-            return c.iterator().next();
-        } else {
-            return null;
-        }
-    }
-
-    /**
      * @return the formating strategy offered by this profile, if any. Returns
      *         <code>null</code> if this profile has no formating strategy.
      */
@@ -124,108 +105,12 @@ public abstract class Profile {
     }
     
     /**
-     * This method produces a string that represents the specific model element
-     * in the context of the specified namespace.
-     * 
-     * @param element
-     *                element to represent.
-     * @param namespace
-     *                context namespace (may be null).
-     * @return a string representing the model element
-     * 
-     * @deprecated for 0.25.4 by maurelio1234. Use
-     *             {@link #getFormatingStrategy()} instead.
-     */
-    @Deprecated
-    public String formatElement(Object element,
-                                         Object namespace) {
-        FormatingStrategy fs = getFormatingStrategy(); 
-        if (fs != null) {
-            return fs.formatElement(element, namespace);
-        } else {
-            return null;
-        }
-    }
-    
-    /**
-     * This method produces a string the represents the collection of model
-     * elements in the context of the specified namespace.
-     * 
-     * @param iter
-     *                iterator over collection
-     * @param namespace
-     *                context namespace (may be null).
-     * @return a string representing the collection
-     * 
-     * @deprecated for 0.25.4 by maurelio1234. Use
-     *             {@link #getFormatingStrategy()} instead.
-     */
-    @Deprecated
-    public String formatCollection(Iterator iter,
-                                            Object namespace) {
-        FormatingStrategy fs = getFormatingStrategy(); 
-        if (fs != null) {
-            return fs.formatCollection(iter, namespace);
-        } else {
-            return null;
-        }        
-    }
-    /**
-     * @return the UML Model that contains the profile model
-     * @throws ProfileException if failed to get profile.
-     * @deprecated for 0.25.4 by tfmorris.  Use {@link #getProfilePackages()}.
-     */
-    @Deprecated
-    public Object getProfileModel() throws ProfileException {
-        if (getModel() == null) {
-            Object profileModel = null;
-            for (Object pkg : getProfilePackages()) {
-                if (Model.getFacade().isAPackage(pkg)) {
-                    profileModel = pkg;
-                    return profileModel;
-                }
-            }
-            profileModel = getProfilePackages().iterator().next();
-
-            return profileModel;
-        } else {
-            return getModel();
-        }
-    }
-    
-    /**
      * @return a collection of the top level UML Packages containing the
      *         profile.
      * @throws ProfileException
      *                 if failed to get profile.
      */
     public abstract Collection getProfilePackages() throws ProfileException;
-    
-    /**
-     * Set the filename to load the profile model from.  This will be
-     * remembered and used as the file for all future loads.
-     * @param filename file name of XMI file containing model to use as profile
-     * 
-     * @throws ProfileException if the given file isn't a valid profile
-     * @deprecated by maurelio1234
-     */
-    @SuppressWarnings("unused")
-    @Deprecated
-    public void setProfileModelFilename(String filename)
-        throws ProfileException {
-        
-    }
-    
-    /**
-     * Return the filename that was or will be used to load the profile/default
-     * model.
-     * @return the filename
-     * @deprecated by maurelio1234
-     */
-    @Deprecated
-    public String getProfileModelFilename() {
-        return "";
-    }
     
     /**
      * @return the display name
