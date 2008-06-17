4b5cc5ab1a36f7906e2ceae613593d1e1bef55ac
Michiel van der Wulp <mvw@tigris.org>
2008-06-17 18:26:47 +0000

    typo
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@14987 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/ui/UMLExpressionBodyField.java
+++ src/argouml-app/src/org/argouml/uml/ui/UMLExpressionBodyField.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2007 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -68,7 +68,7 @@ public class UMLExpressionBodyField extends JTextArea
         getDocument().addDocumentListener(this);
         setToolTipText(Translator.localize("label.body.tooltip"));
         setFont(LookAndFeelMgr.getInstance().getStandardFont());
-        setRows(2); // make it strech vertically
+        setRows(2); // make it stretch vertically
     }
 
     /*
