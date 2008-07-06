c620d8c568dfb7ad99b2e7cad822a0d16f5ef829
Christian López Espínola <penyaskito@tigris.org>
2008-07-06 17:47:30 +0000

    Added infrastructure for new expression models without dependencies to the property panels. Copied some code from the core.
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/xmlpropertypanels-penyaskito-soc08@15184 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ argouml-core-propertypanels-scratch/src/org/argouml/core/propertypanels/ui/UMLExpressionLanguageField.java
@@ -0,0 +1,102 @@
+// $Id$
+// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.core.propertypanels.ui;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import org.argouml.i18n.Translator;
+import org.argouml.ui.LookAndFeelMgr;
+
+/**
+ * This text field shows the language of a UML expression.
+ */
+public class UMLExpressionLanguageField extends JTextField implements
+        DocumentListener {
+
+    private UMLExpressionModel3 model;
+    private boolean notifyModel;
+
+    /**
+     * Creates a new field that selects the language for an expression.
+     *
+     * @param m Expression model, should be shared between
+     * Language and Body fields
+     * @param n Only one of Language and Body fields should
+     * forward events to model
+     */
+    public UMLExpressionLanguageField(UMLExpressionModel3 m, boolean n) {
+        model = m;
+        notifyModel = n;
+        getDocument().addDocumentListener(this);
+        setToolTipText(Translator.localize("label.language.tooltip"));
+        setFont(LookAndFeelMgr.getInstance().getStandardFont());
+    }
+
+    /*
+     * @see org.argouml.uml.ui.UMLUserInterfaceComponent#targetChanged()
+     */
+    public void targetChanged() {
+        if (notifyModel) model.targetChanged();
+        update();
+    }
+
+    /*
+     * @see org.argouml.uml.ui.UMLUserInterfaceComponent#targetReasserted()
+     */
+    public void targetReasserted() {
+    }
+
+    private void update() {
+        String oldText = getText();
+        String newText = model.getLanguage();
+        if (oldText == null || newText == null || !oldText.equals(newText)) {
+            if (oldText != newText) {
+                setText(newText);
+            }
+        }
+    }
+
+    /*
+     * @see javax.swing.event.DocumentListener#changedUpdate(javax.swing.event.DocumentEvent)
+     */
+    public void changedUpdate(final DocumentEvent p1) {
+        model.setLanguage(getText());
+    }
+
+    /*
+     * @see javax.swing.event.DocumentListener#removeUpdate(javax.swing.event.DocumentEvent)
+     */
+    public void removeUpdate(final DocumentEvent p1) {
+        model.setLanguage(getText());
+    }
+
+    /*
+     * @see javax.swing.event.DocumentListener#insertUpdate(javax.swing.event.DocumentEvent)
+     */
+    public void insertUpdate(final DocumentEvent p1) {
+        model.setLanguage(getText());
+    }
+}
---
+++ argouml-core-propertypanels-scratch/src/org/argouml/core/propertypanels/ui/UMLExpressionModel3.java
@@ -0,0 +1,164 @@
+// $Id$
+// Copyright (c) 1996-2007 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.core.propertypanels.ui;
+
+import org.argouml.model.Model;
+import org.argouml.ui.targetmanager.TargetListener;
+
+/**
+ * @author mkl
+ * @author penyaskito
+ */
+public abstract class UMLExpressionModel3 implements TargetListener {
+    private String propertyName;
+    private Object/*MExpression*/ expression;
+    private boolean mustRefresh;
+    private static final String EMPTYSTRING = "";
+
+    /**
+     * The constructor.
+     *
+     * @param c the container of UML user interface components
+     * @param name the name of the property
+     */
+    public UMLExpressionModel3(String name) {
+        propertyName = name;
+        mustRefresh = true;
+    }
+
+    /**
+     * When the target is changed, we must refresh.
+     */
+    public void targetChanged() {
+        mustRefresh = true;
+        expression = null;
+    }
+
+
+    /**
+     * @return the expression
+     */
+    public abstract Object getExpression();
+
+    /**
+     * @param expr the expression
+     */
+    public abstract void setExpression(Object expr);
+
+    /**
+     * @return a new expression
+     */
+    public abstract Object newExpression();
+
+
+    /**
+     * @return the language of the expression
+     */
+    public String getLanguage() {
+        if (mustRefresh) {
+            expression = getExpression();
+        }
+        if (expression == null) {
+            return EMPTYSTRING;
+        }
+        return Model.getDataTypesHelper().getLanguage(expression);
+    }
+
+    /**
+     * @return The body text of the expression.
+     */
+    public String getBody() {
+        if (mustRefresh) {
+            expression = getExpression();
+        }
+        if (expression == null) {
+            return EMPTYSTRING;
+        }
+        return Model.getDataTypesHelper().getBody(expression);
+    }
+
+    /**
+     * @param lang the language of the expression
+     */
+    public void setLanguage(String lang) {
+
+        boolean mustChange = true;
+        if (expression != null) {
+            String oldValue =
+                Model.getDataTypesHelper().getLanguage(expression);
+            if (oldValue != null && oldValue.equals(lang)) {
+                mustChange = false;
+            }
+        }
+        if (mustChange) {
+            String body = EMPTYSTRING;
+            if (expression != null
+                    && Model.getDataTypesHelper().getBody(expression) != null) {
+                body = Model.getDataTypesHelper().getBody(expression);
+            }
+
+            setExpression(lang, body);
+        }
+    }
+
+    /**
+     * @param body the body text of the expression
+     */
+    public void setBody(String body) {
+        boolean mustChange = true;
+        if (expression != null) {
+            Object oldValue = Model.getDataTypesHelper().getBody(expression);
+            if (oldValue != null && oldValue.equals(body)) {
+                mustChange = false;
+            }
+        }
+        if (mustChange) {
+            String lang = null;
+            if (expression != null) {
+                lang = Model.getDataTypesHelper().getLanguage(expression);
+            }
+            if (lang == null) {
+                lang = EMPTYSTRING;
+            }
+
+            setExpression(lang, body);
+        }
+    }
+
+    /**
+     * @param lang the language of the expression
+     * @param body the body text of the expression
+     */
+    private void setExpression(String lang, String body) {
+        // Expressions are DataTypes, not independent model elements
+        // be careful not to reuse them
+        if (mustRefresh || expression == null) {
+            expression = newExpression();
+        }
+        expression = Model.getDataTypesHelper().setLanguage(expression, lang);
+        expression = Model.getDataTypesHelper().setBody(expression, body);
+        setExpression(expression);
+    }
+}
