796bb8495b6adc22ee9f78f59fd39c881b16dc60
Michiel van der Wulp <mvw@tigris.org>
2008-07-15 18:39:58 +0000

    Added a TODO comment only - no functional change. There are a few bugs around this renderer for proppanel fields where the renderer does not update its text when the model changes.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@15289 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/ui/UMLListCellRenderer2.java
+++ src/argouml-app/src/org/argouml/uml/ui/UMLListCellRenderer2.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2007 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -43,6 +43,10 @@ import org.argouml.model.Model;
  * children.
  *
  * This class must be efficient as it is called many 1000's of times.
+ * 
+ * TODO: MVW: Since this class is the only one that knows 
+ * which UML objects influence the rendering, should it 
+ * not be listening to model change events?
  *
  * @author jaap.branderhorst@xs4all.nl
  * @since Jan 2, 2003
