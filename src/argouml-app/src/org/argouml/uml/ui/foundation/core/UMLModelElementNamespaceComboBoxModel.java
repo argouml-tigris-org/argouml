85e0e752fcfe778d99921a13ccf2daf26fe1683b
Bob Tarling <bobtarling@tigris.org>
2008-08-10 14:08:24 +0000

    Issue 5300: Don't rebuild the list on every model element change
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@15534 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
+++ src/argouml-app/src/org/argouml/uml/ui/foundation/core/UMLModelElementNamespaceComboBoxModel.java
@@ -111,8 +111,6 @@ public class UMLModelElementNamespaceComboBoxModel extends UMLComboBoxModel2 {
         if (t != null
                 && evt.getSource() == t
                 && evt.getNewValue() != null) {
-//            setTarget(t); // this fixes issue 3780, but causes issue 3832.
-            buildModelList();
             /* In some cases (see issue 3780) the list remains the same, but
              * the selected item differs. Without the next step,
              * the combo would not be refreshed.
