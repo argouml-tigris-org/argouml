a1300f25d98317d2e25f5cd64b7a947c4e3a8586
Marcos Aurélio <maurelio1234@tigris.org>
2008-08-17 18:36:46 +0000

    adding ocl to ProfileMeta to enforce constraints on profile models
    git-svn-id: http://argouml.tigris.org/svn/argouml/branches/gsoc2008@15576 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- work_issue5042_merged15552_maurelio1234/src/org/argouml/profile/internal/ProfileMeta.java
+++ work_issue5042_merged15552_maurelio1234/src/org/argouml/profile/internal/ProfileMeta.java
@@ -27,7 +27,11 @@ package org.argouml.profile.internal;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.argouml.cognitive.Critic;
+import org.argouml.cognitive.ToDoItem;
 import org.argouml.model.Model;
 import org.argouml.profile.CoreProfileReference;
 import org.argouml.profile.Profile;
@@ -35,6 +39,8 @@ import org.argouml.profile.ProfileException;
 import org.argouml.profile.ProfileModelLoader;
 import org.argouml.profile.ProfileReference;
 import org.argouml.profile.ResourceModelLoader;
+import org.argouml.profile.internal.ocl.CrOCL;
+import org.argouml.profile.internal.ocl.InvalidOclException;
 
 /**
  * Meta Profile which defines the TaggedValues to be used in User defined
@@ -70,8 +76,67 @@ public class ProfileMeta extends Profile {
             model.add(Model.getModelManagementFactory().createModel());
         }
 
+        loadWellFormednessRules();
     }
 
+    private void loadWellFormednessRules() {
+        Set<Critic> critics = new HashSet<Critic>();
+                
+        try {
+            critics.add(new CrOCL("context ModelElement inv: "
+                    + "self.taggedValue->" 
+                    + "exists(x|x.type.name='Dependency') implies "
+                              + "self.stereotype->exists(x|x.name = 'Profile')",
+                 "The 'Dependency' tag definition should be applied"
+                                + " only to profiles.", null,
+                    ToDoItem.MED_PRIORITY, null, null,
+                    "http://argouml.tigris.org/"));
+        } catch (InvalidOclException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            critics.add(new CrOCL("context ModelElement inv: "
+                    + "self.taggedValue->" 
+                    + "exists(x|x.type.name='Figure') or "
+                    + "exists(x|x.type.name='Description') or "
+                    + "exists(x|x.type.name='i18n') or "
+                    + "exists(x|x.type.name='KnowledgeType') or "
+                    + "exists(x|x.type.name='MoreInfoURL') or "
+                    + "exists(x|x.type.name='Priority') or "
+                    + "exists(x|x.type.name='Description') or "
+                    + "exists(x|x.type.name='SupportedDecision') or "
+                    + "exists(x|x.type.name='Headline') "
+                    + "implies "
+                              + "self.stereotype->exists(x|x.name = 'Critic')",
+                              
+                    "Misuse of Metaprofile TaggedValues",
+                    "The 'Figure', 'i18n', 'KnowledgeType', 'MoreInfoURL', " 
+                    + "'Priority', 'SupportedDecision', 'Description' " 
+                    + "and 'Headline' tag definitions "
+                    + "should be applied only to OCL critics.",
+                    
+                    ToDoItem.MED_PRIORITY, null, null,
+                    "http://argouml.tigris.org/"));
+        } catch (InvalidOclException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            critics.add(new CrOCL("context Stereotype inv: "
+                    + "self.namespace.stereotype->exists(x|x.name = 'Profile')",
+                            "Stereotypes should be declared inside a Profile. ",
+                            "Please add the <<Profile>> stereotype to "
+                                   + "the containing Namespace",
+                    ToDoItem.MED_PRIORITY, null, null,
+                    "http://argouml.tigris.org/"));
+        } catch (InvalidOclException e) {
+            e.printStackTrace();
+        }
+
+        setCritics(critics);
+    }
+    
     @Override
     public String getDisplayName() {
         return "MetaProfile";
