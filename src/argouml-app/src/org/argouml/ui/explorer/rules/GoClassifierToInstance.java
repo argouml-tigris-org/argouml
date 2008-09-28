e8618b73a0979fb5d36200d4ddcfc6ae34487c10
Michiel van der Wulp <mvw@tigris.org>
2008-09-28 07:51:54 +0000

    Fix for issue 5258, as given by the attached patch.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@15815 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/ui/explorer/rules/GoClassifierToInstance.java
+++ src/argouml-app/src/org/argouml/ui/explorer/rules/GoClassifierToInstance.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -33,6 +33,8 @@ import org.argouml.i18n.Translator;
 import org.argouml.model.Model;
 
 /**
+ * Rule for Classifier->Instance.
+ * 
  * @author jaap.branderhorst@xs4all.nl
  */
 public class GoClassifierToInstance extends AbstractPerspectiveRule {
@@ -41,7 +43,7 @@ public class GoClassifierToInstance extends AbstractPerspectiveRule {
      * @see org.argouml.ui.explorer.rules.PerspectiveRule#getRuleName()
      */
     public String getRuleName() {
-        return Translator.localize("misc.classifier.collaboration");
+        return Translator.localize("misc.classifier.instance");
     }
 
     /*
