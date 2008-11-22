04882f6dbe1d9fd2c9de3980d8afcd4d22d61bc5
Tom Morris <tfmorris@tigris.org>
2008-11-22 06:40:21 +0000

    Make renderingChanged public so it can be used by Diagram
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16134 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/diagram/state/ui/FigTransition.java
+++ src/argouml-app/src/org/argouml/uml/diagram/state/ui/FigTransition.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -117,14 +117,13 @@ public class FigTransition extends FigEdgeModelElement {
      * 
      * @see org.argouml.uml.diagram.ui.FigEdgeModelElement#getNotationProviderType()
      */
+    @Override
     protected int getNotationProviderType() {
         return NotationProviderFactory2.TYPE_TRANSITION;
     }
 
-    /*
-     * @see org.argouml.uml.diagram.ui.FigEdgeModelElement#renderingChanged()
-     */
-    protected void renderingChanged() {
+    @Override
+    public void renderingChanged() {
         updateDashed();
         super.renderingChanged();
     }
