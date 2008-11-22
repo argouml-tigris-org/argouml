bc138009e95147309616dbae4843a947449ee134
Tom Morris <tfmorris@tigris.org>
2008-11-22 06:23:47 +0000

    Use standard stereotype formatting utility
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16127 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/ActionAddStereotype.java
+++ src/argouml-app/src/org/argouml/uml/ActionAddStereotype.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2007 The Regents of the University of California. All
+// Copyright (c) 1996-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -34,6 +34,7 @@ import org.argouml.kernel.ProjectManager;
 import org.argouml.kernel.ProjectSettings;
 import org.argouml.kernel.UmlModelMutator;
 import org.argouml.model.Model;
+import org.argouml.notation.providers.uml.NotationUtilityUml;
 import org.tigris.gef.undo.UndoableAction;
 
 
@@ -66,9 +67,8 @@ public class ActionAddStereotype extends UndoableAction {
     private static String buildString(Object st) {
         Project p = ProjectManager.getManager().getCurrentProject();
         ProjectSettings ps = p.getProjectSettings();
-        return ps.getLeftGuillemot() 
-            + Model.getFacade().getName(st)
-            + ps.getRightGuillemot();
+        return NotationUtilityUml.generateStereotype(st, 
+                ps.getDefaultDiagramSettings().isUseGuillemets());
     }
 
     /*
