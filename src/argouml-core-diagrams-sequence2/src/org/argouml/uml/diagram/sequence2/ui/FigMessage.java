09046e79a1c77061e67b2c44b9ffa4c34a3c1818
Bob Tarling <bobtarling@tigris.org>
2008-12-16 15:56:13 +0000

    Make safe isSelfMessage safe against a removed message
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16366 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-diagrams-sequence2/src/org/argouml/uml/diagram/sequence2/ui/FigMessage.java
+++ src/argouml-core-diagrams-sequence2/src/org/argouml/uml/diagram/sequence2/ui/FigMessage.java
@@ -200,7 +200,11 @@ public class FigMessage extends FigEdgeModelElement {
      * @return true if they are the same, otherwise false. 
      */
     boolean isSelfMessage() {
-        return getDestination().equals(getSource());
+        if (Model.getUmlFactory().isRemoved(getOwner())) {
+            return getDestFigNode().equals(getSourceFigNode());
+        } else {
+            return getDestination().equals(getSource());
+        }
     }
     
     /**
