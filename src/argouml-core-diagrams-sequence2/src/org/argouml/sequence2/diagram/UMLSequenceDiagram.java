d2cb1180bc0d2926cf9adc47e2e8272df4956a4c
Michiel van der Wulp <mvw@tigris.org>
2009-01-18 08:40:45 +0000

    Added comment only.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16646 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-diagrams-sequence2/src/org/argouml/sequence2/diagram/UMLSequenceDiagram.java
+++ src/argouml-core-diagrams-sequence2/src/org/argouml/sequence2/diagram/UMLSequenceDiagram.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 2007 The Regents of the University of California. All
+// Copyright (c) 2007-2009 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -50,6 +50,7 @@ import org.tigris.gef.presentation.Fig;
 import org.tigris.gef.presentation.FigNode;
 
 /**
+ * The diagram for sequence diagrams.
  *
  * @author penyaskito
  */
