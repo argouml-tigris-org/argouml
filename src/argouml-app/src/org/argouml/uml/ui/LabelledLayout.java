7d216d6b5b739c34bc10398bb0476d69ed39d124
Bob Tarling <bobtarling@tigris.org>
2009-01-18 22:03:12 +0000

    Javadoc and code tidy. No effective change.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16655 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/ui/LabelledLayout.java
+++ src/argouml-app/src/org/argouml/uml/ui/LabelledLayout.java
@@ -1,5 +1,5 @@
-// $Id: JLinkButton.java 14825 2008-05-28 23:21:50Z euluis $
-// Copyright (c) 2008 The Regents of the University of California. All
+// $Id: LabelledLayout.java 14825 2008-05-28 23:21:50Z euluis $
+// Copyright (c) 2008-2009 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
