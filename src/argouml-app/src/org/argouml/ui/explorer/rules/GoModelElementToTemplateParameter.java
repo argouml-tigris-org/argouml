7934e3ba61fdb5041c64a966c5de4ad43a8fc8e6
Alexander Lepekhine <lepekhine@tigris.org>
2009-03-28 18:51:30 +0000

    Added GoModelElementToTemplateParameter rule
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16989 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src/argouml-app/src/org/argouml/ui/explorer/rules/GoModelElementToTemplateParameter.java
@@ -0,0 +1,37 @@
+package org.argouml.ui.explorer.rules;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.argouml.i18n.Translator;
+import org.argouml.model.Model;
+
+public class GoModelElementToTemplateParameter extends AbstractPerspectiveRule {
+
+	@Override
+	public Collection getChildren(Object parent) {
+        if (Model.getFacade().isAModelElement(parent)) {
+            return Model.getFacade().getTemplateParameters(parent);
+        }
+        return Collections.EMPTY_SET;
+	}
+
+	@Override
+	public String getRuleName() {
+        return Translator.localize("misc.model-element.template-parameter");
+	}
+
+	@Override
+	public Set getDependencies(Object parent) {
+        if (Model.getFacade().isAModelElement(parent)) {
+            Set set = new HashSet();
+            set.add(parent);
+            return set;
+        }
+        return Collections.EMPTY_SET;
+	}
+	
+
+}
