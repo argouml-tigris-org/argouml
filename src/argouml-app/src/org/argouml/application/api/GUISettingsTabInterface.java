220dba8988d43bab11f631ad14e1de74c8313529
Michiel van der Wulp <mvw@tigris.org>
2009-03-29 17:52:32 +0000

    comment only
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@16993 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/application/api/GUISettingsTabInterface.java
+++ src/argouml-app/src/org/argouml/application/api/GUISettingsTabInterface.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 1996-2006 The Regents of the University of California. All
+// Copyright (c) 1996-2009 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -36,7 +36,7 @@ import javax.swing.JPanel;
  * be able to undo any change if requested
  * through {@link #handleSettingsTabCancel}.<p>
  * 
- * Warning: Do not access the Projectmanager in the constructor 
+ * Warning: Do not access the ProjectManager in the constructor 
  * of classes that implement this interface! This because the
  * SettingsTabs are created before an initial Project is created.
  *
