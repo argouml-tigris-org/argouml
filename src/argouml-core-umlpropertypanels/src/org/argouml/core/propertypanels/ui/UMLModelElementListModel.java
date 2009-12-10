3bb071140eafd0a5a79b555f2970201ad8a83ca5
Bob Tarling <bobtarling@tigris.org>
2009-12-10 11:33:07 +0000

    Only update ui in AWT thread
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17621 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-umlpropertypanels/src/org/argouml/core/propertypanels/ui/UMLModelElementListModel.java
+++ src/argouml-core-umlpropertypanels/src/org/argouml/core/propertypanels/ui/UMLModelElementListModel.java
@@ -35,7 +35,10 @@ import javax.swing.Action;
 import javax.swing.DefaultListModel;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.ListModel;
 import javax.swing.MenuElement;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ListDataListener;
 
 import org.apache.log4j.Logger;
 import org.argouml.model.AddAssociationEvent;
@@ -300,60 +303,74 @@ abstract class UMLModelElementListModel
      * towards removing some of the overrriding methods used as workarounds for 
      * differences between NSUML and MDR - tfm - 20060302
      */
-    public void propertyChange(PropertyChangeEvent e) {
-        if (e instanceof AttributeChangeEvent) {
-            try {
-                if (isValidEvent(e)) {
-                    rebuildModelList();
-                }
-            } catch (InvalidElementException iee) {
-                return;
-            }
-        } else if (e instanceof AddAssociationEvent) {
-            if (isValidEvent(e)) {
-                Object o = getChangedElement(e);
-                if (o instanceof Collection) {
-                    ArrayList tempList = new ArrayList((Collection) o);
-                    Iterator it = tempList.iterator();
-                    while (it.hasNext()) {
-                        Object o2 = it.next();
-                        addElement(o2);
+    public void propertyChange(final PropertyChangeEvent e) {
+        Runnable doWorkRunnable = new Runnable() {
+            public void run() {
+                try {
+                    if (e instanceof AttributeChangeEvent) {
+                        try {
+                            if (isValidEvent(e)) {
+                                rebuildModelList();
+                            }
+                        } catch (InvalidElementException iee) {
+                            return;
+                        }
+                    } else if (e instanceof AddAssociationEvent) {
+                        if (isValidEvent(e)) {
+                            Object o = getChangedElement(e);
+                            if (o instanceof Collection) {
+                                ArrayList tempList = new ArrayList((Collection) o);
+                                Iterator it = tempList.iterator();
+                                while (it.hasNext()) {
+                                    Object o2 = it.next();
+                                    addElement(o2);
+                                }
+                            } else {
+                                /* TODO: If this is an ordered list, then you have to 
+                                    add in the right location! */
+                                addElement(o); 
+                            }
+                        }
+                    } else if (e instanceof RemoveAssociationEvent) {
+                        boolean valid = false;
+                        if (!(getChangedElement(e) instanceof Collection)) {
+                            valid = contains(getChangedElement(e));
+                        } else {
+                            Collection col = (Collection) getChangedElement(e);
+                            Iterator it = col.iterator();
+                            valid = true;
+                            while (it.hasNext()) {
+                                Object o = it.next();
+                                if (!contains(o)) {
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (valid) {
+                            Object o = getChangedElement(e);
+                            if (o instanceof Collection) {
+                                Iterator it = ((Collection) o).iterator();
+                                while (it.hasNext()) {
+                                    Object o3 = it.next();
+                                    removeElement(o3);
+                                }
+                            } else {
+                                removeElement(o);
+                            }
+                        }
                     }
-                } else {
-                    /* TODO: If this is an ordered list, then you have to 
-                        add in the right location! */
-                    addElement(o); 
-                }
-            }
-        } else if (e instanceof RemoveAssociationEvent) {
-            boolean valid = false;
-            if (!(getChangedElement(e) instanceof Collection)) {
-                valid = contains(getChangedElement(e));
-            } else {
-                Collection col = (Collection) getChangedElement(e);
-                Iterator it = col.iterator();
-                valid = true;
-                while (it.hasNext()) {
-                    Object o = it.next();
-                    if (!contains(o)) {
-                        valid = false;
-                        break;
+                } catch (InvalidElementException e) {
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("updateLayout method accessed "
+                                + "deleted element ", e);
                     }
                 }
-            }
-            if (valid) {
-                Object o = getChangedElement(e);
-                if (o instanceof Collection) {
-                    Iterator it = ((Collection) o).iterator();
-                    while (it.hasNext()) {
-                        Object o3 = it.next();
-                        removeElement(o3);
-                    }
-                } else {
-                    removeElement(o);
-                }
-            }
-        }
+            }  
+        };
+        SwingUtilities.invokeLater(doWorkRunnable);
+        
+        
     }
 
     /**
