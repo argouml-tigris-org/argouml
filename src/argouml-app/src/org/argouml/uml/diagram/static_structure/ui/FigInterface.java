ac7c9184bdf23cb4ae6ac4a140b399fade88220d
Bob Tarling <bobtarling@tigris.org>
2010-01-04 14:49:32 +0000

    Correct the licence header
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17735 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/diagram/static_structure/ui/FigInterface.java
+++ src/argouml-app/src/org/argouml/uml/diagram/static_structure/ui/FigInterface.java
@@ -1,3 +1,17 @@
+/* $Id$
+ *******************************************************************************
+ * Copyright (c) 2010 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bob Tarling
+ *******************************************************************************
+ *
+ * Some portions of this file was previously release using the BSD License:
+ */
 // $Id$
 // Copyright (c) 1996-2009 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
