6065d540aefe53dfb034bb951f1e882fb9f484e7
Linus Tolke <linus@tigris.org>
2010-01-11 21:29:46 +0000

    Added EPL License header.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17778 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-model-euml/src/org/argouml/model/euml/ScopeKindEUMLImpl.java
+++ src/argouml-core-model-euml/src/org/argouml/model/euml/ScopeKindEUMLImpl.java
@@ -1,4 +1,18 @@
-// $Id$
+/* $Id$
+ *****************************************************************************
+ * Copyright (c) 2009 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    tfmorris
+ *****************************************************************************
+ *
+ * Some portions of this file was previously release using the BSD License:
+ */
+
 // Copyright (c) 2007 Tom Morris and other contributors
 // All rights reserved.
 //
