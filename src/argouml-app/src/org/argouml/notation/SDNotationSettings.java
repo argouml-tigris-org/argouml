bce227e0389510cc55d6aae9f05c6b6e60af3a36
Linus Tolke <linus@tigris.org>
2010-01-12 18:49:29 +0000

    Added EPL License header.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17825 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/notation/SDNotationSettings.java
+++ src/argouml-app/src/org/argouml/notation/SDNotationSettings.java
@@ -1,4 +1,18 @@
-// $Id$
+/* $Id$
+ *****************************************************************************
+ * Copyright (c) 2009 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    tfmorris
+ *****************************************************************************
+ *
+ * Some portions of this file was previously release using the BSD License:
+ */
+
 // Copyright (c) 2008 Tom Morris and other contributors. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
