3e0769fb88598b409a2136b2df5c73b37bbe46da
Linus Tolke <linus@tigris.org>
2010-01-12 18:21:28 +0000

    Fixed properties.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17806 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- modules/dev/src/org/argouml/dev/EventPumpInspectorPanel.java
+++ modules/dev/src/org/argouml/dev/EventPumpInspectorPanel.java
@@ -1,4 +1,4 @@
-// $Id: EventPumpInspectorPanel.java 11132 2006-09-06 23:29:12Z bobtarling $
+// $Id$
 // Copyright (c) 2006-2007 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
