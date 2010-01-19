4e242b2fbe153bc5864864873cae42cce8fbb4d3
Linus Tolke <linus@tigris.org>
2010-01-19 21:45:12 +0000

    Added handling of failing svn command. By running them on one file each, they are independent.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17916 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- tools/bin/fix-properties.sh
+++ tools/bin/fix-properties.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+# add max-args 1 to xargs if it can go wrong
+CANGOWRONG="--max-args=1"
+
 svn ls -R > ls-R.tmp
 
 egrep '\.(java|xml|properties|txt|html)$' < ls-R.tmp | 
-  xargs -r -n 1 svn propdel svn:executable
+  xargs -r $CANGOWRONG svn propdel svn:executable
 egrep '\.(java|xml|properties|txt|html)$' < ls-R.tmp | 
   xargs -r svn propset svn:keywords 'Author Date Id Revision'
 egrep '\.(java|xml|txt)$' < ls-R.tmp | 
-  xargs -r svn propset svn:eol-style native
+  xargs -r $CANGOWRONG svn propset svn:eol-style native
 
 # Scripts
 egrep '\.(bat)$' < ls-R.tmp | 
-  xargs -r svn propset svn:eol-style 'CRLF'
+  xargs -r $CANGOWRONG svn propset svn:eol-style 'CRLF'
 egrep '\.(sh)$' < ls-R.tmp | 
-  xargs -r svn propset svn:eol-style 'LF'
+  xargs -r $CANGOWRONG svn propset svn:eol-style 'LF'
 egrep '\.(bat|sh)$' < ls-R.tmp | 
   xargs -r svn propset svn:executable '*'
 
