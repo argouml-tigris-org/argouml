844dc809ec68bf7fff46e6429576764f6ef50fd1
Bob Tarling <bobtarling@tigris.org>
2010-02-14 14:23:29 +0000

    Issue 5971: Only increase classifier-role on message drag when absolutely required
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@17999 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-diagrams-sequence2/src/org/argouml/sequence2/diagram/SelectionMessage.java
+++ src/argouml-core-diagrams-sequence2/src/org/argouml/sequence2/diagram/SelectionMessage.java
@@ -1,13 +1,14 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2010 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    bobtarling
+ *    Bob Tarling
+ *    Christian López Espínola
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -109,7 +110,7 @@ class SelectionMessage extends SelectionRerouteEdge {
         
         // if the edge is near the bottom of the classifier roles,
         // we enlarge all the FigClassifierRoles in the diagram.
-        if (figMessage.getFinalY() < 
+        if (figMessage.getFinalY() > 
                 source.getY() + source.getHeight() - 10) {
             final int newHeight = source.getHeight() + 10;
             final List<Fig> figs = getContent().getLayer().getContents();
