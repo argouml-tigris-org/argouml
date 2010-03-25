9b9dcf7abcf89a8fcfcf5891a9c4fcb44489729a
Bob Tarling <bobtarling@tigris.org>
2010-03-25 18:46:25 +0000

    Reinstate buttons to traverse siblings
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18168 a161b567-7d1e-0410-9ef9-912c70fedb3f

---
+++ src/argouml-core-umlpropertypanels/src/org/argouml/core/propertypanels/ui/NavigateNextAction.java
@@ -0,0 +1,65 @@
+/* $Id$
+ *******************************************************************************
+ * Copyright (c) 2010 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bob Tarling
+ *******************************************************************************
+ */
+
+package org.argouml.core.propertypanels.ui;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+
+import org.argouml.application.helpers.ResourceLoaderWrapper;
+import org.argouml.i18n.Translator;
+import org.argouml.model.Model;
+import org.argouml.ui.targetmanager.TargetManager;
+
+/**
+ * This action changes the target to the owning model element
+ * of the given element.
+ *
+ * @author Bob Tarling
+ */
+class NavigateNextAction extends AbstractAction {
+
+    final Object modelElement;
+    
+    public NavigateNextAction(Object modelElement) {
+        super(Translator.localize("action.navigate-forward"),
+                ResourceLoaderWrapper.lookupIcon("action.navigate-forward"));
+        
+        this.modelElement = modelElement;
+    }
+    public void actionPerformed(ActionEvent arg0) {
+        final Object owner =
+            Model.getFacade().getModelElementContainer(modelElement);
+        Object newTarget = null;
+        List list = null;
+        if (Model.getFacade().isAAttribute(modelElement)) {
+            list = Model.getFacade().getAttributes(owner);
+        }
+        if (Model.getFacade().isAOperation(modelElement)
+                || Model.getFacade().isAReception(modelElement)) {
+            list = Model.getFacade().getOperationsAndReceptions(owner);
+        }
+        if (Model.getFacade().isAParameter(modelElement)) {
+            list = Model.getFacade().getParametersList(owner);
+        }
+        if (list != null) {
+            final int posn = list.indexOf(modelElement);
+            if (posn >= 0 && posn < list.size() - 1) {
+                newTarget = list.get(posn + 1);
+                TargetManager.getInstance().setTarget(newTarget);
+            }
+        }
+    }
+}
