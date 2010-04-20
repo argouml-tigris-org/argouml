645b729636da61d960cec324f6fe664517761d59
Luis Sergio Oliveira <euluis@tigris.org>
2010-04-20 21:25:25 +0000

    issue 6008: only top level packages are returned from getExtentPackages(); now works as documented.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18316 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-model-mdr/src/org/argouml/model/mdr/UmlFactoryMDRImpl.java
+++ src/argouml-core-model-mdr/src/org/argouml/model/mdr/UmlFactoryMDRImpl.java
@@ -1,6 +1,6 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2010 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    Bob Tarling
+ *    Luis Sergio Oliveira (euluis)
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -1624,7 +1625,15 @@ class UmlFactoryMDRImpl extends AbstractUmlModelFactoryMDR implements
         if (pkg == null) {
             return null;
         }
-        return pkg.getModelManagement().getUmlPackage().refAllOfType();
+        Collection<Object> packages = pkg.getModelManagement().getUmlPackage().
+            refAllOfType();
+        Collection<Object> topLevelPackages = new ArrayList<Object>();
+        for (Object pack : packages) {
+            if (Model.getFacade().getNamespace(pack) == null) {
+                topLevelPackages.add(pack);
+            }
+        }
+        return topLevelPackages;
     }
 
 }
