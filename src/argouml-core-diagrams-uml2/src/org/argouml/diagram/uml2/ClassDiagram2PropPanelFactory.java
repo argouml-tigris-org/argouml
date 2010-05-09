a808afd70b137343d8e8463079e01d4dc1168056
Linus Tolke <linus@tigris.org>
2010-05-09 20:11:10 +0000

    Added the EPL license.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18385 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-diagrams-uml2/src/org/argouml/diagram/uml2/ClassDiagram2PropPanelFactory.java
+++ src/argouml-core-diagrams-uml2/src/org/argouml/diagram/uml2/ClassDiagram2PropPanelFactory.java
@@ -1,3 +1,16 @@
+/* $Id$
+ *******************************************************************************
+ * Copyright (c) 2009 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bob Tarling
+ *******************************************************************************
+ */
+
 package org.argouml.diagram.uml2;
 
 
