a09bb80652d16906a2c08e561094768d32d17431
Bob Tarling <bobtarling@tigris.org>
2010-07-16 13:28:21 +0000

    Move all files down (I seem to created a rogue directory in previous commit)
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18519 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-diagrams-activity2/src/org/argouml/activity2/ActivityDiagramModule.java
+++ src/argouml-core-diagrams-activity2/src/org/argouml/activity2/ActivityDiagramModule.java
@@ -1,70 +1,72 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2010 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    bobtarling
+ *    Bob Tarling
  *****************************************************************************
- *
- * Some portions of this file was previously release using the BSD License:
  */
-
-// Copyright (c) 2007 The Regents of the University of California. All
-// Rights Reserved. Permission to use, copy, modify, and distribute this
-// software and its documentation without fee, and without a written
-// agreement is hereby granted, provided that the above copyright notice
-// and this paragraph appear in all copies. This software program and
-// documentation are copyrighted by The Regents of the University of
-// California. The software program and documentation are supplied "AS
-// IS", without any accompanying services from The Regents. The Regents
-// does not warrant that the operation of the program will be
-// uninterrupted or error-free. The end-user understands that the program
-// was developed for research purposes and is advised not to rely
-// exclusively on the program for any reason. IN NO EVENT SHALL THE
-// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
-// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
-// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 package org.argouml.activity2;
 
+import org.apache.log4j.Logger;
+import org.argouml.activity2.diagram.ActivityDiagramFactory;
 import org.argouml.moduleloader.ModuleInterface;
+import org.argouml.notation.Notation;
+import org.argouml.notation.NotationName;
+import org.argouml.notation.NotationProviderFactory2;
+import org.argouml.notation.providers.uml.SDMessageNotationUml;
+import org.argouml.persistence.PersistenceManager;
+import org.argouml.uml.diagram.DiagramFactory;
+import org.argouml.uml.diagram.DiagramFactoryInterface2;
+import org.argouml.uml.diagram.DiagramFactory.DiagramType;
+import org.argouml.uml.ui.PropPanelFactoryManager;
 
-/**
- * The Class Diagram Module description.
- *
- * @see org.argouml.moduleloader.ModuleInterface
- */
 public class ActivityDiagramModule implements ModuleInterface {
 
+    private static final Logger LOG = Logger
+            .getLogger(ActivityDiagramModule.class);
+
+    private ActivityDiagramPropPanelFactory propPanelFactory;
+        
     public boolean enable() {
+        
+        propPanelFactory =
+            new ActivityDiagramPropPanelFactory();
+        PropPanelFactoryManager.addPropPanelFactory(propPanelFactory);
+        // TODO: Remove the casting to DiagramFactoryInterface2
+        // as soon as DiagramFactoryInterface is removed.
+        DiagramFactory.getInstance().registerDiagramFactory(
+                DiagramType.Activity, 
+                (DiagramFactoryInterface2) new ActivityDiagramFactory());
         return true;
     }
 
     public boolean disable() {
+
+        PropPanelFactoryManager.removePropPanelFactory(propPanelFactory);
+
+        // TODO: Remove the casting to DiagramFactoryInterface2
+        // as soon as DiagramFactoryInterface is removed.
+        DiagramFactory.getInstance().registerDiagramFactory(
+                DiagramType.Activity, (DiagramFactoryInterface2) null);
         return true;
     }
 
     public String getName() {
-        return "ArgoUML-Class";
+        return "ArgoUML-Activity";
     }
 
     public String getInfo(int type) {
         switch (type) {
         case DESCRIPTION:
-            return "The new activity diagram implementation";
+            return "Activity diagram implementation";
         case AUTHOR:
-            return "ArgoUML Core Development Team";
+            return "ArgoUML Team";
         case VERSION:
             return "0.28";
         case DOWNLOADSITE:
