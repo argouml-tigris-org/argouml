a1711b7834f82011150109cc59276499f64c45a8
Linus Tolke <linus@tigris.org>
2010-07-24 05:38:58 +0000

    Fixed findbugs warnings.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18552 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-core-model/src/org/argouml/model/UUIDManager.java
+++ src/argouml-core-model/src/org/argouml/model/UUIDManager.java
@@ -1,6 +1,6 @@
 /* $Id$
  *******************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2010 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    mvw
+ *    linus - Linus Tolke
  *******************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -100,7 +101,7 @@ public final class UUIDManager {
 	if (address != null) {
 	    byte[] b = address.getAddress();
 	    for (int i = 0; i < b.length; i++) {
-		s.append((new Byte(b[i])).longValue()).append("-");
+		s.append((long) b[i]).append("-");
 	    }
 	}
 	s.append(uid.toString());
