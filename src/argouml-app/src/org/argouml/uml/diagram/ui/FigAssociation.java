9e0b7b395018aa7b630450d0f3eccea623c1b2f8
Bob Tarling <bobtarling@tigris.org>
2010-09-06 18:45:59 +0000

    Fix permanent loop
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18724 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/diagram/ui/FigAssociation.java
+++ src/argouml-app/src/org/argouml/uml/diagram/ui/FigAssociation.java
@@ -144,7 +144,7 @@ public class FigAssociation extends FigEdgeModelElement {
                     getOwner(), 
                     new String[] {"navigableOwnedEnd"});
         }
-        super.removeFromDiagram();
+        super.removeFromDiagramImpl();
     }
     
     public void propertyChange(PropertyChangeEvent pce) {
