033c4bdf52f2b623227035fd2120b32cae8b8629
Michiel van der Wulp <mvw@tigris.org>
2010-09-20 08:46:17 +0000

    Fix for desc24 of issue 6145: «Open ArgoUML, add a class and double click in the operation compartment. Then type in 'dummy(d1 : String) : List' and you have a Class with the name 'void' outside the model element.»
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@18770 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/notation/providers/uml/NotationUtilityUml.java
+++ src/argouml-app/src/org/argouml/notation/providers/uml/NotationUtilityUml.java
@@ -1,13 +1,13 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2010 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    mvw
+ *    Michiel van der Wulp
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -642,13 +642,8 @@ public final class NotationUtilityUml {
             }
 
             if (p == null) {
-                Object returnType =
-                    ProjectManager.getManager()
-                        .getCurrentProject().findType("void");
-                p = Model.getCoreFactory().buildParameter(
-                            op,
-                            returnType);
-                // op.addParameter(p);
+                /* Leave the type undefined (see issue 6145): */
+                p = Model.getCoreFactory().buildParameter(op, null);
             }
 
             if (name != null) {
