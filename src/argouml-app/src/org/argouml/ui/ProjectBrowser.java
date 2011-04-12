bd34500a3f5c94fb3722ecaf7bfcc889822980a7
Bob Tarling <bobtarling@tigris.org>
2011-04-12 21:49:34 +0000

    Update copyright date and contribs
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@19225 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/ui/ProjectBrowser.java
+++ src/argouml-app/src/org/argouml/ui/ProjectBrowser.java
@@ -1,13 +1,14 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2011 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    euluis
+ *    Luis Sergio Oliveira
+ *    Bob Tarling
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
