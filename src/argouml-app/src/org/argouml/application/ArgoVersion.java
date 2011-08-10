169c005a4172e80975717df9315fb24466c16efa
Linus Tolke <linus@tigris.org>
2011-08-10 19:51:18 +0000

    Now working towards the 0.33.9 (or more likely the 0.34.alpha1) version.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@19653 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/application/ArgoVersion.java
+++ src/argouml-app/src/org/argouml/application/ArgoVersion.java
@@ -57,7 +57,7 @@ final class ArgoVersion {
     /**
      * Version number.
      */
-    private static final String VERSION = "PRE-0.33.8";
+    private static final String VERSION = "PRE-0.33.9";
     private static final String STABLE_VERSION = "0.32";
 
     /**
