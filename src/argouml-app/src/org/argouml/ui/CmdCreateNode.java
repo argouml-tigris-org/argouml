0a26e0a60156aa9da415cf41a8830915db41ff10
Michiel van der Wulp <mvw@tigris.org>
2011-08-29 13:56:27 +0000

    Fix for issue 6182: "Click to place button.new-class" in status bar.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@19690 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/ui/CmdCreateNode.java
+++ src/argouml-app/src/org/argouml/ui/CmdCreateNode.java
@@ -1,6 +1,6 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2011 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    tfmorris
+ *    Michiel van der Wulp
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -63,7 +64,7 @@ public class CmdCreateNode extends CreateNodeAction {
      */
     public CmdCreateNode(Object nodeType, String name) {
         super(nodeType,
-                name,
+                Translator.localize(name),
                 ResourceLoaderWrapper.lookupIconResource(
                         ResourceLoaderWrapper.getImageBinding(name)));
         putToolTip(name);
