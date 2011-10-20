85b1806c5433ab5e5eaf1f16377e747bcfaf797e
Linus Tolke <linus@tigris.org>
2011-10-20 18:38:27 +0000

    Commits now goes towards 0.34.alpha5.
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@19768 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/application/ArgoVersion.java
+++ src/argouml-app/src/org/argouml/application/ArgoVersion.java
@@ -57,7 +57,7 @@ final class ArgoVersion {
     /**
      * Version number.
      */
-    private static final String VERSION = "PRE-0.34.alpha2";
+    private static final String VERSION = "PRE-0.34.alpha5";
     private static final String STABLE_VERSION = "0.32";
 
     /**
