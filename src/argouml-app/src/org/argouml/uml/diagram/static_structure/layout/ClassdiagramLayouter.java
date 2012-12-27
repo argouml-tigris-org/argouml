2f05495fad4ac915689b3c860a77ca6fb563cf23
ArgoUML Nightly Build monitored by Linus Tolke <closettop_nightlybuild@tigris.org>
2012-12-27 00:45:49 +0000

    [Issue 6450] Exception when layout a comment in Class Diagram
    IndexOutOfBoundsException when using "layout" with only a comment in a
    Class Diagram
    
    Contributed-by: elbedd
    
    git-svn-id: http://argouml.tigris.org/svn/argouml/trunk@19903 a161b567-7d1e-0410-9ef9-912c70fedb3f

--- src/argouml-app/src/org/argouml/uml/diagram/static_structure/layout/ClassdiagramLayouter.java
+++ src/argouml-app/src/org/argouml/uml/diagram/static_structure/layout/ClassdiagramLayouter.java
@@ -1,6 +1,6 @@
 /* $Id$
  *****************************************************************************
- * Copyright (c) 2009 Contributors - see below
+ * Copyright (c) 2009-2012 Contributors - see below
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *    tfmorris
+ *    Laurent Braud
  *****************************************************************************
  *
  * Some portions of this file was previously release using the BSD License:
@@ -579,7 +580,11 @@ public class ClassdiagramLayouter implements Layouter {
                     node.getUpNodes().isEmpty() 
                             ? 0 
                             : ((node.getUpNodes().get(0)).getRank());
-
+            // Issue 6450
+            if (nodeRows.size() == 0) {
+                nodeRows.add(new NodeRow(0));
+            }
+            // end Issue 6450
             nodeRows.get(rowInd).addNode(node);
         }
         for (int row = 0; row < nodeRows.size();) {
